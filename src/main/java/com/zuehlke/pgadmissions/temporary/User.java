1dfd40d989cb541fe5540bc614a9061c236ce19e
iky <iky@LONN0163.ads.zuehlke.com>
2012-02-07 16:50:46 +0000

    Initial Login Page

--- src/main/java/com/zuehlke/pgadmissions/temporary/User.java
+++ src/main/java/com/zuehlke/pgadmissions/temporary/User.java
@@ -8,6 +8,10 @@ public class User {
 	private String firstName;
 
 	private String lastName;
+	
+	private String email;
+	
+	private String password;
 
 	private List<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
 
@@ -33,6 +37,22 @@ public class User {
 
 	public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
 		this.phoneNumbers = phoneNumbers;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -6,6 +6,7 @@
    </head>
    <body id="bodyId">
 		<h2>Zuehlke project holding page</h2>
+		<a id="loginLink" href="login">Login</a>
 		<h3>JSP style</h3>
 		<h3>User details</h3>
 		<p>first name: ${user.firstName}</p>

2cbb1d956cb4f5847265c4c4519fcddf0a6582b2
ans <ans@LONN0167.ads.zuehlke.com>
2012-02-08 16:49:49 +0000

    spring.version in pom

---
+++ src/main/java/com/zuehlke/pgadmissions/temporary/User.java
@@ -0,0 +1,58 @@
+package com.zuehlke.pgadmissions.temporary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+	private String firstName;
+
+	private String lastName;
+	
+	private String email;
+	
+	private String password;
+
+	private List<PhoneNumber> phoneNumbers = new ArrayList<PhoneNumber>();
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<PhoneNumber> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}

8df7927033f0b8bd7df232c88fec06c5e1d7be8a
ans <ans@LONN0167.ads.zuehlke.com>
2012-02-08 12:11:38 +0000

    -added initial spring security -default login page -authenticating users based on config defined in security.xml

--- src/main/java/com/zuehlke/pgadmissions/controllers/LoginController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/LoginController.java
@@ -1,13 +1,11 @@
 package com.zuehlke.pgadmissions.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.zuehlke.pgadmissions.temporary.User;
 

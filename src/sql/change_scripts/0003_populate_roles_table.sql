a2569cbcf69e68a0a4553f58df128ea044fa7709
kpa <kpa@localhost>
2012-02-12 21:23:38 +0000

    change db table name case

--- src/sql/change_scripts/0003_populate_roles_table.sql
+++ src/sql/change_scripts/0003_populate_roles_table.sql
@@ -1,6 +1,6 @@
-Insert into application_role(authority) values ('RECRUITER')
+Insert into APPLICATION_ROLE(authority) values ('RECRUITER')
 ;
-Insert into application_role(authority) values ('APPLICANT')
+Insert into APPLICATION_ROLE(authority) values ('APPLICANT')
 ;
-Insert into application_role(authority) values ('REVIEWER')
+Insert into APPLICATION_ROLE(authority) values ('REVIEWER')
 ;

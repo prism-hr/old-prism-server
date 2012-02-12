d581eee8dd2ea4e44809585801d895d89cd33d5d
kpa <kpa@localhost>
2012-02-12 21:25:36 +0000

    more case changes

--- src/sql/test_data/insertUsers.sql
+++ src/sql/test_data/insertUsers.sql
@@ -1,6 +1,6 @@
-Insert into registered_user(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('bob', 'password', 1, 1, 1, 1);
-Insert into registered_user(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('fred', 'password', 1, 1, 1, 1);
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('bob', 'password', 1, 1, 1, 1);
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('fred', 'password', 1, 1, 1, 1);
 
 
-Insert into user_role_link(registered_user_id,application_role_id) values(1, 1);
+Insert into USER_ROLE_LINK (registered_user_id,application_role_id) values(1, 1);
 

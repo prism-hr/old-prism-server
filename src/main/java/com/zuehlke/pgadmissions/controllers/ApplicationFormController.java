e61a669586562e9d5a7cf0eb617c7e658a4cbc5a
iky <iky@LONN0163.ads.zuehlke.com>
2012-02-14 17:15:26 +0000

    added logic to ApplicationFormController to use Hibernate

--- src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationFormController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationFormController.java
@@ -1,5 +1,7 @@
 package com.zuehlke.pgadmissions.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -7,11 +9,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mysql.jdbc.Blob;
+import com.zuehlke.pgadmissions.dao.ApplicationFormDAO;
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
 
+import org.springframework.transaction.annotation.Transactional;
 @Controller
 @RequestMapping("/apply")
 public class ApplicationFormController {
+	
+    private final ApplicationFormDAO applicationDAO;
+	
+    ApplicationFormController(){
+    	this(null);
+    }
+    
+    @Autowired
+	public ApplicationFormController(ApplicationFormDAO applicationDAO) {
+		this.applicationDAO = applicationDAO;
+	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getApplyForm(ModelMap model) {
@@ -19,13 +36,16 @@ public class ApplicationFormController {
 		return "applicationForm";
 	}
 	
+	@Transactional
 	@RequestMapping(value = "/submit")
 	public String getLoginSubmit(@ModelAttribute("application") ApplicationForm application,
 			BindingResult result, ModelMap model) {
-//		if (application.getTitle().isEmpty() || application.getCob().isEmpty() || application.getDob().isEmpty() || 
-//				application.getGender().isEmpty() || application.getNat().isEmpty() || application.getUserId().isEmpty()) {
-//			return "applicationForm";
-//		}
+		//toDO: validation
+		RegisteredUser user = (RegisteredUser)SecurityContextHolder.getContext().getAuthentication().getDetails();
+		application.setUser(user);
+		applicationDAO.save(application);
+		
+		
 		model.addAttribute("application", application);
 		return "applicationFormSubmitted";
 	}

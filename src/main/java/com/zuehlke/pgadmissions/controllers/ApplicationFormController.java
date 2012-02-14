20e12e6f7a461c6a31c510c27eb312a538b3f68f
iky <iky@LONN0163.ads.zuehlke.com>
2012-02-14 17:14:13 +0000

    Backed out changeset: 6438a8cf1200

--- src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationFormController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationFormController.java
@@ -1,7 +1,5 @@
 package com.zuehlke.pgadmissions.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -9,26 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.mysql.jdbc.Blob;
-import com.zuehlke.pgadmissions.dao.ApplicationFormDAO;
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
-import com.zuehlke.pgadmissions.domain.RegisteredUser;
 
-import org.springframework.transaction.annotation.Transactional;
 @Controller
 @RequestMapping("/apply")
 public class ApplicationFormController {
-	
-    private final ApplicationFormDAO applicationDAO;
-	
-    ApplicationFormController(){
-    	this(null);
-    }
-    
-    @Autowired
-	public ApplicationFormController(ApplicationFormDAO applicationDAO) {
-		this.applicationDAO = applicationDAO;
-	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getApplyForm(ModelMap model) {
@@ -36,16 +19,13 @@ public class ApplicationFormController {
 		return "applicationForm";
 	}
 	
-	@Transactional
 	@RequestMapping(value = "/submit")
 	public String getLoginSubmit(@ModelAttribute("application") ApplicationForm application,
 			BindingResult result, ModelMap model) {
-		//toDO: validation
-		RegisteredUser user = (RegisteredUser)SecurityContextHolder.getContext().getAuthentication().getDetails();
-		application.setUser(user);
-		applicationDAO.save(application);
-		
-		
+//		if (application.getTitle().isEmpty() || application.getCob().isEmpty() || application.getDob().isEmpty() || 
+//				application.getGender().isEmpty() || application.getNat().isEmpty() || application.getUserId().isEmpty()) {
+//			return "applicationForm";
+//		}
 		model.addAttribute("application", application);
 		return "applicationFormSubmitted";
 	}

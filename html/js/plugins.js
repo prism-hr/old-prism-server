726af0dff20a72e1d3c580167bc7061e84f73559
D-Mizzy <D-Mizzy@DREWniverse.opusdc.co.uk>
2012-02-17 17:12:15 +0000

    Added an indicative arrow to expandable content headers.

--- html/js/plugins.js
+++ html/js/plugins.js
@@ -7,7 +7,11 @@ $(document).ready(function()
 		var $this    = $(this);
 		var $header  = $this.children('h2').filter(':first');
 		var $content = $this.children('div').filter(':first');
-		$header.bind('click', function() { $content.toggle(); });
+		$header.bind('click', function()
+		{
+			$(this).toggleClass('open');
+			$content.toggle();
+		});
 		$content.hide();
 	});
 	

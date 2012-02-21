478074bcd1741e7a1151fc27663dc4f16e428405
kpa <kpa@localhost>
2012-02-21 15:48:45 +0000

    refactored how db props are resolved to make running though eclipse w.out maven easier

---
+++ src/main/java/com/zuehlke/pgadmissions/dao/ApplicationFormDAO.java
@@ -0,0 +1,63 @@
+package com.zuehlke.pgadmissions.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+
+@Repository
+public class ApplicationFormDAO {
+
+	private final SessionFactory sessionFactory;
+
+	
+	ApplicationFormDAO(){
+		this(null);
+	}
+	
+	@Autowired
+	public ApplicationFormDAO(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public void save(ApplicationForm application) {
+		sessionFactory.getCurrentSession().saveOrUpdate(application);
+	}
+
+	public ApplicationForm get(Integer id) {
+		return (ApplicationForm) sessionFactory.getCurrentSession().get(
+				ApplicationForm.class, id);
+	}
+	
+	public List<ApplicationForm> getApplicationsByUser(RegisteredUser user) {
+		@SuppressWarnings("unchecked")
+		List<ApplicationForm> list = sessionFactory.getCurrentSession()
+				.createCriteria(ApplicationForm.class)
+				.add(Restrictions.eq("user", user)).list();
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<ApplicationForm> getAllApplications() {
+		return  (List<ApplicationForm>)sessionFactory.getCurrentSession()
+				.createCriteria(ApplicationForm.class).list();
+				
+	}
+
+	public List<ApplicationForm> checkIfApplicationIsAlreadyApproved(
+			Integer id) {
+		 @SuppressWarnings("unchecked")
+		List<ApplicationForm> list = sessionFactory.getCurrentSession()
+				.createCriteria(ApplicationForm.class)
+				.add(Restrictions.isNotNull("approved"))
+		 		.add(Restrictions.eq("id", id)).list();
+		 return list;
+	}
+
+
+}

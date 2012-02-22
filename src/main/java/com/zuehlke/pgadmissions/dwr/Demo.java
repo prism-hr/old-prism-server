d025ae5eee0b52551b0505b5b4c74e487841a581
mroach <mroach@localhost>
2012-02-22 14:23:24 +0000

    Author: Manish Dani Date: 22-02-2012 Task: DWR working Desc: --

--- src/main/java/com/zuehlke/pgadmissions/dwr/Demo.java
+++ src/main/java/com/zuehlke/pgadmissions/dwr/Demo.java
@@ -4,7 +4,6 @@ import org.directwebremoting.annotations.RemoteMethod;
 import org.directwebremoting.annotations.RemoteProxy;
 import org.springframework.stereotype.Service;
 
-@Service("springService")
 @RemoteProxy(name="dwrService")
 public class Demo {
 

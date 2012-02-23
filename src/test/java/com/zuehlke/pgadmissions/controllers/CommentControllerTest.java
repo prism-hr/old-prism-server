a48c4f1b3881562ee4895ca24b52580bcfdacaa8
iky <iky@LONN0170.ads.zuehlke.com>
2012-02-23 15:33:33 +0000

    Show comments functionality: - Administrator can see all comments made by all kind of users for a submitted non-approved/rejected application - Approver can see all comments made by all kind of users for a submitted non-approved/rejected application s/he is assigned for approving. - Reviewer same as approver except from comments of other reviewers - Applicant cannot see any comments

--- src/test/java/com/zuehlke/pgadmissions/controllers/CommentControllerTest.java
+++ src/test/java/com/zuehlke/pgadmissions/controllers/CommentControllerTest.java
@@ -2,6 +2,10 @@ package com.zuehlke.pgadmissions.controllers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 import org.easymock.EasyMock;
 import org.junit.After;
@@ -16,6 +20,7 @@ import com.zuehlke.pgadmissions.domain.ApplicationForm;
 import com.zuehlke.pgadmissions.domain.ApplicationReview;
 import com.zuehlke.pgadmissions.domain.CommentModel;
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.ReviewerAssignedModel;
 import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
 import com.zuehlke.pgadmissions.domain.builders.ApplicationReviewBuilder;
 import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
@@ -30,7 +35,7 @@ public class CommentControllerTest {
 	
 	private RegisteredUser admin;
 	private RegisteredUser approver;
-	private RegisteredUser reviewer;
+	private RegisteredUser reviewer, reviewer2;
 	private RegisteredUser applicant;
 	private ApplicationsService applicationsServiceMock;
 	private ApplicationReviewService applicationReviewServiceMock;
@@ -38,7 +43,7 @@ public class CommentControllerTest {
 	ApplicationForm submittedNonApprovedApplication;
 	ApplicationForm submittedApprovedApplication;
 	ApplicationForm unsubmittedApplication;
-	ApplicationReview applicationReviewForSubmittedNonApproved;
+	ApplicationReview applicationReviewForSubmittedNonApproved1, applicationReviewForSubmittedNonApproved2, applicationReviewForSubmittedNonApproved3;
 	UsernamePasswordAuthenticationToken authenticationToken;
 	
 	
@@ -51,6 +56,8 @@ public class CommentControllerTest {
 				.role(new RoleBuilder().authorityEnum(Authority.APPROVER).toRole()).toUser();
 		reviewer = new RegisteredUserBuilder().id(1).username("jane")
 				.role(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole()).toUser();
+		reviewer2 = new RegisteredUserBuilder().id(2).username("john")
+				.role(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole()).toUser();
 		applicant = new RegisteredUserBuilder().id(1).username("fred")
 				.role(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();
 		applicationsServiceMock = EasyMock.createMock(ApplicationsService.class);
@@ -59,7 +66,9 @@ public class CommentControllerTest {
 		submittedNonApprovedApplication = new ApplicationFormBuilder().id(1).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
 		submittedApprovedApplication = new ApplicationFormBuilder().id(2).submissionStatus(SubmissionStatus.SUBMITTED).approvedSatus(ApprovalStatus.APPROVED).approver(approver).toApplicationForm();
 		unsubmittedApplication = new ApplicationFormBuilder().id(3).toApplicationForm();
-		applicationReviewForSubmittedNonApproved = new ApplicationReviewBuilder().id(1).application(submittedNonApprovedApplication).comment("Amazing Research !!!").user(admin).toApplicationReview();
+		applicationReviewForSubmittedNonApproved1 = new ApplicationReviewBuilder().id(1).application(submittedNonApprovedApplication).comment("Amazing Research !!!").user(admin).toApplicationReview();
+		applicationReviewForSubmittedNonApproved2 = new ApplicationReviewBuilder().id(2).application(submittedNonApprovedApplication).comment("I'm not interested").user(reviewer).toApplicationReview();
+		applicationReviewForSubmittedNonApproved3 = new ApplicationReviewBuilder().id(3).application(submittedNonApprovedApplication).comment("I'm interested").user(reviewer2).toApplicationReview();
 	}
 
 	@Test
@@ -102,7 +111,6 @@ public class CommentControllerTest {
 		EasyMock.replay(applicationsServiceMock);
 		ModelAndView modelAndView = controller.getCommentedApplicationPage(2, "Amazing Research !!!");
 		assertEquals("comment", modelAndView.getViewName());
-		System.out.println(((CommentModel)modelAndView.getModel().get("model")).getComment());
 		assertNull(((CommentModel)modelAndView.getModel().get("model")).getComment());
 		assertEquals("You cannot comment on a completed application", ((CommentModel)modelAndView.getModel().get("model")).getMessage());
 		
@@ -136,6 +144,51 @@ public class CommentControllerTest {
 		assertNull(((CommentModel)modelAndView.getModel().get("model")).getComment());
 		assertEquals("You are not authorized to comment on the application", ((CommentModel)modelAndView.getModel().get("model")).getMessage());
 		
+	}
+	
+	@Test
+	public void shouldShowAllCommentsForAdministrator(){
+		authenticationToken.setDetails(admin);
+		SecurityContextImpl secContext = new SecurityContextImpl();
+		secContext.setAuthentication(authenticationToken);
+		SecurityContextHolder.setContext(secContext);
+		List<ApplicationReview> comments = new ArrayList<ApplicationReview>();
+		comments.add(applicationReviewForSubmittedNonApproved1);
+		comments.add(applicationReviewForSubmittedNonApproved2);
+		EasyMock.expect(applicationsServiceMock.getApplicationById(1)).andReturn(submittedNonApprovedApplication);
+		EasyMock.replay(applicationsServiceMock);
+		EasyMock.expect(applicationReviewServiceMock.getApplicationReviewsByApplication(submittedNonApprovedApplication)).andReturn(comments);
+		EasyMock.replay(applicationReviewServiceMock);
+		ModelAndView modelAndView = controller.getAllCommentsForApplication(1);
+		List<ApplicationReview> loadedComments = ((CommentModel) modelAndView.getModelMap().get("model")).getComments();
+		assertEquals(2, loadedComments.size());
+		assertEquals(comments, loadedComments);
+		assertEquals("comments", modelAndView.getViewName());
+		
+	}
+	
+	@Test
+	public void shouldShowAllCommentsForReviewerExceptFromOtherReviewersComments(){
+		authenticationToken.setDetails(reviewer);
+		SecurityContextImpl secContext = new SecurityContextImpl();
+		secContext.setAuthentication(authenticationToken);
+		SecurityContextHolder.setContext(secContext);
+		List<ApplicationReview> comments = new ArrayList<ApplicationReview>();
+		comments.add(applicationReviewForSubmittedNonApproved1); //admin
+		comments.add(applicationReviewForSubmittedNonApproved2); //reviewer
+		comments.add(applicationReviewForSubmittedNonApproved3); //reviewer2
+		EasyMock.expect(applicationsServiceMock.getApplicationById(1)).andReturn(submittedNonApprovedApplication);
+		EasyMock.expect(applicationReviewServiceMock.getApplicationReviewsByApplication(submittedNonApprovedApplication)).andReturn(comments);
+		EasyMock.expect(applicationReviewServiceMock.getVisibleComments(submittedNonApprovedApplication, reviewer)).andReturn(Arrays.asList(applicationReviewForSubmittedNonApproved2, applicationReviewForSubmittedNonApproved1));
+		EasyMock.replay(applicationsServiceMock, applicationReviewServiceMock);
+		ModelAndView modelAndView = controller.getAllCommentsForApplication(1);
+		List<ApplicationReview> loadedComments = ((CommentModel) modelAndView.getModelMap().get("model")).getComments();
+		assertEquals(2, loadedComments.size());
+		assertTrue(loadedComments.contains(applicationReviewForSubmittedNonApproved2));
+		assertTrue(loadedComments.contains(applicationReviewForSubmittedNonApproved1));
+		assertTrue(!loadedComments.contains(applicationReviewForSubmittedNonApproved3));
+		
+		assertEquals("comments", modelAndView.getViewName());
 	}
 
 	@After

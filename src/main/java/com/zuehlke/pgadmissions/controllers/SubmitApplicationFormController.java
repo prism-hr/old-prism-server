abc0b5e8e5465f74d0e52bf0f55f4f851f96228b
ans <ans@LONN0167.ads.zuehlke.com>
2012-03-02 18:32:20 +0000

    refactoring of submit button validation

--- src/main/java/com/zuehlke/pgadmissions/controllers/SubmitApplicationFormController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/SubmitApplicationFormController.java
@@ -3,18 +3,22 @@ package com.zuehlke.pgadmissions.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
 import com.zuehlke.pgadmissions.domain.enums.SubmissionStatus;
+import com.zuehlke.pgadmissions.dto.Address;
+import com.zuehlke.pgadmissions.dto.ApplicationFormDetails;
+import com.zuehlke.pgadmissions.dto.Funding;
+import com.zuehlke.pgadmissions.dto.PersonalDetails;
 import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
 import com.zuehlke.pgadmissions.propertyeditors.UserPropertyEditor;
 import com.zuehlke.pgadmissions.services.ApplicationsService;
@@ -41,14 +45,30 @@ public class SubmitApplicationFormController {
 
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView submitApplication(@ModelAttribute ApplicationForm applicationForm, BindingResult result) {
+	public ModelAndView submitApplication(@ModelAttribute ApplicationFormDetails appForm, @RequestParam Integer applicationFormId, BindingResult result) {
+		ApplicationForm applicationForm = getApplicationForm(applicationFormId);
 		RegisteredUser user = (RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
 		if (applicationForm == null || !user.equals(applicationForm.getApplicant()) || applicationForm.isSubmitted()) {
 			throw new ResourceNotFoundException();
 		}
 
 		ApplicationFormValidator validator = new ApplicationFormValidator();
-		validator.validate(applicationForm, result);
+		PersonalDetails ps = new PersonalDetails();
+		ps.setFirstName(applicationForm.getApplicant().getFirstName());
+		ps.setLastName(applicationForm.getApplicant().getLastName());
+		ps.setEmail(applicationForm.getApplicant().getEmail());
+		
+		appForm.setPersonalDetails(ps);
+		
+		Address addr = new Address();
+		addr.setAddress(applicationForm.getApplicant().getAddress());
+		appForm.setAddress(addr);
+		
+		Funding fund = new Funding();
+		fund.setFunding(applicationForm.getFunding());
+		appForm.setFunding(fund);
+		
+		validator.validate(appForm, result);
 		if (result.hasErrors()) {
 			return new ModelAndView("redirect:/application?view=errors", "id", applicationForm.getId());
 		}
@@ -69,12 +89,10 @@ public class SubmitApplicationFormController {
 		return new ApplicationForm();
 	}
 
-	@ModelAttribute("applicationForm")
-	public ApplicationForm getApplicationForm(Integer applicationFormId) {
-		RegisteredUser approver = (RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
+	private ApplicationForm getApplicationForm(Integer applicationFormId) {
+		RegisteredUser user = (RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
 		ApplicationForm applicationForm = applicationService.getApplicationById(applicationFormId);
-		Assert.notNull(applicationForm);
-		if(applicationForm == null || !approver.canSee(applicationForm)){
+		if(applicationForm == null || !user.canSee(applicationForm)){
 			throw new ResourceNotFoundException();
 		}
 		return applicationForm;

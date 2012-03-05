41a86cba81255242a3d484554f79922792ddb4e1
dmaughan <dmaughan@localhost>
2012-03-05 17:18:29 +0000

    Adding template for qualifications section of application form.

---
+++ src/main/java/com/zuehlke/pgadmissions/validators/AddressValidator.java
@@ -0,0 +1,21 @@
+package com.zuehlke.pgadmissions.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.zuehlke.pgadmissions.dto.Address;
+
+public class AddressValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Address.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "user.address.notempty");
+	}
+
+}

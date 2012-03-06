7aef54f819730543dafc4ee8e3a233bd3b7deaf9
kpa <kpa@localhost>
2012-03-06 09:45:47 +0000

    fixed js bug

--- src/main/webapp/WEB-INF/design/default/js/application/formActions.js
+++ src/main/webapp/WEB-INF/design/default/js/application/formActions.js
@@ -1,110 +1,101 @@
-	
-/*
-	 * Submit application form on click of submit button.
-	 */ 
-	$('#submitButton').click(function(){
-		$('#submitApplicationForm').submit();
-	});
-	
+$(document).ready(function(){	
 	/*
-	 * Submit commnet on click of comment submit button.
-	 */ 
-	$('#commentSubmitButton').click(function(){
-		$('#commentField').val($('#comment').val());
-		$('#commentForm').submit();
-	});
-	/*
-	 * Keep the view state of the form open or close depending on the user role.
-	 */
-	$(window).load(function(){
+		 * Submit application form on click of submit button.
+		 */ 
+		$('#submitButton').click(function(){
+			$('#submitApplicationForm').submit();
+		});
 		
-		if($("input#form-display-state").val() == "close"){
-			// close everything
-			$('section.folding > div').removeClass("open").hide();
-			$('section.folding > h2').removeClass("open");
-		}
+		/*
+		 * Submit commnet on click of comment submit button.
+		 */ 
+		$('#commentSubmitButton').click(function(){
+			$('#commentField').val($('#comment').val());
+			$('#commentForm').submit();
+		});
+		/*
+		 * Keep the view state of the form open or close depending on the user role.
+		 */
+		$(window).load(function(){
+			
+			if($("input#form-display-state").val() == "close"){
+				// close everything
+				$('section.folding > div').removeClass("open").hide();
+				$('section.folding > h2').removeClass("open");
+			}
+			
+			// To make uncompleted functionalities disable.
+			$(".disabledEle").attr("disabled", "disabled");
+			
+		});
 		
-		// To make uncompleted functionalities disable.
-		$(".disabledEle").attr("disabled", "disabled");
 		
-	});
-	
-	
-	/* Cases for comment section */
-	
-	/* 1. Depending on the which view user has requested, the comment form will be displayed. Temparary.
-	commentForm */
-	
-	var viewType = $('input#view-type-personal-form').val();
-	
-	/* 1.1 Show / Hide the whole Comment Box */
-	
-	if(viewType == "comments"){
-		$('div#show-comment-button-div').hide();
-		$('#commentForm').show();
-	
-		var commentCount = $('#prev-comment-div').val();
+		/* Cases for comment section */
 		
-		if(commentCount > 0){
-			$('.comment').show();
+		/* 1. Depending on the which view user has requested, the comment form will be displayed. Temparary.
+		commentForm */
+		
+		var viewType = $('input#view-type-personal-form').val();
+		
+		/* 1.1 Show / Hide the whole Comment Box */
+		
+		if(viewType == "comments"){
+			$('div#show-comment-button-div').hide();
+			$('#commentForm').show();
+		
+			var commentCount = $('#prev-comment-div').val();
+			
+			if(commentCount > 0){
+				$('.comment').show();
+			}else{
+				$('.comment').hide();
+			}
+			
 		}else{
-			$('.comment').hide();
-		}
+			$('div#show-comment-button-div').show();
+			$('#commentForm').hide();
+		};
+	
 		
-	}else{
-		$('div#show-comment-button-div').show();
-		$('#commentForm').hide();
-	};
-
-	
-	/* 2. Inside the Comment Box, on click of the close button, hide the Comment Box */
-	
-	$('#comment-close-button').click(function(){
-		//$('#comment').css("display", "none");
-		$('div#show-comment-button-div').show();
-		$('#commentForm').hide();
-	});
-
-	/* 3. Outside the Comment Box, on click of the comment button, show the Comment Box and 
-	 * hide the comment button */
-	
-	$('#comment-button').click(function(){
+		/* 2. Inside the Comment Box, on click of the close button, hide the Comment Box */
 		
-		$('div#show-comment-button-div').hide();
-		$('#commentForm').show();
+		$('#comment-close-button').click(function(){
+			//$('#comment').css("display", "none");
+			$('div#show-comment-button-div').show();
+			$('#commentForm').hide();
+		});
+	
+		/* 3. Outside the Comment Box, on click of the comment button, show the Comment Box and 
+		 * hide the comment button */
 		
-		var commentCount = $('#prev-comment-div').val();
+		$('#comment-button').click(function(){
+			
+			$('div#show-comment-button-div').hide();
+			$('#commentForm').show();
+			
+			var commentCount = $('#prev-comment-div').val();
+			
+			if(commentCount > 0){
+				$('.comment').show();
+			}else{
+				$('.comment').hide();
+			}
+			
+		});
+	
+
 		
-		if(commentCount > 0){
-			$('.comment').show();
-		}else{
-			$('.comment').hide();
-		}
 		
-	});
+		/* Tasks */
+		
+	
+		/* Admin: Until the comment box have any characters, the submit button should not
+		 * be enabled.*/
+	
+		
+		/* Extra */
+		//<input type ="hidden" id="prev-comment-div" value="${prevComments}"/>
+		
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/* Tasks */
-	
-
-	/* Admin: Until the comment box have any characters, the submit button should not
-	 * be enabled.*/
-
-	
-	/* Extra */
-	//<input type ="hidden" id="prev-comment-div" value="${prevComments}"/>
-	
 });

101a5ff0454e96f5b3387c3333806161d4eb0624
ans <ans@LONN0167.ads.zuehlke.com>
2012-03-07 19:20:19 +0000

    fixed merging issue

--- src/main/java/com/zuehlke/pgadmissions/pagemodels/ApplicationPageModel.java
+++ src/main/java/com/zuehlke/pgadmissions/pagemodels/ApplicationPageModel.java
@@ -12,7 +12,7 @@ import com.zuehlke.pgadmissions.dto.PersonalDetails;
 import com.zuehlke.pgadmissions.dto.QualificationDTO;
 
 public class ApplicationPageModel extends PageModel {
-	
+
 	private PersonalDetails personalDetails;
 	private Address address;
 	private Funding funding;
@@ -22,7 +22,7 @@ public class ApplicationPageModel extends PageModel {
 	private List<Countries> countries;
 	private EmploymentPosition employmentPosition;
 	private List<AddressPurpose> addressPurposes;
-	
+
 
 	public ApplicationForm getApplicationForm() {
 		return applicationForm;
@@ -35,34 +35,34 @@ public class ApplicationPageModel extends PageModel {
 	public void setPersonalDetails(PersonalDetails personalDetails) {
 		this.personalDetails = personalDetails;
 	}
-	
+
 	public Address getAddress() {
 		return address;
 	}
-	
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
+
 	public Funding getFunding() {
 		return funding;
 	}
-	
+
 	public void setFunding(Funding funding) {
 		this.funding = funding;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
 	public void setApplicationForm(ApplicationForm applicationForm) {
 		this.applicationForm = applicationForm;
-		
+
 	}
 
 	public QualificationDTO getQualification() {
@@ -71,30 +71,30 @@ public class ApplicationPageModel extends PageModel {
 	public void setQualification(QualificationDTO qualificationDto) {
 		this.qualification = qualificationDto;
 	}
-	
+
 	public void setCountries(List<Countries> countries) {
 		this.countries = countries;
 	}
-	
+
 	public List<Countries> getCountries() {
 		return countries;
 	}
 
 	public void setEmploymentPosition(EmploymentPosition employmentPosition) {
 		this.employmentPosition = employmentPosition;
-		
+
 	}
-	
+
 	public void setAddressPurposes(List<AddressPurpose> addressPurposes) {
 		this.addressPurposes = addressPurposes;
 	}
-	
+
 	public EmploymentPosition getEmploymentPosition() {
 		return employmentPosition;
 	}
-	
-	public void setAddressPurposes(List<AddressPurpose> addressPurposes) {
-		this.addressPurposes = addressPurposes;
+
+	public List<AddressPurpose> getAddressPurposes() {
+		return addressPurposes;
 	}
 
 

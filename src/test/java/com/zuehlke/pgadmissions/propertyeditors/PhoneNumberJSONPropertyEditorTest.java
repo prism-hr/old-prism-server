3fc8dd478aa2f0b6f85e2a87c769600e71ce9edc
kpa <kpa@localhost>
2012-03-12 09:47:38 +0000

    adding new classes!!!1

---
+++ src/test/java/com/zuehlke/pgadmissions/propertyeditors/PhoneNumberJSONPropertyEditorTest.java
@@ -0,0 +1,64 @@
+package com.zuehlke.pgadmissions.propertyeditors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.domain.Telephone;
+import com.zuehlke.pgadmissions.domain.builders.TelephoneBuilder;
+import com.zuehlke.pgadmissions.domain.enums.PhoneType;
+
+
+public class PhoneNumberJSONPropertyEditorTest {
+	private PhoneNumberJSONPropertyEdito editor;
+	private SimpleDateFormat dateFormat;
+	
+
+	@Test	
+	public void shouldParseAndSetAsValue() throws ParseException{
+		editor.setAsText("{\"type\": \"MOBILE\", \"number\": \"something\"}");
+		Telephone expected = new TelephoneBuilder().telephoneType(PhoneType.MOBILE).telephoneNumber("something").toTelephone();
+		Telephone telephone =   (Telephone) editor.getValue();
+		assertEquals(expected.getTelephoneType(), telephone.getTelephoneType());
+		assertEquals(expected.getTelephoneNumber(), telephone.getTelephoneNumber());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void shouldThrowIllegalArgumentExceptionIfAStringNotInTheRightFormat(){			
+		editor.setAsText("{type: 'MOBILE' number: 'something'}");		
+	}
+	
+	@Test	
+	public void shouldReturNullIfStringIsNull(){			
+		editor.setAsText(null);
+		assertNull(editor.getValue());		
+	}
+	@Test	
+	public void shouldReturNullIfStringIsEmpty(){			
+		editor.setAsText("");
+		assertNull(editor.getValue());		
+	}
+	
+	@Test	
+	public void shouldReturnNullIfValueIsNull(){			
+		editor.setValue(null);
+		assertNull(editor.getAsText());
+	}
+	
+	@Test	
+	public void shouldReturnCorrectjsonString() throws ParseException{			
+		editor.setValue(new TelephoneBuilder().telephoneType(PhoneType.MOBILE).telephoneNumber("something").toTelephone());
+		assertEquals("{\"type\": \"MOBILE\", \"number\": \"something\"}", editor.getAsText());
+	}
+	
+	@Before
+	public void setup(){
+		
+		editor = new PhoneNumberJSONPropertyEdito();
+	}
+}

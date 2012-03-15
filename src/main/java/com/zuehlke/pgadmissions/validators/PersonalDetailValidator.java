03e36232f711483907430c832e7a22088755d19c
kpa <kpa@localhost>
2012-03-15 09:46:24 +0000

    Added some validation of languages

--- src/main/java/com/zuehlke/pgadmissions/validators/PersonalDetailValidator.java
+++ src/main/java/com/zuehlke/pgadmissions/validators/PersonalDetailValidator.java
@@ -6,6 +6,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import com.zuehlke.pgadmissions.domain.LanguageProficiency;
 import com.zuehlke.pgadmissions.domain.Nationality;
 import com.zuehlke.pgadmissions.domain.PersonalDetail;
 
@@ -32,6 +33,70 @@ public class PersonalDetailValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "residenceStatus", "personalDetails.residenceStatus.notempty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "dateOfBirth", "personalDetails.dateOfBirth.notempty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "application", "personalDetails.application.notempty");
+		validateCandidateNationalities(target, errors);
+		
+		validateMaternalGuardianNationalities(target, errors);
+	
+		validatePaternalGuardianNationalities(target, errors);		
+		
+		validateLanguageProficiencies(target, errors);
+	}
+
+	private void validateLanguageProficiencies(Object target, Errors errors) {
+		int numberOfPrimaries;
+		if(((PersonalDetail)target).getLanguageProficiencies().isEmpty()){
+			errors.rejectValue("languageProficiencies", "personalDetails.languageProficiencies.notempty");
+		}
+		
+		numberOfPrimaries = 0;
+		for (LanguageProficiency proficiency : ((PersonalDetail)target).getLanguageProficiencies()) {
+			if(proficiency.isPrimary()){
+				numberOfPrimaries++;
+			}
+		}
+		if(numberOfPrimaries > 1){
+			errors.rejectValue("languageProficiencies", "personalDetails.languageProficiencies.unique");
+		}
+		if( ((PersonalDetail)target).getLanguageProficiencies().size() > 1 && numberOfPrimaries ==0){
+			errors.rejectValue("languageProficiencies", "personalDetails.languageProficiencies.noprimary");
+		}
+	}
+
+	private void validatePaternalGuardianNationalities(Object target, Errors errors) {
+		int numberOfPrimaries;
+		numberOfPrimaries = 0;
+		for (Nationality nationality : ((PersonalDetail)target).getPaternalGuardianNationalities()) {
+			if(nationality.isPrimary()){
+				numberOfPrimaries++;
+			}
+		}
+		if(numberOfPrimaries > 1){
+			errors.rejectValue("paternalGuardianNationalities", "personalDetails.paternalGuardianNationalities.unique");
+		}
+		if( ((PersonalDetail)target).getPaternalGuardianNationalities().size() > 1 && numberOfPrimaries ==0){
+			errors.rejectValue("paternalGuardianNationalities", "personalDetails.paternalGuardianNationalities.noprimary");
+		}
+	}
+
+	private void validateMaternalGuardianNationalities(Object target, Errors errors) {
+		int numberOfPrimaries;
+		
+		
+		numberOfPrimaries = 0;
+		for (Nationality nationality : ((PersonalDetail)target).getMaternalGuardianNationalities()) {
+			if(nationality.isPrimary()){
+				numberOfPrimaries++;
+			}
+		}
+		if(numberOfPrimaries > 1){
+			errors.rejectValue("maternalGuardianNationalities", "personalDetails.maternalGuardianNationalities.unique");
+		}
+		if( ((PersonalDetail)target).getMaternalGuardianNationalities().size() > 1 && numberOfPrimaries ==0){
+			errors.rejectValue("maternalGuardianNationalities", "personalDetails.maternalGuardianNationalities.noprimary");
+		}
+	}
+
+	private void validateCandidateNationalities(Object target, Errors errors) {
 		if(((PersonalDetail)target).getCandidateNationalities().isEmpty()){
 			errors.rejectValue("candidateNationalities", "personalDetails.candidateNationalities.notempty");
 		}
@@ -46,32 +111,6 @@ public class PersonalDetailValidator implements Validator {
 		}
 		if( ((PersonalDetail)target).getCandidateNationalities().size() > 1 && numberOfPrimaries ==0){
 			errors.rejectValue("candidateNationalities", "personalDetails.candidateNationalities.noprimary");
-		}
-		
-		numberOfPrimaries = 0;
-		for (Nationality nationality : ((PersonalDetail)target).getMaternalGuardianNationalities()) {
-			if(nationality.isPrimary()){
-				numberOfPrimaries++;
-			}
-		}
-		if(numberOfPrimaries > 1){
-			errors.rejectValue("maternalGuardianNationalities", "personalDetails.maternalGuardianNationalities.unique");
-		}
-		if( ((PersonalDetail)target).getMaternalGuardianNationalities().size() > 1 && numberOfPrimaries ==0){
-			errors.rejectValue("maternalGuardianNationalities", "personalDetails.maternalGuardianNationalities.noprimary");
-		}
-		
-		numberOfPrimaries = 0;
-		for (Nationality nationality : ((PersonalDetail)target).getPaternalGuardianNationalities()) {
-			if(nationality.isPrimary()){
-				numberOfPrimaries++;
-			}
-		}
-		if(numberOfPrimaries > 1){
-			errors.rejectValue("paternalGuardianNationalities", "personalDetails.paternalGuardianNationalities.unique");
-		}
-		if( ((PersonalDetail)target).getPaternalGuardianNationalities().size() > 1 && numberOfPrimaries ==0){
-			errors.rejectValue("paternalGuardianNationalities", "personalDetails.paternalGuardianNationalities.noprimary");
 		}
 	}
 

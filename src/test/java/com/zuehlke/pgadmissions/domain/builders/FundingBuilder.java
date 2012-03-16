52b47cd35c9f4a27211878f6db470d6ce2b33a44
ans <ans@LONN0167.ads.zuehlke.com>
2012-03-16 10:13:57 +0000

    temporarily removed the dynamic table from programme details validation of supervisor email

---
+++ src/test/java/com/zuehlke/pgadmissions/domain/builders/FundingBuilder.java
@@ -0,0 +1,55 @@
+package com.zuehlke.pgadmissions.domain.builders;
+
+import java.util.Date;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Funding;
+import com.zuehlke.pgadmissions.domain.enums.FundingType;
+
+public class FundingBuilder {
+
+	
+	private ApplicationForm application;
+	private FundingType type;
+	private String description;
+	private String value;
+	private Date awardDate;
+	
+	public FundingBuilder application(ApplicationForm application) {
+		this.application = application;
+		return this;
+	}
+	
+	public FundingBuilder type(FundingType type) {
+		this.type = type;
+		return this;
+	}
+	
+	public FundingBuilder description(String description) {
+		this.description = description;
+		return this;
+	}
+	
+	public FundingBuilder value(String value) {
+		this.value = value;
+		return this;
+	}
+	
+	public FundingBuilder awardDate(Date awardDate) {
+		this.awardDate = awardDate;
+		return this;
+	}
+	
+	public Funding toFunding() {
+		Funding funding = new Funding();
+		funding.setApplication(application);
+		funding.setType(type);
+		funding.setValue(value);
+		funding.setDescription(description);
+		funding.setAwardDate(awardDate);
+		return funding;
+	}
+	
+	
+	
+}

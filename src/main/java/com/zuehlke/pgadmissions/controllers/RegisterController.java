397290c7b4c3bebdb94bd39db2ea0ae9d8fc1b18
iky <iky@LONN0170.ads.zuehlke.com>
2012-03-16 16:59:47 +0000

    Registration form generates and saves activation code, Controller logic to enable account

---
+++ src/main/java/com/zuehlke/pgadmissions/controllers/RegisterController.java
@@ -0,0 +1,132 @@
+package com.zuehlke.pgadmissions.controllers;
+
+import java.math.BigInteger;
+import java.net.Authenticator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.service.ServiceRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DirectFieldBindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.dto.ApplicantRecordDTO;
+import com.zuehlke.pgadmissions.pagemodels.RegisterPageModel;
+import com.zuehlke.pgadmissions.services.ApplicantRecordService;
+import com.zuehlke.pgadmissions.services.UserService;
+import com.zuehlke.pgadmissions.validators.ApplicantRecordValidator;
+
+@Controller
+@RequestMapping(value = { "/register" })
+public class RegisterController {
+
+	private static final String REGISTER_APPLICANT_VIEW_NAME = "public/register/register_applicant";
+	private final UserService userService;
+	private final ApplicantRecordService applicantRecordService;
+	private final ApplicantRecordValidator validator;
+
+	RegisterController() {
+		this(null, null, null);
+	}
+
+	@Autowired
+	public RegisterController(ApplicantRecordService applicantRecordService, ApplicantRecordValidator validator,
+			UserService userService) {
+		this.applicantRecordService = applicantRecordService;
+		this.validator = validator;
+		this.userService = userService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getRegisterPage() throws NoSuchAlgorithmException {
+		RegisterPageModel model = new RegisterPageModel();
+		model.setRecord(new ApplicantRecordDTO());
+		return new ModelAndView(REGISTER_APPLICANT_VIEW_NAME, "model", model);
+	}
+
+	@RequestMapping(value = "/submit", method = RequestMethod.POST)
+	public ModelAndView getRegisterSubmitPage(@ModelAttribute("record") ApplicantRecordDTO record, BindingResult errors)
+			throws NoSuchAlgorithmException {
+		validator.validate(record, errors);
+
+		RegisterPageModel model = new RegisterPageModel();
+		ModelMap modelMap = new ModelMap();
+		if (errors.hasFieldErrors("firstname"))
+			modelMap.put("firstnameErrorCode", errors.getFieldError("firstname").getCode());
+		if (errors.hasFieldErrors("lastname"))
+			modelMap.put("lastnameErrorCode", errors.getFieldError("lastname").getCode());
+		if (errors.hasFieldErrors("email"))
+			modelMap.put("emailfirstnameErrorCode", errors.getFieldError("email").getCode());
+		if (errors.hasFieldErrors("password"))
+			modelMap.put("passwordErrorCode", errors.getFieldError("password").getCode());
+		if (errors.hasFieldErrors("confirmPassword"))
+			modelMap.put("confirmPasswordErrorCode", errors.getFieldError("confirmPassword").getCode());
+		if (!errors.hasErrors()) {
+			if (record.getPassword() != null)
+				record.setPassword(createHash(record.getPassword()));
+			SecureRandom random = new SecureRandom();
+			String activationCode = new BigInteger(80, random).toString(32);
+			RegisteredUser user = new RegisteredUser();
+			user.setUsername(record.getEmail());
+			user.setFirstName(record.getFirstname());
+			user.setLastName(record.getLastname());
+			user.setEmail(record.getEmail());
+			user.setAccountNonExpired(true);
+			user.setAccountNonLocked(true);
+			user.setPassword(record.getPassword());
+			user.setEnabled(true);
+			user.setActivationCode(activationCode);
+			user.setCredentialsNonExpired(true);
+			user.getRoles().add(userService.getRoleById(2));
+			userService.save(user);
+			System.out.println("user id" + user.getId());
+
+			model.setMessage("You have been successfully registered. Please check your emails to activate your account");
+			model.setRecord(new ApplicantRecordDTO());
+			return new ModelAndView(REGISTER_APPLICANT_VIEW_NAME, "model", model);
+		}
+		modelMap.put("record", record);
+		return new ModelAndView("redirect:/register", modelMap);
+	}
+
+	@RequestMapping(value = "/activateAccount", method = RequestMethod.POST)
+	public ModelAndView activateAccount(@ModelAttribute RegisteredUser regUser, @RequestParam String activationCode){
+		RegisterPageModel model = new RegisterPageModel();
+		if(activationCode.equals(regUser.getActivationCode())){
+			regUser.setEnabled(true);
+			userService.save(regUser);
+		}
+		model.setUser(regUser);
+		return new ModelAndView("/", "model", model);
+	}
+	
+	@ModelAttribute("record")
+	public ApplicantRecordDTO getApplicantRecord(Integer id) {
+		System.out.println("HERE");
+		return new ApplicantRecordDTO();
+	}
+
+	public String createHash(String password) throws NoSuchAlgorithmException {
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		md5.update(password.getBytes());
+		byte byteData[] = md5.digest();
+		// convert bytes to hex chars
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/dao/mappings/LanguageProficiencyMappingTest.java
@@ -0,0 +1,47 @@
+package com.zuehlke.pgadmissions.dao.mappings;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.domain.Language;
+import com.zuehlke.pgadmissions.domain.LanguageProficiency;
+import com.zuehlke.pgadmissions.domain.builders.LanguageBuilder;
+import com.zuehlke.pgadmissions.domain.enums.LanguageAptitude;
+
+public class LanguageProficiencyMappingTest extends AutomaticRollbackTestCase {
+
+	@Test
+	public void shouldSaveAndLoadLanguageProficiency() {
+		Language language = new LanguageBuilder().name("aaaaa").toLanguage();
+		sessionFactory.getCurrentSession().save(language);
+
+
+		flushAndClearSession();
+
+		LanguageProficiency proficiency = new LanguageProficiency();
+		proficiency.setLanguage(language);
+		proficiency.setPrimary(true);
+		proficiency.setAptitude(LanguageAptitude.ELEMENTARY);
+		sessionFactory.getCurrentSession().save(proficiency);
+
+		assertNotNull(proficiency.getId());
+
+		LanguageProficiency reloadedLanguageProficiency = (LanguageProficiency) sessionFactory.getCurrentSession().get(LanguageProficiency.class, proficiency.getId());
+		assertSame(proficiency, reloadedLanguageProficiency);
+		assertEquals(proficiency, reloadedLanguageProficiency);
+		flushAndClearSession();
+
+		reloadedLanguageProficiency = (LanguageProficiency) sessionFactory.getCurrentSession().get(LanguageProficiency.class, proficiency.getId());
+		assertNotSame(proficiency, reloadedLanguageProficiency);
+		assertEquals(proficiency, reloadedLanguageProficiency);
+		assertEquals(language, reloadedLanguageProficiency.getLanguage());
+		assertTrue(reloadedLanguageProficiency.isPrimary());
+		assertEquals(LanguageAptitude.ELEMENTARY, reloadedLanguageProficiency.getAptitude());
+
+	}
+}

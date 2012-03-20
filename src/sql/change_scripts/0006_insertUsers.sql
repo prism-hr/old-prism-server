497ce408cec8f403a73bec50cd6fc18739736ae4
iky <iky@LONN0170.ads.zuehlke.com>
2012-03-20 09:26:00 +0000

    changed test data passwords to hashed value

--- src/sql/change_scripts/0006_insertUsers.sql
+++ src/sql/change_scripts/0006_insertUsers.sql
@@ -1,10 +1,10 @@
-Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('bob', 'password', 1, 1, 1, 1)
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('bob', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1, 1, 1)
 ;
-Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('fred', 'password', 1, 1, 1, 1)
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('fred', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1, 1, 1)
 ;
-Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('jane', 'password', 1, 1, 1, 1)
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('jane', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1, 1, 1)
 ;
-Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('mark', 'password', 1, 1, 1, 1)
+Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled) values ('mark', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1, 1, 1)
 ;
 Insert into USER_ROLE_LINK (registered_user_id,application_role_id) values(1, 1)
 ;

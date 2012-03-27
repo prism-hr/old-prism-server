a544732d52b3c356679990a9401cc963b648bb9f
ans <ans@LONN0167.ads.zuehlke.com>
2012-03-27 11:55:58 +0100

    print pdf buttons integrated with styling

---
+++ src/main/java/com/zuehlke/pgadmissions/pagemodels/PageModel.java
@@ -0,0 +1,106 @@
+package com.zuehlke.pgadmissions.pagemodels;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.ApplicationReview;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.Role;
+
+public class PageModel {
+
+	private List<ObjectError> errorObjs;
+	private ApplicationForm applicationForm;
+	private RegisteredUser user;
+	private List<ApplicationReview> applicationComments = new ArrayList<ApplicationReview>();
+	private String view;
+	private BindingResult result;
+	private String userRoles;
+	private List<String> globalErrorCodes = new ArrayList<String>();
+
+	public BindingResult getResult() {
+		return result;
+	}
+
+	public void setResult(BindingResult result) {
+		this.result = result;
+	}
+
+	public String getView() {
+		return view;
+	}
+
+	public List<ObjectError> getErrorObjs() {
+		return errorObjs;
+	}
+
+	public void setErrorObjs(List<ObjectError> errorObjs) {
+		this.errorObjs = errorObjs;
+	}
+
+	public ApplicationForm getApplicationForm() {
+		return applicationForm;
+	}
+
+	public void setApplicationForm(ApplicationForm applicationForm) {
+		this.applicationForm = applicationForm;
+	}
+
+	public RegisteredUser getUser() {
+		return user;
+	}
+
+	public void setUser(RegisteredUser user) {
+		this.user = user;
+		setUserRoles(user);
+	}
+
+	public List<ApplicationReview> getApplicationComments() {
+		return applicationComments;
+	}
+
+	public void setApplicationComments(List<ApplicationReview> applicationComments) {
+		this.applicationComments = applicationComments;
+	}
+
+	public void setView(String view) {
+		this.view = view;
+
+	}
+
+	public boolean hasError(String fieldname){
+		if(result != null && result.getFieldError(fieldname) != null){
+			return true;
+		}
+		return false;
+	}
+
+	private void setUserRoles(RegisteredUser user) {
+		StringBuilder userRoles = new StringBuilder();
+		Collection<Role> authorities = user.getAuthorities();
+		if (user != null && authorities!= null) {
+			for (Role role : authorities) {
+				userRoles.append(role.getAuthority());
+				userRoles.append(";");
+			}
+		}
+		this.userRoles = userRoles.toString();
+	}
+
+	public String getUserRoles() {
+		return userRoles;
+	}
+
+	public List<String> getGlobalErrorCodes() {
+		return globalErrorCodes;
+	}
+
+	public void setGlobalErrorCodes(List<String> globalErrorCodes) {
+		this.globalErrorCodes = globalErrorCodes;
+	}
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/domain/RegisteredUserTest.java
@@ -0,0 +1,144 @@
+package com.zuehlke.pgadmissions.domain;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
+import com.zuehlke.pgadmissions.domain.builders.DocumentBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProjectBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RefereeBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ReferenceBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RoleBuilder;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+import com.zuehlke.pgadmissions.domain.enums.DocumentType;
+import com.zuehlke.pgadmissions.domain.enums.SubmissionStatus;
+
+public class RegisteredUserTest {
+
+	@Test
+	public void shouldReturnTrueIfUserIsInRole(){
+		RegisteredUser user = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole(), new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		assertTrue(user.isInRole(Authority.APPLICANT));
+		
+	}
+	
+	@Test
+	public void shouldReturnFalseIfUserIsNotInRole(){
+		RegisteredUser user = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole(), new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		assertFalse(user.isInRole(Authority.REVIEWER));
+		
+	}
+	
+	@Test
+	public void shouldReturnTrueIfUserIsInRolePassedAsString(){
+		RegisteredUser user = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole(), new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		assertTrue(user.isInRole("APPLICANT"));
+		
+	}
+	
+	@Test
+	public void shouldReturnFalseIfUserIsNotInRolePassedAsString(){
+		RegisteredUser user = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole(), new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		assertFalse(user.isInRole("REVIEWER"));
+		
+	}
+	
+	@Test
+	public void shouldReturnFalseIStringIsNotAuthorityValue(){
+		RegisteredUser user = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole(), new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		assertFalse(user.isInRole("bob"));
+		
+	}
+	
+	@Test
+	public void shouldReturnTrueIfUserIsApplicantAndOwnerOfForm(){
+		RegisteredUser applicant = new RegisteredUserBuilder().id(1).roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();		
+		ApplicationForm applicationForm = new ApplicationFormBuilder().applicant(applicant).toApplicationForm();
+		assertTrue(applicant.canSee(applicationForm));
+		
+	}
+	
+	@Test
+	public void shouldReturnFalseIfUserIsApplicantAndNotOwnerOfForm(){
+		RegisteredUser applicantOne = new RegisteredUserBuilder().id(1).roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();		
+		RegisteredUser applicantTwo = new RegisteredUserBuilder().id(2).roles(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().applicant(applicantOne).toApplicationForm();
+		assertFalse(applicantTwo.canSee(applicationForm));
+		
+	}
+
+	@Test
+	public void shouldReturnTrueIfUserIsAdministrator(){
+		RegisteredUser administrator = new RegisteredUserBuilder().roles(new RoleBuilder().authorityEnum(Authority.SUPERADMINISTRATOR).toRole()).toUser();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		assertTrue(administrator.canSee(applicationForm));
+		
+	}
+	
+	@Test
+	public void shouldReturnTrueIfUserIsItsReviewer(){
+		RegisteredUser reviewer = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole()).toUser();
+		Set<RegisteredUser> reviewers = new HashSet<RegisteredUser>();
+		reviewers.add(reviewer);
+		ApplicationForm applicationForm = new ApplicationFormBuilder().reviewers(reviewers).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		assertTrue(reviewer.canSee(applicationForm));
+		
+	}
+
+	
+	@Test
+	public void shouldReturnFalseIfUserIsNotItsReviewer(){
+		RegisteredUser reviewer = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole()).toUser();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().toApplicationForm();
+		assertFalse(reviewer.canSee(applicationForm));
+		
+	}
+	
+
+	
+	@Test
+	public void shouldReturnFalseForAnyoneNotAnApplicantIfUnsubmittedApplication() {
+		RegisteredUser reviewer = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole()).toUser();
+		Set<RegisteredUser> reviewers = new HashSet<RegisteredUser>();
+		reviewers.add(reviewer);
+		ApplicationForm applicationForm = new ApplicationFormBuilder().reviewers(reviewers).toApplicationForm();
+		assertFalse(reviewer.canSee(applicationForm));
+	}
+	
+	@Test
+	public void shouldReturnTrueForAnApplicantIfUnsubmittedApplication() {
+		RegisteredUser applicant = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().submissionStatus(SubmissionStatus.UNSUBMITTED).applicant(applicant).toApplicationForm();
+		assertTrue(applicant.canSee(applicationForm));
+	}
+	
+	
+	@Test
+	public void shouldReturnTrueIfUserIsItsApproverOfProgramToWhichApplicationProjectBelongs(){
+		RegisteredUser approver = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.APPROVER).toRole()).toUser();
+		Program program = new ProgramBuilder().id(1).approver(approver).toProgram();
+		Project project = new ProjectBuilder().id(1).program(program).toProject();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().project(project).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		assertTrue(approver.canSee(applicationForm));
+		
+	}
+	
+	@Test
+	public void shouldReturnFalseIfUserIsNotApproverOfProgramToWhichApplicationProjectBelongs(){
+		RegisteredUser approver = new RegisteredUserBuilder().id(1).role(new RoleBuilder().authorityEnum(Authority.APPROVER).toRole()).toUser();
+		Program program = new ProgramBuilder().id(1).toProgram();
+		Project project = new ProjectBuilder().id(1).program(program).toProject();
+		ApplicationForm applicationForm = new ApplicationFormBuilder().project(project).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		assertFalse(approver.canSee(applicationForm));
+	}
+	
+	
+	
+}

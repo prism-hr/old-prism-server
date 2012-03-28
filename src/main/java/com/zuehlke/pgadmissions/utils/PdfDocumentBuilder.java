c40caeb813ff23743bdcfbe5f26b7b3a5e9fe0ac
ans <ans@LONN0167.ads.zuehlke.com>
2012-03-28 08:17:54 +0100

    tiffs

--- src/main/java/com/zuehlke/pgadmissions/utils/PdfDocumentBuilder.java
+++ src/main/java/com/zuehlke/pgadmissions/utils/PdfDocumentBuilder.java
@@ -430,7 +430,8 @@ public class PdfDocumentBuilder {
 	
 	private void addSupportingDocuments(ApplicationForm application, Document document) throws DocumentException, MalformedURLException, IOException {
 		for (com.zuehlke.pgadmissions.domain.Document doc : application.getSupportingDocuments()) {
-			if (doc.getFileName().endsWith(".jpg") || doc.getFileName().endsWith("bmp") || doc.getFileName().endsWith("jpeg") || doc.getFileName().endsWith("png")) {
+			if (doc.getFileName().endsWith(".jpg") || doc.getFileName().endsWith("bmp") || doc.getFileName().endsWith("jpeg") || doc.getFileName().endsWith("png")
+					|| doc.getFileName().endsWith(".tiff") || doc.getFileName().endsWith(".tif")) {
 				Image image = Image.getInstance(doc.getContent());
 				document.add(image);
 			}

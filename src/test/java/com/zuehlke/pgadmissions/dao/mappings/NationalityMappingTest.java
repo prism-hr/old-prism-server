b3bb964915f54d03b0b88d6beaa58f34bc771450
iky <iky@LONN0170.ads.zuehlke.com>
2012-04-02 16:33:17 +0100

    Merge

---
+++ src/test/java/com/zuehlke/pgadmissions/dao/mappings/NationalityMappingTest.java
@@ -0,0 +1,54 @@
+package com.zuehlke.pgadmissions.dao.mappings;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.domain.Country;
+import com.zuehlke.pgadmissions.domain.Document;
+import com.zuehlke.pgadmissions.domain.Nationality;
+import com.zuehlke.pgadmissions.domain.builders.CountryBuilder;
+import com.zuehlke.pgadmissions.domain.builders.DocumentBuilder;
+import com.zuehlke.pgadmissions.domain.enums.NationalityType;
+
+public class NationalityMappingTest extends AutomaticRollbackTestCase {
+
+	@Test
+	public void shouldSaveAndLoadNationality() {
+		Country country = new CountryBuilder().code("aa").name("aaaaa").toCountry();
+		sessionFactory.getCurrentSession().save(country);
+
+		Document document1 = new DocumentBuilder().content("aa".getBytes()).fileName("bob").toDocument();
+		Document document2 = new DocumentBuilder().content("bb".getBytes()).fileName("fred").toDocument();
+		save(document1, document2);
+
+		flushAndClearSession();
+
+		Nationality nationality = new Nationality();
+		nationality.setCountry(country);
+		nationality.setSupportingDocuments(Arrays.asList(document1, document2));
+		nationality.setType(NationalityType.CANDIDATE);
+		sessionFactory.getCurrentSession().save(nationality);
+
+		assertNotNull(nationality.getId());
+
+		Nationality reloadedNationality = (Nationality) sessionFactory.getCurrentSession().get(Nationality.class, nationality.getId());
+		assertSame(nationality, reloadedNationality);
+		assertEquals(nationality, reloadedNationality);
+		flushAndClearSession();
+
+		reloadedNationality = (Nationality) sessionFactory.getCurrentSession().get(Nationality.class, nationality.getId());
+		assertNotSame(nationality, reloadedNationality);
+		assertEquals(nationality, reloadedNationality);
+		assertEquals(country, reloadedNationality.getCountry());
+		assertEquals(2, reloadedNationality.getSupportingDocuments().size());
+		assertTrue(reloadedNationality.getSupportingDocuments().containsAll(Arrays.asList(document1, document2)));
+		assertEquals(NationalityType.CANDIDATE, reloadedNationality.getType());
+	}
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/domain/builders/PersonalDetailsBuilder.java
@@ -0,0 +1,144 @@
+package com.zuehlke.pgadmissions.domain.builders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Country;
+import com.zuehlke.pgadmissions.domain.LanguageProficiency;
+import com.zuehlke.pgadmissions.domain.Nationality;
+import com.zuehlke.pgadmissions.domain.PersonalDetail;
+import com.zuehlke.pgadmissions.domain.Telephone;
+import com.zuehlke.pgadmissions.domain.enums.Gender;
+import com.zuehlke.pgadmissions.domain.enums.ResidenceStatus;
+
+public class PersonalDetailsBuilder {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Gender gender;
+	private String email;
+	private Date dateOfBirth;
+	private Date residenceFromDate;
+	private Country country;
+	private Country residenceCountry;
+	private ResidenceStatus residenceStatus;
+	private ApplicationForm applicationForm;
+	private List<Telephone> phoneNumbers = new ArrayList<Telephone>();
+	private String messenger;
+	private List<Nationality> candiateNationalities = new ArrayList<Nationality>();
+	private List<Nationality> maternalGuardianNationalities= new ArrayList<Nationality>();
+	private List<Nationality> paternalGuardianNationalities= new ArrayList<Nationality>();
+	
+	private List<LanguageProficiency> languageProficiencies = new ArrayList<LanguageProficiency>();
+
+	public PersonalDetailsBuilder id(Integer id) {
+		this.id = id;
+		return this;
+	}
+
+	public PersonalDetailsBuilder languageProficiencies(LanguageProficiency... languageProficiencies) {
+		this.languageProficiencies.addAll(Arrays.asList(languageProficiencies));
+		return this;
+	}
+	
+	
+	public PersonalDetailsBuilder residenceFromDate(Date residenceFromDate) {
+		this.residenceFromDate = residenceFromDate;
+		return this;
+	}
+	
+	public PersonalDetailsBuilder paternalGuardianNationalities(Nationality... nationalities) {
+		this.maternalGuardianNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder maternalGuardianNationalities(Nationality... nationalities) {
+		this.paternalGuardianNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder candiateNationalities(Nationality... nationalities) {
+		this.candiateNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+
+	public PersonalDetailsBuilder phoneNumbers(Telephone... phoneNumbers) {
+		this.phoneNumbers.addAll(Arrays.asList(phoneNumbers));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder messengers(String messenger) {
+		this.messenger = messenger;
+		return this;
+	}
+	public PersonalDetailsBuilder firstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public PersonalDetailsBuilder lastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public PersonalDetailsBuilder gender(Gender gender) {
+		this.gender = gender;
+		return this;
+	}
+
+	public PersonalDetailsBuilder email(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public PersonalDetailsBuilder dateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+		return this;
+	}
+
+	public PersonalDetailsBuilder country(Country country) {
+		this.country = country;
+		return this;
+	}
+
+	public PersonalDetailsBuilder residenceCountry(Country residenceCountry) {
+		this.residenceCountry = residenceCountry;
+		return this;
+	}
+
+	public PersonalDetailsBuilder residenceStatus(ResidenceStatus residenceStatus) {
+		this.residenceStatus = residenceStatus;
+		return this;
+	}
+
+	public PersonalDetailsBuilder applicationForm(ApplicationForm applicationForm) {
+		this.applicationForm = applicationForm;
+		return this;
+	}
+
+	public PersonalDetail toPersonalDetails() {
+		PersonalDetail personalDetails = new PersonalDetail();
+		personalDetails.setId(id);
+		personalDetails.setApplication(applicationForm);
+		personalDetails.setCountry(country);
+		personalDetails.setDateOfBirth(dateOfBirth);
+		personalDetails.setEmail(email);
+		personalDetails.setFirstName(firstName);
+		personalDetails.setGender(gender);
+		personalDetails.setLastName(lastName);
+		personalDetails.setResidenceCountry(residenceCountry);
+		personalDetails.setResidenceStatus(residenceStatus);
+		personalDetails.setPhoneNumbers(phoneNumbers);
+		personalDetails.setMaternalGuardianNationalities(maternalGuardianNationalities);
+		personalDetails.setPaternalGuardianNationalities(paternalGuardianNationalities);
+		personalDetails.setCandidateNationalities(candiateNationalities);
+		personalDetails.setLanguageProficiencies(languageProficiencies);
+		personalDetails.setMessenger(messenger);
+		personalDetails.setResidenceFromDate(residenceFromDate);
+		return personalDetails;
+	}
+}

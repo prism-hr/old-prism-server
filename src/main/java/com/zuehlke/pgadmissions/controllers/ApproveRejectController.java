b60f5f9263a6a4013f8de69d4cd6c5cb901324c7
iky <iky@LONN0170.ads.zuehlke.com>
2012-04-04 18:51:47 +0100

    initial referee registration stuff

---
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApproveRejectController.java
@@ -0,0 +1,77 @@
+package com.zuehlke.pgadmissions.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.enums.ApprovalStatus;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+import com.zuehlke.pgadmissions.exceptions.CannotApproveApplicationException;
+import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
+import com.zuehlke.pgadmissions.pagemodels.PageModel;
+import com.zuehlke.pgadmissions.services.ApplicationsService;
+
+@Controller
+@RequestMapping(value = { "/approveOrReject" })
+public class ApproveRejectController {
+
+	
+	private final ApplicationsService applicationsService;
+
+	ApproveRejectController() {
+		this(null);
+	}
+
+	@Autowired
+	public ApproveRejectController(ApplicationsService applicationsService) {
+		this.applicationsService = applicationsService;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView applyDecision(@ModelAttribute ApplicationForm applicationForm, @RequestParam ApprovalStatus decision) {
+		RegisteredUser approver = (RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
+		if(!(approver.isInRole(Authority.APPROVER) || approver.isInRole(Authority.ADMINISTRATOR))){
+			throw new ResourceNotFoundException();
+		}
+		
+		if (approver.isInRole(Authority.ADMINISTRATOR) && decision == ApprovalStatus.APPROVED){
+			throw new ResourceNotFoundException();
+		}
+		if(!applicationForm.isReviewable()){
+			throw new CannotApproveApplicationException();
+		}
+				
+		applicationForm.setApprovalStatus(decision);
+		applicationForm.setApprover(approver);
+		applicationsService.save(applicationForm);		
+		return new ModelAndView("redirect:/applications", "decision", decision.toString().toLowerCase()); 
+	}
+
+	
+	@ModelAttribute("applicationForm")
+	public ApplicationForm getApplicationForm(Integer id) {
+		RegisteredUser approver = (RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails();
+		ApplicationForm applicationForm = applicationsService.getApplicationById(id);
+		if(applicationForm == null || !approver.canSee(applicationForm)){
+			throw new ResourceNotFoundException();
+		}
+		return applicationForm;
+	}
+
+	@RequestMapping(value="/decisionmade", method = RequestMethod.GET)
+	public ModelAndView getApprovedOrRejectedPage(ApplicationForm applicationForm) {
+		PageModel pageModel = new PageModel();
+		pageModel.setApplicationForm(applicationForm);
+		pageModel.setUser((RegisteredUser) SecurityContextHolder.getContext().getAuthentication().getDetails());
+		return new ModelAndView("/reviewer/approvedOrRejected", "model", pageModel);
+	}
+
+
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/validators/NewAdminUserDTOValidatorTest.java
@@ -0,0 +1,80 @@
+package com.zuehlke.pgadmissions.validators;
+
+import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.validation.DirectFieldBindingResult;
+
+import com.zuehlke.pgadmissions.dto.NewAdminUserDTO;
+
+
+public class NewAdminUserDTOValidatorTest {
+
+	private NewAdminUserDTOValidator validator;
+	private NewAdminUserDTO newAdminUserDTO;
+	
+	@Test
+	public void shouldSupportAdminUser() {
+		assertTrue(validator.supports(NewAdminUserDTO.class));
+	}
+	
+	@Test
+	public void shouldAcceptAdminUser() {
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(0, mappingResult.getErrorCount());
+	}
+	
+	@Test
+	public void shouldRejectAdminUserIfNullFirstName() {
+		newAdminUserDTO.setNewUserFirstName(null);
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+	}
+	
+	@Test
+	public void shouldRejectAdminUserIfNullLastName() {
+		newAdminUserDTO.setNewUserLastName(null);
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+	}
+	
+	@Test
+	public void shouldRejectAdminUserIfNullEmail() {
+		newAdminUserDTO.setNewUserEmail(null);
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+	}
+	
+	@Test
+	public void shouldRejectAdminUserIfInvalidEmail() {
+		newAdminUserDTO.setNewUserEmail("test");
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+	}
+	
+	@Test
+	public void shouldRejectAdminUserIfNullProgram() {
+		newAdminUserDTO.setProgramForNewUser(null);
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(newAdminUserDTO, "adminUser");
+		validator.validate(newAdminUserDTO, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+	}
+
+	@Before
+	public void setup(){
+		validator = new NewAdminUserDTOValidator();
+		
+		newAdminUserDTO = new NewAdminUserDTO();
+		newAdminUserDTO.setProgramForNewUser(2);
+		newAdminUserDTO.setNewUserEmail("test@gmail.com");
+		newAdminUserDTO.setNewUserFirstName("test");
+		newAdminUserDTO.setNewUserLastName("test");
+	}
+}

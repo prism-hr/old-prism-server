bd11a39dc26576115e154ea680dc5dd913fc7a29
kpa <kpa@localhost>
2012-04-10 17:12:59 +0100

    Forgot to chek this in..

---
+++ src/test/java/com/zuehlke/pgadmissions/dao/RoleDAOTest.java
@@ -0,0 +1,75 @@
+package com.zuehlke.pgadmissions.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.dao.mappings.AutomaticRollbackTestCase;
+import com.zuehlke.pgadmissions.domain.Role;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+
+public class RoleDAOTest extends AutomaticRollbackTestCase {
+
+	
+	@Test(expected = NullPointerException.class)
+	public void shouldThrowNullPointerException() {
+		RoleDAO roleDAO = new RoleDAO();
+		roleDAO.getRoleByAuthority(Authority.ADMINISTRATOR);
+	}
+	
+	@Test
+	public void shoudlGetRoleByApplicantAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.APPLICANT);
+		assertNotNull(relaodedRole);
+
+	}
+
+	@Test
+	public void shoudlGetRoleByAdminAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.ADMINISTRATOR);
+		assertNotNull(relaodedRole);
+		
+	}
+	
+	@Test
+	public void shoudlGetRoleByReviewerAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.REVIEWER);
+		assertNotNull(relaodedRole);
+		
+	}
+	
+	@Test
+	public void shoudlGetRoleBySuperadminAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.SUPERADMINISTRATOR);
+		assertNotNull(relaodedRole);
+		
+	}
+	
+	@Test
+	public void shoudlGetRoleByApproverAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.APPROVER);
+		assertNotNull(relaodedRole);
+		
+	}
+	@Test
+	public void shoudlGetRoleByRefereeAuthority() {
+		flushAndClearSession();
+		RoleDAO roleDAO = new RoleDAO(sessionFactory);
+		Role relaodedRole = roleDAO.getRoleByAuthority(Authority.REFEREE);
+		assertEquals(Authority.REFEREE, relaodedRole.getAuthorityEnum());
+		assertNotNull(relaodedRole);
+		
+	}
+	
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/domain/builders/PersonalDetailsBuilder.java
@@ -0,0 +1,136 @@
+package com.zuehlke.pgadmissions.domain.builders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Country;
+import com.zuehlke.pgadmissions.domain.PersonalDetail;
+import com.zuehlke.pgadmissions.domain.Telephone;
+import com.zuehlke.pgadmissions.domain.enums.CheckedStatus;
+import com.zuehlke.pgadmissions.domain.enums.Gender;
+
+public class PersonalDetailsBuilder {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private Gender gender;
+	private String email;
+	private Date dateOfBirth;
+	private Country country;
+	private Country residenceCountry;
+	private ApplicationForm applicationForm;
+	private List<Telephone> phoneNumbers = new ArrayList<Telephone>();
+	private String messenger;
+	private List<Country> candiateNationalities = new ArrayList<Country>();
+	private List<Country> maternalGuardianNationalities= new ArrayList<Country>();
+	private List<Country> paternalGuardianNationalities= new ArrayList<Country>();
+	private CheckedStatus englishFirstLanguage;
+	private CheckedStatus requiresVisa;
+	
+
+	public PersonalDetailsBuilder id(Integer id) {
+		this.id = id;
+		return this;
+	}
+	
+	public PersonalDetailsBuilder englishFirstLanguage(CheckedStatus englishFirstLanguage) {
+		this.englishFirstLanguage = englishFirstLanguage;
+		return this;
+	}
+	
+	public PersonalDetailsBuilder requiresVisa(CheckedStatus requiresVisa) {
+		this.requiresVisa = requiresVisa;
+		return this;
+	}
+	
+	
+	public PersonalDetailsBuilder paternalGuardianNationalities(Country... nationalities) {
+		this.paternalGuardianNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder maternalGuardianNationalities(Country... nationalities) {
+		this.maternalGuardianNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder candiateNationalities(Country... nationalities) {
+		this.candiateNationalities.addAll(Arrays.asList(nationalities));
+		return this;
+	}
+
+	public PersonalDetailsBuilder phoneNumbers(Telephone... phoneNumbers) {
+		this.phoneNumbers.addAll(Arrays.asList(phoneNumbers));
+		return this;
+	}
+	
+	public PersonalDetailsBuilder messengers(String messenger) {
+		this.messenger = messenger;
+		return this;
+	}
+	public PersonalDetailsBuilder firstName(String firstName) {
+		this.firstName = firstName;
+		return this;
+	}
+
+	public PersonalDetailsBuilder lastName(String lastName) {
+		this.lastName = lastName;
+		return this;
+	}
+
+	public PersonalDetailsBuilder gender(Gender gender) {
+		this.gender = gender;
+		return this;
+	}
+
+	public PersonalDetailsBuilder email(String email) {
+		this.email = email;
+		return this;
+	}
+
+	public PersonalDetailsBuilder dateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+		return this;
+	}
+
+	public PersonalDetailsBuilder country(Country country) {
+		this.country = country;
+		return this;
+	}
+
+	public PersonalDetailsBuilder residenceCountry(Country residenceCountry) {
+		this.residenceCountry = residenceCountry;
+		return this;
+	}
+
+
+	public PersonalDetailsBuilder applicationForm(ApplicationForm applicationForm) {
+		this.applicationForm = applicationForm;
+		return this;
+	}
+
+	public PersonalDetail toPersonalDetails() {
+		PersonalDetail personalDetails = new PersonalDetail();
+		personalDetails.setId(id);
+		personalDetails.setApplication(applicationForm);
+		personalDetails.setCountry(country);
+		personalDetails.setDateOfBirth(dateOfBirth);
+		personalDetails.setEmail(email);
+		personalDetails.setFirstName(firstName);
+		personalDetails.setGender(gender);
+		personalDetails.setLastName(lastName);
+		personalDetails.setResidenceCountry(residenceCountry);
+		personalDetails.setPhoneNumbers(phoneNumbers);
+		personalDetails.setMaternalGuardianNationalities(maternalGuardianNationalities);
+		personalDetails.setPaternalGuardianNationalities(paternalGuardianNationalities);
+		personalDetails.setCandidateNationalities(candiateNationalities);
+		personalDetails.setMessenger(messenger);
+		personalDetails.setEnglishFirstLanguage(englishFirstLanguage);
+		personalDetails.setRequiresVisa(requiresVisa);
+		return personalDetails;
+	}
+}

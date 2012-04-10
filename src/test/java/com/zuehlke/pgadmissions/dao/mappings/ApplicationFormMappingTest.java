88d6c47b9ecca867175ad5fe07f73721f4672789
ans <ans@LONN0167.ads.zuehlke.com>
2012-04-10 17:51:41 +0100

    programme section updated in pdf

---
+++ src/test/java/com/zuehlke/pgadmissions/dao/mappings/ApplicationFormMappingTest.java
@@ -0,0 +1,261 @@
+package com.zuehlke.pgadmissions.dao.mappings;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.dao.CountriesDAO;
+import com.zuehlke.pgadmissions.dao.LanguageDAO;
+import com.zuehlke.pgadmissions.domain.Address;
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.ApplicationReview;
+import com.zuehlke.pgadmissions.domain.Country;
+import com.zuehlke.pgadmissions.domain.Document;
+import com.zuehlke.pgadmissions.domain.PersonalDetail;
+import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.Project;
+import com.zuehlke.pgadmissions.domain.Qualification;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.builders.AddressBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ApplicationReviewBuilder;
+import com.zuehlke.pgadmissions.domain.builders.CountryBuilder;
+import com.zuehlke.pgadmissions.domain.builders.DocumentBuilder;
+import com.zuehlke.pgadmissions.domain.builders.PersonalDetailsBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProjectBuilder;
+import com.zuehlke.pgadmissions.domain.builders.QualificationBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.enums.CheckedStatus;
+import com.zuehlke.pgadmissions.domain.enums.DocumentType;
+import com.zuehlke.pgadmissions.domain.enums.Gender;
+import com.zuehlke.pgadmissions.domain.enums.QualificationLevel;
+import com.zuehlke.pgadmissions.domain.enums.SubmissionStatus;
+import com.zuehlke.pgadmissions.services.CountryService;
+
+public class ApplicationFormMappingTest extends AutomaticRollbackTestCase {
+
+	private RegisteredUser user;
+	private Program program;
+	private Project project;
+
+	@Test
+	public void shouldSaveAndLoadApplicationForm() {
+
+		ApplicationForm application = new ApplicationForm();
+		application.setApplicant(user);
+		application.setProject(project);
+		application.setSubmissionStatus(SubmissionStatus.UNSUBMITTED);
+
+		assertNotNull(application.getPersonalDetails());
+		assertNull(application.getId());
+
+		sessionFactory.getCurrentSession().save(application);
+
+		assertNotNull(application.getId());
+		Integer id = application.getId();
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertSame(application, reloadedApplication);
+
+		flushAndClearSession();
+
+		reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertNotSame(application, reloadedApplication);
+		assertEquals(application, reloadedApplication);
+
+		assertEquals(user, reloadedApplication.getApplicant());
+		assertEquals(project, reloadedApplication.getProject());
+		assertEquals(SubmissionStatus.UNSUBMITTED, reloadedApplication.getSubmissionStatus());
+
+		assertNotNull(application.getPersonalDetails());
+
+		assertNull(application.getPersonalDetails().getId());
+	}
+
+	@Test
+	public void shouldLoadApplicationFormWithPersonalDetails() throws ParseException {
+		Country country1 = new CountryBuilder().code("AA").name("AA").toCountry();
+		Country country2 = new CountryBuilder().code("CC").name("CC").toCountry();
+		save(country1, country2);
+
+		ApplicationForm application = new ApplicationFormBuilder().applicant(user).project(project).submissionStatus(SubmissionStatus.UNSUBMITTED)
+				.toApplicationForm();
+
+		sessionFactory.getCurrentSession().save(application);
+		flushAndClearSession();
+		PersonalDetail personalDetails = new PersonalDetailsBuilder().country(country1).dateOfBirth(new SimpleDateFormat("dd/MM/yyyy").parse("01/06/1980"))
+				.email("email").firstName("firstName").gender(Gender.MALE).lastName("lastname").residenceCountry(country2).englishFirstLanguage(CheckedStatus.YES)
+				.requiresVisa(CheckedStatus.NO).applicationForm(application).toPersonalDetails();
+
+		sessionFactory.getCurrentSession().save(personalDetails);
+		flushAndClearSession();
+
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, application.getId());
+		assertEquals(personalDetails, reloadedApplication.getPersonalDetails());
+
+	}
+
+	@Test
+	public void shouldSaveAndLoadApplicationFormWithReviewer() {
+
+		ApplicationForm application = new ApplicationForm();
+		application.setProject(project);
+		application.setApplicant(user);
+		application.setSubmissionStatus(SubmissionStatus.SUBMITTED);
+		application.setReviewers(Arrays.asList(user));
+
+		assertNull(application.getId());
+
+		sessionFactory.getCurrentSession().save(application);
+
+		assertNotNull(application.getId());
+		Integer id = application.getId();
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertSame(application, reloadedApplication);
+
+		flushAndClearSession();
+
+		reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertNotSame(application, reloadedApplication);
+		assertEquals(application, reloadedApplication);
+
+		assertEquals(user, reloadedApplication.getApplicant());
+		assertEquals(project, reloadedApplication.getProject());
+		assertEquals(SubmissionStatus.SUBMITTED, reloadedApplication.getSubmissionStatus());
+		Assert.assertEquals(1, reloadedApplication.getReviewers().size());
+		Assert.assertTrue(reloadedApplication.getReviewers().contains(user));
+	}
+
+	@Test
+	public void shouldSaveAndLoadApplicationFormWithAddress() {
+		ApplicationForm application = new ApplicationForm();
+		application.setProject(project);
+		application.setApplicant(user);
+		application.setSubmissionStatus(SubmissionStatus.SUBMITTED);
+		CountryService countriesService = new CountryService(new CountriesDAO(sessionFactory));
+		
+		Address address = new AddressBuilder().application(application).country(countriesService.getCountryById(2)).location("london")
+				.toAddress();
+		application.setAddresses(Arrays.asList(address));
+
+		assertNull(application.getId());
+
+		sessionFactory.getCurrentSession().save(application);
+
+		assertNotNull(application.getId());
+		Integer id = application.getId();
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertSame(application, reloadedApplication);
+
+		flushAndClearSession();
+
+		reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertNotSame(application, reloadedApplication);
+		assertEquals(application, reloadedApplication);
+
+		assertEquals(user, reloadedApplication.getApplicant());
+		assertEquals(project, reloadedApplication.getProject());
+		assertEquals(SubmissionStatus.SUBMITTED, reloadedApplication.getSubmissionStatus());
+		Assert.assertEquals(1, reloadedApplication.getAddresses().size());
+		Assert.assertTrue(reloadedApplication.getAddresses().contains(address));
+
+	}
+	@Test
+	public void shouldLoadApplicationFormWithCVAndPersonalStatement() {
+
+		ApplicationForm application = new ApplicationForm();
+		application.setProject(project);
+		application.setApplicant(user);
+		Document cv = new DocumentBuilder().fileName("bob").type(DocumentType.CV).content("aaa!".getBytes()).toDocument();
+		Document personalStatement = new DocumentBuilder().fileName("bob").type(DocumentType.PERSONAL_STATEMENT).content("aaa!".getBytes()).toDocument();
+		save(cv, personalStatement);
+		flushAndClearSession();
+		application.setCv(cv);
+		application.setPersonalStatement(personalStatement);
+		
+		sessionFactory.getCurrentSession().save(application);
+		flushAndClearSession();
+		
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, application.getId());
+		
+		assertEquals(cv, reloadedApplication.getCv());
+		assertEquals(personalStatement, application.getPersonalStatement());
+	}
+
+	@Test
+	public void shouldLoadApplicationFormWithComments() {
+
+		ApplicationForm application = new ApplicationForm();
+		application.setProject(project);
+		application.setApplicant(user);
+
+		sessionFactory.getCurrentSession().save(application);
+		Integer id = application.getId();
+		flushAndClearSession();
+
+		ApplicationReview applicationReviewOne = new ApplicationReviewBuilder().application(application).comment("comment1").user(user).toApplicationReview();
+		ApplicationReview applicationReviewTwo = new ApplicationReviewBuilder().application(application).comment("comment2").user(user).toApplicationReview();
+		save(applicationReviewOne, applicationReviewTwo);
+
+		flushAndClearSession();
+
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertEquals(2, reloadedApplication.getApplicationComments().size());
+		assertTrue(reloadedApplication.getApplicationComments().containsAll(Arrays.asList(applicationReviewOne, applicationReviewTwo)));
+	}
+
+	@Test
+	public void shouldSaveQualificationsWithApplication() throws ParseException {
+
+		ApplicationForm application = new ApplicationForm();
+		application.setProject(project);
+		application.setApplicant(user);
+
+		// sessionFactory.getCurrentSession().save(application);
+		// Integer id = application.getId();
+		// flushAndClearSession();
+		LanguageDAO languageDAO = new LanguageDAO(sessionFactory);
+		CountriesDAO countriesDAO = new CountriesDAO(sessionFactory);
+		Qualification qualification1 = new QualificationBuilder().awardDate(new SimpleDateFormat("yyyy/MM/dd").parse("2011/02/02")).grade("")
+				.institution("").languageOfStudy(languageDAO.getLanguageById(1)).level(QualificationLevel.COLLEGE).subject("").isCompleted(CheckedStatus.YES)
+				.startDate(new SimpleDateFormat("yyyy/MM/dd").parse("2006/09/09")).type("").institutionCountry(countriesDAO.getAllCountries().get(0)).toQualification();
+		Qualification qualification2 = new QualificationBuilder().awardDate(new SimpleDateFormat("yyyy/MM/dd").parse("2011/02/02")).grade("").isCompleted(CheckedStatus.YES)
+				.institution("").languageOfStudy(languageDAO.getLanguageById(2)).level(QualificationLevel.COLLEGE).subject("")
+				.startDate(new SimpleDateFormat("yyyy/MM/dd").parse("2006/09/09")).type("").institutionCountry(countriesDAO.getAllCountries().get(0)).toQualification();
+
+		application.getQualifications().addAll(Arrays.asList(qualification1, qualification2));
+
+		sessionFactory.getCurrentSession().saveOrUpdate(application);
+		flushAndClearSession();
+
+		Integer id = application.getId();
+		ApplicationForm reloadedApplication = (ApplicationForm) sessionFactory.getCurrentSession().get(ApplicationForm.class, id);
+		assertEquals(2, reloadedApplication.getQualifications().size());
+
+	}
+
+	@Before
+	public void setup() {
+		user = new RegisteredUserBuilder().firstName("Jane").lastName("Doe").email("email@test.com").username("username").password("password")
+				.accountNonExpired(false).accountNonLocked(false).credentialsNonExpired(false).enabled(false).toUser();
+
+		program = new ProgramBuilder().code("doesntexist").description("blahblab").title("another title").toProgram();
+		project = new ProjectBuilder().code("neitherdoesthis").description("hello").title("title two").program(program).toProject();
+		save(user, program, project);
+
+		flushAndClearSession();
+	}
+
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/services/ApplicationsServiceTest.java
@@ -0,0 +1,232 @@
+package com.zuehlke.pgadmissions.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+
+import com.zuehlke.pgadmissions.dao.AddressDAO;
+import com.zuehlke.pgadmissions.dao.ApplicationFormDAO;
+import com.zuehlke.pgadmissions.dao.EmploymentPositionDAO;
+import com.zuehlke.pgadmissions.dao.FundingDAO;
+import com.zuehlke.pgadmissions.dao.QualificationDAO;
+import com.zuehlke.pgadmissions.dao.RefereeDAO;
+import com.zuehlke.pgadmissions.domain.Address;
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Funding;
+import com.zuehlke.pgadmissions.domain.Project;
+import com.zuehlke.pgadmissions.domain.Qualification;
+import com.zuehlke.pgadmissions.domain.Referee;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.builders.AddressBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProjectBuilder;
+import com.zuehlke.pgadmissions.domain.builders.QualificationBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RoleBuilder;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+import com.zuehlke.pgadmissions.domain.enums.QualificationLevel;
+import com.zuehlke.pgadmissions.domain.enums.SubmissionStatus;
+
+public class ApplicationsServiceTest {
+
+	private RegisteredUser user;
+	private ApplicationFormDAO applicationFormDAOMock;
+	private ApplicationsService applicationsService;
+	private AddressDAO addressDAOMock;
+	private QualificationDAO qualificationDAOMock;
+	private FundingDAO fundingDAOMock;
+	private EmploymentPositionDAO employmentDAOMock;
+	private RefereeDAO refereeDAOMock;
+
+	@Test
+	public void shouldgetListOfApplicationsForApplicant() {
+		ApplicationForm form = new ApplicationFormBuilder().id(1).toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getApplicationsByApplicant(user)).andReturn(Arrays.asList(form));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(user);
+		Assert.assertTrue(visibleApplications.contains(form));
+		Assert.assertEquals(1, visibleApplications.size());
+	}
+
+	@Test
+	public void shouldGetListOfApplicationsForAssignedReviewer() {
+		RegisteredUser reviewer = new RegisteredUserBuilder().id(2).username("tom").roles(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole())
+				.toUser();
+		Set<RegisteredUser> reviewers = new HashSet<RegisteredUser>();
+		reviewers.add(reviewer);
+		ApplicationForm underReviewForm = new ApplicationFormBuilder().id(1).reviewers(reviewers).submissionStatus(SubmissionStatus.SUBMITTED)
+				.toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getAllApplications()).andReturn(Arrays.asList(underReviewForm));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(reviewer);
+		Assert.assertEquals(1, visibleApplications.size());
+		Assert.assertTrue(visibleApplications.contains(underReviewForm));
+	}
+
+	@Test
+	public void shouldNotGetListOfApplicationsForUnAssignedReviewer() {
+		RegisteredUser reviewer = new RegisteredUserBuilder().id(2).username("tom").roles(new RoleBuilder().authorityEnum(Authority.REVIEWER).toRole())
+				.toUser();
+		ApplicationForm underReviewForm = new ApplicationFormBuilder().id(1).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getAllApplications()).andReturn(Arrays.asList(underReviewForm));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(reviewer);
+		Assert.assertEquals(0, visibleApplications.size());
+		Assert.assertFalse(visibleApplications.contains(underReviewForm));
+	}
+
+	@Test
+	public void shouldNotGetListOfApplicationsForUnAssignedAdministrator() {
+		RegisteredUser administrator = new RegisteredUserBuilder().id(2).username("tom")
+				.roles(new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		ApplicationForm underReviewForm = new ApplicationFormBuilder().id(1)
+				.project(new ProjectBuilder().program(new ProgramBuilder().toProgram()).toProject()).submissionStatus(SubmissionStatus.SUBMITTED)
+				.toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getAllApplications()).andReturn(Arrays.asList(underReviewForm));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(administrator);
+		Assert.assertEquals(0, visibleApplications.size());
+		Assert.assertFalse(visibleApplications.contains(underReviewForm));
+	}
+
+	@Test
+	public void shouldGetListOfApplicationsForAssignedAdministrator() {
+		RegisteredUser administrator = new RegisteredUserBuilder().id(2).username("tom")
+				.roles(new RoleBuilder().authorityEnum(Authority.ADMINISTRATOR).toRole()).toUser();
+		ProgramBuilder programBuilder = new ProgramBuilder();
+		programBuilder.administrators(administrator);
+		ApplicationForm underReviewForm = new ApplicationFormBuilder().id(1).project(new ProjectBuilder().program(programBuilder.toProgram()).toProject())
+				.submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getAllApplications()).andReturn(Arrays.asList(underReviewForm));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(administrator);
+		Assert.assertEquals(1, visibleApplications.size());
+		Assert.assertTrue(visibleApplications.contains(underReviewForm));
+	}
+
+	@Test
+	public void shouldGetListOfApplicationsForSuperAdministrator() {
+		RegisteredUser superAdministrator = new RegisteredUserBuilder().id(2).username("tom")
+				.roles(new RoleBuilder().authorityEnum(Authority.SUPERADMINISTRATOR).toRole()).toUser();
+		ApplicationForm underReviewForm = new ApplicationFormBuilder().id(1).submissionStatus(SubmissionStatus.SUBMITTED).toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getAllApplications()).andReturn(Arrays.asList(underReviewForm));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(superAdministrator);
+		Assert.assertEquals(1, visibleApplications.size());
+		Assert.assertTrue(visibleApplications.contains(underReviewForm));
+	}
+
+	@Test
+	public void shouldGetMostRecentApplicationFirst() throws InterruptedException {
+		ApplicationForm app1 = new ApplicationFormBuilder().id(1).submissionStatus(SubmissionStatus.SUBMITTED).appDate(new Date()).toApplicationForm();
+		Thread.sleep(1000);
+		ApplicationForm app2 = new ApplicationFormBuilder().id(2).submissionStatus(SubmissionStatus.SUBMITTED).appDate(new Date()).toApplicationForm();
+		EasyMock.expect(applicationFormDAOMock.getApplicationsByApplicant(user)).andReturn(Arrays.asList(app1, app2));
+		EasyMock.replay(applicationFormDAOMock);
+		List<ApplicationForm> visibleApplications = applicationsService.getVisibleApplications(user);
+		Assert.assertEquals(2, visibleApplications.size());
+		Assert.assertEquals(app2, visibleApplications.get(0));
+		Assert.assertEquals(app1, visibleApplications.get(1));
+	}
+
+	@Test
+	public void shouldDelegateDeleteAddressToDAO() {
+		Address address = new AddressBuilder().id(1).toAddress();
+		addressDAOMock.delete(address);
+		EasyMock.replay(addressDAOMock);
+		applicationsService.deleteAddress(address);
+		EasyMock.verify(addressDAOMock);
+	}
+
+	@Test
+	public void shouldDelegateApplicationSaveToDAO() {
+		ApplicationForm application = EasyMock.createMock(ApplicationForm.class);
+		applicationFormDAOMock.save(application);
+		EasyMock.replay(application, applicationFormDAOMock);
+		applicationsService.save(application);
+		EasyMock.verify(applicationFormDAOMock);
+	}
+
+	@Test
+	public void shouldGetApplicationById() {
+		ApplicationForm application = EasyMock.createMock(ApplicationForm.class);
+		EasyMock.expect(applicationFormDAOMock.get(234)).andReturn(application);
+
+		EasyMock.replay(application, applicationFormDAOMock);
+		Assert.assertEquals(application, applicationsService.getApplicationById(234));
+	}
+
+	@Test
+	public void shouldGetAddressById() {
+		Address address = EasyMock.createMock(Address.class);
+		EasyMock.expect(applicationFormDAOMock.getAdddressById(23)).andReturn(address);
+
+		EasyMock.replay(address, applicationFormDAOMock);
+		Assert.assertEquals(address, applicationsService.getAddressById(23));
+	}
+
+	@Test
+	public void shouldCreateAndSaveNewApplicationForm() {
+		Project project = new ProjectBuilder().id(1).toProject();
+		RegisteredUser registeredUser = new RegisteredUserBuilder().id(1).toUser();
+		final ApplicationForm newApplicationForm = new ApplicationFormBuilder().id(1).toApplicationForm();
+		applicationsService = new ApplicationsService(applicationFormDAOMock, addressDAOMock, qualificationDAOMock, fundingDAOMock, employmentDAOMock,
+				refereeDAOMock) {
+
+			@Override
+			ApplicationForm newApplicationForm() {
+				return newApplicationForm;
+			}
+		};
+		applicationFormDAOMock.save(newApplicationForm);
+		EasyMock.replay(applicationFormDAOMock);
+		ApplicationForm returnedForm = applicationsService.createAndSaveNewApplicationForm(registeredUser, project);
+		EasyMock.verify(applicationFormDAOMock);
+		assertSame(newApplicationForm, returnedForm);
+		assertEquals(registeredUser, returnedForm.getApplicant());
+		assertEquals(project, returnedForm.getProject());
+
+	}
+
+	@Before
+	public void setUp() {
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(null, null);
+		user = new RegisteredUserBuilder().id(1).username("bob").role(new RoleBuilder().authorityEnum(Authority.APPLICANT).toRole()).toUser();
+		authenticationToken.setDetails(user);
+		SecurityContextImpl secContext = new SecurityContextImpl();
+		secContext.setAuthentication(authenticationToken);
+		SecurityContextHolder.setContext(secContext);
+
+		applicationFormDAOMock = EasyMock.createMock(ApplicationFormDAO.class);
+		addressDAOMock = EasyMock.createMock(AddressDAO.class);
+		qualificationDAOMock = EasyMock.createMock(QualificationDAO.class);
+		fundingDAOMock = EasyMock.createMock(FundingDAO.class);
+		employmentDAOMock = EasyMock.createMock(EmploymentPositionDAO.class);
+		refereeDAOMock = EasyMock.createMock(RefereeDAO.class);
+		applicationsService = new ApplicationsService(applicationFormDAOMock, addressDAOMock, qualificationDAOMock, fundingDAOMock, employmentDAOMock,
+				refereeDAOMock);
+	}
+
+	@After
+	public void tearDown() {
+		SecurityContextHolder.clearContext();
+	}
+}

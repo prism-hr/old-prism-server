5820962cca6f49cf4e2c73af387fe8cb9ee9e2b3
iky <iky@LONN0170.ads.zuehlke.com>
2012-04-25 09:48:00 +0100

    send email to admins if not validated and last email sent before 2 weeks - currently checks every 2 secs

--- src/main/java/com/zuehlke/pgadmissions/utils/MailTimerTask.java
+++ src/main/java/com/zuehlke/pgadmissions/utils/MailTimerTask.java
@@ -1,5 +1,7 @@
 package com.zuehlke.pgadmissions.utils;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.TimerTask;
 
@@ -38,10 +40,31 @@ public class MailTimerTask extends TimerTask{
 		public void run() {
 			transaction = getSessionFactory().getCurrentSession().beginTransaction();
 			List<ApplicationForm> applications = applicationsService.getAllApplicationsStillInValidationStageAndAfterDueDate();
+			System.out.println(applications);
 			for (ApplicationForm applicationForm : applications) {
-				service.printMe(applicationForm);
+				if(isLastMailSentTwoWeeksOld(applicationForm)){
+					service.sendMailToAdmins(applicationForm);
+				}
 			}
 			transaction.commit();		
+		}
+		
+		public boolean isLastMailSentTwoWeeksOld(ApplicationForm applicationForm){
+			Date lastDateMailWasSent = applicationForm.getLastEmailReminderDate();
+			Calendar calendar  = Calendar.getInstance();
+			Date today = calendar.getTime();
+			int daysBetween = 0;
+			if(lastDateMailWasSent!=null){
+			while (today.after(lastDateMailWasSent)) {
+				 calendar.add(Calendar.DAY_OF_MONTH, -1);  
+				 today = calendar.getTime();
+				 daysBetween++;
+				}
+			}
+			if(daysBetween >= 14){
+				return true;
+			}
+			return false;
 		}
 
 		public SessionFactory getSessionFactory() {

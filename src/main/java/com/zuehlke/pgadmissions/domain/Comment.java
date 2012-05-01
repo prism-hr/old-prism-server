cde7af22fe99a4f7b8799b083a90cd54e01d86b1
kpa <kpa@localhost>
2012-05-01 11:37:16 +0100

    Start introducing inheritance hireachy for comments

--- src/main/java/com/zuehlke/pgadmissions/domain/Comment.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/Comment.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -18,6 +21,8 @@ import org.hibernate.annotations.GenerationTime;
 
 
 @Entity(name="COMMENT")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="comment_type")
 @Access(AccessType.FIELD) 
 public class Comment extends DomainObject<Integer>{
 

e025864765c431b4918d948a53efec9ccf7d5818
kpa <kpa@localhost>
2012-05-22 16:58:15 +0100

    small tweak

--- src/main/webapp/WEB-INF/design/default/js/admin/comment/timelineApplication.js
+++ src/main/webapp/WEB-INF/design/default/js/admin/comment/timelineApplication.js
@@ -1,40 +1,47 @@
 $(document).ready(function(){
 
 	$('#application').html("");
+	$('#application').hide();
 	$.get("/pgadmissions/comments/view",
 			{
 				id:  $('#applicationId').val(),				
 				cacheBreaker: new Date().getTime() 
 			},
 			function(data) {
-				$('#timeline').html(data);
+				$('#timeline').html(data);				
 			}
 	);
 	
 	
 	$('#timelineBtn').click(function() {
 		$('#application').html("");
+		$('#application').hide();
+		$('#timeline').show();
 		$.get("/pgadmissions/comments/view",
 				{
 					id:  $('#applicationId').val(),				
 					cacheBreaker: new Date().getTime() 
 				},
 				function(data) {
-					$('#timeline').html(data);
+					$('#timeline').html(data);				
+						
 				}
 		);
 	});
 	
 	$('#applicationBtn').click(function() {
 		$('#timeline').html("");
+		$('#timeline').hide();
+		$('#application').show();
 		$.get("/pgadmissions/application?view=view",
 				{
-			embeddedApplication: "true",				
-			applicationId:  $('#applicationId').val(),				
-			cacheBreaker: new Date().getTime() 
+				embeddedApplication: "true",				
+				applicationId:  $('#applicationId').val(),				
+				cacheBreaker: new Date().getTime() 
 				},
 				function(data) {
 					$('#application').html(data);
+				
 				}
 		);
 	});
--- src/main/webapp/WEB-INF/freemarker/private/staff/admin/comment/timeline_application.ftl
+++ src/main/webapp/WEB-INF/freemarker/private/staff/admin/comment/timeline_application.ftl
@@ -6,12 +6,14 @@
 
 
 <div class="buttons">						        		
-		<button type="button" id="timelineBtn">Timeline</button>
+		<button type="button" id="timelineBtn" class="blue" >Timeline</button>
 		<button class="blue" id="applicationBtn" type="button" >Application</button>						        
 </div>
-
-<span id="timeline">
-</span>
-
-<span id="application">
-</span>
+<br/>
+<div id="timelineview">
+	<span id="timeline">
+	</span>
+	
+	<div id="application">
+	</div>
+</div>

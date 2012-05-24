1786a664c3fd043f1569a5b4e831598a76dbc6ae
dmaughan <dmaughan@localhost>
2012-05-24 11:49:19 +0100

    updated warning indicator for programme details section.

--- src/main/webapp/WEB-INF/design/default/js/application/programme.js
+++ src/main/webapp/WEB-INF/design/default/js/application/programme.js
@@ -128,16 +128,22 @@ $(document).ready(function(){
 	});
 	
 	
-	$('#programmeSaveButton').on("click",function(){
-			if( $("#acceptTermsPDValue").val() =='NO'){ 
-				//$("span[name='nonAcceptedPD']").html('You must agree to the terms and conditions');
-				$(this).parent().parent().find('.terms-box').css({borderColor: 'red', color: 'red'});
-			}
-			else{
-				$("span[name='nonAcceptedPD']").html('');
-				postProgrammeData('close');
-			}
+	// Secondary agreement of terms.
+	$('#programmeSaveButton').on("click",function()
+	{
+		if ($("#acceptTermsPDValue").val() =='NO')
+		{
+			// Highlight the information bar and terms box.
+			var $form = $('#programmeDetailsSection form');
+			$('.terms-box, .section-info-bar', $form).css({ borderColor: 'red', color: 'red' });
+		}
+		else
+		{
+			$("span[name='nonAcceptedPD']").html('');
+			postProgrammeData('close');
+		}
 	});	
+	
 	function validateEmail(email) { 
 		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 	    var result = pattern.test(email);

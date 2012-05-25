2bd58e0367983cceb92367db404ef6ad14b45dad
iky <iky@LONN0170.ads.zuehlke.com>
2012-05-25 16:17:58 +0100

    allow process & sending ref mails on ref edit in all modifiable stages

---
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationListController.java
@@ -0,0 +1,60 @@
+package com.zuehlke.pgadmissions.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.services.ApplicationsService;
+import com.zuehlke.pgadmissions.services.UserService;
+
+@Controller
+@RequestMapping(value = {"","applications"})
+public class ApplicationListController {
+
+	private static final String APPLICATION_LIST_VIEW_NAME = "private/my_applications_page";
+	private final ApplicationsService applicationsService;
+	private final UserService userService;
+	
+	ApplicationListController(){
+		this(null, null);
+	}
+
+	@Autowired
+	public ApplicationListController(ApplicationsService applicationsService, UserService userService) {
+		this.applicationsService = applicationsService;
+		this.userService = userService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getApplicationListPage() {			
+		return APPLICATION_LIST_VIEW_NAME;
+	}
+
+	@ModelAttribute("user")
+	public RegisteredUser getUser() {
+		return userService.getCurrentUser();
+	}
+	
+	@ModelAttribute("applications")
+	public List<ApplicationForm> getApplications() {
+		return applicationsService.getVisibleApplications(getUser());
+	}
+
+	@ModelAttribute("message")
+	public String getMessage(@RequestParam(required=false) boolean submissionSuccess, @RequestParam(required=false) String decision) {
+		if(submissionSuccess){
+	       return "Your application has been successfully submitted.";
+		}
+		if(decision != null){
+		    return "The application was successfully " + decision +".";
+		}	
+		return null;
+	}
+}
---
+++ src/main/java/com/zuehlke/pgadmissions/mail/RegistryMailSender.java
@@ -0,0 +1,67 @@
+package com.zuehlke.pgadmissions.mail;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.mail.internet.InternetAddress;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Component;
+
+import com.zuehlke.pgadmissions.dao.RegistryUserDAO;
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.RegistryUser;
+import com.zuehlke.pgadmissions.services.UserService;
+import com.zuehlke.pgadmissions.utils.Environment;
+
+@Component
+public class RegistryMailSender extends MailSender {
+
+	private final RegistryUserDAO registryUserDAO;
+	private final UserService userService;
+
+	RegistryMailSender(){
+		this(null, null, null, null);
+	}
+	
+	@Autowired
+	public RegistryMailSender(MimeMessagePreparatorFactory mimeMessagePreparatorFactory, JavaMailSender mailSender, RegistryUserDAO registryUserDAO,
+			UserService userService) {
+		super(mimeMessagePreparatorFactory, mailSender);
+		this.registryUserDAO = registryUserDAO;
+		this.userService = userService;
+	}
+
+	public void sendApplicationToRegistryContacts(ApplicationForm applicationForm) throws UnsupportedEncodingException {
+		List<RegistryUser> registryContacts = registryUserDAO.getAllRegistryUsers();
+		InternetAddress[] toAddresses = new InternetAddress[registryContacts.size()];
+		int counter = 0;
+		for (RegistryUser registryUser : registryContacts) {
+			toAddresses[counter++] = new InternetAddress(registryUser.getEmail(), registryUser.getFirstname() + " " + registryUser.getLastname());
+		}
+		RegisteredUser currentUser = userService.getCurrentUser();
+		InternetAddress ccAdminAddres = new InternetAddress(currentUser.getEmail(), currentUser.getFirstName() + " " + currentUser.getLastName());
+		String subject = "Application " + applicationForm.getId() + " for UCL " + applicationForm.getProgram().getTitle() + " - Validation Request";
+		String templatename = "private/staff/admin/mail/registry_validation_request.ftl";
+
+		MimeMessagePreparator mimeMessagePreparator = mimeMessagePreparatorFactory.getMimeMessagePreparator(toAddresses,
+				new InternetAddress[] { ccAdminAddres }, subject, templatename, createModel(applicationForm, currentUser), ccAdminAddres);
+		javaMailSender.send(mimeMessagePreparator);
+
+	}
+
+	public Map<String, Object> createModel(ApplicationForm applicationForm, RegisteredUser currentAdminUser) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("application", applicationForm);
+		model.put("sender", currentAdminUser);
+		model.put("host", Environment.getInstance().getApplicationHostName());
+		return model;
+
+	}
+
+}

5c4be86ea23b427f6fd20890131b10615df78923
iky <iky@LONN0170.ads.zuehlke.com>
2012-05-28 18:23:18 +0100

    allow reviewer/interviewer to add their feedback in new rounds

---
+++ src/main/java/com/zuehlke/pgadmissions/dao/UserDAO.java
@@ -0,0 +1,199 @@
+package com.zuehlke.pgadmissions.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Interviewer;
+import com.zuehlke.pgadmissions.domain.NotificationRecord;
+import com.zuehlke.pgadmissions.domain.PendingRoleNotification;
+import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.ReviewComment;
+import com.zuehlke.pgadmissions.domain.Reviewer;
+import com.zuehlke.pgadmissions.domain.Role;
+import com.zuehlke.pgadmissions.domain.Supervisor;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+
+@Repository
+public class UserDAO {
+
+	private final SessionFactory sessionFactory;
+
+	UserDAO() {
+		this(null);
+	}
+
+	@Autowired
+	public UserDAO(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void save(RegisteredUser user) {
+		sessionFactory.getCurrentSession().saveOrUpdate(user);
+	}
+
+	public RegisteredUser get(Integer id) {
+		return (RegisteredUser) sessionFactory.getCurrentSession().get(RegisteredUser.class, id);
+	}
+
+	public RegisteredUser getUserByUsername(String username) {
+		return (RegisteredUser) sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).add(Restrictions.eq("username", username))
+				.add(Restrictions.eq("enabled", true)).uniqueResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getAllUsers() {
+		return sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).list();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getUsersInRole(Role role) {
+		return sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).createCriteria("roles").add(Restrictions.eq("id", role.getId())).list();
+
+	}
+
+	public RegisteredUser getUserByActivationCode(String activationCode) {
+		return (RegisteredUser) sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).add(Restrictions.eq("activationCode", activationCode))
+				.uniqueResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getUsersForProgram(Program program) {
+		List<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		users.addAll(getUsersInRole((Role) sessionFactory.getCurrentSession().createCriteria(Role.class)
+				.add(Restrictions.eq("authorityEnum", Authority.SUPERADMINISTRATOR)).uniqueResult()));
+
+		List<RegisteredUser> administrators = sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class)
+				.createCriteria("programsOfWhichAdministrator").add(Restrictions.eq("id", program.getId())).list();
+		for (RegisteredUser admin : administrators) {
+			if (!users.contains(admin)) {
+				users.add(admin);
+			}
+		}
+		List<RegisteredUser> approvers = sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).createCriteria("programsOfWhichApprover")
+				.add(Restrictions.eq("id", program.getId())).list();
+		for (RegisteredUser approver : approvers) {
+			if (!users.contains(approver)) {
+				users.add(approver);
+			}
+		}
+		List<RegisteredUser> reviewers = sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).createCriteria("programsOfWhichReviewer")
+				.add(Restrictions.eq("id", program.getId())).list();
+		for (RegisteredUser reviewer : reviewers) {
+			if (!users.contains(reviewer)) {
+				users.add(reviewer);
+			}
+		}
+		List<RegisteredUser> interviewers = sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class)
+				.createCriteria("programsOfWhichInterviewer").add(Restrictions.eq("id", program.getId())).list();
+		for (RegisteredUser interviewer : interviewers) {
+			if (!users.contains(interviewer)) {
+				users.add(interviewer);
+			}
+		}
+		List<RegisteredUser> supervisors = sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class)
+				.createCriteria("programsOfWhichSupervisor").add(Restrictions.eq("id", program.getId())).list();
+		for (RegisteredUser supervisor : supervisors) {
+			if (!users.contains(supervisor)) {
+				users.add(supervisor);
+			}
+		}
+		return users;
+	}
+
+	public RegisteredUser getUserByEmail(String email) {
+		return (RegisteredUser) sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).add(Restrictions.eq("enabled", true))
+				.add(Restrictions.eq("email", email)).uniqueResult();
+	}
+
+	public RegisteredUser getUserByEmailIncludingDisabledAccounts(String email) {
+		return (RegisteredUser) sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class).add(Restrictions.eq("email", email)).uniqueResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getInternalUsers() {
+		return sessionFactory
+				.getCurrentSession()
+				.createCriteria(RegisteredUser.class)
+				.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+				.createAlias("roles", "role")
+				.add(Restrictions.and(Restrictions.not(Restrictions.eq("role.authorityEnum", Authority.APPLICANT)),
+						Restrictions.not(Restrictions.eq("role.authorityEnum", Authority.REFEREE)))).addOrder(Order.asc("firstName"))
+				.addOrder(Order.asc("lastName")).list();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getUsersWithPendingRoleNotifications() {	
+		
+		return sessionFactory.getCurrentSession().createCriteria(RegisteredUser.class, "user").setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).add(Restrictions.eq("enabled", false))
+				.createAlias("pendingRoleNotifications", "pendingRoleNotification").list();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getAllPreviousInterviewersOfProgram(Program program) {
+		List<Interviewer> interviewers = sessionFactory.getCurrentSession().createCriteria(Interviewer.class).createAlias("interview", "interview").createAlias("interview.application", "application")
+				.add(Restrictions.eq("application.program", program)).list();
+		List<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		for (Interviewer interviewer : interviewers) {
+			if(!users.contains(interviewer.getUser())){
+				users.add(interviewer.getUser());
+			}
+		}
+		return users;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getAllPreviousReviewersOfProgram(Program program) {
+		List<Reviewer> reviewers = sessionFactory.getCurrentSession().createCriteria(Reviewer.class).createAlias("reviewRound", "reviewRound").createAlias("reviewRound.application", "application")
+				.add(Restrictions.eq("application.program", program)).list();
+		List<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		for (Reviewer reviewer : reviewers) {
+			if(!users.contains(reviewer.getUser())){
+				users.add(reviewer.getUser());
+			}
+		}
+		return users;
+	}
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getReviewersWillingToInterview(ApplicationForm applicationForm) {
+		List<ReviewComment> reviews = sessionFactory.getCurrentSession().createCriteria(ReviewComment.class).createAlias("reviewer", "reviewer")
+				.createAlias("reviewer.reviewRound", "reviewRound").createAlias("reviewRound.application", "application")
+				.add(Restrictions.eq("application", applicationForm))
+				.add(Restrictions.eqProperty("application.latestReviewRound","reviewer.reviewRound"))
+				.add(Restrictions.eq("willingToInterview", true))
+				.list();
+		List<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		for (ReviewComment reviewComment : reviews) {
+			if(!users.contains(reviewComment.getUser())){
+				users.add(reviewComment.getUser());
+			}
+		}
+		return users;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<RegisteredUser> getAllPreviousSupervisorsOfProgram(Program program) {
+		List<Supervisor> supervisors = sessionFactory.getCurrentSession().createCriteria(Supervisor.class).createAlias("approvalRound", "approvalRound").createAlias("approvalRound.application", "application")
+				.add(Restrictions.eq("application.program", program)).list();
+		List<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		for (Supervisor supervisor : supervisors) {
+			if(!users.contains(supervisor.getUser())){
+				users.add(supervisor.getUser());
+			}
+		}
+		return users;
+	}
+
+}
---
+++ src/test/java/com/zuehlke/pgadmissions/services/MailServiceTest.java
@@ -0,0 +1,182 @@
+package com.zuehlke.pgadmissions.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import javax.mail.internet.InternetAddress;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.NotificationRecord;
+import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.Referee;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
+import com.zuehlke.pgadmissions.domain.builders.NotificationRecordBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RefereeBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.enums.NotificationType;
+import com.zuehlke.pgadmissions.mail.MimeMessagePreparatorFactory;
+
+public class MailServiceTest {
+
+	private MailService mailService;
+	private ApplicationsService applicationsServiceMock;
+	private JavaMailSender javaMailSenderMock;
+	private MimeMessagePreparatorFactory mimeMessagePreparatorFactoryMock;
+
+
+	
+	
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldSendUpdatedEmailToAdmins() throws UnsupportedEncodingException, ParseException {
+
+		RegisteredUser administratorOne = new RegisteredUserBuilder().id(1).firstName("benny").lastName("brack").email("bb@test.com").toUser();
+		RegisteredUser administratorTwo = new RegisteredUserBuilder().id(2).firstName("henry").lastName("harck").email("hh@test.com").toUser();
+		Program program = new ProgramBuilder().administrators(administratorOne, administratorTwo).toProgram();
+	
+		NotificationRecord notificationRecord = new NotificationRecordBuilder().id(1).notificationType(NotificationType.UPDATED_NOTIFICATION).notificationDate(new SimpleDateFormat("dd MM yyyy").parse("01 06 2011")).toNotificationRecord();
+		ApplicationForm form = new ApplicationFormBuilder().id(2).program(program).notificationRecords(notificationRecord).toApplicationForm();
+		
+		MimeMessagePreparator preparatorMock1 = EasyMock.createMock(MimeMessagePreparator.class);
+		MimeMessagePreparator preparatorMock2 = EasyMock.createMock(MimeMessagePreparator.class);
+	
+		InternetAddress toAddress1 = new InternetAddress("bb@test.com", "benny brack");
+		InternetAddress toAddress2 = new InternetAddress("hh@test.com", "harck");
+			
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress1), EasyMock.eq("Application Updated"),
+						EasyMock.eq("private/staff/admin/mail/application_updated_confirmation.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock1);
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress2), EasyMock.eq("Application Updated"),
+						EasyMock.eq("private/staff/admin/mail/application_updated_confirmation.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock2);
+		javaMailSenderMock.send(preparatorMock1);
+		javaMailSenderMock.send(preparatorMock2);
+		applicationsServiceMock.save(form);
+		EasyMock.replay(applicationsServiceMock, mimeMessagePreparatorFactoryMock, javaMailSenderMock);
+
+		mailService.sendApplicationUpdatedMailToAdmins(form);
+		EasyMock.verify(applicationsServiceMock, javaMailSenderMock, mimeMessagePreparatorFactoryMock);		
+		assertSame(notificationRecord, form.getNotificationForType(NotificationType.UPDATED_NOTIFICATION));
+		assertEquals(DateUtils.truncate(new Date(), Calendar.DATE), DateUtils.truncate(form.getNotificationForType(NotificationType.UPDATED_NOTIFICATION).getDate(), Calendar.DATE));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldSendWithdrawnNotificationToReferees() throws UnsupportedEncodingException, ParseException {
+		Program program = new ProgramBuilder().toProgram();
+		ApplicationForm form = new ApplicationFormBuilder().id(2).program(program).applicant(new RegisteredUser()).toApplicationForm();
+		
+		RegisteredUser refereeOne = new RegisteredUserBuilder().id(1).firstName("benny").lastName("brack").email("bb@test.com").toUser();
+		RegisteredUser refereeTwo = new RegisteredUserBuilder().id(2).firstName("henry").lastName("harck").email("hh@test.com").toUser();
+		Referee referee1 = new RefereeBuilder().application(form).id(2).user(refereeTwo).toReferee();
+		Referee referee2 = new RefereeBuilder().application(form).id(2).id(1).user(refereeOne).toReferee();
+		
+		
+		
+		MimeMessagePreparator preparatorMock1 = EasyMock.createMock(MimeMessagePreparator.class);
+		MimeMessagePreparator preparatorMock2 = EasyMock.createMock(MimeMessagePreparator.class);
+		
+		InternetAddress toAddress1 = new InternetAddress("bb@test.com", "benny brack");
+		InternetAddress toAddress2 = new InternetAddress("hh@test.com", "harck");
+		
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress1), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock1);
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress2), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock2);
+		javaMailSenderMock.send(preparatorMock1);
+		javaMailSenderMock.send(preparatorMock2);
+		EasyMock.replay(applicationsServiceMock, mimeMessagePreparatorFactoryMock, javaMailSenderMock);
+		
+		mailService.sendWithdrawMailToReferees(Arrays.asList(referee1, referee2));
+		EasyMock.verify(applicationsServiceMock, javaMailSenderMock, mimeMessagePreparatorFactoryMock);		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldSendWithdrawnNotificationToAdmins() throws UnsupportedEncodingException, ParseException {
+		
+		RegisteredUser admin1 = new RegisteredUserBuilder().id(1).firstName("benny").lastName("brack").email("bb@test.com").toUser();
+		RegisteredUser admin2 = new RegisteredUserBuilder().id(2).firstName("henry").lastName("harck").email("hh@test.com").toUser();
+		Program program = new ProgramBuilder().administrators(admin1, admin2).toProgram();
+		
+		ApplicationForm form = new ApplicationFormBuilder().id(2).program(program).toApplicationForm();
+		
+		MimeMessagePreparator preparatorMock1 = EasyMock.createMock(MimeMessagePreparator.class);
+		MimeMessagePreparator preparatorMock2 = EasyMock.createMock(MimeMessagePreparator.class);
+		
+		InternetAddress toAddress1 = new InternetAddress("bb@test.com", "benny brack");
+		InternetAddress toAddress2 = new InternetAddress("hh@test.com", "harck");
+		
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress1), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock1);
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress2), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock2);
+		javaMailSenderMock.send(preparatorMock1);
+		javaMailSenderMock.send(preparatorMock2);
+		EasyMock.replay(applicationsServiceMock, mimeMessagePreparatorFactoryMock, javaMailSenderMock);
+		
+		mailService.sendWithdrawToAdmins(form);
+		EasyMock.verify(applicationsServiceMock, javaMailSenderMock, mimeMessagePreparatorFactoryMock);		
+	}
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldSendWithdrawnNotificationToReviewers() throws UnsupportedEncodingException, ParseException {
+		
+		RegisteredUser reviewer1 = new RegisteredUserBuilder().id(1).firstName("benny").lastName("brack").email("bb@test.com").toUser();
+		RegisteredUser reviewer2 = new RegisteredUserBuilder().id(2).firstName("henry").lastName("harck").email("hh@test.com").toUser();
+		Program program = new ProgramBuilder().reviewers(reviewer1, reviewer2).toProgram();
+		
+		ApplicationForm form = new ApplicationFormBuilder().id(2).program(program).toApplicationForm();
+		
+		MimeMessagePreparator preparatorMock1 = EasyMock.createMock(MimeMessagePreparator.class);
+		MimeMessagePreparator preparatorMock2 = EasyMock.createMock(MimeMessagePreparator.class);
+		
+		InternetAddress toAddress1 = new InternetAddress("bb@test.com", "benny brack");
+		InternetAddress toAddress2 = new InternetAddress("hh@test.com", "harck");
+		
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress1), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock1);
+		EasyMock.expect(
+				mimeMessagePreparatorFactoryMock.getMimeMessagePreparator(EasyMock.eq(toAddress2), EasyMock.eq("Application Withdrawn"),
+						EasyMock.eq("private/staff/mail/application_withdrawn_notification.ftl"), EasyMock.isA(Map.class), (InternetAddress) EasyMock.isNull())).andReturn(preparatorMock2);
+		javaMailSenderMock.send(preparatorMock1);
+		javaMailSenderMock.send(preparatorMock2);
+		EasyMock.replay(applicationsServiceMock, mimeMessagePreparatorFactoryMock, javaMailSenderMock);
+		
+		mailService.sendWithdrawToReviewers(form);
+		EasyMock.verify(applicationsServiceMock, javaMailSenderMock, mimeMessagePreparatorFactoryMock);		
+	}
+	@Before
+	public void setUp() {
+		applicationsServiceMock = EasyMock.createMock(ApplicationsService.class);
+		javaMailSenderMock = EasyMock.createMock(JavaMailSender.class);
+		mimeMessagePreparatorFactoryMock = EasyMock.createMock(MimeMessagePreparatorFactory.class);
+		
+		mailService = new MailService(mimeMessagePreparatorFactoryMock, javaMailSenderMock, applicationsServiceMock);
+
+	}
+
+}

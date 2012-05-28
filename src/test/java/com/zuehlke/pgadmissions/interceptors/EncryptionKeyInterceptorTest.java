975f7427e767baa68c5d21f8cf303771b4f60e89
kpa <kpa@localhost>
2012-05-28 18:22:11 +0100

    Add some tests for encryption classes

---
+++ src/test/java/com/zuehlke/pgadmissions/interceptors/EncryptionKeyInterceptorTest.java
@@ -0,0 +1,49 @@
+package com.zuehlke.pgadmissions.interceptors;
+
+import static org.junit.Assert.*;
+
+import javax.crypto.SecretKey;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+
+public class EncryptionKeyInterceptorTest {
+
+	@Test
+	public void shouldGetKeyFromSessionAndPutInContetHolder() throws Exception{
+		SecretKey keyMock = EasyMock.createMock(SecretKey.class);
+		EncryptionKeyInterceptor interceptor = new EncryptionKeyInterceptor();
+		MockHttpSession session = new MockHttpSession();
+		session.putValue("key", keyMock);
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setSession(session);
+			
+		assertTrue(interceptor.preHandle(request, null, null));
+		assertSame(keyMock, KeyContextHolder.getContext());
+	}
+
+	@Test
+	public void shouldClearContext() throws Exception{
+		SecretKey keyMock = EasyMock.createMock(SecretKey.class);
+		EncryptionKeyInterceptor interceptor = new EncryptionKeyInterceptor();
+		KeyContextHolder.setContext(keyMock);
+			
+		interceptor.postHandle(null,null, null, null);
+		assertNull(KeyContextHolder.getContext());
+	}
+	
+	
+	@Before
+	public void setUp(){
+		KeyContextHolder.clearContext();
+	}
+
+	@After
+	public void tearDown(){
+		KeyContextHolder.clearContext();
+	}
+}

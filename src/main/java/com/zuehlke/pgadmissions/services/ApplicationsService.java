5d039dbe59a1145e2020d8b30dc362a0e0f8a1de
mroach <mroach@localhost>
2012-06-06 16:14:30 +0100

    Task: only numbers in value of award field Desc: -- Author: Manish Dani Date: 06-06-12

---
+++ src/main/java/com/zuehlke/pgadmissions/services/ApplicationsService.java
@@ -0,0 +1,182 @@
+package com.zuehlke.pgadmissions.services;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zuehlke.pgadmissions.dao.ApplicationFormDAO;
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Event;
+import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
+import com.zuehlke.pgadmissions.domain.enums.SearchCategory;
+import com.zuehlke.pgadmissions.domain.enums.SortCategory;
+import com.zuehlke.pgadmissions.domain.enums.SortOrder;
+
+@Service("applicationsService")
+public class ApplicationsService {
+
+	private final ApplicationFormDAO applicationFormDAO;
+
+	ApplicationsService() {
+		this(null);
+	}
+
+	@Autowired
+	public ApplicationsService(ApplicationFormDAO applicationFormDAO) {
+		this.applicationFormDAO = applicationFormDAO;
+	}
+
+	/**
+	 * @deprecated use {@link #getAllVisibleAndMatchedApplications(RegisteredUser, SearchCategory, String, SortCategory, SortOrder)} instead.
+	 * @param user
+	 * @return
+	 */
+	@Deprecated
+	@Transactional
+	public List<ApplicationForm> getVisibleApplications(RegisteredUser user) {
+		return getAllVisibleAndMatchedApplications(user, null, null, null, null);
+	}
+
+	public ApplicationForm getApplicationById(Integer id) {
+		return applicationFormDAO.get(id);
+	}
+
+	public ApplicationForm getApplicationByApplicationNumber(String applicationNumber) {
+		return applicationFormDAO.getApplicationByApplicationNumber(applicationNumber);
+	}
+
+	@Transactional
+	public void save(ApplicationForm application) {
+		applicationFormDAO.save(application);
+
+	}
+
+	@Transactional
+	public ApplicationForm createAndSaveNewApplicationForm(RegisteredUser user, Program program) {
+		String thisYear = new SimpleDateFormat("yyyy").format(new Date());
+		ApplicationForm applicationForm = newApplicationForm();
+		applicationForm.setApplicant(user);
+		applicationForm.setProgram(program);
+		int runningCount = applicationFormDAO.getApplicationsInProgramThisYear(program, thisYear);
+		applicationForm.setApplicationNumber(program.getCode() + "-" + thisYear + "-" + String.format("%06d", ++runningCount));
+		applicationFormDAO.save(applicationForm);
+		return applicationForm;
+	}
+
+	ApplicationForm newApplicationForm() {
+		return new ApplicationForm();
+	}
+
+	@Transactional
+	public List<ApplicationForm> getApplicationsDueUpdateNotification() {
+		return applicationFormDAO.getApplicationsDueUpdateNotification();
+	}
+
+	public ApplicationFormStatus getStageComingFrom(ApplicationForm application) {
+		List<Event> events = application.getEventsSortedByDate();
+		Event previousEvent;
+		if (events.size() == 1) {
+			return events.get(0).getNewStatus();
+		}
+		if (events.size() == 2) {
+			if (events.get(1).getNewStatus() == ApplicationFormStatus.REJECTED) {
+				return events.get(0).getNewStatus();
+			}
+			return events.get(0).getNewStatus();
+		}
+		if (events.size() > 2) {
+			for (int i = 0; i < events.size(); i++) {
+				if (events.get(i).getNewStatus() == ApplicationFormStatus.REJECTED) {
+					previousEvent = events.get(i - 1);
+					if (previousEvent.getNewStatus() == ApplicationFormStatus.REVIEW || previousEvent.getNewStatus() == ApplicationFormStatus.INTERVIEW) {
+						return previousEvent.getNewStatus();
+					}
+					if (previousEvent.getNewStatus() == ApplicationFormStatus.APPROVAL) {
+						Event previousOfApproval = events.get(i - 2);
+						if (previousOfApproval.getNewStatus() == ApplicationFormStatus.REVIEW || previousOfApproval.getNewStatus() == ApplicationFormStatus.INTERVIEW || previousOfApproval.getNewStatus() == ApplicationFormStatus.VALIDATION) {
+							return previousOfApproval.getNewStatus();
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Transactional
+	public List<ApplicationForm> getAllVisibleAndMatchedApplications(RegisteredUser user,// 
+			SearchCategory searchCategory, String term) {
+		return getAllVisibleAndMatchedApplications(user, searchCategory, term, null, null);
+	}
+
+	@Transactional
+	public List<ApplicationForm> getAllVisibleAndMatchedApplications(RegisteredUser user,// 
+			SearchCategory searchCategory, String term,//
+			SortCategory sortCategory, SortOrder sortOrder) {
+
+		List<ApplicationForm> visibleAndMatchedApplications = new ArrayList<ApplicationForm>();
+		List<ApplicationForm> visibleApplications = applicationFormDAO.getVisibleApplications(user);
+
+		if (searchCategory == null) {
+			visibleAndMatchedApplications = visibleApplications;
+		} else {
+			if (term == null) {
+				throw new IllegalArgumentException("Search term cannot be null when a search-category is set!");
+			}
+			for (ApplicationForm applicationForm : visibleApplications) {
+				if (searchCategory == SearchCategory.APPLICATION_NUMBER) {
+					if (applicationForm.getApplicationNumber().toLowerCase().contains(term.toLowerCase())) {
+						visibleAndMatchedApplications.add(applicationForm);
+					}
+				}
+				if (searchCategory == SearchCategory.PROGRAMME_NAME) {
+					String fullProgramName = applicationForm.getProgram().getCode() + applicationForm.getProgram().getTitle();
+					if (fullProgramName.toLowerCase().contains(term.toLowerCase())) {
+						visibleAndMatchedApplications.add(applicationForm);
+					}
+				}
+				if (searchCategory == SearchCategory.APPLICANT_NAME) {
+					String fullApplicantName = applicationForm.getApplicant().getFirstName() + applicationForm.getApplicant().getLastName();
+					if (fullApplicantName.toLowerCase().contains(term.toLowerCase())) {
+						visibleAndMatchedApplications.add(applicationForm);
+					}
+				}
+				if (searchCategory == SearchCategory.APPLICATION_STATUS) {
+					ApplicationFormStatus matchedStatus = null;
+					for (ApplicationFormStatus status : ApplicationFormStatus.values()) {
+						if (status.displayValue().toLowerCase().contains(term.toLowerCase())) {
+							matchedStatus = status;
+						}
+					}
+					if (matchedStatus != null) {
+						if (applicationForm.getStatus() == matchedStatus) {
+							visibleAndMatchedApplications.add(applicationForm);
+						}
+					}
+				}
+			}
+		}
+		sortApplicationList(visibleAndMatchedApplications, sortCategory, sortOrder);
+		return visibleAndMatchedApplications;
+	}
+
+	private List<ApplicationForm> sortApplicationList(List<ApplicationForm> applications, SortCategory sortCategory, SortOrder sortOrder) {
+		if (sortCategory == null) {// natural ordering on submitted date.
+			Collections.sort(applications);
+			return applications;
+		}
+		if (sortOrder == null) {
+			throw new IllegalArgumentException("Sort order cannot be null when a sort-category is set!");
+		}
+		Collections.sort(applications, sortCategory.getComparator(sortOrder));
+		return applications;
+	}
+}
---
+++ src/main/webapp/WEB-INF/design/default/js/applicationList/formActions.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {
+	
+	populateApplicationList();
+	
+	$('#searchBtn').click(function(){
+		populateApplicationList();
+	});
+	
+	$(document).on('change', 'select.actionType', function() {
+		var name = this.name;
+		var id = name.substring(5).replace(']', '');
+
+		if ($(this).val() == 'view') {
+			window.location.href = "/pgadmissions/application?view=view&applicationId=" + id;
+		}else if($(this).val() == 'assignReviewer') {
+			window.location.href = "/pgadmissions/review/assignReviewers?applicationId=" + id;
+		}else if($(this).val() == 'assignInterviewer') {
+			window.location.href = "/pgadmissions/interview/assignInterviewers?applicationId=" + id;
+		}else if($(this).val() == 'approve') {
+			window.location.href = "/pgadmissions/approved/moveToApproved?applicationId=" + id;
+		}else if($(this).val() == 'reject') {
+			window.location.href = "/pgadmissions/rejectApplication?applicationId=" + id;
+		}else if($(this).val() == 'comment') {
+			window.location.href = "/pgadmissions/comment?applicationId=" + id;
+		}else if($(this).val() == 'print') {
+			window.location.href = "/pgadmissions/print?applicationFormId=" + id;
+		}else if($(this).val() == 'reference') {
+			window.location.href = "/pgadmissions/referee/addReferences?application=" + id;
+		}else if($(this).val() == 'validate') {
+			window.location.href = "/pgadmissions/progress?application=" + id;
+		}else if($(this).val() == 'review') {
+			window.location.href = "/pgadmissions/reviewFeedback?applicationId=" + id;
+		}else if($(this).val() == 'interviewFeedback') {
+			window.location.href = "/pgadmissions/interviewFeedback?applicationId=" + id;
+		}else if($(this).val() == 'restartApproval') {
+			$.post("/pgadmissions/approval/move",
+					{
+						applicationId: id
+					}, 
+					function(data) {
+						window.location.href = "/pgadmissions/approval/moveToApproval?applicationId=" + id;
+					}
+			);
+		}
+		else if($(this).val() == 'withdraw') {
+				if(confirm("Are you sure you want to withdraw the application? You will not be able to submit a withdrawn application."))
+				{
+					$.post("/pgadmissions/withdraw",
+					{
+						applicationId: id
+					}, 
+					function(data) {
+						window.location.href = "/pgadmissions/applications";
+					}
+				);
+				}
+			}
+	});
+	
+	
+	$('#search-box button').click(function() { doApplicationSearch(); });
+	$('#searchCategory').change(function() { doApplicationSearch(); });
+	
+	function doApplicationSearch()
+	{
+		if($('#searchCategory').val() != "")
+		{
+			$.get("/pgadmissions/search",
+				{
+					searchCategory: $('#searchCategory').val(),
+					searchTerm:  $('#searchTerm').val()
+				},
+				function(data)
+				{
+					$('#applicationListSection').html(data);
+					addToolTips();
+				}
+			);
+		}
+	}
+
+
+	$('#manageUsersButton').click(function(){
+		window.location.href = "/pgadmissions/manageUsers/showPage";
+	});
+	
+	$('#configuration').click(function(){
+		window.location.href = "/pgadmissions/configuration";
+	});
+
+	$(document).on('click', "input[name*='appDownload']", function(){		
+		var id = this.id;
+		id = id.replace('appDownload_', '');
+	
+		var currentAppList = $('#appList').val();		
+		if ($(this).attr('checked')){
+			$('#appList').val(currentAppList + id + ";");
+		} else {
+			$('#appList').val(currentAppList.replace(id  +";", ''));
+		}
+	
+		
+	});
+
+	$('#downloadAll').click(function(){
+		var appListValue = $('#appList').val();
+		if (appListValue==''){
+			alert("At least one application must be selected for download!");
+		} else {
+			window.location.href = "/pgadmissions/print/all?appList="+$('#appList').val();
+		}
+	});
+
+});
+
+function populateApplicationList() {
+	searchCategory = $('#searchCategory').val();
+	searchTerm = $('#searchTerm').val(); 
+	sortCategory = $('#sort-column').val();
+	sortOrder = $('#sort-order').val();
+	$.get("/pgadmissions/applications/section",
+		{
+			searchCategory: searchCategory,
+			searchTerm:  searchTerm,
+			sortCategory: sortCategory,
+			order: sortOrder
+		},
+		function(data) {
+			$('#applicationListSection').html(data);
+			addToolTips();
+		}
+	);
+}
+
+function sortList(column) {
+	oldValue = $('#sort-column').val();
+	newValue = column.id;
+	
+	if( oldValue == newValue) {
+		flipSortOrder();
+	} else {
+		$('#sort-column').val(newValue);
+		$('#sort-order').val("ASCENDING");
+	}
+	populateApplicationList();
+}
+
+function flipSortOrder() {
+	oldOrder = $('#sort-order').val();
+	if( oldOrder == "ASCENDING") {
+		$('#sort-order').val("DESCENDING");
+	} else {
+		$('#sort-order').val("ASCENDING");
+	}
+}

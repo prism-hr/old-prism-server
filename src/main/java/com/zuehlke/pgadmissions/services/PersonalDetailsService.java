c943c33552175725053bfefc17c1e91ba5a09ff9
iky <iky@LONN0170.ads.zuehlke.com>
2012-06-07 10:28:02 +0100

    restart approval by redirecting to supervisors page

---
+++ src/main/java/com/zuehlke/pgadmissions/services/PersonalDetailsService.java
@@ -0,0 +1,31 @@
+package com.zuehlke.pgadmissions.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zuehlke.pgadmissions.dao.PersonalDetailDAO;
+import com.zuehlke.pgadmissions.domain.PersonalDetails;
+
+@Service
+public class PersonalDetailsService {
+
+	private final PersonalDetailDAO personalDetailDAO;
+
+	PersonalDetailsService() {
+		this(null);
+	}
+
+	@Autowired
+	public PersonalDetailsService(PersonalDetailDAO personalDetailDAO) {
+		this.personalDetailDAO = personalDetailDAO;
+
+	}
+	
+	@Transactional
+	public void save(PersonalDetails personalDetails) {
+		personalDetailDAO.save(personalDetails);
+		
+	}
+
+}

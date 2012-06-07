a82cfc6c5a25652aea32c000af3d8d4270519668
dmaughan <dmaughan@localhost>
2012-06-07 15:48:54 +0100

    updating the JS for numeric input (number pad didn't work).

--- src/main/webapp/WEB-INF/design/default/js/application/funding.js
+++ src/main/webapp/WEB-INF/design/default/js/application/funding.js
@@ -3,15 +3,23 @@ $(document).ready(function(){
 	var fundImgCount = 0;
 	
 	$('#fundingValue').keydown(function(event) {
-		// Allow only backspace and delete
-		if ( event.keyCode == 46 || event.keyCode == 8 ) {
-			// let it happen, don't do anything
+		// http://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+		// Allow: backspace, delete, tab, escape, and enter
+		if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+				 // Allow: Ctrl+A
+				(event.keyCode == 65 && event.ctrlKey === true) || 
+				 // Allow: home, end, left, right
+				(event.keyCode >= 35 && event.keyCode <= 39))
+		{
+						 // let it happen, don't do anything
+						 return;
 		}
-		else {
-			// Ensure that it is a number and stop the keypress
-			if (event.keyCode < 48 || event.keyCode > 57 ) {
-				event.preventDefault();	
-			}	
+		else
+		{
+				// Ensure that it is a number and stop the keypress
+				if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+						event.preventDefault(); 
+				}   
 		}
 	});
 	

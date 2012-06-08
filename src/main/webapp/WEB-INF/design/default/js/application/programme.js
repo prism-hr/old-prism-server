716c9cf5098c7e93d725bd060222d4be28172c4a
dmaughan <dmaughan@localhost>
2012-06-08 10:48:30 +0100

    Cleaning up the programme details script.

--- src/main/webapp/WEB-INF/design/default/js/application/programme.js
+++ src/main/webapp/WEB-INF/design/default/js/application/programme.js
@@ -1,3 +1,7 @@
+// ===============================================================================
+// PROGRAMME DETAILS section
+// ===============================================================================
+
 $(document).ready(function()
 {
 	$("#acceptTermsPDValue").val("NO");
@@ -10,30 +14,40 @@ $(document).ready(function()
 	
 	$('#supervisors').toggle($("#supervisors tbody").children().length > 0);
 	
-	$("input[name*='acceptTermsPDCB']").click(function() {
+
+	// -------------------------------------------------------------------------------
+	// "Accept terms" checkbox.
+	// -------------------------------------------------------------------------------
+	$("input[name*='acceptTermsPDCB']").click(function()
+	{
 		if ($("#acceptTermsPDValue").val() =='YES'){
 			$("#acceptTermsPDValue").val("NO");
 		} else {	
 			$("#acceptTermsPDValue").val("YES");
 			
+			// Remove any styling from the terms box.
+			$('#programmeDetailsSection .terms-box').attr('style','');
 			/*
-			$(".terms-box").attr('style','');
 			$("#prog-info-bar-div").switchClass("section-error-bar", "section-info-bar", 1);
 			$("#prog-info-bar-span").switchClass("invalid-info-text", "info-text", 1);
 			$("#prog-info-bar-div .row span.error-hint").remove();
 			*/
+
 			progImgCount = 0;
 			
 			$.post("/pgadmissions/acceptTerms", {  
 				applicationId: $("#applicationId").val(), 
 				acceptedTerms: $("#acceptTermsPDValue").val()
 			},
-			function(data) {
-			});
+			function(data) {}
+			);
 		}
-		});
+	});
 	
-	// Delete supervisor
+
+	// -------------------------------------------------------------------------------
+	// Delete supervisor button.
+	// -------------------------------------------------------------------------------
 	$("#supervisor_div").on("click", "a[name=\"deleteSupervisor\"]", function()
 	{	
 		var id = this.id;
@@ -60,7 +74,67 @@ $(document).ready(function()
 		}
 	});
 	
-	// Edit supervisor
+	
+	// -------------------------------------------------------------------------------
+	// Add supervisor button.
+	// -------------------------------------------------------------------------------
+	$('#addSupervisorButton').on('click', function()
+	{
+		var errors = 0;
+		
+		if ($('#supervisorFirstname').val() == "")
+		{
+			$("span[name='superFirstname']").html('First name cannot be empty.').show();
+			errors++;
+		}		
+		if ($('#supervisorLastname').val() == "")
+		{
+			$("span[name='superLastname']").html('Last name cannot be empty.').show();
+			errors++;
+		}		
+		if (!validateEmail($('#supervisorEmail').val()))
+		{
+			$("span[name='superEmail']").html('Email is not valid.').show();
+			errors++;
+		}
+		
+		if (errors == 0)
+		{
+			//replaceWithLoader($(this));
+			$("#supervisors").show();
+			$("#supervisor_div span.invalid").html('').hide();
+			var aware = ($('#awareSupervisor').val() =="YES") ? 'Yes' : 'No';
+
+			unsavedSupervisors++;
+			$('table#supervisors tbody').append(
+				'<tr class="' + (aware == "Yes" ? "aware" : "unaware") + '">' +
+				'<td data-desc="Supervisor ' + (aware == "Yes" ? "aware" : "unaware") + ' of application">' + $('#supervisorFirstname').val() + ' '+ $('#supervisorLastname').val() + ' (' + $('#supervisorEmail').val() + ')</td>' +
+				'<td>' +
+				'<a class=\"button-delete\" id="usd_'+unsavedSupervisors+'" name=\"deleteSupervisor\">delete</a> ' +
+				'<a class=\"button-edit\" id="us_'+unsavedSupervisors+'" name=\"editSupervisorLink\">edit</a>' +
+				'<input type="hidden" id="us_'+unsavedSupervisors+'firstname" value="' + $('#supervisorFirstname').val()+'"/>'	+								
+				'<input type="hidden" id="us_'+unsavedSupervisors+'lastname" value="' + $('#supervisorLastname').val()+'"/>'	+								
+				'<input type="hidden" id="us_'+unsavedSupervisors+'email" value="' + $('#supervisorEmail').val()+'"/>'	+								
+				'<input type="hidden" id="us_'+unsavedSupervisors+'aware" value="' + $('#awareSupervisor').val()+'"/>'	+								
+				'<input type="hidden" name="suggestedSupervisors" id="'+unsavedSupervisors+'_ussupervisors" value=' +"'" + '{"id":"' +  $('#supervisorId').val()+ '","firstname":"' +  $('#supervisorFirstname').val()+ '","lastname":"' +  $('#supervisorLastname').val()+ '","email":"' +  $('#supervisorEmail').val() +  '", "awareSupervisor":"' + $('#awareSupervisor').val() + '"} ' + "'" + "/>" +
+				'</td>' +
+				'</tr>');
+			addToolTips();
+		
+			$("input[name='sFN']").val($('#supervisorFirstname').val());
+			$("input[name='sLN']").val($('#supervisorLastname').val());
+			$("input[name='sEM']").val($('#supervisorEmail').val());
+			$("input[name='sAS']").val($('#awareSupervisor').val());
+			$('#supervisorId, #supervisorFirstname, #supervisorLastname, #supervisorEmail').val('');
+			$("#awareSupervisorCB").attr('checked', false);
+			$("#awareSupervisor").val("NO");
+		}
+	});
+
+
+	// -------------------------------------------------------------------------------
+	// Edit supervisor button.
+	// -------------------------------------------------------------------------------
 	$("#supervisor_div").on("click", "a[name=\"editSupervisorLink\"]", function(){
 		var id = this.id;
 		if(id.indexOf("us_") != -1){
@@ -122,102 +196,9 @@ $(document).ready(function()
 	});
 	
 	
-	// Save programme details.
-	$('#programmeSaveButton').on("click",function()
-	{
-		if ($("#acceptTermsPDValue").val() == 'NO')
-		{ 
-			$(this).parent().parent().find('.terms-box').css({borderColor: 'red', color: 'red'});
-				
-			$("#prog-info-bar-div").switchClass("section-info-bar", "section-error-bar", 1);
-			$("#prog-info-bar-span").switchClass("info-text", "invalid-info-text", 1);
-				
-			if (progImgCount == 0)
-			{
-				$("#prog-info-bar-div .row").prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
-				progImgCount = progImgCount + 1;
-			}
-			addToolTips();
-		}
-		else
-		{
-			$("span[name='nonAcceptedPD']").html('');
-
-			// Check for a "dirty" supervisor form.
-			if (!isFormEmpty('#supervisor_div'))
-			{
-				$('#addSupervisorButton').trigger('click');
-				// If there was an error submitting the non-empty form, don't continue.
-				if ($('#supervisor_div .invalid').length > 0)
-				{
-					return false;
-				}
-			}
-			
-			postProgrammeData('close');
-		}
-	});	
-	function validateEmail(email) { 
-		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-	    var result = pattern.test(email);
-		return result;
-	} 
-	
-	// Add supervisor button
-	$('#addSupervisorButton').on('click', function()
-	{
-		var errors = 0;
-		
-		if ($('#supervisorFirstname').val() == "")
-		{
-			$("span[name='superFirstname']").html('First name cannot be empty.').show();
-			errors++;
-		}		
-		if ($('#supervisorLastname').val() == "")
-		{
-			$("span[name='superLastname']").html('Last name cannot be empty.').show();
-			errors++;
-		}		
-		if (!validateEmail($('#supervisorEmail').val()))
-		{
-			$("span[name='superEmail']").html('Email is not valid.').show();
-			errors++;
-		}
-		
-		if (errors == 0)
-		{
-			//replaceWithLoader($(this));
-			$("#supervisors").show();
-			$("#supervisor_div span.invalid").html('').hide();
-			var aware = ($('#awareSupervisor').val() =="YES") ? 'Yes' : 'No';
-
-			unsavedSupervisors++;
-			$('table#supervisors tbody').append(
-				'<tr class="' + (aware == "Yes" ? "aware" : "unaware") + '">' +
-				'<td data-desc="Supervisor ' + (aware == "Yes" ? "aware" : "unaware") + ' of application">' + $('#supervisorFirstname').val() + ' '+ $('#supervisorLastname').val() + ' (' + $('#supervisorEmail').val() + ')</td>' +
-				'<td>' +
-				'<a class=\"button-delete\" id="usd_'+unsavedSupervisors+'" name=\"deleteSupervisor\">delete</a> ' +
-				'<a class=\"button-edit\" id="us_'+unsavedSupervisors+'" name=\"editSupervisorLink\">edit</a>' +
-				'<input type="hidden" id="us_'+unsavedSupervisors+'firstname" value="' + $('#supervisorFirstname').val()+'"/>'	+								
-				'<input type="hidden" id="us_'+unsavedSupervisors+'lastname" value="' + $('#supervisorLastname').val()+'"/>'	+								
-				'<input type="hidden" id="us_'+unsavedSupervisors+'email" value="' + $('#supervisorEmail').val()+'"/>'	+								
-				'<input type="hidden" id="us_'+unsavedSupervisors+'aware" value="' + $('#awareSupervisor').val()+'"/>'	+								
-				'<input type="hidden" name="suggestedSupervisors" id="'+unsavedSupervisors+'_ussupervisors" value=' +"'" + '{"id":"' +  $('#supervisorId').val()+ '","firstname":"' +  $('#supervisorFirstname').val()+ '","lastname":"' +  $('#supervisorLastname').val()+ '","email":"' +  $('#supervisorEmail').val() +  '", "awareSupervisor":"' + $('#awareSupervisor').val() + '"} ' + "'" + "/>" +
-				'</td>' +
-				'</tr>');
-			addToolTips();
-		
-			$("input[name='sFN']").val($('#supervisorFirstname').val());
-			$("input[name='sLN']").val($('#supervisorLastname').val());
-			$("input[name='sEM']").val($('#supervisorEmail').val());
-			$("input[name='sAS']").val($('#awareSupervisor').val());
-			$('#supervisorId, #supervisorFirstname, #supervisorLastname, #supervisorEmail').val('');
-			$("#awareSupervisorCB").attr('checked', false);
-			$("#awareSupervisor").val("NO");
-		}
-	});
-
-	// Update supervisor.	
+	// -------------------------------------------------------------------------------
+	// Update supervisor button.
+	// -------------------------------------------------------------------------------
 	$('#updateSupervisorButton').on('click', function()
 	{
 		var errors = 0;
@@ -282,21 +263,76 @@ $(document).ready(function()
 		$("#awareSupervisorCB").attr('checked', false);
 		$("#awareSupervisor").val("NO");
 	});
+
+
+	// -------------------------------------------------------------------------------
+	// Save programme details.
+	// -------------------------------------------------------------------------------
+	$('#programmeSaveButton').on("click",function()
+	{
+		if ($("#acceptTermsPDValue").val() == 'NO')
+		{ 
+			$('#programmeDetailsSection .terms-box').css({borderColor: 'red', color: 'red'});
+				
+			$("#prog-info-bar-div").switchClass("section-info-bar", "section-error-bar", 1);
+			$("#prog-info-bar-span").switchClass("info-text", "invalid-info-text", 1);
+				
+			if (progImgCount == 0)
+			{
+				$("#prog-info-bar-div .row").prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
+				progImgCount = progImgCount + 1;
+			}
+			addToolTips();
+		}
+		else
+		{
+			$("span[name='nonAcceptedPD']").html('');
+
+			// Check for a "dirty" supervisor form.
+			if (!isFormEmpty('#supervisor_div'))
+			{
+				$('#addSupervisorButton').trigger('click');
+				// If there was an error submitting the non-empty form, don't continue.
+				if ($('#supervisor_div .invalid').length > 0)
+				{
+					return false;
+				}
+			}
+			
+			postProgrammeData('close');
+		}
+	});	
+
+
+	// -------------------------------------------------------------------------------
+	// Validation of an email address.
+	// -------------------------------------------------------------------------------
+	function validateEmail(email)
+	{ 
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		var result = pattern.test(email);
+		return result;
+	} 
 	
-	
+
+	// -------------------------------------------------------------------------------
 	// Close Programme Details section.
+	// -------------------------------------------------------------------------------
 	$('#programmeCloseButton').click(function()
 	{
 		$('#programme-H2').trigger('click');
 		return false;
 	});
+
 	
 	bindDatePicker('#startDate');
 	addToolTips();
-
 });
 
 
+// -------------------------------------------------------------------------------
+// Post programme details.
+// -------------------------------------------------------------------------------
 function postProgrammeData(message)
 {
 	var postData = {

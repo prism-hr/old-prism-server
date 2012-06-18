17310f53ad3567f5ee6d910cc4bd7b42c712bc0b
mroach <mroach@localhost>
2012-06-18 12:18:00 +0100

    Task: send feedback button Desc: -- Author: Manish Dani Date: 15-06-12

---
+++ src/main/java/com/zuehlke/pgadmissions/dao/custom/ReferrerEnumUserType.java
@@ -0,0 +1,13 @@
+package com.zuehlke.pgadmissions.dao.custom;
+
+import com.zuehlke.pgadmissions.domain.enums.Referrer;
+
+
+public class ReferrerEnumUserType extends EnumUserType<Referrer> {
+
+	public ReferrerEnumUserType() {
+		super(Referrer.class);
+		
+	}
+
+}

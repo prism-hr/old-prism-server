14c161ce4c3ed57ebd580a3573a8ef7972115521
dmaughan <dmaughan@localhost>
2012-06-19 12:48:34 +0100

    Replacing alert() in new download scripts.

--- src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
+++ src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
 
-	$('#uploadFields').on('change','#commentDocument', function(event){	
-		if(this.files[0].size < 10485760){
+	$('#uploadFields').on('change','#commentDocument', function(event)
+	{	
+		if (this.files[0].size < 10485760)
+		{
 			$('#commentDocumentProgress').html("uploading file...");
 			$('#commentDocument').attr("readonly", "readonly");
 			commentDocumentUpload();
 			$('#commentDocument').removeAttr("readonly");
-		}else{
-			 alert("Sorry, document must be at most 10MB.");
-		 }
+		}
+		else
+		{
+			$('#uploadFields').append('<span class="invalid">Document must be at most 10MB.</span>');
+		}
 	});
 
 	$('#commentUploadedDocument').on('click', 'span[name="supportingDocumentSpan"] a[name="delete"]', commentDocumentDelete);

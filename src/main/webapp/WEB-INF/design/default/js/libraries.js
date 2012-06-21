c3823e42f0d7b35af7e47bc885dd1ab9aa056b6a
dmaughan <dmaughan@localhost>
2012-06-21 14:32:28 +0100

    Cleaning up some absolutely terrible code.

--- src/main/webapp/WEB-INF/design/default/js/libraries.js
+++ src/main/webapp/WEB-INF/design/default/js/libraries.js
@@ -398,8 +398,8 @@ var firstAvailCol;if(typeof(matrix[rowIndex])=="undefined"){matrix[rowIndex]=[];
 
 
 /* ajaxFileUpload */
-
 jQuery.extend({
+	
 	createUploadIframe: function(id, uri)
 	{
 		//create frame
@@ -407,10 +407,12 @@ jQuery.extend({
 		var iframeHtml = '<iframe id="' + frameId + '" name="' + frameId + '" style="position:absolute; top:-9999px; left:-9999px"';
 		if(window.ActiveXObject)
 		{
-			if(typeof uri== 'boolean'){
+			if (typeof uri== 'boolean')
+			{
 				iframeHtml += ' src="' + 'javascript:false' + '"';
 			}
-			else if(typeof uri== 'string'){
+			else if (typeof uri== 'string')
+			{
 				iframeHtml += ' src="' + uri + '"';
 			}	
 		}
@@ -418,15 +420,16 @@ jQuery.extend({
 		jQuery(iframeHtml).appendTo(document.body);
 		return jQuery('#' + frameId).get(0);			
 	},
+	
 	createUploadForm: function(id, fileElementId, data)
 	{
 		//create form	
 		var formId = 'jUploadForm' + id;
 		var fileId = 'jUploadFile' + id;
 		var form = jQuery('<form  action="" method="POST" name="' + formId + '" id="' + formId + '" enctype="multipart/form-data"></form>');	
-		if(data)
+		if (data)
 		{
-			for(var i in data)
+			for (var i in data)
 			{
 				jQuery('<input type="hidden" name="' + i + '" value="' + data[i] + '" />').appendTo(form);
 			}			
@@ -445,7 +448,8 @@ jQuery.extend({
 		return form;
 	},
 
-	ajaxFileUpload: function(s) {
+	ajaxFileUpload: function(s)
+	{
 		// TODO introduce global settings, allowing the client to modify them for all requests, not only timeout		
 		s = jQuery.extend({}, jQuery.ajaxSettings, s);
 		var id = new Date().getTime()        
@@ -469,16 +473,18 @@ jQuery.extend({
 			var io = document.getElementById(frameId);
 			try 
 			{				
-				if(io.contentWindow)
+				if (io.contentWindow)
 				{
 					 xml.responseText = io.contentWindow.document.body?io.contentWindow.document.body.innerHTML:null;
 					 xml.responseXML = io.contentWindow.document.XMLDocument?io.contentWindow.document.XMLDocument:io.contentWindow.document;
-				}else if(io.contentDocument)
+				}
+				else if(io.contentDocument)
 				{
 					xml.responseText = io.contentDocument.document.body?io.contentDocument.document.body.innerHTML:null;
 					xml.responseXML = io.contentDocument.document.XMLDocument?io.contentDocument.document.XMLDocument:io.contentDocument.document;
 				}						
-			}catch(e)
+			}
+			catch(e)
 			{
 				//jQuery.handleError(s, xml, null, e);
 			}
@@ -495,66 +501,84 @@ jQuery.extend({
 						var data = jQuery.uploadHttpData( xml, s.dataType );    
 						// If a local callback was specified, fire it and pass it the data
 						if ( s.success )
+						{
 							s.success( data, status );
+						}
 
 						// Fire the global callback
-						if( s.global )
+						if ( s.global )
+						{
 							jQuery.event.trigger( "ajaxSuccess", [xml, s] );
-						} else
-								//jQuery.handleError(s, xml, status);
-				} catch(e) 
+						}
+						else
+						{
+							//jQuery.handleError(s, xml, status);
+						}
+					}
+				}
+				catch(e) 
 				{
 					status = "error";
 					//jQuery.handleError(s, xml, status, e);
 				}
 
 				// The request was completed
-				if( s.global )
+				if ( s.global )
+				{
 					jQuery.event.trigger( "ajaxComplete", [xml, s] );
+				}
 
 				// Handle the global AJAX counter
 				if ( s.global && ! --jQuery.active )
+				{
 					jQuery.event.trigger( "ajaxStop" );
+				}
 
 				// Process result
 				if ( s.complete )
+				{
 					s.complete(xml, status);
+				}
 
 				jQuery(io).unbind()
 
 				setTimeout(function()
-				{	try 
+				{
+					try 
 					{
 						jQuery(io).remove();
 						jQuery(form).remove();	
-						
-					} catch(e) 
+					}
+					catch(e) 
 					{
 						//jQuery.handleError(s, xml, null, e);
-					}									
+					}
+				}, 100);
 
-				}, 100)
-
-				xml = null
-
+				xml = null;
 			}
 		}
+		
 		// Timeout checker
 		if ( s.timeout > 0 ) 
 		{
-			setTimeout(function(){
-					// Check to see if the request is still happening
-					if( !requestDone ) uploadCallback( "timeout" );
+			setTimeout(function()
+			{
+				// Check to see if the request is still happening
+				if ( !requestDone )
+				{
+					uploadCallback( "timeout" );
+				}
 			}, s.timeout);
 		}
+		
 		try 
 		{
-
 			var form = jQuery('#' + formId);
 			jQuery(form).attr('action', s.url);
 			jQuery(form).attr('method', 'POST');
 			jQuery(form).attr('target', frameId);
-			if(form.encoding)
+			if (form.encoding)
 			{
 				jQuery(form).attr('encoding', 'multipart/form-data');      			
 			}
@@ -563,28 +587,36 @@ jQuery.extend({
 				jQuery(form).attr('enctype', 'multipart/form-data');			
 			}			
 			jQuery(form).submit();
-		} catch(e) 
+		}
+		catch(e) 
 		{			
 			//jQuery.handleError(s, xml, null, e);
 		}
 		
 		jQuery('#' + frameId).load(uploadCallback	);
-			return {abort: function () {}};	
-    },
+		return {abort: function () {}};	
+	},
 
-    uploadHttpData: function( r, type ) {
-			var data = !type;
-			data = type == "xml" || data ? r.responseXML : r.responseText;
-			// If the type is "script", eval it in global context
-			if ( type == "script" )
-				jQuery.globalEval( data );
-			// Get the JavaScript object, if JSON is used.
-			if ( type == "json" )
-				eval( "data = " + data );
-			// evaluate scripts within html
-			if ( type == "html" )
-				jQuery("<div>").html(data).evalScripts();
+	uploadHttpData: function( r, type )
+	{
+		var data = !type;
+		data = type == "xml" || data ? r.responseXML : r.responseText;
+		// If the type is "script", eval it in global context
+		if ( type == "script" )
+		{
+			jQuery.globalEval( data );
+		}
+		// Get the JavaScript object, if JSON is used.
+		if ( type == "json" )
+		{
+			eval( "data = " + data );
+		}
+		// evaluate scripts within html
+		if ( type == "html" )
+		{
+			jQuery("<div>").html(data).evalScripts();
+		}
 
-			return data;
-    }
+		return data;
+	}
 })

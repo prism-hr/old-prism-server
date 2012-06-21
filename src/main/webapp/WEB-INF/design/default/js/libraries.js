ae1e60c2159846f4038164509b22aac0a5412daf
dmaughan <dmaughan@localhost>
2012-06-21 12:45:19 +0100

    moving ajax file upload script into libraries.js.

--- src/main/webapp/WEB-INF/design/default/js/libraries.js
+++ src/main/webapp/WEB-INF/design/default/js/libraries.js
@@ -395,3 +395,204 @@ var firstAvailCol;if(typeof(matrix[rowIndex])=="undefined"){matrix[rowIndex]=[];
  * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
  */
 ;(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||a.ownerDocument||a;return d.browser.safari||e.compatMode=='BackCompat'?e.body:e.documentElement})};d.fn.scrollTo=function(n,j,b){if(typeof j=='object'){b=j;j=0}if(typeof b=='function')b={onAfter:b};if(n=='max')n=9e9;b=d.extend({},k.defaults,b);j=j||b.speed||b.duration;b.queue=b.queue&&b.axis.length>1;if(b.queue)j/=2;b.offset=p(b.offset);b.over=p(b.over);return this._scrollable().each(function(){var q=this,r=d(q),f=n,s,g={},u=r.is('html,body');switch(typeof f){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(f)){f=p(f);break}f=d(f,this);case'object':if(f.is||f.style)s=(f=d(f)).offset()}d.each(b.axis.split(''),function(a,i){var e=i=='x'?'Left':'Top',h=e.toLowerCase(),c='scroll'+e,l=q[c],m=k.max(q,i);if(s){g[c]=s[h]+(u?0:l-r.offset()[h]);if(b.margin){g[c]-=parseInt(f.css('margin'+e))||0;g[c]-=parseInt(f.css('border'+e+'Width'))||0}g[c]+=b.offset[h]||0;if(b.over[h])g[c]+=f[i=='x'?'width':'height']()*b.over[h]}else{var o=f[h];g[c]=o.slice&&o.slice(-1)=='%'?parseFloat(o)/100*m:o}if(/^\d+$/.test(g[c]))g[c]=g[c]<=0?0:Math.min(g[c],m);if(!a&&b.queue){if(l!=g[c])t(b.onAfterFirst);delete g[c]}});t(b.onAfter);function t(a){r.animate(g,j,b.easing,a&&function(){a.call(this,n,b)})}}).end()};k.max=function(a,i){var e=i=='x'?'Width':'Height',h='scroll'+e;if(!d(a).is('html,body'))return a[h]-d(a)[e.toLowerCase()]();var c='client'+e,l=a.ownerDocument.documentElement,m=a.ownerDocument.body;return Math.max(l[h],m[h])-Math.min(l[c],m[c])};function p(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+
+
+/* ajaxFileUpload */
+jQuery.extend({
+createUploadIframe: function(id, uri)
+{
+//create frame
+var frameId = 'jUploadFrame' + id;
+var iframeHtml = '<iframe id="' + frameId + '" name="' + frameId + '" style="position:absolute; top:-9999px; left:-9999px"';
+if (window.ActiveXObject)
+{
+if (typeof uri== 'boolean'){
+iframeHtml += ' src="' + 'javascript:false' + '"';
+
+}
+else if(typeof uri== 'string'){
+iframeHtml += ' src="' + uri + '"';
+
+}	
+}
+iframeHtml += ' />';
+jQuery(iframeHtml).appendTo(document.body);
+
+return jQuery('#' + frameId).get(0);			
+},
+createUploadForm: function(id, fileElementId, data)
+{
+//create form	
+var formId = 'jUploadForm' + id;
+var fileId = 'jUploadFile' + id;
+var form = jQuery('<form  action="" method="POST" name="' + formId + '" id="' + formId + '" enctype="multipart/form-data"></form>');	
+if(data)
+{
+for(var i in data)
+{
+jQuery('<input type="hidden" name="' + i + '" value="' + data[i] + '" />').appendTo(form);
+}			
+}		
+var oldElement = jQuery('#' + fileElementId);
+var newElement = jQuery(oldElement).clone();
+jQuery(oldElement).attr('id', fileId);
+jQuery(oldElement).before(newElement);
+jQuery(oldElement).appendTo(form);
+
+
+
+//set attributes
+jQuery(form).css('position', 'absolute');
+jQuery(form).css('top', '-1200px');
+jQuery(form).css('left', '-1200px');
+jQuery(form).appendTo('body');		
+return form;
+},
+
+ajaxFileUpload: function(s) {
+// TODO introduce global settings, allowing the client to modify them for all requests, not only timeout		
+s = jQuery.extend({}, jQuery.ajaxSettings, s);
+var id = new Date().getTime()        
+var form = jQuery.createUploadForm(id, s.fileElementId, (typeof(s.data)=='undefined'?false:s.data));
+var io = jQuery.createUploadIframe(id, s.secureuri);
+var frameId = 'jUploadFrame' + id;
+var formId = 'jUploadForm' + id;		
+// Watch for a new set of requests
+if ( s.global && ! jQuery.active++ )
+{
+jQuery.event.trigger( "ajaxStart" );
+}            
+var requestDone = false;
+// Create the request object
+var xml = {}   
+if ( s.global )
+jQuery.event.trigger("ajaxSend", [xml, s]);
+// Wait for a response to come back
+var uploadCallback = function(isTimeout)
+{			
+var io = document.getElementById(frameId);
+try 
+{				
+if(io.contentWindow)
+{
+xml.responseText = io.contentWindow.document.body?io.contentWindow.document.body.innerHTML:null;
+xml.responseXML = io.contentWindow.document.XMLDocument?io.contentWindow.document.XMLDocument:io.contentWindow.document;
+
+}else if(io.contentDocument)
+{
+xml.responseText = io.contentDocument.document.body?io.contentDocument.document.body.innerHTML:null;
+xml.responseXML = io.contentDocument.document.XMLDocument?io.contentDocument.document.XMLDocument:io.contentDocument.document;
+}						
+}catch(e)
+{
+jQuery.handleError(s, xml, null, e);
+}
+if ( xml || isTimeout == "timeout") 
+{				
+requestDone = true;
+var status;
+try {
+status = isTimeout != "timeout" ? "success" : "error";
+// Make sure that the request was successful or notmodified
+if ( status != "error" )
+{
+// process the data (runs the xml through httpData regardless of callback)
+var data = jQuery.uploadHttpData( xml, s.dataType );    
+// If a local callback was specified, fire it and pass it the data
+if ( s.success )
+s.success( data, status );
+
+// Fire the global callback
+if( s.global )
+jQuery.event.trigger( "ajaxSuccess", [xml, s] );
+} else
+jQuery.handleError(s, xml, status);
+} catch(e) 
+{
+status = "error";
+jQuery.handleError(s, xml, status, e);
+}
+
+// The request was completed
+if( s.global )
+jQuery.event.trigger( "ajaxComplete", [xml, s] );
+
+// Handle the global AJAX counter
+if ( s.global && ! --jQuery.active )
+jQuery.event.trigger( "ajaxStop" );
+
+// Process result
+if ( s.complete )
+s.complete(xml, status);
+
+jQuery(io).unbind()
+
+setTimeout(function()
+{	try 
+{
+jQuery(io).remove();
+jQuery(form).remove();	
+
+} catch(e) 
+{
+jQuery.handleError(s, xml, null, e);
+}									
+
+}, 100)
+
+xml = null
+
+}
+}
+// Timeout checker
+if ( s.timeout > 0 ) 
+{
+setTimeout(function(){
+// Check to see if the request is still happening
+if( !requestDone ) uploadCallback( "timeout" );
+}, s.timeout);
+}
+try 
+{
+
+var form = jQuery('#' + formId);
+jQuery(form).attr('action', s.url);
+jQuery(form).attr('method', 'POST');
+jQuery(form).attr('target', frameId);
+if(form.encoding)
+{
+jQuery(form).attr('encoding', 'multipart/form-data');      			
+}
+else
+{	
+jQuery(form).attr('enctype', 'multipart/form-data');			
+}			
+jQuery(form).submit();
+
+} catch(e) 
+{			
+jQuery.handleError(s, xml, null, e);
+}
+
+jQuery('#' + frameId).load(uploadCallback	);
+return {abort: function () {}};	
+
+},
+
+uploadHttpData: function( r, type ) {
+var data = !type;
+data = type == "xml" || data ? r.responseXML : r.responseText;
+// If the type is "script", eval it in global context
+if ( type == "script" )
+jQuery.globalEval( data );
+// Get the JavaScript object, if JSON is used.
+if ( type == "json" )
+eval( "data = " + data );
+// evaluate scripts within html
+if ( type == "html" )
+jQuery("<div>").html(data).evalScripts();
+
+return data;
+}
+})
+

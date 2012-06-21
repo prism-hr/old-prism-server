d9229be2117c7122384ef37802fa9cdd86da34a0
dmaughan <dmaughan@localhost>
2012-06-21 14:32:28 +0100

    Cleaning up some absolutely terrible code.

---
+++ src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+	$('#uploadFields').on('change','#commentDocument', function(event)
+	{	
+		$('#uploadFields span.invalid').remove();
+		
+		if (this.files[0].size < 10485760)
+		{
+			$('#commentDocumentProgress').html("uploading file...");
+			$('#commentDocument').attr("readonly", "readonly");
+			commentDocumentUpload();
+			$('#commentDocument').removeAttr("readonly");
+		}
+		else
+		{
+			$('#uploadFields').append('<span class="invalid">Document must be at most 10MB.</span>');
+		}
+	});
+
+	$('#commentUploadedDocument').on('click', 'span[name="supportingDocumentSpan"] a[name="delete"]', commentDocumentDelete);
+	
+});
+
+function commentDocumentDelete(){
+	 var id = $(this).attr("id");
+	$.post("/pgadmissions/delete/asyncdelete",
+		{
+			documentId: id
+		},
+		function(data) {			
+			$('#' +id).parent().remove()
+		}
+	);
+
+	
+}
+
+function commentDocumentUpload()
+{	
+	
+	$("#commentDocumentProgress").ajaxStart(function(){
+		$(this).show();
+	})
+	.ajaxComplete(function(){
+		$(this).hide();
+		$('#commentDocumentProgress').html("");
+		$('#commentDocument').val("");
+		
+	});
+
+	$.ajaxFileUpload
+	(
+		{
+			url:'/pgadmissions/documents/async',
+			secureuri:false,
+			
+			fileElementId:'commentDocument',	
+			dataType:'text',
+			data:{type:'COMMENT'},
+			success: function (data)
+			{		
+				$('#commentUploadedDocument').append(data);
+				$('#uploadFields').addClass('uploaded');
+				$('#commentUploadedDocument').show();
+				
+			}
+		}
+	);
+}

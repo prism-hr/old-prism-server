a7eda70e2455763f0f2133a20e43e92ed466cf73
dmaughan <dmaughan@localhost>
2012-06-26 14:12:25 +0100

    Updating these scripts.

--- src/main/webapp/WEB-INF/design/default/js/application/qualifications.js
+++ src/main/webapp/WEB-INF/design/default/js/application/qualifications.js
@@ -153,10 +153,11 @@ $(document).ready(function(){
 			
 			$(this).parent().parent().find('.terms-box').css({borderColor: 'red', color: 'red'});
 			
-			$("#qual-info-bar-div").switchClass("section-info-bar", "section-error-bar", 1);
-			$("#qual-info-bar-span").switchClass("info-text", "invalid-info-text", 1);
-			if(qualImgCount == 0){
-				$("#qual-info-bar-div .row").prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
+			var $infobar = $('#qual-info-bar-div.section-info-bar');
+			$infobar.switchClass("section-info-bar", "section-error-bar", 1);
+			if ($infobar)
+			{
+				$infobar.prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
 				qualImgCount = qualImgCount + 1;
 			}
 			addToolTips();

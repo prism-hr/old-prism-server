4a8e5f962bbf1784768401f2a8220993bc3f9ac6
dmaughan <dmaughan@localhost>
2012-06-27 14:58:12 +0100

    adding functionality to empty a form (which is what the clear button is meant to do).

---
+++ src/main/java/com/zuehlke/pgadmissions/controllers/workflow/validation/EmailRegistryController.java
@@ -0,0 +1,89 @@
+package com.zuehlke.pgadmissions.controllers.workflow.validation;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Comment;
+import com.zuehlke.pgadmissions.domain.NotificationRecord;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.enums.CommentType;
+import com.zuehlke.pgadmissions.domain.enums.NotificationType;
+import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
+import com.zuehlke.pgadmissions.mail.RegistryMailSender;
+import com.zuehlke.pgadmissions.services.ApplicationsService;
+import com.zuehlke.pgadmissions.services.CommentService;
+import com.zuehlke.pgadmissions.services.UserService;
+import com.zuehlke.pgadmissions.utils.CommentFactory;
+
+@RequestMapping("/registryHelpRequest")
+@Controller
+public class EmailRegistryController {
+
+	private final Logger log = Logger.getLogger(EmailRegistryController.class);
+	private final RegistryMailSender registryMailSender;
+	private final ApplicationsService applicationsService;
+	private final UserService userService;
+	private final CommentService commentService;
+	private final CommentFactory commentFactory;
+
+	EmailRegistryController() {
+		this(null, null, null, null, null);
+	}
+
+	@Autowired
+	public EmailRegistryController(RegistryMailSender registryMailSender, ApplicationsService applicationsService, UserService userService,
+			CommentService commentService, CommentFactory commentFactory) {
+		this.registryMailSender = registryMailSender;
+		this.applicationsService = applicationsService;
+		this.userService = userService;
+		this.commentService = commentService;
+		this.commentFactory = commentFactory;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView sendHelpRequestToRegistryContacts(@ModelAttribute("applicationForm") ApplicationForm applicationForm) {
+		ModelAndView modelAndView = new ModelAndView("private/common/simpleMessage");
+		try {
+			registryMailSender.sendApplicationToRegistryContacts(applicationForm);
+			NotificationRecord notificationRecord = applicationForm.getNotificationForType(NotificationType.REGISTRY_HELP_REQUEST);
+			if (notificationRecord == null) {
+				notificationRecord = new NotificationRecord(NotificationType.REGISTRY_HELP_REQUEST);
+				applicationForm.getNotificationRecords().add(notificationRecord);
+			}
+			notificationRecord.setDate(new Date());
+			applicationsService.save(applicationForm);
+			commentService.save(commentFactory.createComment(applicationForm, getCurrentUser(),
+					"Request for assistance in validating application details send to UCL central registry office.", CommentType.GENERIC, null));
+			modelAndView.getModel().put("message", "registry.email.send");
+		} catch (Throwable e) {
+			log.error("Send email to registry contacts failed:", e);
+			modelAndView.getModel().put("message", "registry.email.failed");
+		}
+
+		return modelAndView;
+	}
+
+	@ModelAttribute("applicationForm")
+	public ApplicationForm getApplicationForm(@RequestParam String applicationId) {
+		ApplicationForm applicationForm = applicationsService.getApplicationByApplicationNumber(applicationId);
+		if (applicationForm == null || !getCurrentUser().hasAdminRightsOnApplication(applicationForm)) {
+			throw new ResourceNotFoundException();
+		}
+		return applicationForm;
+	}
+
+	@ModelAttribute("user")
+	public RegisteredUser getCurrentUser() {
+		return userService.getCurrentUser();
+	}
+
+}
---
+++ src/main/java/com/zuehlke/pgadmissions/validators/UserDTOValidator.java
@@ -0,0 +1,39 @@
+package com.zuehlke.pgadmissions.validators;
+
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+import com.zuehlke.pgadmissions.dto.UserDTO;
+
+@Component
+public class UserDTOValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> clazz) {		
+		return clazz.isAssignableFrom(UserDTO.class);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		UserDTO user = (UserDTO) target;	
+		if( !(user.getSelectedAuthorities().length == 1 && user.getSelectedAuthorities()[0] == Authority.SUPERADMINISTRATOR)){
+			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "selectedProgram", "dropdown.radio.select.none");		
+		}
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "text.field.empty");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "text.field.empty");
+		
+		
+		if(user.getSelectedAuthorities() == null || user.getSelectedAuthorities().length == 0){
+			errors.rejectValue("selectedAuthorities", "dropdown.radio.select.none");
+		}
+
+		if (!EmailValidator.getInstance().isValid(user.getEmail())) {
+			errors.rejectValue("email", "text.email.notvalid");
+		}
+	}
+
+}

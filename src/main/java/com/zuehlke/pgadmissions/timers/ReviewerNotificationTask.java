11c5ec6c983cdba58a77dbb04694d2598a9743e9
mroach <mroach@localhost>
2012-06-28 14:31:36 +0100

    Task: more fixes Desc: -- Author: Manish Dani Date: 28-06-12

---
+++ src/main/java/com/zuehlke/pgadmissions/timers/ReviewerNotificationTask.java
@@ -0,0 +1,54 @@
+package com.zuehlke.pgadmissions.timers;
+
+import java.util.Date;
+import java.util.List;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.zuehlke.pgadmissions.dao.ReviewerDAO;
+import com.zuehlke.pgadmissions.domain.Reviewer;
+import com.zuehlke.pgadmissions.mail.ReviewerMailSender;
+
+public class ReviewerNotificationTask extends TimerTask {
+	private final Logger log = Logger.getLogger(ReviewerNotificationTask.class);
+	private final SessionFactory sessionFactory;
+	private final ReviewerMailSender reviewerMailSender;
+	private final ReviewerDAO reviewerDAO;
+
+	public ReviewerNotificationTask(SessionFactory sessionFactory, ReviewerMailSender reviewerMailSender, ReviewerDAO reviewerDAO) {
+		this.sessionFactory = sessionFactory;
+		this.reviewerMailSender = reviewerMailSender;
+		this.reviewerDAO = reviewerDAO;
+	}
+
+	@Override
+	public void run() {
+		log.info("Reviewer Notification Task Running");
+		Transaction transaction = sessionFactory.getCurrentSession().beginTransaction();
+		List<Reviewer> reviewersDueNotification = reviewerDAO.getReviewersDueNotification();
+
+		transaction.commit();
+		for (Reviewer reviewer : reviewersDueNotification) {
+			transaction = sessionFactory.getCurrentSession().beginTransaction();
+			sessionFactory.getCurrentSession().refresh(reviewer);
+			try {
+				reviewerMailSender.sendReviewerNotification(reviewer);
+				reviewer.setLastNotified(new Date());
+				reviewerDAO.save(reviewer);
+				transaction.commit();
+				log.info("notification send to reviewer " + reviewer.getUser().getEmail());
+			} catch (Throwable e) {
+				transaction.rollback();
+				log.warn("error while sending notification to reviewer " + reviewer.getUser().getEmail(), e);
+
+			}
+
+		}
+		log.info("Reviewer Notification Task complete");
+
+	}
+
+}

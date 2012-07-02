3b0903fec13857fe120c707a48c97a8c430fad7c
mroach <mroach@localhost>
2012-07-02 16:28:07 +0100

    Merge with c05e91d628f26cb77b8a69cc988361b7fbba5376

---
+++ src/test/java/com/zuehlke/pgadmissions/validators/UpdateUserRolesDTOValidatorTest.java
@@ -0,0 +1,73 @@
+package com.zuehlke.pgadmissions.validators;
+
+import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DirectFieldBindingResult;
+
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+import com.zuehlke.pgadmissions.dto.UpdateUserRolesDTO;
+
+public class UpdateUserRolesDTOValidatorTest {
+
+	private UpdateUserRolesDTOValidator validator;
+	private UpdateUserRolesDTO user;
+
+	@Before
+	public void setup() {
+		validator = new  UpdateUserRolesDTOValidator();
+		user = new UpdateUserRolesDTO();
+		user.setSelectedUser(new RegisteredUserBuilder().id(5).toUser());
+		user.setSelectedProgram(new ProgramBuilder().id(4).toProgram());
+		user.setSelectedAuthorities(Authority.REVIEWER);
+	}
+
+	@Test
+	public void shouldSupportUpdateUserRolesDTO() {
+		assertTrue(validator.supports(UpdateUserRolesDTO.class));
+	}
+
+	@Test
+	public void shouldNotRejectValidUser() {
+		BindingResult mappingResult = new BeanPropertyBindingResult(user, "*");
+		validator.validate(user, mappingResult);
+		Assert.assertEquals(0, mappingResult.getErrorCount());
+	}
+
+	@Test
+	public void shouldRejectIfUserEmpty() {
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "selectedUser");
+		user.setSelectedUser(null);
+		validator.validate(user, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+		Assert.assertEquals("dropdown.radio.select.none", mappingResult.getFieldError("selectedUser").getCode());
+	}
+
+		
+	@Test
+	public void shouldRejectIfProgramIsNullAndAuthoritiesNotSuperadminOnly() {
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "selectedProgram");
+		user.setSelectedProgram(null);
+		user.setSelectedAuthorities(Authority.REVIEWER, Authority.SUPERADMINISTRATOR);
+		validator.validate(user, mappingResult);
+		Assert.assertEquals(1, mappingResult.getErrorCount());
+		Assert.assertEquals("dropdown.radio.select.none", mappingResult.getFieldError("selectedProgram").getCode());
+	}
+	@Test
+	public void shouldNotRejectIfProgramIsNullAndAuthoritiesIsSuperadmin() {
+		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "selectedProgram");
+		user.setSelectedProgram(null);
+		user.setSelectedAuthorities(Authority.SUPERADMINISTRATOR);
+		validator.validate(user, mappingResult);
+		Assert.assertEquals(0, mappingResult.getErrorCount());
+		
+	}
+	
+	
+}

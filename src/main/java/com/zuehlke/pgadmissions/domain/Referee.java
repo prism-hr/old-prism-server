48e31f63a68304b5ace052eb6c7cf8e9bf1fbf05
kpa <kpa@localhost>
2012-07-02 12:21:24 +0100

    "flush" hibernate bug fix

---
+++ src/main/java/com/zuehlke/pgadmissions/domain/Referee.java
@@ -0,0 +1,214 @@
+package com.zuehlke.pgadmissions.domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.apache.commons.lang.StringUtils;
+
+@Entity(name = "APPLICATION_FORM_REFEREE")
+@Access(AccessType.FIELD)
+public class Referee extends DomainObject<Integer> {
+
+	private static final long serialVersionUID = 4591043630090924738L;
+
+	@Column(name = "last_notified")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastNotified;
+
+	@ManyToOne
+	@JoinColumn(name = "registered_user_id")
+	private RegisteredUser user;
+
+	@OneToOne(mappedBy = "referee")
+	private ReferenceComment reference;
+
+	@ManyToOne
+	@JoinColumn(name = "application_form_id")
+	private ApplicationForm application;
+
+	@Column(name = "phone")
+	private String phoneNumber;
+
+	@Column(name = "skype")
+	private String messenger;
+
+	@Column(name = "firstname")
+	private String firstname;
+
+	@Column(name = "lastname")
+	private String lastname;
+
+	@Column(name = "job_employer")
+	private String jobEmployer;
+
+	@Column(name = "job_title")
+	private String jobTitle;
+
+	@Column(name = "address_location")
+	private String addressLocation;
+
+	@OneToOne
+	@JoinColumn(name = "country_id")
+	private Country addressCountry;
+
+	@Column(name = "email")
+	private String email;
+
+	private boolean declined = false;
+
+	public ApplicationForm getApplication() {
+		return application;
+	}
+
+	public void setApplication(ApplicationForm application) {
+		this.application = application;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getJobEmployer() {
+		return jobEmployer;
+	}
+
+	public void setJobEmployer(String jobEmployer) {
+		this.jobEmployer = jobEmployer;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getAddressLocation() {
+		return addressLocation;
+	}
+
+	public void setAddressLocation(String addressLocation) {
+		this.addressLocation = addressLocation;
+	}
+
+	public Country getAddressCountry() {
+		return addressCountry;
+	}
+
+	public void setAddressCountry(Country addressCountry) {
+		this.addressCountry = addressCountry;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+
+	}
+
+	@Override
+	@Id
+	@GeneratedValue
+	@Access(AccessType.PROPERTY)
+	public Integer getId() {
+		return id;
+	}
+
+
+	public boolean hasProvidedReference() {
+		return reference != null;
+	}
+
+	public ReferenceComment getReference() {
+		return reference;
+	}
+
+	public void setReference(ReferenceComment reference) {
+		this.reference = reference;
+	}
+
+	public String getMessenger() {
+		return messenger;
+	}
+
+	public void setMessenger(String messenger) {
+		if (StringUtils.isBlank(messenger)) {
+			this.messenger = null;
+		} else {
+			this.messenger = messenger;
+		}
+	}
+
+	public RegisteredUser getUser() {
+		return user;
+	}
+
+	public void setUser(RegisteredUser user) {
+		this.user = user;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isDeclined() {
+		return declined;
+	}
+
+	public void setDeclined(boolean declined) {
+		this.declined = declined;
+	}
+
+	public Date getLastNotified() {
+		return lastNotified;
+	}
+
+	public void setLastNotified(Date lastNotified) {
+		this.lastNotified = lastNotified;
+	}
+
+	public boolean isEditable() {
+		return !hasProvidedReference() && !declined && (application == null || application.isModifiable());
+	}
+
+	public boolean hasResponded() {
+		return isDeclined() || hasProvidedReference();
+		
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/design/default/js/application/qualifications.js
@@ -0,0 +1,379 @@
+$(document).ready(function(){
+	
+	var qualImgCount = 0;
+	
+	$("#acceptTermsQDValue").val("NO");
+	
+	// -------------------------------------------------------------------------------
+	// Close button.
+	// -------------------------------------------------------------------------------
+	$('#qualificationsCloseButton').click(function(){
+		$('#qualifications-H2').trigger('click');
+		return false;
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// Checkbox to mark the qualification as current.
+	// -------------------------------------------------------------------------------
+	$("input[name*='currentQualificationCB']").click(function()
+	{
+		if ($("#currentQualification").val() =='YES')
+		{
+			// Uncheck the box
+			$("#currentQualification").val("NO");
+			$("#qualificationAwardDate").val("");
+			$("#qualificationAwardDate").attr("disabled", "disabled");
+			$("#proofOfAward").val("");
+			$("#proofOfAward").attr("disabled", "disabled");
+			$("#quali-grad-id em").remove();
+			$("#quali-grad-id").text("Expected Grade / Result / GPA").append('<em>*</em>');
+			$("#quali-award-date-lb").text("Award Date").addClass("grey-label");
+			$("#quali-proof-of-award-lb").text("Proof of award (PDF)").addClass("grey-label");
+			
+			if ($('#uploadFields').hasClass('uploaded'))
+			{
+				$('#uploadFields').addClass('upload-delete');
+				$('#uploadFields a.button-edit').hide();
+			}
+		}
+		else
+		{		
+			// Check the box
+			$("#currentQualification").val("YES");
+			$("#qualificationAwardDate").removeAttr("disabled", "disabled");	
+			$("#proofOfAward").removeAttr("disabled");
+			$("#quali-grad-id em").remove();
+			$("#quali-grad-id").text("Grade / Result / GPA").append('<em>*</em>');
+			$("#quali-award-date-lb em").remove();
+			$("#quali-award-date-lb").append('<em>*</em>').removeClass("grey-label");
+			$("#quali-proof-of-award-lb").removeClass("grey-label");
+
+			if ($('#uploadFields').hasClass('uploaded'))
+			{
+				$('#uploadFields').removeClass('upload-delete');
+				$('#uploadFields a.button-edit').show();
+			}
+		}
+		
+	
+	});
+
+	
+	// -------------------------------------------------------------------------------
+	// Delete a qualification.
+	// -------------------------------------------------------------------------------
+	$('a[name="deleteQualificationButton"]').click(function()
+	{
+
+			var id = $(this).attr("id").replace("qualification_", "");
+			$.ajax({
+				type: 'POST',
+				 statusCode: {
+					  401: function() {
+						  window.location.reload();
+					  }
+				  },
+				url:"/pgadmissions/deleteentity/qualification",
+				data:	{
+						id: id	
+					}, 
+				success:	function(data) {
+						$('#qualificationsSection').html(data);
+					}	
+					
+			});
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// "Accept terms" checkbox.
+	// -------------------------------------------------------------------------------
+	$("input[name*='acceptTermsQDCB']").click(function()
+	{
+		if ($("#acceptTermsQDValue").val() =='YES')
+		{
+			$("#acceptTermsQDValue").val("NO");
+		}
+		else
+		{	
+			$("#acceptTermsQDValue").val("YES");
+
+			qualImgCount = 0;
+			
+			$.ajax({
+				type: 'POST',
+				 statusCode: {
+					  401: function() {
+						  window.location.reload();
+					  }
+				  },
+				url:"/pgadmissions/acceptTerms",
+				data:{  
+					applicationId: $("#applicationId").val(), 
+					acceptedTerms: $("#acceptTermsQDValue").val()
+				},
+				success: function(data) {}
+			});
+		}
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// Add a qualification.
+	// -------------------------------------------------------------------------------
+	$('#addQualificationButton').click(function()
+	{
+		if ($('#acceptTermsQDValue').length != 0 &&  $("#acceptTermsQDValue").val() =='NO')
+		{
+			$(this).parent().parent().parent().parent().find('.terms-box').css({borderColor: 'red', color: 'red'});
+			
+			var $infobar = $('#qual-info-bar-div.section-info-bar');
+			$infobar.switchClass("section-info-bar", "section-error-bar", 1);
+			if ($infobar)
+			{
+				$infobar.prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
+				qualImgCount = qualImgCount + 1;
+			}
+			addToolTips();
+		}
+		else
+		{
+			$("span[name='nonAcceptedQD']").html('');
+			postQualificationData('add');
+		}
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// Save qualification.
+	// -------------------------------------------------------------------------------
+	$('#qualificationsSaveButton').click(function()
+	{
+		if ($("#acceptTermsQDValue").val() =='NO')
+		{ 
+			// Highlight the information bar and terms box.
+			$(this).parent().parent().find('.terms-box').css({borderColor: 'red', color: 'red'});
+			
+			var $infobar = $('#qual-info-bar-div.section-info-bar');
+			$infobar.switchClass("section-info-bar", "section-error-bar", 1);
+			if ($infobar)
+			{
+				$infobar.prepend('<span class=\"error-hint\" data-desc=\"Please provide all mandatory fields in this section.\"></span>');
+				qualImgCount = qualImgCount + 1;
+			}
+			addToolTips();
+		}
+		else
+		{
+			$("span[name='nonAcceptedQD']").html('');
+			
+			// Check for a "dirty" qualification form. If there is data try to submit it.
+			if (!isFormEmpty('#qualificationsSection form'))
+			{
+				postQualificationData('close');
+			}
+			else
+			{
+				$('#qualificationsCloseButton').trigger('click');
+			}
+		}
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// Edit a qualification.
+	// -------------------------------------------------------------------------------
+	$('a[name="editQualificationLink"]').click(function()
+	{
+		var id = this.id;
+		id = id.replace('qualification_', '');	
+		$.ajax({
+			 type: 'GET',
+			 statusCode: {
+				  401: function() {
+					  window.location.reload();
+				  }
+			  },
+				url:"/pgadmissions/update/getQualification",
+				data:{
+					applicationId:  $('#applicationId').val(),
+					qualificationId: id,
+					message: 'edit',					
+					cacheBreaker: new Date().getTime()
+				},
+				success: function(data)
+				{
+					$('#qualificationsSection').html(data);
+					
+					if ($("#currentQualificationCB").is(":checked"))
+					{
+						$("#qualificationAwardDate").removeAttr("disabled");
+						$("#proofOfAward").removeAttr("disabled");
+						$("#quali-grad-id em").remove();
+						$("#quali-grad-id").text("Grade / Result / GPA").append('<em>*</em>');
+						$("#quali-award-date-lb em").remove();
+						$("#quali-award-date-lb").append('<em>*</em>').removeClass("grey-label");
+						$("#quali-proof-of-award-lb").removeClass("grey-label");
+					}
+					else
+					{	
+						$("#qualificationAwardDate").attr("disabled", "disabled");
+						$("#proofOfAward").attr("disabled", "disabled");
+						$("#quali-grad-id em").remove();
+						$("#quali-grad-id").text("Expected Grade / Result / GPA").append('<em>*</em>');
+						$("#quali-award-date-lb em").remove();
+						$("#quali-award-date-lb").text("Award Date").addClass("grey-label");
+						$("#quali-proof-of-award-lb").text("Proof of award (PDF)").addClass("grey-label");
+					}
+					
+					// Cheap way of changing the button text.
+					$('#addQualificationButton').html('Update');
+				}
+		});
+	});
+	
+
+	// -------------------------------------------------------------------------------
+	// Clear button.
+	// -------------------------------------------------------------------------------
+	$('a[name="qualificationCancelButton"]').click(function()
+	{
+		$.ajax({
+			 type: 'GET',
+			 statusCode: {
+				  401: function() {
+					  window.location.reload();
+				  }
+			  },
+				url:"/pgadmissions/update/getQualification",
+				data:{
+					applicationId:  $('#applicationId').val(),
+					message: 'cancel',					
+					cacheBreaker: new Date().getTime()
+				},
+				success:function(data) {
+					$('#qualificationsSection').html(data);
+				}
+		});
+	});
+
+	
+	bindDatePicker('#qualificationStartDate');
+	bindDatePicker('#qualificationAwardDate');
+	addToolTips();
+	
+  // Generic file upload solution...
+	watchUpload($('#proofOfAward'));	
+});
+
+function postQualificationData(message)
+{
+	$('#qualificationsSection > div').append('<div class="ajax" />');
+
+	$.ajax({
+		type: 'POST',
+		 statusCode: {
+			  401: function() {
+				  window.location.reload();
+			  }
+		  },
+		url:"/pgadmissions/update/editQualification",
+		data:{  
+			qualificationSubject: $("#qualificationSubject").val(), 
+			qualificationInstitution: $("#qualificationInstitution").val(), 
+			qualificationType: $("#qualificationType").val(),
+			qualificationGrade: $("#qualificationGrade").val(),
+			qualificationScore: $("#qualificationScore").val(),
+			qualificationStartDate: $("#qualificationStartDate").val(),
+			qualificationLanguage: $("#qualificationLanguage").val(),
+			qualificationAwardDate: $("#qualificationAwardDate").val(),
+			completed: $("#currentQualification").val(),			
+			qualificationId: $("#qualificationId").val(),
+			applicationId:  $('#applicationId').val(),
+			application:  $('#applicationId').val(),
+			institutionCountry: $('#institutionCountry').val(),
+			proofOfAward: $('#document_PROOF_OF_AWARD').val(),
+			message:message
+		},
+		success:function(data) {
+			$('#qualificationsSection').html(data);
+			markSectionError('#qualificationsSection');
+	
+			if (message == 'close')
+			{
+				// Close the section only if there are no errors.
+				var errorCount = $('#qualificationsSection .invalid:visible').length;
+				if (errorCount == 0)
+				{
+					$('#qualifications-H2').trigger('click');
+				}
+			}
+		},
+    complete: function()
+    {
+			$('#qualificationsSection div.ajax').remove();
+    }
+	});
+}
+function ajaxProofOfAwardDelete(){
+	
+	if($('#profOfAwardId') && $('#profOfAwardId').val() && $('#profOfAwardId').val() != ''){
+		$.ajax({
+			type: 'POST',
+			 statusCode: {
+				  401: function() {
+					  window.location.reload();
+				  }
+			  },
+			url:"/pgadmissions/delete/asyncdelete",
+			data:{
+				documentId: $('#profOfAwardId').val()				
+			}				
+		});
+
+	}
+}
+function ajaxProofOfAwardUpload()
+{	
+	// Showing/hiding progress bar when we're uploading the file via AJAX.	
+	$("#progress").ajaxStart(function()
+		{
+			$(this).show();
+		})
+		.ajaxComplete(function()
+		{
+			$(this).hide();
+			$('#progress').html("");
+		}
+	);
+
+	// Remove any previous error messages.
+	$('#qualUploadedDocument').find('span.invalid').remove();
+
+	// Functionality for uploading files via AJAX (immediately on selection).
+	$.ajaxFileUpload(
+	{
+		url:            '/pgadmissions/documents/async',
+		secureuri:      false,
+		fileElementId:  'proofOfAward',	
+		dataType:       'text',
+		data:           { type: 'PROOF_OF_AWARD' },
+		success: function(data)
+		{	
+			if ($(data).find('span.invalid').length == 0)
+			{
+				// i.e. if there are no uploading errors, which would be indicated by the presence of a SPAN.invalid tag.
+				$('#qualUploadedDocument').html(data);
+				$('#qualUploadedDocument').show();
+				$('#uploadFields').addClass('uploaded');
+				$('span[name="supportingDocumentSpan"] a.button-edit').attr({'id':'editQualiPOA','data-desc':'Edit Proof Of Award'});
+			}
+			else
+			{
+				$('#qualUploadedDocument').append(data);
+			}
+		}
+	});
+
+}

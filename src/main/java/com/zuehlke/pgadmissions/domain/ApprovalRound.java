9a41734eca15dafc3f3f55ef2495fe66feeac22e
dmaughan <dmaughan@localhost>
2012-07-03 14:30:43 +0100

    Removing old code.

---
+++ src/main/java/com/zuehlke/pgadmissions/domain/ApprovalRound.java
@@ -0,0 +1,87 @@
+package com.zuehlke.pgadmissions.domain;
+
+import java.util.ArrayList;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+@Entity(name = "APPROVAL_ROUND")
+@Access(AccessType.FIELD)
+public class ApprovalRound extends DomainObject<Integer> {
+
+
+	private static final long serialVersionUID = 1068777060574638531L;
+	
+	@OneToMany(cascade = { javax.persistence.CascadeType.PERSIST, javax.persistence.CascadeType.REMOVE })
+	@org.hibernate.annotations.Cascade({ org.hibernate.annotations.CascadeType.SAVE_UPDATE })
+	@JoinColumn(name = "approval_round_id")
+	private List<Supervisor> supervisors = new ArrayList<Supervisor>();	
+	
+	@ManyToOne
+	@JoinColumn(name = "application_form_id")	
+	private ApplicationForm application;	
+	
+	@Column(name = "created_date", insertable = false)
+	@Generated(GenerationTime.INSERT)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+	
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	@Id
+	@GeneratedValue
+	@Access(AccessType.PROPERTY)
+	public Integer getId() {
+		return id;
+	}
+
+
+	public ApplicationForm getApplication() {
+		return application;
+	}
+
+	public void setApplication(ApplicationForm application) {
+		this.application = application;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public List<Supervisor> getSupervisors() {
+		return supervisors;
+	}
+
+	public void setSupervisors(List<Supervisor> supervisors) {		
+		this.supervisors.clear();
+		for (Supervisor supervisor : supervisors) {
+			if(supervisor != null){
+				this.supervisors.add(supervisor);
+			}
+		}
+	}
+
+}
---
+++ src/main/java/com/zuehlke/pgadmissions/domain/enums/StudyOption.java
@@ -0,0 +1,37 @@
+package com.zuehlke.pgadmissions.domain.enums;
+
+public enum StudyOption {
+	
+	FULL_TIME("Full time"), FULL_TIME_DISTANCE("Full time by distance"), PART_TIME("Part time"), PART_TIME_DISTANCE("Part time by distance"), MODULAR("Modular/flexible study");
+
+	private final String displayValue;
+	private String freeVal;
+
+	private StudyOption(String displayValue) {
+		this.displayValue = displayValue;
+		this.freeVal = displayValue;
+	}
+
+	public String displayValue() {
+		return displayValue;
+	}
+	
+	public String getFreeVal() {
+		return freeVal;
+	}
+	
+	public void setFreeVal(String freeVal) {
+		this.freeVal = freeVal;
+	}
+	
+	public static StudyOption fromString(String text) {
+	    if (text != null) {
+	      for (StudyOption b : StudyOption.values()) {
+	        if (text.equalsIgnoreCase(b.displayValue)) {
+	          return b;
+	        }
+	      }
+	    }
+	    return null;
+	  }
+}

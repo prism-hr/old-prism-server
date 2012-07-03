1ff300a46b1a81ec55ded64b3e95f88c7998982c
kpa <kpa@localhost>
2012-07-03 15:51:25 +0100

    prevent adding yourself as referee

--- src/main/java/com/zuehlke/pgadmissions/validators/RefereeValidator.java
+++ src/main/java/com/zuehlke/pgadmissions/validators/RefereeValidator.java
@@ -1,17 +1,29 @@
 package com.zuehlke.pgadmissions.validators;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.zuehlke.pgadmissions.domain.Referee;
+import com.zuehlke.pgadmissions.services.UserService;
 
 @Service
 public class RefereeValidator extends FormSectionObjectValidator implements Validator {
 
 	private static final int MAXIMUM_ADDRESS_CHARS = 500;
+	private final UserService userService;
+
+	RefereeValidator(){
+		this(null);
+	}
+	@Autowired
+	public RefereeValidator(UserService userService) {
+		this.userService = userService;
+	
+	}
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -24,7 +36,11 @@ public class RefereeValidator extends FormSectionObjectValidator implements Vali
 		Referee referee = (Referee) target;
 		if (!EmailValidator.getInstance().isValid(referee.getEmail())) {
 			errors.rejectValue("email", "text.email.notvalid");
-		}	
+		}
+		if(userService.getCurrentUser().getEmail().equals(referee.getEmail())){
+			errors.rejectValue("email", "text.email.notyourself");
+		}
+		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "addressCountry", "dropdown.radio.select.none");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "addressLocation", "text.field.empty");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "jobEmployer", "text.field.empty");

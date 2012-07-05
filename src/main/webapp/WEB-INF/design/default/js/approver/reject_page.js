94a03f1d3a4938e18f17cd929a0155f6c9253d77
kpa <kpa@localhost>
2012-07-05 10:43:51 +0100

    account validation bugfix

---
+++ src/main/webapp/WEB-INF/design/default/js/approver/reject_page.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+	$('input:radio[name=rejectionReason]').click(function() {
+	
+		var postData ={ 
+				applicationId : $('#applicationId').val(),
+				rejectionReason : $(this).val()
+		};
+		
+		if( $('#includeProspectusLink:checked').val() !== undefined ){
+			postData.includeProspectusLink = true;
+		}else{
+			postData.includeProspectusLink = false;
+		}
+		
+		$.ajax({
+			type: 'POST',
+			 statusCode: {
+				  401: function() {
+					  window.location.reload();
+				  }
+			  },
+				url:"/pgadmissions/rejectApplication/rejectionText", 
+				data:$.param(postData),
+				success:function(data) {
+					$('#emailText').html(data);
+					addToolTips();
+				}
+		});
+	});
+	
+	$('#includeProspectusLink').click(function() {
+		
+	
+		var postData ={ 
+				applicationId : $('#applicationId').val()	
+		};
+		
+		if( $('input:radio[name=rejectionReason]:checked').val() !== undefined ){
+			postData.rejectionReason = $('input:radio[name=rejectionReason]:checked').val() ;
+		}
+		
+		if( $('#includeProspectusLink:checked').val() !== undefined ){
+			postData.includeProspectusLink = true;
+		}else{
+			postData.includeProspectusLink = false;
+		}
+		
+		$.ajax({
+				type: 'POST',
+				statusCode: {
+					  401: function() {
+						  window.location.reload();
+					  }
+				  },
+				url:"/pgadmissions/rejectApplication/rejectionText", 
+				data:$.param(postData),
+				success: function(data) {
+					$('#emailText').html(data);
+					addToolTips();
+				}
+		});
+	});
+	
+
+});
+

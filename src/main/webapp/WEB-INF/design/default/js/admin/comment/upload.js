140550fcfda6f90af417dec1519dc8050eb50dce
dmaughan <dmaughan@localhost>
2012-07-06 14:02:14 +0100

    Adding a reference uses a completely different JS file.

--- src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
+++ src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
@@ -19,6 +19,26 @@ $(document).ready(function(){
 
 	$('#commentUploadedDocument').on('click', 'span[name="supportingDocumentSpan"] a[name="delete"]', commentDocumentDelete);
 	
+	
+	$('#referenceSaveButton').click(function()
+	{
+		if (!validateReference())
+		{
+			return false;
+		}
+		
+		var message = 'Please confirm that you are satisfied with your comments. <b>You will not be able to change them.</b>';
+		var onOk    = function()
+		{
+			$('#documentUploadForm').submit();
+		};
+		var onCancel = function(){};
+		
+		modalPrompt(message, onOk, onCancel);
+		return false;
+	});
+	
+
 });
 
 function commentDocumentDelete()
@@ -90,4 +110,30 @@ function commentDocumentUpload()
 			}
 		}
 	);
+}
+
+function validateReference()
+{
+	var errors = 0;
+	$('#reviewForm span.invalid').remove();
+	
+	if ($('#comment').val() == '')
+	{
+		$('#comment').after('<span class="invalid">You must make an entry.</span>');
+		errors++;
+	}
+
+	if ($('input[name="suitableForUCL"]:checked').length == 0)
+	{
+		$('#field-issuitableucl').append('<span class="invalid">You must make a selection.</span>');
+		errors++;
+	}
+
+	if ($('input[name="suitableForProgramme"]:checked').length == 0)
+	{
+		$('#field-issuitableprog').append('<span class="invalid">You must make a selection.</span>');
+		errors++;
+	}
+	
+	return (errors == 0);
 }

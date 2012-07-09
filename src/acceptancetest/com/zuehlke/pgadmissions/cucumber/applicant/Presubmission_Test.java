715083dbe48f8696c039d8436a99cb2f3c94756e
dmaughan <dmaughan@localhost>
2012-07-09 12:46:59 +0100

    Updated.

---
+++ src/acceptancetest/com/zuehlke/pgadmissions/cucumber/applicant/Presubmission_Test.java
@@ -0,0 +1,12 @@
+package com.zuehlke.pgadmissions.cucumber.applicant;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+import cucumber.junit.Feature;
+
+@RunWith(Cucumber.class)
+@Feature("cucumber/applicant/presubmission.feature")
+public class Presubmission_Test {
+
+}

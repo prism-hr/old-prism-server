30b181974df0ead2ee5094d8b6748141889be6a7
dmaughan <dmaughan@localhost>
2012-07-09 11:52:48 +0100

    References buttons in one column.

---
+++ src/main/java/com/zuehlke/pgadmissions/utils/EncryptionUtils.java
@@ -0,0 +1,51 @@
+package com.zuehlke.pgadmissions.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncryptionUtils {
+
+	private final static Random RANDOM = new SecureRandom();
+	private final static int PASSWORD_LENGTH = 8;
+	private final static char[] LETTER_STORE = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',// 
+			'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',// 
+			'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',// 
+			'2', '3', '4', '5', '6', '7', '8', '9' };
+
+	public String getMD5Hash(String string) {
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update(string.getBytes());
+			byte byteData[] = md5.digest();
+			// convert bytes to hex chars
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String generateUUID() {
+		return UUID.randomUUID().toString();
+	}
+
+	public String generateUserPassword() {
+		RANDOM.setSeed(System.currentTimeMillis());
+
+		StringBuilder pw = new StringBuilder();
+		for (int i = 0; i < PASSWORD_LENGTH; i++) {
+			int letterIndex = (int) (RANDOM.nextDouble() * LETTER_STORE.length);
+			pw.append(LETTER_STORE[letterIndex]);
+		}
+		return pw.toString();
+	}
+}
---
+++ src/main/webapp/WEB-INF/freemarker/private/common/feedback.ftl
@@ -0,0 +1,2 @@
+<!-- Feedback button. -->
+<a id="feedbackButton" href="mailto:prism@ucl.ac.uk?subject=Feedback">Send Feedback</a>                      

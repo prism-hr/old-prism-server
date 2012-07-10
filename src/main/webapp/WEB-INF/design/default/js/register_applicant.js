626f456622bdade4412b86618fc772f70484a87a
kpa <kpa@localhost>
2012-07-10 11:46:12 +0100

    order users on user management pages

---
+++ src/main/webapp/WEB-INF/design/default/js/register_applicant.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+{
+  
+	// Clear form fields on focus
+	var el = $('input[type=text], input[type=password]');
+	var cssObjBefore = {'color': '#808080' }
+	var cssObjAfter = {'color': '#000' }
+	
+	el.each(function(){
+		$(this).css(cssObjBefore);
+	});
+	
+	el.focus(function(e) {
+		if (e.target.value == e.target.defaultValue)
+			e.target.value = '';
+		$(this).css(cssObjAfter);
+	});
+	el.blur(function(e) {
+		if (e.target.value == '')
+			e.target.value = e.target.defaultValue;
+	}); 
+
+
+	// Resend confirmation email button.
+	$('#resend').click(function()
+	{
+		var url = $(this).attr('href');
+		$('#site-message').append('<div class="ajax" />');
+		window.location.href = url;
+	});
+  
+});

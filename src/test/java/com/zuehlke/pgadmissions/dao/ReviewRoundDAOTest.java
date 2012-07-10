93391cdfdfaa8961f52c3458b4bb478da71de519
kpa <kpa@localhost>
2012-07-10 14:53:12 +0100

    bug fic

---
+++ src/test/java/com/zuehlke/pgadmissions/dao/ReviewRoundDAOTest.java
@@ -0,0 +1,70 @@
+package com.zuehlke.pgadmissions.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.zuehlke.pgadmissions.dao.mappings.AutomaticRollbackTestCase;
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.ReviewRound;
+import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ReviewRoundBuilder;
+import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
+import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
+
+public class ReviewRoundDAOTest extends AutomaticRollbackTestCase {
+
+
+	private ReviewRoundDAO dao;
+	private RegisteredUser applicant;
+	private Program program;
+
+	@Test
+	public void shouldSaveReviewRound() {
+		ApplicationForm application = new ApplicationFormBuilder().id(2).program(program).applicant(applicant).status(ApplicationFormStatus.VALIDATION).toApplicationForm();
+		save(application);
+		flushAndClearSession();
+		
+		ReviewRound reviewRound = new ReviewRoundBuilder().application(application).toReviewRound();
+		dao.save(reviewRound);
+		assertNotNull(reviewRound.getId());
+		flushAndClearSession();
+
+		ReviewRound returnedReviewRound = (ReviewRound) sessionFactory.getCurrentSession().get(ReviewRound.class,reviewRound.getId());
+		assertEquals(returnedReviewRound, reviewRound);
+		
+	}
+	
+	@Test
+	public void shouldGetReviewRounderById() {
+		ApplicationForm application = new ApplicationFormBuilder().id(1).program(program).applicant(applicant).status(ApplicationFormStatus.VALIDATION).toApplicationForm();
+		save(application);
+		flushAndClearSession();
+		
+		ReviewRound reviewRound = new ReviewRoundBuilder().toReviewRound();
+		dao.save(reviewRound);
+		assertNotNull(reviewRound.getId());
+		flushAndClearSession();
+		assertEquals(reviewRound, dao.getReviewRoundById(reviewRound.getId()));
+
+	}
+	
+	
+	@Before
+	public void setUp() {
+		super.setUp();
+		applicant = new RegisteredUserBuilder().firstName("Jane").lastName("Doe").email("email@test.com").username("username").password("password")
+				.accountNonExpired(false).accountNonLocked(false).credentialsNonExpired(false).enabled(false).toUser();
+		program = new ProgramBuilder().code("doesntexist").title("another title").toProgram();
+		
+		save(applicant, program, applicant);
+		
+		dao = new ReviewRoundDAO(sessionFactory);
+	}
+	
+}

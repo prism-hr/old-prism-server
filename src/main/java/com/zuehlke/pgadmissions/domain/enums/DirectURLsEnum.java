d9aa0d7b08adc05b51418851ba0e5aecd36d8b13
kpa <kpa@localhost>
2012-07-10 14:34:31 +0100

    add context to registrtion confirmation email

--- src/main/java/com/zuehlke/pgadmissions/domain/enums/DirectURLsEnum.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/enums/DirectURLsEnum.java
@@ -4,6 +4,7 @@ public enum DirectURLsEnum {
 	
 	ADD_REVIEW("/reviewFeedback?applicationId="), // 
 	VIEW_APPLIATION_PRIOR_TO_INTERVIEW("/application?view=view&applicationId="), //
+	VIEW_APPLIATION_AS_SUPERVISOR("/application?view=view&applicationId="), //
 	ADD_REFERENCE("/referee/addReferences?applicationId=")
 	;
 	//ToDo: add ADD_APPROVAL_FEEDBACK when implemented

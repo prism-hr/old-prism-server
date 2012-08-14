5192676bd740a303626a97e61e7663c280f03553
ked <ked@LONN0166.ads.zuehlke.com>
2012-08-14 13:58:25 +0100

    * Deploy a clean production instance: With the programme listing page removed.
    Made the ProgramController configurable so that it can be disabled in the
    production environment.

--- src/main/java/com/zuehlke/pgadmissions/controllers/ProgramController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ProgramController.java
@@ -1,12 +1,14 @@
 package com.zuehlke.pgadmissions.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.zuehlke.pgadmissions.dao.ProgramDAO;
+import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
 import com.zuehlke.pgadmissions.pagemodels.MainPageModel;
 
 @Controller
@@ -15,24 +17,36 @@ public class ProgramController {
 	
 	private static final String PROJECTS_VIEW_NAME = "private/pgStudents/programs/program_list_page";
 
+	private final ProgramDAO programDAO;
+
+	@Value("${programController.enabled}") 
+	private boolean enabled; 
+	
 	ProgramController() {
 		this(null);
 	}
 
-	private final ProgramDAO programDAO;
-
-	@Autowired
+    @Autowired
 	public ProgramController(ProgramDAO programDAO) {
 		this.programDAO = programDAO;
 	}
 	
-
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView getProgramsPage() {
-		
-		MainPageModel mainPageModel = new MainPageModel();
-		mainPageModel.setPrograms(programDAO.getAllPrograms());
-		return new ModelAndView(PROJECTS_VIEW_NAME, "model", mainPageModel);
+		if (enabled) {
+		    MainPageModel mainPageModel = new MainPageModel();
+		    mainPageModel.setPrograms(programDAO.getAllPrograms());
+		    return new ModelAndView(PROJECTS_VIEW_NAME, "model", mainPageModel);
+		} else {
+		    throw new ResourceNotFoundException();
+		}
 	}
-
+	
+	public boolean isEnabled() {
+	    return enabled;
+	}
+	
+	public void setEnabled(boolean enabled) {
+	    this.enabled = enabled;
+	}
 }

ca6a087077d6ba2213732d99e085dbd4590768be
ked <ked@LONN0166.ads.zuehlke.com>
2012-08-21 18:58:09 +0100

    * 7788 Enabled the JSR-303 Bean Validation API so that we can enforce validation rules on the domain objects through annotations. This facilitates the work needed for adding all the necessary validation rules for particular fields. The AbstractValidator allows the existing validators to live side by side with the new Bean Validation API. I've added a custom and configurable constraint validator.

---
+++ src/main/java/com/zuehlke/pgadmissions/validators/ESAPIConstraint.java
@@ -0,0 +1,29 @@
+package com.zuehlke.pgadmissions.validators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = ESAPIConstraintValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ESAPIConstraint {
+
+    boolean allowNull() default false;
+    
+    int maxLength() default 50;
+    
+    String rule() default "SafeString";
+    
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String message() default "{text.field.nonextendedascii}";
+}

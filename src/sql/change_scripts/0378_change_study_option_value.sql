d119ccf935731378af318f935f2d8ecc7d5300f1
iky <iky@LONN0170.ads.zuehlke.com>
2012-09-03 13:53:11 +0100

    minor change

--- src/sql/change_scripts/0378_change_study_option_value.sql
+++ src/sql/change_scripts/0378_change_study_option_value.sql
@@ -1,4 +1,4 @@
-delete from program_instance where study_option='full-time'
+delete from PROGRAM_INSTANCE where study_option='full-time'
 ;
 insert into PROGRAM_INSTANCE(deadline, study_option, academic_year, start_date, program_id) values('2020-08-24', 'FULL_TIME', '2020', '2011-08-24', (select id from PROGRAM where code = 'ABC'))
 ;

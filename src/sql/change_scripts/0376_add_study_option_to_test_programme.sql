b031c1981d1d3ac06ef6f3e239a0b1dbfe7684cb
iky <iky@LONN0170.ads.zuehlke.com>
2012-09-03 14:22:46 +0100

    revert study option for test programme

--- src/sql/change_scripts/0376_add_study_option_to_test_programme.sql
+++
@@ -1,2 +0,0 @@
-insert into PROGRAM_INSTANCE(deadline, study_option, academic_year, start_date, program_id) values('2020-08-24', 'FULL-TIME', '2020', '2011-08-24', (select id from PROGRAM where code = 'ABC'))
-;

1cd44dde1d4c1a2817b26c5316f74f721c8a3fc3
iky <iky@LONN0170.ads.zuehlke.com>
2012-09-03 11:59:54 +0100

    drop duplicated qualification type data

--- src/sql/change_scripts/0376_add_study_option_to_test_programme.sql
+++ src/sql/change_scripts/0376_add_study_option_to_test_programme.sql
@@ -1,2 +1,2 @@
-insert into PROGRAM_INSTANCE(program_id, deadline, sequence, study_option) values((select id from PROGRAM where code = 'ABC'), '2020-08-24',4,'FULL-TIME')
+insert into PROGRAM_INSTANCE(deadline, study_option, academic_year, start_date, program_id) values('2020-08-24', 'FULL-TIME', '2020', '2011-08-24', (select id from PROGRAM where code = 'ABC'))
 ;

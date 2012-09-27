07fc49f5d30494fcc8078701f6da5563c73af71f
ked <ked@LONN0166.ads.zuehlke.com>
2012-09-27 11:06:18 +0100

    * Fix for #8377 - File Uploader Not Working in Admin Scenarios in IE.

--- src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
+++ src/main/webapp/WEB-INF/design/default/js/admin/comment/upload.js
@@ -1,5 +1,91 @@
+function watchUploadComment($field)
+{
+	var $container = $field.parent('div.field');
+	
+	$("#commentUploadedDocument").on('click', '.button-delete', function() {
+		deleteUploadedFileComment($(this).attr("id"));
+		$(this).parent().parent().parent().remove();
+	});
+
+	$container.on('change', $field, function()
+	{
+		var input    = this.children[0];
+		$container.addClass('posting');
+		doUploadComment($(input));
+		$field.removeAttr("readonly");
+	});
+}
+
+// ------------------------------------------------------------------------------
+// Delete an uploaded file referenced by a hidden field.
+// ------------------------------------------------------------------------------
+function deleteUploadedFileComment(id) {
+	if (id) {
+		$.ajax({
+			type: 'POST',
+			statusCode: {
+				401: function() {
+					window.location.reload();
+				}
+			},
+			url: "/pgadmissions/delete/asyncdelete",
+			data: { documentId: id }
+		});
+	}
+}
+
+// ------------------------------------------------------------------------------
+// Upload a file from an INPUT field using AJAX.
+// ------------------------------------------------------------------------------
+function doUploadComment($upload_field)
+{	
+  var $container  = $upload_field.parent('div.field');
+  var $hidden     = $container.find('span input');
+  var $hfParent   = $hidden.parent();
+  var $progress   = $container.find('span.progress');
+
+	// Remove any previous error messages.
+	$container.find('span.invalid').remove();
+
+	$.ajaxFileUpload({
+    url:            '/pgadmissions/documents/async',
+    secureuri:      false,
+    fileElementId:  $upload_field.attr('id'),	
+    dataType:       'text',
+    data:           { type: $upload_field.attr('data-type') },
+    success: function (data)
+    {		
+			$container.removeClass('posting');
+			if ($(data).find('span.invalid').length > 0)
+			{
+				// There was an uploading error.
+				$container.append(data);
+			}
+			else if ($(data).find('input').length == 0)
+			{
+				// There was (probably) a server error.
+				$container.append('<span class="invalid">You must upload a PDF document (2Mb). </span>');
+			}
+			else  
+			{
+				$container.addClass('uploaded');
+				$("<div class=\"row\"><div class=\"field\">" + data + "</span></div>").appendTo($('#commentUploadedDocument'));
+				$('#commentUploadedDocument').show();
+				$('#commentDocument').show();
+				$('#commentDocument').val("");
+				$('.uploaded-file').show();
+			}
+    },
+	error: function()
+	{
+		$container.append('<span class="invalid">Upload failed; please retry.</span>');
+	}
+  });
+}
+
+
 $(document).ready(function() {
-	watchUpload($('#commentDocument'), null);
+	watchUploadComment($('#commentDocument'));
 	
 	$('#referenceSaveButton').click(function()
 	{
@@ -18,8 +104,6 @@ $(document).ready(function() {
 		modalPrompt(message, onOk, onCancel);
 		return false;
 	});
-	
-
 });
 
 function validateReference()
--- src/main/webapp/WEB-INF/freemarker/private/staff/admin/comment/documents_snippet.ftl
+++ src/main/webapp/WEB-INF/freemarker/private/staff/admin/comment/documents_snippet.ftl
@@ -3,25 +3,20 @@
 	<span class="hint" data-desc="<@spring.message 'validateApp.document'/>"></span>
 	<div class="field" id="uploadFields">
         <input id="commentDocument" class="full" data-type="COMMENT" data-reference="Comment" type="file" name="file" value="" />                   
-        <span id="commentUploadedDocument">
-            <input type="hidden" class="file" id="document_COMMENT" value=""/>
-            <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
-        </span>
+        <input type="hidden" class="file" id="document_COMMENT" value=""/>
+        <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
         <span id="commentDocumentProgress" class="progress" style="display: none;"></span>
-        
-        
-		<span id="commentUploadedDocument" class="uploaded-files">
-			<#if comment??>
-				<#list comment.documents as document>
-				<span class="uploaded-file" name="supportingDocumentSpan">
-					<input type="text" value="${encrypter.encrypt(document.id)}" name="documents"/>	
-					<a class="uploaded-filename" href="<@spring.url '/download?documentId=${encrypter.encrypt(document.id)}'/>" target="_blank">${document.fileName?html}</a>
-					<a name="delete" data-desc="Delete" class="button-delete button-hint" id="${encrypter.encrypt(document.id)}">delete</a>
-				</span>
-				</#list>
-			</#if>
-		</span>
-		
-        
 	</div>
 </div>
+	
+<span id="commentUploadedDocument" class="uploaded-files">
+    <#if comment??>
+        <#list comment.documents as document>
+            <span class="uploaded-file" name="supportingDocumentSpan">
+                <input type="text" value="${encrypter.encrypt(document.id)}" name="documents"/>	
+                <a class="uploaded-filename" href="<@spring.url '/download?documentId=${encrypter.encrypt(document.id)}'/>" target="_blank">${document.fileName?html}</a>
+                <a name="delete" data-desc="Delete" class="button-delete button-hint" id="${encrypter.encrypt(document.id)}">delete</a>
+            </span>
+        </#list>
+    </#if>
+</span>

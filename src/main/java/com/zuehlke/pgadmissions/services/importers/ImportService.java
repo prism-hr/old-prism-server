1cf48aaae4586fc296281387c54cef0a3666cf05
maciej charatonik <maciej.charatonik@smtsoftware.com>
2012-10-24 14:27:41 +0100

    minor algorithm change + code format

--- src/main/java/com/zuehlke/pgadmissions/services/importers/ImportService.java
+++ src/main/java/com/zuehlke/pgadmissions/services/importers/ImportService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.apache.xerces.impl.xpath.regex.REUtil;
 import org.springframework.stereotype.Service;
 
 import com.zuehlke.pgadmissions.domain.CodeObject;
@@ -12,68 +13,92 @@ import com.zuehlke.pgadmissions.domain.ImportedObject;
 import com.zuehlke.pgadmissions.referencedata.adapters.ImportData;
 
 @Service
-public class ImportService implements Comparator<CodeObject> {
+public class ImportService {
 
-	@Override
-	public int compare(CodeObject o1, CodeObject o2) {
-		return o1.getCode().compareTo(o2.getCode());
+	private static class ImportedObjectComparator implements Comparator<ImportedObject> {
+		@Override
+		public int compare(ImportedObject o1, ImportedObject o2) {
+			int compareResult = o1.getCode().compareTo(o2.getCode());
+			if (compareResult == 0) {
+				if (o1.getEnabled())
+					return -1;
+				else if (o2.getEnabled())
+					return 1;
+				else
+					return 0;
+			} else {
+				return compareResult;
+			}
+		}
+
 	}
-	
-	//Requires RandomAccess lists to run fast
+
+	private static class CodeComparator implements Comparator<CodeObject> {
+
+		@Override
+		public int compare(CodeObject o1, CodeObject o2) {
+			return o1.getCode().compareTo(o2.getCode());
+		}
+
+	}
+
+	private final ImportedObjectComparator importedObjectComparator = new ImportedObjectComparator();
+	private final CodeComparator codeComparator = new CodeComparator();
+
+	// Requires RandomAccess lists to run fast
 	@SuppressWarnings("unchecked")
-	public <T extends ImportedObject, U extends ImportData> List<T> merge(
-			List<T> currentData,
-			List<U> importData) {
+	public <T extends ImportedObject, U extends ImportData> List<T> merge(List<T> currentData, List<U> importData) {
 		List<T> changes = new ArrayList<T>();
-		Collections.sort(currentData, this);
-		Collections.sort(importData, this);
+		Collections.sort(currentData, importedObjectComparator);
+		Collections.sort(importData, codeComparator);
 		int i = 0, j = 0;
 		T currentElement;
 		U importElement;
-		while(i<currentData.size() && j<importData.size()) {
+		while (i < currentData.size() && j < importData.size()) {
 			currentElement = currentData.get(i);
 			importElement = importData.get(j);
-			int comparisionResult = compare(currentElement, importElement);
-			if(comparisionResult==0) {
-				if(!importElement.equalAttributes(currentElement)) {
+			int comparisionResult = codeComparator.compare(currentElement, importElement);
+			if (comparisionResult == 0) {
+				if (!importElement.equalAttributes(currentElement)) {
 					disableElement(currentElement, changes);
 					ImportedObject domainObject = importElement.createDomainObject();
 					changes.add((T) domainObject);
-				} else if(!currentElement.getEnabled()) {
+				} else if (!currentElement.getEnabled()) {
 					currentElement.setEnabled(true);
 					changes.add(currentElement);
 				}
-				i++;
+				do {
+					i++;
+				} while (i < currentData.size() && currentData.get(i).getCode().equals(currentElement.getCode()));
 				j++;
-			} else if(comparisionResult<0) {
+			} else if (comparisionResult < 0) {
 				disableElement(currentElement, changes);
 				i++;
-			} else if(comparisionResult>0) {
+			} else if (comparisionResult > 0) {
 				ImportedObject domainObject = importElement.createDomainObject();
 				changes.add((T) domainObject);
 				j++;
 			}
 		}
-		while(i<currentData.size()) {
+		while (i < currentData.size()) {
 			currentElement = currentData.get(i);
 			disableElement(currentElement, changes);
 			i++;
 		}
-		while(j<importData.size()) {
+		while (j < importData.size()) {
 			importElement = importData.get(j);
 			ImportedObject domainObject = importElement.createDomainObject();
 			changes.add((T) domainObject);
 			j++;
-		}	
+		}
 		return changes;
 	}
 
-	private <T extends ImportedObject> void disableElement(T currentElement,
-			List<T> changes) {
-		if(currentElement.getEnabled()) {			
+	private <T extends ImportedObject> void disableElement(T currentElement, List<T> changes) {
+		if (currentElement.getEnabled()) {
 			currentElement.setEnabled(false);
 			changes.add(currentElement);
 		}
 	}
-	
+
 }

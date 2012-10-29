70a925e560263f4ab1614c52c4c3abcdf5d41d8f
maciej charatonik <maciej.charatonik@smtsoftware.com>
2012-10-29 09:24:01 +0000

    import of xml reference data

--- src/main/java/com/zuehlke/pgadmissions/services/importers/ProgrammesImporter.java
+++ src/main/java/com/zuehlke/pgadmissions/services/importers/ProgrammesImporter.java
@@ -1,37 +1,71 @@
 package com.zuehlke.pgadmissions.services.importers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.zuehlke.pgadmissions.dao.ProgramInstanceDAO;
+import com.zuehlke.pgadmissions.domain.ProgramInstance;
 import com.zuehlke.pgadmissions.exceptions.XMLDataImportException;
+import com.zuehlke.pgadmissions.referencedata.adapters.PrismProgrammeAdapter;
 import com.zuehlke.pgadmissions.referencedata.jaxb.Programmes;
+import com.zuehlke.pgadmissions.referencedata.jaxb.Programmes.Programme;
 
 @Service
 public class ProgrammesImporter implements Importer {
 	
+	private static final Logger log = Logger.getLogger(ProgrammesImporter.class);
+	
 	private final JAXBContext context;
+	private final URL xmlFileLocation;
+	private final ProgramInstanceDAO programDAO;
+	private final ImportService importService;
 	
-	@Value("${xml.data.import.prismProgrammes.url}")
-	private URL xmlFileLocation;
-	
-	public ProgrammesImporter() throws JAXBException {
+	@Autowired
+	public ProgrammesImporter(ProgramInstanceDAO programDAO, ImportService importService,
+			@Value("${xml.data.import.prismProgrammes.url}") URL xmlFileLocation) throws JAXBException {
+		this.programDAO = programDAO;
+		this.importService = importService;
+		this.xmlFileLocation = xmlFileLocation;
 		context = JAXBContext.newInstance(Programmes.class);
 	}
 
 	@Override
+	@Transactional
 	public void importData() throws XMLDataImportException {
+		log.info("Starting the import from xml file: " + xmlFileLocation);
 		try {
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        Programmes programmes = (Programmes) unmarshaller.unmarshal(xmlFileLocation);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			Programmes programmes = (Programmes) unmarshaller.unmarshal(xmlFileLocation);
+			List<PrismProgrammeAdapter> importData = createAdapter(programmes);
+			List<ProgramInstance> currentData = programDAO.getAllProgramInstances();
+			List<ProgramInstance> changes = importService.merge(currentData, importData);
+			for (ProgramInstance programInstance : changes) {
+				programDAO.save(programInstance);
+			}
+			log.info("Import done. Wrote " + changes.size() + " change(s) to the database.");
 		} catch (Throwable e) {
 			throw new XMLDataImportException("Error during the import of file: " + xmlFileLocation, e);
 		}
 	}
+
+	private List<PrismProgrammeAdapter> createAdapter(Programmes programmes) {
+		List<PrismProgrammeAdapter> result = new ArrayList<PrismProgrammeAdapter>(programmes.getProgramme().size());
+		for (Programme programme : programmes.getProgramme()) {
+			result.add(new PrismProgrammeAdapter(programme));
+		}
+		return result;
+	}
+
 
 }
--- src/main/java/com/zuehlke/pgadmissions/services/importers/SourcesOfInterestImporter.java
+++ src/main/java/com/zuehlke/pgadmissions/services/importers/SourcesOfInterestImporter.java
@@ -1,37 +1,73 @@
 package com.zuehlke.pgadmissions.services.importers;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.zuehlke.pgadmissions.dao.SourcesOfInterestDAO;
 import com.zuehlke.pgadmissions.exceptions.XMLDataImportException;
+import com.zuehlke.pgadmissions.referencedata.adapters.SourceOfInterestAdapter;
 import com.zuehlke.pgadmissions.referencedata.jaxb.SourcesOfInterest;
+import com.zuehlke.pgadmissions.referencedata.jaxb.SourcesOfInterest.SourceOfInterest;
 
 @Service
 public class SourcesOfInterestImporter implements Importer {
+	
+	private static final Logger log = Logger.getLogger(SourcesOfInterestImporter.class);
 
 	private final JAXBContext context;
 
-	@Value("${xml.data.import.sourcesOfInterest.url}")
-	private URL xmlFileLocation;
+	private final URL xmlFileLocation;
 
-	public SourcesOfInterestImporter() throws JAXBException {
+	private final SourcesOfInterestDAO sourcesOfInterestDAO;
+
+	private final ImportService importService;
+
+	@Autowired
+	public SourcesOfInterestImporter(SourcesOfInterestDAO sourcesOfInterestDAO, ImportService importService,
+			@Value("${xml.data.import.sourcesOfInterest.url}") URL xmlFileLocation) throws JAXBException {
+		this.sourcesOfInterestDAO = sourcesOfInterestDAO;
+		this.importService = importService;
+		this.xmlFileLocation = xmlFileLocation;
 		context = JAXBContext.newInstance(SourcesOfInterest.class);
 	}
 
 	@Override
+	@Transactional
 	public void importData() throws XMLDataImportException {
+		log.info("Starting the import from xml file: " + xmlFileLocation);
 		try {
 			Unmarshaller unmarshaller = context.createUnmarshaller();
 			SourcesOfInterest sourcesOfInterest = (SourcesOfInterest) unmarshaller.unmarshal(xmlFileLocation);
+			List<SourceOfInterestAdapter> importData = createAdapter(sourcesOfInterest);
+			List<com.zuehlke.pgadmissions.domain.SourcesOfInterest> currentData = sourcesOfInterestDAO.getAllSourcesOfInterest();
+			List<com.zuehlke.pgadmissions.domain.SourcesOfInterest> changes = importService.merge(currentData, importData);
+			for (com.zuehlke.pgadmissions.domain.SourcesOfInterest sourceOfInterest : changes) {
+				sourcesOfInterestDAO.save(sourceOfInterest);
+			}
+			log.info("Import done. Wrote " + changes.size() + " change(s) to the database.");
 		} catch (Throwable e) {
 			throw new XMLDataImportException("Error during the import of file: " + xmlFileLocation, e);
 		}
 	}
+
+	private List<SourceOfInterestAdapter> createAdapter(SourcesOfInterest sourcesOfInterest) {
+		List<SourceOfInterestAdapter> result = new ArrayList<SourceOfInterestAdapter>(sourcesOfInterest.getSourceOfInterest().size());
+		for (SourceOfInterest sourceOfInterest : sourcesOfInterest.getSourceOfInterest()) {
+			result.add(new SourceOfInterestAdapter(sourceOfInterest));
+		}
+		return result;
+	}
+
 
 }

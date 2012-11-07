c4a3cb67e4765fcc880cc5d65fa64666c7a915b7
wzabowowski <wojciech.zaborowski@smtsoftware.com>
2012-11-07 19:23:35 +0000

    ucl export service - work in progress

--- src/main/java/com/zuehlke/pgadmissions/services/uclexport/Phase1Task.java
+++ src/main/java/com/zuehlke/pgadmissions/services/uclexport/Phase1Task.java
@@ -3,22 +3,27 @@ package com.zuehlke.pgadmissions.services.uclexport;
 import com.zuehlke.pgadmissions.admissionsservice.jaxb.AdmissionsApplicationResponse;
 import com.zuehlke.pgadmissions.admissionsservice.jaxb.ObjectFactory;
 import com.zuehlke.pgadmissions.admissionsservice.jaxb.SubmitAdmissionsApplicationRequest;
+import com.zuehlke.pgadmissions.dao.ApplicationFormTransferDAO;
+import com.zuehlke.pgadmissions.dao.ProgramInstanceDAO;
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
 import com.zuehlke.pgadmissions.domain.ApplicationFormTransfer;
 import com.zuehlke.pgadmissions.services.exporters.SubmitAdmissionsApplicationRequestBuilder;
+import org.springframework.ws.client.core.WebServiceTemplate;
 
 /**
  * Taks for phase 1 of application form transfer: this is:
  *   1. try to call PORTICO webserwice
  *   2. handle errors
  *   3. schedule phase 2 if there were no errors
- *
  */
 class Phase1Task implements Runnable {
     private Integer applicationId;
     private Long transferId;
     private TransferListener listener;
     private UclExportServiceImpl uclExportService;
+    private ApplicationFormTransferDAO applicationFormTransferDAO;
+    private ProgramInstanceDAO programInstanceDAO;
+    private WebServiceTemplate webServiceTemplate;
 
     Phase1Task(UclExportServiceImpl uclExportService, Integer applicationId, Long transferId, TransferListener listener) {
         this.uclExportService = uclExportService;
@@ -29,19 +34,21 @@ class Phase1Task implements Runnable {
 
     @Override
     public void run() {
+        //todo: refactor to calling back into UclExportService because of Spring dependency not available here
         listener.transferStarted();
 
         //retrieve AppliationForm and ApplicationFormTransfer instances from the db
-        ApplicationFormTransfer transfer = uclExportService.applicationFormTransferDAO.getById(transferId);
+        ApplicationFormTransfer transfer = applicationFormTransferDAO.getById(transferId);
         ApplicationForm applicationForm  = transfer.getApplicationForm();
 
         //try to call the webservice
         try  {
-            SubmitAdmissionsApplicationRequest request = new SubmitAdmissionsApplicationRequestBuilder(uclExportService.programInstanceDAO,new ObjectFactory())
+
+            SubmitAdmissionsApplicationRequest request = new SubmitAdmissionsApplicationRequestBuilder(programInstanceDAO,new ObjectFactory())
                 .applicationForm(applicationForm).toSubmitAdmissionsApplicationRequest();
-            AdmissionsApplicationResponse response = (AdmissionsApplicationResponse) uclExportService.webServiceTemplate.marshalSendAndReceive(request);
+            AdmissionsApplicationResponse response = (AdmissionsApplicationResponse) webServiceTemplate.marshalSendAndReceive(request);
         }  catch (Throwable e) {
-            //webservice call failed - we
+            //webservice call failed - just call
 
         }
 
@@ -49,12 +56,11 @@ class Phase1Task implements Runnable {
 
 
 
-
         //schedule phase 2
 
         //handle possible websersice call errors
-            //decide about handling strategy
-            //store the error information
+        //choosehandling strategy
+        //store the error information
 
     }
 }

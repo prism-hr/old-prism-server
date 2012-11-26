a04ac05d96075c790dbaccd8e35fce3c6470f1f8
ked <ked@LONN0166.ads.zuehlke.com>
2012-11-26 15:11:17 +0000

    * Partial fix for #8983 * Changes the link in the application_approval_reminder to point to the registration form if the user is not enabled. * Added a test case to verify the contents of the mail message.

---
+++ src/test/java/com/zuehlke/pgadmissions/mail/BaseEmailTestWithFreemarkerSupport.java
@@ -0,0 +1,34 @@
+package com.zuehlke.pgadmissions.mail;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.cache.FileTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.TemplateException;
+
+public abstract class BaseEmailTestWithFreemarkerSupport {
+
+    protected FakeLoggingMailSender fakeLoggingMailSender = new FakeLoggingMailSender();
+    
+    protected FreeMarkerConfigurer freeMarkerConfigurer;
+    
+    protected TemplateLoader templateLoader;
+
+    protected MimeMessagePreparatorFactory mimeMessagePreparatorFactory;
+
+    @Before
+    @SuppressWarnings("deprecation")
+    public void prepareFreemarkerConfig() throws IOException, TemplateException {
+            freeMarkerConfigurer = new FreeMarkerConfigurer();
+            templateLoader = new FileTemplateLoader(new File("src/main/webapp/WEB-INF/freemarker/"));
+            TemplateLoader[] templateLoaders = new TemplateLoader[] { templateLoader };
+            freeMarkerConfigurer.setTemplateLoaders(templateLoaders);
+            freeMarkerConfigurer.afterPropertiesSet();
+            mimeMessagePreparatorFactory = new MimeMessagePreparatorFactory(freeMarkerConfigurer, false);
+            fakeLoggingMailSender = new FakeLoggingMailSender();
+    }
+}

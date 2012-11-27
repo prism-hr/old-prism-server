837f5bddce8cc481332ae0c100d07966b53982bb
ked <ked@LONN0166.ads.zuehlke.com>
2012-11-27 16:57:19 +0000

    * Changed the validation regular expressions to be in sync with PORTICO. * Their regular expressions are not perfect either!

--- src/test/java/com/zuehlke/pgadmissions/validators/ESAPIRegularExpressionTest.java
+++ src/test/java/com/zuehlke/pgadmissions/validators/ESAPIRegularExpressionTest.java
@@ -14,9 +14,13 @@ public class ESAPIRegularExpressionTest {
             "mkyong.100@yahoo.com", "mkyong111@mkyong.com", "mkyong-100@mkyong.net", "mkyong.100@mkyong.com.au",
             "mkyong@1.com", "mkyong@gmail.com.com", "mkyong+100@gmail.com", "mkyong-100@yahoo-test.com" };
     
-    private String[] invalidEmailAddresses = new String[] { "mkyong", "mkyong@.com.my", "mkyong123@gmail.a",
-            "mkyong123@.com", "mkyong123@.com.com", ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
-            "mkyong..2002@gmail.com", "mkyong.@gmail.com", "mkyong@mkyong@gmail.com", "mkyong@gmail.com.1a" };
+//    private String[] invalidEmailAddresses = new String[] { "mkyong", "mkyong@.com.my", "mkyong123@gmail.a",
+//            "mkyong123@.com", "mkyong123@.com.com", ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
+//            "mkyong..2002@gmail.com", "mkyong.@gmail.com", "mkyong@mkyong@gmail.com", "mkyong@gmail.com.1a" };
+    
+    private String[] invalidEmailAddresses = new String[] { "mkyong", "mkyong@.com.my",
+            "mkyong123@.com", "mkyong123@.com.com", ".mkyong@mkyong.com", "mkyong()*@gmail.com",
+            "mkyong..2002@gmail.com", "mkyong.@gmail.com", "mkyong@mkyong@gmail.com"};
 
     @Before
     public void setUp() {
@@ -51,5 +55,15 @@ public class ESAPIRegularExpressionTest {
         for (String emailAddress : invalidEmailAddresses) {
             assertFalse(String.format("The email address [%s] should be invalid.", emailAddress), ESAPI.validator().isValidInput("Email", emailAddress, "Email", 255, false));
         }
+    }
+    
+    @Test
+    public void shouldAcceptPhoneNumber() {
+        assertTrue(ESAPI.validator().isValidInput("PhoneNumber", "+44 (0) 20 7911 5000", "PhoneNumber", 255, false));
+    }
+    
+    @Test
+    public void shouldRejectPhoneNumber() {
+        assertFalse(ESAPI.validator().isValidInput("PhoneNumber", "+44 (0)20-7911 5000  368664149 ", "PhoneNumber", 255, false));
     }
 }

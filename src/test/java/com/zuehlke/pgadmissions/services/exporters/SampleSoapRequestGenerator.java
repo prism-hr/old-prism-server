7469d0cfe6b5bcbd714b16b1665bdb5e78e0cfd4
ked <ked@LONN0166.ads.zuehlke.com>
2012-11-28 10:21:35 +0000

    * Updated the sample request generator for the web service test to actualy write the SOAP request to a file which then makes it easier to send the request in SOAP-UI.

--- src/test/java/com/zuehlke/pgadmissions/services/exporters/SampleSoapRequestGenerator.java
+++ src/test/java/com/zuehlke/pgadmissions/services/exporters/SampleSoapRequestGenerator.java
@@ -8,20 +8,21 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.Marshaller;
-import org.springframework.oxm.XmlMappingException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.WebServiceIOException;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.client.SoapFaultClientException;
 
@@ -38,10 +39,8 @@ import com.zuehlke.pgadmissions.utils.StacktraceDump;
 @ContextConfiguration("/testUclIntegrationContext.xml")
 public class SampleSoapRequestGenerator extends UclIntegrationBaseTest {
 
-    private static final Logger LOG = Logger.getLogger(SampleSoapRequestGenerator.class);
-    
     @Autowired
-    WebServiceTemplate webServiceTemplate;
+    private WebServiceTemplate webServiceTemplate;
     
     private ApplicationFormDAO applicationFormDAO;
     
@@ -53,9 +52,20 @@ public class SampleSoapRequestGenerator extends UclIntegrationBaseTest {
     
     private ApplicationForm validApplicationForm;
     
+    /**
+     * This test collects all the completed application forms (except test applications) from the database 
+     * and sends them to the configured web service. It enhances the data with 
+     * PassportInformation, LanguageQualifications if they do not exist. This 
+     * is because the current production data does not include such information 
+     * whereas the web service declares some of that information mandatory. Additionally,
+     * the application form statuses are changed randomly to be either APPROVED, REJECTED or 
+     * WITHDRAWN. 
+     * <p>
+     * Run this test when connected to the UCL network.
+     */
     @Test
     @Ignore
-    public void generateSampleSoapRequestsFromProductionData() throws XmlMappingException, IOException {
+    public void generateSampleSoapRequestsFromProductionData() {
         long idx = 0;
         Marshaller marshaller = webServiceTemplate.getMarshaller();
         
@@ -107,13 +117,19 @@ public class SampleSoapRequestGenerator extends UclIntegrationBaseTest {
                 // Generate request
                 SubmitAdmissionsApplicationRequest request = requestBuilder.applicationForm(form).build();
                 
-                // Save request to file
-                marshaller.marshal(request, new StreamResult(new File("request_" + ++idx + ".txt")));
+                final ByteArrayOutputStream requestMessageBuffer = new ByteArrayOutputStream(5000);
+
+                WebServiceMessageCallback webServiceMessageCallback = new WebServiceMessageCallback() {
+                    public void doWithMessage(WebServiceMessage webServiceMessage) throws IOException, TransformerException {
+                        webServiceMessage.writeTo(requestMessageBuffer);
+                    }
+                };
                 
                 // Send request and record response
                 try {
-                    LOG.info(String.format("Sending request [id=%s]", idx));
-                    AdmissionsApplicationResponse response = (AdmissionsApplicationResponse) webServiceTemplate.marshalSendAndReceive(request);
+                    AdmissionsApplicationResponse response = (AdmissionsApplicationResponse) webServiceTemplate.marshalSendAndReceive(request, webServiceMessageCallback);
+                    // Save request to file
+                    requestMessageBuffer.writeTo(new FileOutputStream(new File("request_" + ++idx + ".txt")));
                     marshaller.marshal(response, new StreamResult(new File("response_success_" + idx + ".txt")));
                 } catch (WebServiceIOException e) {
                     String forException = StacktraceDump.printRootCauseStackTrace(e);
@@ -128,7 +144,7 @@ public class SampleSoapRequestGenerator extends UclIntegrationBaseTest {
                     responseMessageBuffer.writeTo(new FileOutputStream(new File("response_error_" + idx + ".txt")));
                 }
             } catch (Throwable e) {
-                e.printStackTrace();
+                StacktraceDump.printRootCauseStackTrace(e);
             }
         }
     }

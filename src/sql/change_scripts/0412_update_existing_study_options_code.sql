7c277ae3cd7fdd83ab7f149511da917ab09b4ae3
ked <ked@LONN0166.ads.zuehlke.com>
2013-01-10 12:36:09 +0000

    * Fix for #9425

---
+++ src/sql/change_scripts/0412_update_existing_study_options_code.sql
@@ -0,0 +1,4 @@
+UPDATE pgadmissions.application_form_programme_detail SET STUDY_CODE = 'F+++++' WHERE STUDY_CODE = "1"
+;
+UPDATE pgadmissions.application_form_programme_detail SET STUDY_CODE = 'P+++++' WHERE STUDY_CODE = "31"
+;

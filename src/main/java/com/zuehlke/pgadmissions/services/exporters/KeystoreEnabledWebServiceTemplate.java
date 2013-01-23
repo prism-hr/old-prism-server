319bfc6f057ba545fcf088661ac97277b4162db4
ked <ked@LONN0166.ads.zuehlke.com>
2013-01-23 17:54:58 +0000

    * Added a bit of logging to the keystore enabled webservice template.

--- src/main/java/com/zuehlke/pgadmissions/services/exporters/KeystoreEnabledWebServiceTemplate.java
+++ src/main/java/com/zuehlke/pgadmissions/services/exporters/KeystoreEnabledWebServiceTemplate.java
@@ -1,11 +1,14 @@
 package com.zuehlke.pgadmissions.services.exporters;
 
+import org.apache.log4j.Logger;
 import org.springframework.core.io.Resource;
 import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 public class KeystoreEnabledWebServiceTemplate extends WebServiceTemplate {
 
+    private final Logger log = Logger.getLogger(KeystoreEnabledWebServiceTemplate.class);
+    
     private Resource keyStoreLocation;
 
     private String keyStorePassword;
@@ -20,8 +23,8 @@ public class KeystoreEnabledWebServiceTemplate extends WebServiceTemplate {
         try {
             System.getProperties().setProperty("javax.net.ssl.keyStore", keyStoreLocation.getFile().getAbsolutePath());
             System.getProperties().setProperty("javax.net.ssl.keyStorePassword", keyStorePassword);
-        } catch (Exception e) {
-            // do nothing
+        } catch (Throwable t) {
+            log.error("Could not set javax.net.ssl.keyStore, or javax.net.ssl.keyStorePassword", t);
         }
         return super.marshalSendAndReceive(uri, requestPayload, requestCallback);
     }

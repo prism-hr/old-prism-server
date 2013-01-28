5357f6642e452fceccd2acbd1939950cc6d69798
ked <ked@LONN0166.ads.zuehlke.com>
2013-01-28 09:39:38 +0000

    * Fix for #9575. Limiting the regular expression to not include the non-printable characters.

--- src/test/java/com/zuehlke/pgadmissions/validators/ESAPIRegularExpressionTest.java
+++ src/test/java/com/zuehlke/pgadmissions/validators/ESAPIRegularExpressionTest.java
@@ -14,10 +14,6 @@ public class ESAPIRegularExpressionTest {
             "mkyong.100@yahoo.com", "mkyong111@mkyong.com", "mkyong-100@mkyong.net", "mkyong.100@mkyong.com.au",
             "mkyong@1.com", "mkyong@gmail.com.com", "mkyong+100@gmail.com", "mkyong-100@yahoo-test.com" };
     
-//    private String[] invalidEmailAddresses = new String[] { "mkyong", "mkyong@.com.my", "mkyong123@gmail.a",
-//            "mkyong123@.com", "mkyong123@.com.com", ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
-//            "mkyong..2002@gmail.com", "mkyong.@gmail.com", "mkyong@mkyong@gmail.com", "mkyong@gmail.com.1a" };
-    
     private String[] invalidEmailAddresses = new String[] { "mkyong", "mkyong@.com.my",
             "mkyong123@.com", "mkyong123@.com.com", ".mkyong@mkyong.com", "mkyong()*@gmail.com",
             "mkyong..2002@gmail.com", "mkyong.@gmail.com", "mkyong@mkyong@gmail.com"};
@@ -41,6 +37,16 @@ public class ESAPIRegularExpressionTest {
         String russianName = StringEscapeUtils.unescapeJava("\\u0410\\u0444\\u0430\\u043d\\u0430\\u0441\\u0438\\u0439");
         assertFalse(ESAPI.validator().isValidInput("Input", chineseName, "ExtendedAscii", 30, false));
         assertFalse(ESAPI.validator().isValidInput("Input", russianName, "ExtendedAscii", 30, false));        
+    }
+    
+    @Test
+    public void shouldAcceptCommentWithNewlineAndTab() {
+        String comment = "" 
+                + "Lorem ipsum dolor sit amet, tempus pellentesque eget, nec risus curabitur, \n\t" 
+                + "arcu in mauris mattis, luctus tempor. Ipsum tincidunt, sit quam per vestibulum \n\t" 
+                + "vehicula morbi elit, nunc eu id praesent, velit elementum vel lacus ipsum.";
+        
+        assertFalse(ESAPI.validator().isValidInput("Input", comment, "ExtendedAscii", 250, false));
     }
     
     @Test

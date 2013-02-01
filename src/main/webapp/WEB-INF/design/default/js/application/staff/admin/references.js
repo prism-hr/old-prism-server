1d1b5212cdf5f35a59e9b6cdd62224bcec3d28c0
jfi <jfi@LONN0167.ads.zuehlke.com>
2013-02-01 15:02:09 +0000

    Applied fixes to Send To UCL admin form: "Save" reference button changed to "Add" "Save" button in reference section sends new reference data if user started typing

--- src/main/webapp/WEB-INF/design/default/js/application/staff/admin/references.js
+++ src/main/webapp/WEB-INF/design/default/js/application/staff/admin/references.js
@@ -53,11 +53,11 @@ $(document).ready(function() {
     });
 
     // --------------------------------------------------------------------------------
-    // POST REFERENCE DATA
+    // POST ADD REFERENCE DATA
     // --------------------------------------------------------------------------------
-    $('button[id="saveReferenceButton"]').each(function() {
+    $('button[id="addReferenceButton"]').each(function() {
     	$(this).on("click", function() {
-            postReferenceData();
+            postAddReferenceData();
         });
     });
     
@@ -126,6 +126,22 @@ function showProperRefereeEntry() {
 }
 
 function postRefereesData() {
+    var refereeId = $('#editedRefereeId').val();
+    
+    var suitableUCL = "";
+    if ($('input:radio[name=suitableForUCL_' + refereeId + ']:checked').length > 0) {
+        suitableUCL = $('input:radio[name=suitableForUCL_' + refereeId + ']:checked').val();
+    }
+
+    var suitableForProgramme = "";
+    if ($('input:radio[name=suitableForProgramme_' + refereeId + ']:checked').length > 0) {
+        suitableForProgramme = $('input:radio[name=suitableForProgramme_' + refereeId + ']:checked').val();
+    }
+    
+    var $ref_doc_upload_field = $('input:file[id=referenceDocument_' + refereeId + ']');
+    var $ref_doc_container  = $ref_doc_upload_field.parent('div.field');
+    var $ref_doc_hidden     = $ref_doc_container.find('span input');
+	
     var sendToPorticoData = {
             referees : new Array()
     };
@@ -151,10 +167,19 @@ function postRefereesData() {
         data :  {
             applicationId : $('#applicationId').val(),
             sendToPorticoData: JSON.stringify(sendToPorticoData),
+            comment: $('#refereeComment_' + refereeId).val(),
+            referenceDocument: $ref_doc_hidden.val(),
+            suitableForUCL : suitableUCL,
+            suitableForProgramme : suitableForProgramme, 
+            editedRefereeId : $('#editedRefereeId').val(),
             cacheBreaker: new Date().getTime()
         },
         success : function(data) {
-			$('#referee-H2').trigger('click');
+        	$("#referencesSection").html(data);
+        	$("#referee_" + $("#editedRefereeId").val()).show();
+        	if($('#anyReferenceErrors').val() == 'false'){
+        		$('#referee-H2').trigger('click');
+        	}
             
         },
         complete : function() {
@@ -163,7 +188,7 @@ function postRefereesData() {
     });
 }
 
-function postReferenceData() {
+function postAddReferenceData() {
     var refereeId = $('#editedRefereeId').val();
     
     var suitableUCL = "";

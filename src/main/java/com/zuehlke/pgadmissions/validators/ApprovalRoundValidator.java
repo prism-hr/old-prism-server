a86f8079b60afc9799b9dae3a707a24c151de6f2
jfi <jfi@LONN0167.ads.zuehlke.com>
2013-02-07 16:26:37 +0000

    Added word validation of project abstract (max. 200 words)

--- src/main/java/com/zuehlke/pgadmissions/validators/ApprovalRoundValidator.java
+++ src/main/java/com/zuehlke/pgadmissions/validators/ApprovalRoundValidator.java
@@ -3,6 +3,7 @@ package com.zuehlke.pgadmissions.validators;
 import java.util.Date;
 
 import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -12,6 +13,8 @@ import com.zuehlke.pgadmissions.domain.Supervisor;
 
 @Component
 public class ApprovalRoundValidator extends AbstractValidator {
+    
+    private static final int MAX_ABSTRACT_WORD_COUNT = 200;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -44,6 +47,14 @@ public class ApprovalRoundValidator extends AbstractValidator {
             ValidationUtils.rejectIfEmptyOrWhitespace(errors, "projectTitle", "text.field.empty");
             ValidationUtils.rejectIfEmptyOrWhitespace(errors, "projectAbstract", "text.field.empty");
         }
+        
+        String projectAbstract = approvalRound.getProjectAbstract();
+        if(projectAbstract != null){
+            int wordCount = countWords(projectAbstract);
+            if(wordCount > MAX_ABSTRACT_WORD_COUNT){
+                errors.rejectValue("projectAbstract", "text.field.maxwords", new Object[]{MAX_ABSTRACT_WORD_COUNT}, null);
+            }
+        }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "recommendedStartDate", "text.field.empty");
         Date startDate = approvalRound.getRecommendedStartDate();
@@ -59,5 +70,9 @@ public class ApprovalRoundValidator extends AbstractValidator {
         }
 
     }
-
+    
+    private int countWords(String text){
+        return StringUtils.split(text, "\t\n\r ").length;
+    }
+    
 }

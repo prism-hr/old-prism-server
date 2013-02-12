33db4b4bc94f63f0f4561e7530224f0b60b24a5c
ked <ked@LONN0166.ads.zuehlke.com>
2013-02-12 15:16:31 +0000

    * I think I didn't use the BooleanUtils API correctly

--- src/main/java/com/zuehlke/pgadmissions/validators/PersonalDetailsValidator.java
+++ src/main/java/com/zuehlke/pgadmissions/validators/PersonalDetailsValidator.java
@@ -79,7 +79,7 @@ public class PersonalDetailsValidator extends FormSectionObjectValidator impleme
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "englishFirstLanguage", "dropdown.radio.select.none");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "requiresVisa", "dropdown.radio.select.none");
 		
-		if (BooleanUtils.toBoolean(personalDetail.getPassportAvailable()) && BooleanUtils.isTrue(personalDetail.getRequiresVisa())) {
+		if (BooleanUtils.toBoolean(personalDetail.getPassportAvailable()) && BooleanUtils.toBoolean(personalDetail.getRequiresVisa())) {
 		    try {
 		        errors.pushNestedPath("passportInformation");
     	        ValidationUtils.invokeValidator(passportInformationValidator, personalDetail.getPassportInformation(), errors);

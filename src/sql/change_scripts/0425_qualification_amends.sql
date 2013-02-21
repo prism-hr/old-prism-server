4b79125b3a0cb929adb5cba46f68b052201acba9
jfi <jfi@LONN0167.ads.zuehlke.com>
2013-02-21 16:51:18 +0000

    Qualifications amends SQL script added

---
+++ src/sql/change_scripts/0425_qualification_amends.sql
@@ -0,0 +1,208 @@
+UPDATE application_form_qualification SET subject = 'Management Research', title = 'MSc (Mres)' WHERE id = 80
+;
+UPDATE application_form_qualification SET subject = 'Business Administration', title = 'MBA' WHERE id = 81
+;
+UPDATE application_form_qualification SET subject = 'Psychology and Communication', title = 'Bachelor of Arts' WHERE id = 82
+;
+UPDATE application_form_qualification SET subject = 'Management Research', title = 'MSc (Mres)' WHERE id = 83
+;
+UPDATE application_form_qualification SET subject = 'Business Administration', title = 'MBA' WHERE id = 84
+;
+UPDATE application_form_qualification SET subject = 'Psychology and Communication', title = 'Bachelor of Arts' WHERE id = 85
+;
+UPDATE application_form_qualification SET subject = 'Electronic and Electrical Engineering ', title = 'Bachelor of Engineering' WHERE id = 113
+;
+UPDATE application_form_qualification SET subject = 'Science and Engineering', title = 'University Preparatory Certificate' WHERE id = 114
+;
+UPDATE application_form_qualification SET subject = 'Mechatronics', title = 'Bachelor' WHERE id = 116
+;
+UPDATE application_form_qualification SET subject = 'LabVIEW Associated Developer', title = 'Certificate' WHERE id = 119
+;
+UPDATE application_form_qualification SET subject = 'Computer Graphics, Vision and Imaging', title = 'MSc' WHERE id = 157
+;
+UPDATE application_form_qualification SET subject = 'Advanced Materials', title = 'Postgraduate Diploma' WHERE id = 162
+;
+UPDATE application_form_qualification SET subject = 'Bioprocess Engineering', title = 'MSc' WHERE id = 167
+;
+UPDATE application_form_qualification SET subject = 'Microbiology', title = 'BSc' WHERE id = 168
+;
+UPDATE application_form_qualification SET subject = 'Bioprocess Engineering', title = 'MSc' WHERE id = 169
+;
+UPDATE application_form_qualification SET subject = 'Software Engineering', title = 'Associate Degree' WHERE id = 187
+;
+UPDATE application_form_qualification SET subject = 'Quantitative Finance', title = 'MSc' WHERE id = 188
+;
+UPDATE application_form_qualification SET subject = 'Economics', title = 'BSc' WHERE id = 189
+;
+UPDATE application_form_qualification SET subject = 'Advanced Materials', title = 'Postgraduate Diploma' WHERE id = 195
+;
+UPDATE application_form_qualification SET subject = 'Computer Networks', title = 'MSc' WHERE id = 202
+;
+UPDATE application_form_qualification SET subject = 'Nuclear Applications', title = 'European Master of Science' WHERE id = 204
+;
+UPDATE application_form_qualification SET subject = 'Nuclear Applications', title = 'European Master of Science' WHERE id = 205
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 206
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 208
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 210
+;
+UPDATE application_form_qualification SET subject = 'Innovation, Entrepreneurship and Business Development', title = 'Master' WHERE id = 211
+;
+UPDATE application_form_qualification SET subject = 'Business Administration', title = 'Master' WHERE id = 212
+;
+UPDATE application_form_qualification SET subject = 'Accountancy', title = 'Ordinary National Diploma' WHERE id = 214
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 231
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 234
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 236
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 238
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'BSc (Hons)' WHERE id = 259
+;
+UPDATE application_form_qualification SET subject = 'Electronic Science and Technology', title = 'BEng' WHERE id = 260
+;
+UPDATE application_form_qualification SET subject = 'COMPUTER SCIENCE', title = 'ADVANCED DIPLOMA' WHERE id = 277
+;
+UPDATE application_form_qualification SET subject = 'Economics (Major in Insurance)', title = 'Bachelor' WHERE id = 279
+;
+UPDATE application_form_qualification SET subject = 'English', title = 'Level 2 Certificate' WHERE id = 281
+;
+UPDATE application_form_qualification SET subject = 'English', title = 'Level 1 Certificate' WHERE id = 282
+;
+UPDATE application_form_qualification SET subject = 'Built environment', title = 'Advanced GNVQ' WHERE id = 297
+;
+UPDATE application_form_qualification SET subject = 'Applied Mathematics & Physics', title = 'Joint Honours' WHERE id = 304
+;
+UPDATE application_form_qualification SET subject = 'Thermal Power (Gas Turbine Technology)', title = 'MSc' WHERE id = 319
+;
+UPDATE application_form_qualification SET subject = 'Shipping Management', title = 'MSc' WHERE id = 320
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'MSc' WHERE id = 339
+;
+UPDATE application_form_qualification SET subject = 'Software Engineering', title = 'BSc' WHERE id = 341
+;
+UPDATE application_form_qualification SET subject = 'Chemical engineering', title = 'BSc' WHERE id = 342
+;
+UPDATE application_form_qualification SET subject = 'Quantitative Finance', title = 'Certificate (CQF)' WHERE id = 358
+;
+UPDATE application_form_qualification SET subject = 'Quantitative Finance', title = 'Certificate (CQF)' WHERE id = 360
+;
+UPDATE application_form_qualification SET subject = 'Criminology', title = 'BA (Hons)' WHERE id = 370
+;
+UPDATE application_form_qualification SET subject = 'Chemistry with Honours', title = 'MChem' WHERE id = 386
+;
+UPDATE application_form_qualification SET subject = 'International Trade & Economic Law', title = 'LL.M.' WHERE id = 416
+;
+UPDATE application_form_qualification SET subject = 'Economics', title = 'BA' WHERE id = 420
+;
+UPDATE application_form_qualification SET subject = 'Financial Mathematics', title = 'MSc' WHERE id = 421
+;
+UPDATE application_form_qualification SET subject = 'Stem cell and Regenerative Medicine', title = 'MSc' WHERE id = 455
+;
+UPDATE application_form_qualification SET subject = 'Stem Cell and Regenerative Medicine', title = 'MSc' WHERE id = 458
+;
+UPDATE application_form_qualification SET subject = 'Actuarial Sciences', title = 'Master Classes' WHERE id = 463
+;
+UPDATE application_form_qualification SET subject = 'Stem Cell and Regenerative Medicine', title = 'MSc' WHERE id = 466
+;
+UPDATE application_form_qualification SET subject = 'Radiation & Environmental Protection', title = 'MSc' WHERE id = 483
+;
+UPDATE application_form_qualification SET subject = 'Business Administration', title = 'MBA' WHERE id = 511
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'MSc (Mres)' WHERE id = 512
+;
+UPDATE application_form_qualification SET subject = 'Engineering', title = 'Bachelor' WHERE id = 554
+;
+UPDATE application_form_qualification SET subject = 'Biotechnology', title = 'BSc' WHERE id = 562
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'BSc' WHERE id = 564
+;
+UPDATE application_form_qualification SET subject = 'Programme and Project Management', title = 'MSc' WHERE id = 566
+;
+UPDATE application_form_qualification SET subject = 'English', title = 'GCSE' WHERE id = 571
+;
+UPDATE application_form_qualification SET subject = 'Civil Engineering by Research', title = 'MSc' WHERE id = 575
+;
+UPDATE application_form_qualification SET subject = 'Civil Engineering (Bridge Engineering)', title = 'BEng' WHERE id = 576
+;
+UPDATE application_form_qualification SET subject = 'Computer Systems Engineering', title = 'BEng' WHERE id = 595
+;
+UPDATE application_form_qualification SET subject = 'Electrical and Electronic Engineering', title = 'BEng (Hons)' WHERE id = 612
+;
+UPDATE application_form_qualification SET subject = 'IT, IR, maths', title = 'BSc' WHERE id = 628
+;
+UPDATE application_form_qualification SET subject = 'Information Security', title = 'MSc' WHERE id = 629
+;
+UPDATE application_form_qualification SET subject = 'Neuroscience', title = 'Master' WHERE id = 630
+;
+UPDATE application_form_qualification SET subject = 'Foundation Studies (Art and Design)', title = 'ABC Level 3 Diploma' WHERE id = 634
+;
+UPDATE application_form_qualification SET subject = 'Thermal Power Engineering', title = 'BEng' WHERE id = 668
+;
+UPDATE application_form_qualification SET subject = 'Thermal Physics Engineering', title = 'MSc' WHERE id = 669
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'Bachelor' WHERE id = 682
+;
+UPDATE application_form_qualification SET subject = 'Adaptive Architecture and Computation', title = 'MSc' WHERE id = 683
+;
+UPDATE application_form_qualification SET subject = 'Architecture', title = 'Diploma (RIBA part 2)' WHERE id = 685
+;
+UPDATE application_form_qualification SET subject = 'Architecture', title = 'BA (Hons)' WHERE id = 686
+;
+UPDATE application_form_qualification SET subject = 'Foundation Studies in Art & Design', title = 'BTEC Diploma' WHERE id = 687
+;
+UPDATE application_form_qualification SET subject = 'Marketing and Strategy', title = 'MBA' WHERE id = 691
+;
+UPDATE application_form_qualification SET subject = 'Interpretation and Translation', title = 'BA' WHERE id = 699
+;
+UPDATE application_form_qualification SET subject = 'International Economics and Trade', title = 'Bachelor of Economics' WHERE id = 700
+;
+UPDATE application_form_qualification SET subject = 'Computer Science & Engineering', title = 'Bachelor of Technology' WHERE id = 706
+;
+UPDATE application_form_qualification SET subject = 'Cyber Security & Management', title = 'MSc' WHERE id = 707
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'BSc (Hons)' WHERE id = 708
+;
+UPDATE application_form_qualification SET subject = 'Computer Science', title = 'BSc (Hons)' WHERE id = 709
+;
+UPDATE application_form_qualification SET subject = 'Mathematics', title = 'Master' WHERE id = 710
+;
+UPDATE application_form_qualification SET subject = 'Mathematics', title = 'Msci' WHERE id = 711
+;
+UPDATE application_form_qualification SET subject = 'English Language', title = 'GCSE' WHERE id = 722
+;
+UPDATE application_form_qualification SET subject = 'English Literature', title = 'GCSE' WHERE id = 723
+;
+UPDATE application_form_qualification SET subject = 'Mathematics', title = 'A Level' WHERE id = 724
+;
+UPDATE application_form_qualification SET subject = 'Cisco Network Associate', title = 'Certificate' WHERE id = 727
+;
+UPDATE application_form_qualification SET subject = 'Photonic and Optical Engineering', title = 'MSc' WHERE id = 741
+;
+UPDATE application_form_qualification SET subject = 'Photonic Systems Development', title = 'Mres' WHERE id = 742
+;
+UPDATE application_form_qualification SET subject = 'Computer Engineering', title = 'BEng' WHERE id = 761
+;
+UPDATE application_form_qualification SET subject = 'Advanced Software Engineering', title = 'MSc' WHERE id = 762
+;
+UPDATE application_form_qualification SET subject = 'Advanced Computer Science', title = 'MSc' WHERE id = 773
+;
+UPDATE application_form_qualification SET subject = 'English Language and Study skills', title = 'Course Y, Z, A, B, C and D' WHERE id = 775
+;
+UPDATE application_form_qualification SET subject = 'Education', title = 'General Diploma' WHERE id = 776
+;
+UPDATE application_form_qualification SET subject = 'Law', title = 'Bachelor of Law' WHERE id = 785
+;
+UPDATE application_form_qualification SET subject = 'Finance (Second Degree)', title = 'Bachelor of Economics' WHERE id = 786
+;
+UPDATE application_form_qualification SET subject = 'Chemistry', title = 'A Level' WHERE id = 794
+;
+UPDATE application_form_qualification SET subject = 'Physics', title = 'A Level' WHERE id = 797
+;
+UPDATE application_form_qualification SET subject = 'Informatik', title = 'Diplom (FH)' WHERE id = 798
+;

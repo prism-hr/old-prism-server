fc851e8212d5a1bced68cf0b0ae9c41a3e1bda6f
jfi <jfi@zuhlke.com>
2013-03-20 11:21:46 +0000

    Fixed checking if email is null in registration page

--- src/test/java/com/zuehlke/pgadmissions/validators/RegisterFormValidatorTest.java
+++ src/test/java/com/zuehlke/pgadmissions/validators/RegisterFormValidatorTest.java
@@ -24,208 +24,220 @@ public class RegisterFormValidatorTest {
 
     @Autowired
     private Validator validator;
-    
-	private RegisterFormValidator recordValidator;
-    
-	private UserService userServiceMock;
-    
-	private RegisteredUser user;
-	
-	@Test
-	public void shouldSupportApplicantRecordValidator() {
-		assertTrue(recordValidator.supports(RegisteredUser.class));
-	}
 
-	@Before
-	public void setup() {
-		user = new RegisteredUserBuilder().id(4).username("email").firstName("Hans-Peter").lastName("Mueller").email("meuston@gmail.com").confirmPassword("12345678").password("12345678").build();
-		userServiceMock = EasyMock.createMock(UserService.class);
-		recordValidator = new RegisterFormValidator(userServiceMock);
-		recordValidator.setValidator((javax.validation.Validator) validator);
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfFirstNameIsEmpty() {
-		user.setFirstName(null);
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "firstName");
+    private RegisterFormValidator recordValidator;
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("text.field.empty", mappingResult.getFieldError("firstName").getCode());
-	}
+    private UserService userServiceMock;
 
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfLasttNameIsEmpty() {
-		user.setLastName(null);
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "lastName");
+    private RegisteredUser user;
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("text.field.empty", mappingResult.getFieldError("lastName").getCode());
-	}
+    @Test
+    public void shouldSupportApplicantRecordValidator() {
+        assertTrue(recordValidator.supports(RegisteredUser.class));
+    }
 
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfEmailNotValidEmail() {
-		user.setEmail("nonvalidemail");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
+    @Before
+    public void setup() {
+        user = new RegisteredUserBuilder().id(4).username("email").firstName("Hans-Peter").lastName("Mueller").email("meuston@gmail.com")
+                .confirmPassword("12345678").password("12345678").build();
+        userServiceMock = EasyMock.createMock(UserService.class);
+        recordValidator = new RegisterFormValidator(userServiceMock);
+        recordValidator.setValidator((javax.validation.Validator) validator);
+    }
 
-		EasyMock.expect(userServiceMock.getUserByEmail("nonvalidemail")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("You must enter a valid email address.", mappingResult.getFieldError("email").getDefaultMessage());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfNoConfirmPassword() {
-		user.setConfirmPassword(null);
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfFirstNameIsEmpty() {
+        user.setFirstName(null);
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "firstName");
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("text.field.empty", mappingResult.getFieldError("confirmPassword").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfPasswordsDoNotMatch() {
-		user.setConfirmPassword("12345");
-		user.setPassword("12345678");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("text.field.empty", mappingResult.getFieldError("firstName").getCode());
+    }
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("user.passwords.notmatch", mappingResult.getFieldError("confirmPassword").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfPasswordLessThan8Chars() {
-		user.setPassword("12");
-		user.setConfirmPassword("12");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfLasttNameIsEmpty() {
+        user.setLastName(null);
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "lastName");
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("user.password.small", mappingResult.getFieldError("password").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfPasswordMoreThan15Chars() {
-		user.setPassword("1234567891234567");
-		user.setConfirmPassword("1234567891234567");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("text.field.empty", mappingResult.getFieldError("lastName").getCode());
+    }
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("user.password.large", mappingResult.getFieldError("password").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldNotRejectIfContainsSpecialChars() {
-		user.setPassword(" 12o*-lala");
-		user.setConfirmPassword(" 12o*-lala");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfEmailNotValidEmail() {
+        user.setEmail("nonvalidemail");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(0, mappingResult.getErrorCount());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldAcceptPasswordWithOnlyChars() {
-		user.setPassword("oooooooooo");
-		user.setConfirmPassword("oooooooooo");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+        EasyMock.expect(userServiceMock.getUserByEmail("nonvalidemail")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("You must enter a valid email address.", mappingResult.getFieldError("email").getDefaultMessage());
+    }
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(0, mappingResult.getErrorCount());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldAcceptPasswordWithOnlyNumbersAndLettes() {
-		user.setPassword("ooo12ooo3oo1");
-		user.setConfirmPassword("ooo12ooo3oo1");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfNoConfirmPassword() {
+        user.setConfirmPassword(null);
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
 
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(0, mappingResult.getErrorCount());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldNotRejectIfPasswordsMatch() {
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
-	
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(0, mappingResult.getErrorCount());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfEmailAlreadyExistAndUserIsNewuser() {
-		user.setId(null);
-		user.setEmail("meuston@gmail.com");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
-		
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(new RegisteredUserBuilder().id(5).build());
-		EasyMock.replay(userServiceMock);		
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("user.email.alreadyexists", mappingResult.getFieldError("email").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldRejectIfEmailAlreadyExistAndUserIsExistingUserButNotUserWithEmail() {
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("text.field.empty", mappingResult.getFieldError("confirmPassword").getCode());
+    }
 
-		user.setEmail("meuston@gmail.com");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
-		
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(new RegisteredUserBuilder().id(5).build());
-		EasyMock.replay(userServiceMock);		
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(1, mappingResult.getErrorCount());
-		Assert.assertEquals("user.email.alreadyexists", mappingResult.getFieldError("email").getCode());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void shouldNotRejectIfEmailAlreadyExistAndUserIsExistingUserAndIsUserWithEmail() {
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfPasswordsDoNotMatch() {
+        user.setConfirmPassword("12345");
+        user.setPassword("12345678");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
 
-		user.setEmail("meuston@gmail.com");
-		DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
-		
-		EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
-		EasyMock.replay(userServiceMock);		
-		recordValidator.validate(user, mappingResult);
-		Assert.assertEquals(0, mappingResult.getErrorCount());
-	}
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("user.passwords.notmatch", mappingResult.getFieldError("confirmPassword").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfPasswordLessThan8Chars() {
+        user.setPassword("12");
+        user.setConfirmPassword("12");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("user.password.small", mappingResult.getFieldError("password").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfPasswordMoreThan15Chars() {
+        user.setPassword("1234567891234567");
+        user.setConfirmPassword("1234567891234567");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("user.password.large", mappingResult.getFieldError("password").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldNotRejectIfContainsSpecialChars() {
+        user.setPassword(" 12o*-lala");
+        user.setConfirmPassword(" 12o*-lala");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(0, mappingResult.getErrorCount());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldAcceptPasswordWithOnlyChars() {
+        user.setPassword("oooooooooo");
+        user.setConfirmPassword("oooooooooo");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(0, mappingResult.getErrorCount());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldAcceptPasswordWithOnlyNumbersAndLettes() {
+        user.setPassword("ooo12ooo3oo1");
+        user.setConfirmPassword("ooo12ooo3oo1");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "password");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(0, mappingResult.getErrorCount());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldNotRejectIfPasswordsMatch() {
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "confirmPassword");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(0, mappingResult.getErrorCount());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfEmailEmpty() {
+        user.setEmail(null);
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
+
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("text.field.empty", mappingResult.getFieldError("email").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfEmailAlreadyExistAndUserIsNewuser() {
+        user.setId(null);
+        user.setEmail("meuston@gmail.com");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(new RegisteredUserBuilder().id(5).build());
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("user.email.alreadyexists", mappingResult.getFieldError("email").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldRejectIfEmailAlreadyExistAndUserIsExistingUserButNotUserWithEmail() {
+
+        user.setEmail("meuston@gmail.com");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(new RegisteredUserBuilder().id(5).build());
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(1, mappingResult.getErrorCount());
+        Assert.assertEquals("user.email.alreadyexists", mappingResult.getFieldError("email").getCode());
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldNotRejectIfEmailAlreadyExistAndUserIsExistingUserAndIsUserWithEmail() {
+
+        user.setEmail("meuston@gmail.com");
+        DirectFieldBindingResult mappingResult = new DirectFieldBindingResult(user, "email");
+
+        EasyMock.expect(userServiceMock.getUserByEmail("meuston@gmail.com")).andReturn(user);
+        EasyMock.replay(userServiceMock);
+        recordValidator.validate(user, mappingResult);
+        Assert.assertEquals(0, mappingResult.getErrorCount());
+    }
 }

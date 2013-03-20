edb6f581e27be0aebcfd1701def676c66070e515
ked <ked@LONN0166.ads.zuehlke.com>
2013-03-20 17:59:45 +0000

    * Fixing "error: cannot find symbol" because we've removed the log4j stuff from the normal code and replaced it with SLF4J.

--- src/main/java/com/zuehlke/pgadmissions/controllers/PrintController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/PrintController.java
@@ -9,7 +9,8 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestBindingException;
@@ -30,7 +31,7 @@ import com.zuehlke.pgadmissions.services.UserService;
 @RequestMapping("/print")
 public class PrintController {
 
-    private Logger log = Logger.getLogger(PrintController.class);
+    private final Logger log = LoggerFactory.getLogger(PdfModelBuilder.class);
     
 	private final ApplicationsService applicationSevice;
 	

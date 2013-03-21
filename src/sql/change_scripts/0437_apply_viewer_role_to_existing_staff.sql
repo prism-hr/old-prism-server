5fdeff6fd02614adfa63c475ceb752a035d12a31
jfi <jfi@zuhlke.com>
2013-03-21 11:43:33 +0000

    Updated viewer role script to reflect changes in USER_ROLE_LINK table as well

--- src/sql/change_scripts/0437_apply_viewer_role_to_existing_staff.sql
+++ src/sql/change_scripts/0437_apply_viewer_role_to_existing_staff.sql
@@ -1,3 +1,4 @@
+-- Modify program viewer link (viewer privilleges for all non-applicant users)
 INSERT INTO PROGRAM_VIEWER_LINK 
 
 SELECT interviewer_id AS viewer_id, program_id
@@ -21,5 +22,32 @@ FROM PROGRAM_APPROVER_LINK
 UNION DISTINCT
 
 SELECT administrator_id AS viewer_id, program_id
+FROM PROGRAM_ADMINISTRATOR_LINK
+;
+
+
+-- Modify user role link (viewer privilleges for all non-applicant users)
+INSERT INTO USER_ROLE_LINK
+SELECT interviewer_id AS viewer_id, 9
+FROM PROGRAM_INTERVIEWER_LINK
+
+UNION DISTINCT
+
+SELECT reviewer_id AS viewer_id, 9
+FROM PROGRAM_REVIEWER_LINK
+
+UNION DISTINCT
+
+SELECT supervisor_id AS viewer_id, 9
+FROM PROGRAM_SUPERVISOR_LINK
+
+UNION DISTINCT
+
+SELECT registered_user_id AS viewer_id, 9
+FROM PROGRAM_APPROVER_LINK
+
+UNION DISTINCT
+
+SELECT administrator_id AS viewer_id, 9
 FROM PROGRAM_ADMINISTRATOR_LINK
 ;

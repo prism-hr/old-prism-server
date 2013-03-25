d9d312b0f9c9a2587857a25e514a9b97e539971b
cls <cls@localhost>
2013-03-25 10:57:46 +0000

    * PRISM-41 Throttling web service transfer (work in progress) Added unit tests for throttle service

--- src/test/java/com/zuehlke/pgadmissions/controllers/ConfigurationControllerTest.java
+++ src/test/java/com/zuehlke/pgadmissions/controllers/ConfigurationControllerTest.java
@@ -375,7 +375,7 @@ public class ConfigurationControllerTest {
 	@Test
 	public void shouldUpdateThrottle() {
 		throttleserviceMock.updateThrottleWithNewValues(false, "15");
-		EasyMock.expect(throttleserviceMock.hasSwitchedFromFalseToTrue(false)).andReturn(false);
+		EasyMock.expect(throttleserviceMock.userTurnedOnThrottle(false)).andReturn(false);
 		replay(throttleserviceMock);
 		
 		Map<String, String> result = controller.updateThrottle(15, false, "15");
@@ -389,7 +389,7 @@ public class ConfigurationControllerTest {
 		String errorMessage = "The throttling batch size must be a number";
 		throttleserviceMock.updateThrottleWithNewValues(false, "15khg");
 		EasyMock.expectLastCall().andThrow(new NumberFormatException());
-		EasyMock.expect(throttleserviceMock.hasSwitchedFromFalseToTrue(false)).andReturn(false);
+		EasyMock.expect(throttleserviceMock.userTurnedOnThrottle(false)).andReturn(false);
 		replay(throttleserviceMock);
 		
 		Map<String, String> result = controller.updateThrottle(15, false, "15khg");
@@ -402,7 +402,7 @@ public class ConfigurationControllerTest {
 	@Test
 	public void shouldTriggerSendingApplicationsToPorticoIfTheSwitchHasBeenSetToTrue() {
 	    throttleserviceMock.updateThrottleWithNewValues(true, "15");
-	    EasyMock.expect(throttleserviceMock.hasSwitchedFromFalseToTrue(true)).andReturn(true);
+	    EasyMock.expect(throttleserviceMock.userTurnedOnThrottle(true)).andReturn(true);
 	    queueServiceMock.sendQueuedApprovedApplicationsToPortico();
 	    
 	    replay(throttleserviceMock, queueServiceMock);

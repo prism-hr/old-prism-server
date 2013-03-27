c71d98016d162bf90186ac54f9df79211093fbd2
jfi <jfi@zuhlke.com>
2013-03-27 18:42:28 +0000

    Added tests for previous commits

--- src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationListController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationListController.java
@@ -30,25 +30,30 @@ import com.zuehlke.pgadmissions.services.UserService;
 @RequestMapping(value = { "", "applications" })
 public class ApplicationListController {
 
-	private static final String APPLICATION_LIST_PAGE_VIEW_NAME = "private/my_applications_page";
-	private static final String APPLICATION_LIST_SECTION_VIEW_NAME = "private/my_applications_section";
-	
-	private final ApplicationsService applicationsService;
-	
-	private final UserService userService;
-	
-	private final ApplicationsFiltersPropertyEditor filtersPropertyEditor;
+    private static final String APPLICATION_LIST_PAGE_VIEW_NAME = "private/my_applications_page";
+    private static final String APPLICATION_LIST_SECTION_VIEW_NAME = "private/my_applications_section";
 
-	ApplicationListController() {
-		this(null, null, null);
-	}
+    private final ApplicationsService applicationsService;
 
-	@Autowired
-	public ApplicationListController(ApplicationsService applicationsService, UserService userService, ApplicationsFiltersPropertyEditor filtersPropertyEditor) {
-		this.applicationsService = applicationsService;
-		this.userService = userService;
-		this.filtersPropertyEditor = filtersPropertyEditor;
-	}
+    private final UserService userService;
+
+    private final ApplicationsFiltersPropertyEditor filtersPropertyEditor;
+
+    ApplicationListController() {
+        this(null, null, null);
+    }
+
+    @Autowired
+    public ApplicationListController(ApplicationsService applicationsService, UserService userService, ApplicationsFiltersPropertyEditor filtersPropertyEditor) {
+        this.applicationsService = applicationsService;
+        this.userService = userService;
+        this.filtersPropertyEditor = filtersPropertyEditor;
+    }
+
+    @InitBinder(value = "applicationSearchDTO")
+    public void registerPropertyEditors(WebDataBinder binder) {
+        binder.registerCustomEditor(List.class, "filters", filtersPropertyEditor);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public String getApplicationListPage(Model model, HttpSession session) {
@@ -62,74 +67,58 @@ public class ApplicationListController {
         model.addAttribute("filters", applicationsFilters);
         return APPLICATION_LIST_PAGE_VIEW_NAME;
     }
-    
-    @InitBinder(value = "applicationSeachDTO")
-    public void registerPropertyEditors(WebDataBinder binder) {
-        binder.registerCustomEditor(List.class, "filters", filtersPropertyEditor);
+
+    @RequestMapping(value = "/section", method = RequestMethod.GET)
+    public String getApplicationListSection(@ModelAttribute("applicationSearchDTO") ApplicationSearchDTO dto, Model model) {
+        List<ApplicationForm> applications = applicationsService.getAllVisibleAndMatchedApplications(getUser(), dto.getFilters(), dto.getSortCategory(),
+                dto.getOrder(), dto.getBlockCount());
+        model.addAttribute("applications", applications);
+        return APPLICATION_LIST_SECTION_VIEW_NAME;
     }
-    
-	@RequestMapping(value = "/section", method = RequestMethod.GET)
-	public String getApplicationListSection(@ModelAttribute("applicationSeachDTO") ApplicationSearchDTO dto, 
-			Model model) {
-	    
-		List<ApplicationForm> applications = getApplications(dto.getFilters(), dto.getSortCategory(), dto.getOrder(),
-				dto.getBlockCount());
-		model.addAttribute("applications", applications);
-		return APPLICATION_LIST_SECTION_VIEW_NAME;
-	}
-	
-	@RequestMapping(value = "/saveFilters", method = RequestMethod.POST)
-	@ResponseBody
-	public String saveFiltersAsDefault(@ModelAttribute("applicationSeachDTO") ApplicationSearchDTO dto){
-	    RegisteredUser user = getUser();
+
+    @RequestMapping(value = "/saveFilters", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveFiltersAsDefault(@ModelAttribute("applicationSearchDTO") ApplicationSearchDTO dto) {
+        RegisteredUser user = getUser();
         userService.clearApplicationsFilters(user);
-	    for(ApplicationsFilter filter : dto.getFilters()){
-	        userService.addFilter(user, filter);
-	    }
-	    return "OK";
-	}
+        for (ApplicationsFilter filter : dto.getFilters()) {
+            userService.addFilter(user, filter);
+        }
+        return "OK";
+    }
 
-	public List<ApplicationForm> getApplications(List<ApplicationsFilter> filters,
-			SortCategory sortCategory, SortOrder sortOrder, Integer blockCount) {
-		return applicationsService.getAllVisibleAndMatchedApplications(getUser(), filters, sortCategory, sortOrder, blockCount);
-	}
+    @ModelAttribute("user")
+    public RegisteredUser getUser() {
+        return userService.getCurrentUser();
+    }
 
-	public List<ApplicationForm> getApplications(List<ApplicationsFilter> filters) {
-		return getApplications(filters, null, null, null);
-	}
+    @ModelAttribute("searchCategories")
+    public SearchCategory[] getSearchCategories() {
+        return SearchCategory.values();
+    }
 
-	@ModelAttribute("user")
-	public RegisteredUser getUser() {
-		return userService.getCurrentUser();
-	}
+    @ModelAttribute("applications")
+    public List<ApplicationForm> getApplications() {
+        return java.util.Collections.emptyList();
+    }
 
-	@ModelAttribute("searchCategories")
-	public SearchCategory[] getSearchCategories() {
-		return SearchCategory.values();
-	}
+    @ModelAttribute("message")
+    public String getMessage(@RequestParam(required = false) boolean submissionSuccess, @RequestParam(required = false) String decision,
+            @RequestParam(required = false) String message) {
+        if (submissionSuccess) {
+            return "Your application has been successfully submitted.";
+        }
+        if (decision != null) {
+            return "The application was successfully " + decision + ".";
+        }
+        if (message != null) {
+            return message;
+        }
+        return null;
+    }
 
-	@ModelAttribute("applications")
-	public List<ApplicationForm> getApplications() {
-		return java.util.Collections.emptyList();
-	}
-
-	@ModelAttribute("message")
-	public String getMessage(@RequestParam(required = false) boolean submissionSuccess,
-			@RequestParam(required = false) String decision, @RequestParam(required = false) String message) {
-		if (submissionSuccess) {
-			return "Your application has been successfully submitted.";
-		}
-		if (decision != null) {
-			return "The application was successfully " + decision + ".";
-		}
-		if (message != null) {
-			return message;
-		}
-		return null;
-	}
-
-	@ModelAttribute("messageApplication")
-	public ApplicationForm getApplicationForm(@RequestParam(required = false) String application) {
-		return applicationsService.getApplicationByApplicationNumber(application);
-	}
+    @ModelAttribute("messageApplication")
+    public ApplicationForm getApplicationForm(@RequestParam(required = false) String application) {
+        return applicationsService.getApplicationByApplicationNumber(application);
+    }
 }

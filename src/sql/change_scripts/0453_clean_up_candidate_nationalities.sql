84f9299ada9439b594a0c4381f5fd06d5140ef0e
ked <ked@LONN0166.ads.zuehlke.com>
2013-04-17 16:31:48 +0100

    * Fixing the SQL script. Table names need to be upper case.

--- src/sql/change_scripts/0453_clean_up_candidate_nationalities.sql
+++ src/sql/change_scripts/0453_clean_up_candidate_nationalities.sql
@@ -1,6 +1,6 @@
-ALTER TABLE application_form_personal_detail ADD COLUMN first_nationality INT(10) DEFAULT -1 not null 
+ALTER TABLE APPLICATION_FORM_PERSONAL_DETAIL ADD COLUMN first_nationality INT(10) DEFAULT -1 not null 
 ;
-ALTER TABLE application_form_personal_detail ADD COLUMN second_nationality INT(10) DEFAULT NULL 
+ALTER TABLE APPLICATION_FORM_PERSONAL_DETAIL ADD COLUMN second_nationality INT(10) DEFAULT NULL 
 ;
 CREATE PROCEDURE clean_up_nationalities()
 BEGIN
@@ -9,7 +9,7 @@ BEGIN
 	DECLARE nation INT(10);
 	DECLARE previous_personal_detail_id INT(10) DEFAULT -1;	
 	-- sort the results, making sure that entry from the same candidate are next to each other
-	DECLARE link_table_cursor CURSOR FOR SELECT candidate_personal_details_id,candidate_language_id FROM candidate_nationality_link order by candidate_personal_details_id;
+	DECLARE link_table_cursor CURSOR FOR SELECT candidate_personal_details_id,candidate_language_id FROM CANDIDATE_NATIONALITY_LINK order by candidate_personal_details_id;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	OPEN link_table_cursor;
 	process_loop: LOOP 
@@ -20,10 +20,10 @@ BEGIN
 		END IF;		
 		-- process for the fisrt time, update first_nationality field
 		IF personal_detail_id!=previous_personal_detail_id THEN 
-			UPDATE application_form_personal_detail SET first_nationality =nation WHERE id=personal_detail_id;
+			UPDATE APPLICATION_FORM_PERSONAL_DETAIL SET first_nationality =nation WHERE id=personal_detail_id;
 		-- process for the second time, update second_nationality field
 		ELSE
-			UPDATE application_form_personal_detail SET second_nationality=nation WHERE id=personal_detail_id;
+			UPDATE APPLICATION_FORM_PERSONAL_DETAIL SET second_nationality=nation WHERE id=personal_detail_id;
 		END IF;
 		SET previous_personal_detail_id=personal_detail_id;
 	END LOOP;

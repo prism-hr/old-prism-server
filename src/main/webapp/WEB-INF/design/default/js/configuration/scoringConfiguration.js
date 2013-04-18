9c18851125b4c74b4e0cd87808b1565148079d9c
jfi <jfi@zuhlke.com>
2013-04-18 16:51:50 +0100

    PRISM-233 First version of storing score definitions: schema update, POJOs, services, controller and view

---
+++ src/main/webapp/WEB-INF/design/default/js/configuration/scoringConfiguration.js
@@ -0,0 +1,59 @@
+var errorCodes= {
+		401 : function() { window.location.reload(); },
+		500 : function() { window.location.href = "/pgadmissions/error"; },
+		404 : function() { window.location.href = "/pgadmissions/404"; },
+		400 : function() { window.location.href = "/pgadmissions/400"; },
+		403 : function() { window.location.href = "/pgadmissions/404"; }
+		};
+
+$(document).ready(function() {
+	$(document).on('change', 'select#programSelect', function() {
+		updateScoringDefinition();
+	});
+	
+	$(document).on('change', 'select#stageSelect', function() {
+		updateScoringDefinition();
+	});
+    
+    $('#save-scoring').click(function() {
+    	$('div.content-box-inner').css({position : 'relative'}).append('<div class="ajax" />');
+    	$.ajax({
+	        type : 'POST',
+		    statusCode : errorCodes,
+		    url : "/pgadmissions/configuration/editScoringDefinition",
+		    data: {
+		    	programCode : $('#programSelect').val(),
+		    	scoringStage : $('#stageSelect').val(),
+		    	scoringContent : $('#scoringConfigurationContent').val()
+	    	},
+		    success : function(data) {
+	   		},
+	   		complete : function() {
+	   			$('div.ajax').remove();
+			}
+    	});
+    });
+    
+});
+
+function updateScoringDefinition(){
+	if($('#programSelect').val() == ""){
+		$('#scoringConfigurationContent').val("");
+	} else {
+		$.ajax({
+	        type : 'GET',
+		    statusCode : errorCodes,
+		    url : "/pgadmissions/configuration/getScoringDefinition",
+		    data: {
+		    	programCode : $('#programSelect').val(),
+		    	scoringStage : $('#stageSelect').val()
+	    	},
+		    success : function(data) {
+		    	$('#scoringConfigurationContent').val(data);
+	   		},
+	   		complete : function() {
+	   			$('div.ajax').remove();
+			}
+    	});
+	}
+}

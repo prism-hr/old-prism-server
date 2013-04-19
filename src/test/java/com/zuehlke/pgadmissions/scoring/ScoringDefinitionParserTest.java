d161c90a1a44ff658c1d4add2874fd8c8aaec32e
wlu <wlu@localhost>
2013-04-19 16:39:47 +0100

    Reverted to the customized exception.

--- src/test/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParserTest.java
+++ src/test/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParserTest.java
@@ -5,8 +5,6 @@ import static junit.framework.Assert.assertEquals;
 import java.io.File;
 import java.util.List;
 
-import javax.xml.bind.JAXBException;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,50 +16,56 @@ import com.zuehlke.pgadmissions.scoring.jaxb.Question;
 
 public class ScoringDefinitionParserTest {
 
-    private ScoringDefinitionParser parser;
+	private ScoringDefinitionParser parser;
 
-    @Test
-    public void shouldValidateSingleText() throws Exception {
-        File f = new File("src/test/resources/scoring/simpleText.xml");
-        String xmlContent = Files.toString(f, Charsets.UTF_8);
+	@Test
+	public void shouldValidateSingleText() throws Exception {
+		File f = new File("src/test/resources/scoring/simpleText.xml");
+		String xmlContent = Files.toString(f, Charsets.UTF_8);
 
-        CustomQuestions scoreDefinition = parser.parseScoringDefinition(xmlContent);
+		CustomQuestions scoreDefinition = parser
+				.parseScoringDefinition(xmlContent);
 
-        List<Question> questions = scoreDefinition.getQuestion();
-        assertEquals(1, questions.size());
-    }
+		List<Question> questions = scoreDefinition.getQuestion();
+		assertEquals(1, questions.size());
+	}
 
-    @Test
-    public void shouldInvalidateSingleTextWithoutLabel() throws Exception {
-        File f = new File("src/test/resources/scoring/simpleTextWithoutLabel.xml");
-        String xmlContent = Files.toString(f, Charsets.UTF_8);
+	@Test
+	public void shouldInvalidateSingleTextWithoutLabel() throws Exception {
+		File f = new File(
+				"src/test/resources/scoring/simpleTextWithoutLabel.xml");
+		String xmlContent = Files.toString(f, Charsets.UTF_8);
 
-        try {
-            parser.parseScoringDefinition(xmlContent);
-            Assert.fail();
-        } catch (JAXBException e) {
-            Assert.assertEquals("cvc-complex-type.2.4.a: Invalid content was found starting with element 'required'. One of '{label}' is expected.", e
-                    .getLinkedException().getLocalizedMessage());
-        }
-    }
+		try {
+			parser.parseScoringDefinition(xmlContent);
+			Assert.fail();
+		} catch (ScoringDefinitionParseException e) {
+			Assert.assertEquals(
+					"cvc-complex-type.2.4.a: Invalid content was found starting with element 'required'. One of '{label}' is expected.",
+					e.getLocalizedMessage());
+		}
+	}
 
-    @Test
-    public void shouldInvalidateSingleTextWithoutRequiredField() throws Exception {
-        File f = new File("src/test/resources/scoring/simpleTextWithoutRequiredField.xml");
-        String xmlContent = Files.toString(f, Charsets.UTF_8);
+	@Test
+	public void shouldInvalidateSingleTextWithoutRequiredField()
+			throws Exception {
+		File f = new File(
+				"src/test/resources/scoring/simpleTextWithoutRequiredField.xml");
+		String xmlContent = Files.toString(f, Charsets.UTF_8);
 
-        try {
-            parser.parseScoringDefinition(xmlContent);
-            Assert.fail();
-        } catch (JAXBException e) {
-            Assert.assertEquals("cvc-complex-type.2.4.b: The content of element 'question' is not complete. One of '{required}' is expected.", e
-                    .getLinkedException().getLocalizedMessage());
-        }
-    }
+		try {
+			parser.parseScoringDefinition(xmlContent);
+			Assert.fail();
+		} catch (ScoringDefinitionParseException e) {
+			Assert.assertEquals(
+					"cvc-complex-type.2.4.b: The content of element 'question' is not complete. One of '{required}' is expected.",
+					e.getLocalizedMessage());
+		}
+	}
 
-    @Before
-    public void setUp() throws Exception {
-        parser = new ScoringDefinitionParser();
-    }
+	@Before
+	public void setUp() throws Exception {
+		parser = new ScoringDefinitionParser();
+	}
 
 }

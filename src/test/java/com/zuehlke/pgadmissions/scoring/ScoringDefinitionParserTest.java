e9250cdecbb3c2ccb225638c8bbdc6a4bd3d1e57
wlu <wlu@localhost>
2013-04-22 13:02:14 +0100

    PRISM-233 Added validation for different types of questions.

--- src/test/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParserTest.java
+++ src/test/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParserTest.java
@@ -13,6 +13,7 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import com.zuehlke.pgadmissions.scoring.jaxb.CustomQuestions;
 import com.zuehlke.pgadmissions.scoring.jaxb.Question;
+import com.zuehlke.pgadmissions.scoring.jaxb.QuestionType;
 
 public class ScoringDefinitionParserTest {
 
@@ -59,6 +60,95 @@ public class ScoringDefinitionParserTest {
 		} catch (ScoringDefinitionParseException e) {
 			Assert.assertEquals(
 					"cvc-complex-type.2.4.b: The content of element 'question' is not complete. One of '{required}' is expected.",
+					e.getLocalizedMessage());
+		}
+	}
+
+	@Test
+	public void shouldInvalidateMalformattedMinDate() {
+		CustomQuestions customQuestions = new CustomQuestions();
+		List<Question> questions = customQuestions.getQuestion();
+		Question question = new Question();
+		String minDateForTest = "2013-004-22";
+		question.setType(QuestionType.DATE);
+		question.setMinDate(minDateForTest);
+		questions.add(question);
+
+		try {
+			parser.validateScoringDefinition(customQuestions);
+			Assert.fail();
+		} catch (ScoringDefinitionParseException e) {
+			Assert.assertEquals("Mulformated date " + minDateForTest
+					+ ". Expected format is yyyy-mm-dd, or \"TODAY\"",
+					e.getLocalizedMessage());
+		}
+	}
+
+	@Test
+	public void shouldValidateMaxDateWithCorrectFormat()
+			throws ScoringDefinitionParseException {
+		CustomQuestions customQuestions = new CustomQuestions();
+		List<Question> questions = customQuestions.getQuestion();
+		Question question = new Question();
+		question.setType(QuestionType.DATE);
+		String maxDateForTest = "2013-04-22";
+		question.setMaxDate(maxDateForTest);
+		questions.add(question);
+
+		parser.validateScoringDefinition(customQuestions);
+	}
+
+	@Test
+	public void shouldValidateMaxDateWithTheValueOfTODAY()
+			throws ScoringDefinitionParseException {
+		CustomQuestions customQuestions = new CustomQuestions();
+		List<Question> questions = customQuestions.getQuestion();
+		Question question = new Question();
+		question.setType(QuestionType.DATE);
+		String maxDateForTest = "TODAY";
+		question.setMaxDate(maxDateForTest);
+		questions.add(question);
+
+		parser.validateScoringDefinition(customQuestions);
+	}
+
+	@Test
+	public void shouldRequireBothMinDateAndMaxDateForDateRangeField() {
+		CustomQuestions customQuestions = new CustomQuestions();
+		List<Question> questions = customQuestions.getQuestion();
+		Question question = new Question();
+		String label = "testQuestion";
+		question.setLabel(label);
+		question.setType(QuestionType.DATE_RANGE);
+		question.setMinDate("TODAY");
+		questions.add(question);
+
+		try {
+			parser.validateScoringDefinition(customQuestions);
+			Assert.fail();
+		} catch (ScoringDefinitionParseException e) {
+			Assert.assertEquals(
+					"Both minDate and maxDate elements are required for dateRange type "
+							+ label, e.getLocalizedMessage());
+		}
+	}
+
+	@Test
+	public void shouldRequireOptionsForDropdown() {
+		CustomQuestions customQuestions = new CustomQuestions();
+		List<Question> questions = customQuestions.getQuestion();
+		Question question = new Question();
+		String label = "testDropDown";
+		question.setLabel(label);
+		question.setType(QuestionType.DROPDOWN);
+		questions.add(question);
+
+		try {
+			parser.validateScoringDefinition(customQuestions);
+			Assert.fail();
+		} catch (ScoringDefinitionParseException e) {
+			Assert.assertEquals(
+					"Options element is required for dropdown type " + label,
 					e.getLocalizedMessage());
 		}
 	}

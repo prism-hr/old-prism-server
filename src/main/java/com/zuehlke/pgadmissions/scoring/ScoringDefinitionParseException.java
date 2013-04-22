5fc535c6688030560d13856518bda0c6770c9efa
wlu <wlu@localhost>
2013-04-22 13:02:14 +0100

    PRISM-233 Added validation for different types of questions.

--- src/main/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParseException.java
+++ src/main/java/com/zuehlke/pgadmissions/scoring/ScoringDefinitionParseException.java
@@ -4,6 +4,10 @@ public class ScoringDefinitionParseException extends Exception {
 
     private static final long serialVersionUID = -3439108158204403306L;
 
+    public ScoringDefinitionParseException(String message){
+    	super(message);
+    }
+    
     public ScoringDefinitionParseException(String message, Throwable cause) {
         super(message, cause);
     }

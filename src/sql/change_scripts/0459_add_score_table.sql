d58c2cfc6836d0749fc3409449f6f5f4c0b63b3e
jfi <jfi@zuhlke.com>
2013-04-22 10:37:51 +0100

    PRISM-233 Clean-up in change script files, added score table change script

---
+++ src/sql/change_scripts/0459_add_score_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE SCORE (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  comment_id INTEGER UNSIGNED,
+  score_position INTEGER UNSIGNED,
+  question_type VARCHAR(15) NOT NULL,
+  question VARCHAR(200) NOT NULL,
+  text_response VARCHAR(200),
+  date_response TIMESTAMP,
+  second_date_response TIMESTAMP,
+  rating_response INTEGER UNSIGNED,
+
+  CONSTRAINT comment_fk FOREIGN KEY (comment_id) REFERENCES COMMENT(id),
+  PRIMARY KEY(id)
+) ENGINE = InnoDB
+;

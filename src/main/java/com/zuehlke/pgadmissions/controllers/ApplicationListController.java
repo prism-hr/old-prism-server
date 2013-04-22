7948a4e90cd88f2682469212d897befc967a0631
info <info@bomboca.co.uk>
2013-04-22 12:59:14 +0100

    PRISM-66 Application pop-out summary (Action to return JSON object and request to that endpoint to get the data - some accuracy information still pending)

--- src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationListController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/ApplicationListController.java
@@ -1,14 +1,21 @@
 package com.zuehlke.pgadmissions.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.print.DocFlavor.STRING;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.bouncycastle.cert.ocsp.Req;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,12 +31,22 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.visualization.datasource.DataSourceHelper;
 import com.google.visualization.datasource.DataSourceRequest;
 import com.google.visualization.datasource.base.DataSourceException;
 import com.google.visualization.datasource.datatable.DataTable;
+import com.ibm.icu.text.DateFormat;
+import com.ibm.icu.text.SimpleDateFormat;
+import com.ibm.icu.util.Calendar;
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
 import com.zuehlke.pgadmissions.domain.ApplicationsFilter;
+import com.zuehlke.pgadmissions.domain.Document;
+import com.zuehlke.pgadmissions.domain.EmploymentPosition;
+import com.zuehlke.pgadmissions.domain.Funding;
+import com.zuehlke.pgadmissions.domain.PersonalDetails;
+import com.zuehlke.pgadmissions.domain.Qualification;
+import com.zuehlke.pgadmissions.domain.ReferenceComment;
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
 import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
 import com.zuehlke.pgadmissions.domain.enums.SearchCategory;
@@ -88,7 +105,7 @@ public class ApplicationListController {
         if (applicationSearchDTO.getFilters() == null || reloadFilters) {
             // filters not initialized in session or filter reload requested
             RegisteredUser user = getUser();
-            if(!user.isStoredFilters()){
+            if (!user.isStoredFilters()) {
                 applicationsFilters = getActiveApplicationFilters();
             } else {
                 applicationsFilters = user.getApplicationsFilters();
@@ -138,6 +155,103 @@ public class ApplicationListController {
     public String saveFiltersAsDefault(@ModelAttribute("applicationSearchDTO") ApplicationSearchDTO dto) {
         userService.setFilters(getUser(), dto.getFilters());
         return "OK";
+    }
+
+    @RequestMapping(value = "/getApplicationDetails", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> getApplicationDetails(@RequestParam String applicationId){
+        ApplicationForm application = applicationsService.getApplicationByApplicationNumber(applicationId);
+        
+        Map<String , String> result = Maps.newHashMap();
+        Map<String, String> applicantResult = Maps.newHashMap();
+        RegisteredUser applicant = application.getApplicant();
+        PersonalDetails personalDetails = application.getPersonalDetails();
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        
+        result.put("applicationSubmissionDate",  dateFormat.format(application.getSubmittedDate()));
+        result.put("applicationUpdateDate", dateFormat.format(application.getLastUpdated()));
+        result.put("numberOfActiveApplications", "0");
+        
+        applicantResult.put("title", personalDetails.getTitle().getDisplayValue());
+        applicantResult.put("name", applicant.getDisplayName());
+        applicantResult.put("phoneNumber", personalDetails.getPhoneNumber());
+        applicantResult.put("email", applicant.getEmail());
+        
+        List<Qualification> qualifications = application.getQualifications();
+        
+        if (qualifications.size() > 0) {
+            Collections.sort(qualifications, new Comparator<Qualification>() {
+                public int compare(Qualification q1, Qualification q2) {
+                    return q1.getQualificationAwardDate().compareTo(q2.getQualificationAwardDate());
+                }
+            });
+            
+            Qualification mostRecentQualification = qualifications.get(0);
+            applicantResult.put("mostRecentQualification", mostRecentQualification.getQualificationTitle() + " in " + mostRecentQualification.getQualificationSubject());
+        }
+        else {
+            applicantResult.put("mostRecentQualification", "None provided");
+        }
+        
+        List<EmploymentPosition> employments = application.getEmploymentPositions();
+        
+        if (employments.size() > 0) {
+            Collections.sort(employments, new Comparator<EmploymentPosition>() {
+               public int compare(EmploymentPosition e1, EmploymentPosition e2) {
+                   
+                   Date e1Date = e1.getEndDate();
+                   Date e2Date = e2.getEndDate();
+                   
+                   if (e1Date == null) {
+                       return -1;
+                   }
+                   else if (e2Date == null) {
+                       return 1;
+                   }
+                   
+                   return e1Date.compareTo(e2Date);
+               }
+            });
+            
+            
+            applicantResult.put("mostRecentEmployment", employments.get(0).getEmployerName());
+        }
+        else {
+            applicantResult.put("mostRecentEmployment", "None provided");
+        }
+        
+        List<Funding> fundings = application.getFundings();
+        List<String> fundingsResult = new ArrayList<String>();
+        
+        if (fundings.size() > 0) {
+            for (int i = 0; i < fundings.size(); i++) {
+                fundingsResult.add(fundings.get(i).getDescription());
+            }
+        }
+        else {
+            fundingsResult.add("None provided");
+        }
+        
+        applicantResult.put("fundingRequirements", gson.toJson(fundingsResult));
+        
+        // What is the criteria to select fundings? All?
+        
+        List<ReferenceComment> references = application.getReferencesToSendToPortico();
+        result.put("numberOfReferences", Integer.toString(references.size()));
+        
+        // No of References responded? application.getReferencesToSendToPortico()?
+        Document personalStatement = application.getPersonalStatement();
+        
+        result.put("personalStatement", personalStatement.getFileName());
+        
+        // How to reference the statement?
+        
+        result.put("applicationStatus", application.getStatus().displayValue());
+        
+        result.put("applicant", gson.toJson(applicantResult));
+//        new Gson().toJson(result);
+        return result;
     }
 
     @ModelAttribute("user")
@@ -201,7 +315,7 @@ public class ApplicationListController {
     public ApplicationForm getApplicationForm(@RequestParam(required = false) String application) {
         return applicationsService.getApplicationByApplicationNumber(application);
     }
-    
+
     private List<ApplicationsFilter> getActiveApplicationFilters() {
         List<ApplicationsFilter> filters = Lists.newArrayListWithExpectedSize(3);
         filters.add(getFilterForNonStatus(ApplicationFormStatus.APPROVED));

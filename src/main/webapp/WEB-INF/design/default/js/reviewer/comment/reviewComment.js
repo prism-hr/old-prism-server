e7c737ddcdec9640983ad66dc828abbafdcd2767
jfi <jfi@zuhlke.com>
2013-04-23 10:53:39 +0100

    PRISM-233 Added date field support (validation not finished)

--- src/main/webapp/WEB-INF/design/default/js/reviewer/comment/reviewComment.js
+++ src/main/webapp/WEB-INF/design/default/js/reviewer/comment/reviewComment.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+	
+	registerBindPickers($('#scoring-questions'));
 	
 	$('#cancelReviewBtn').click(function() {
 		window.location.href = "/pgadmissions/reviewFeedback?applicationId=" +  $('#applicationId').val();

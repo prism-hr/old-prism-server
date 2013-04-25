39a2e5f6fc48ad6a42bd2f9479054d029dadb75d
info <info@bomboca.co.uk>
2013-04-25 11:00:24 +0100

    Missing changes from previous commit

--- src/main/java/com/zuehlke/pgadmissions/validators/FeedbackCommentValidator.java
+++ src/main/java/com/zuehlke/pgadmissions/validators/FeedbackCommentValidator.java
@@ -2,6 +2,7 @@ package com.zuehlke.pgadmissions.validators;
 
 import java.util.List;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -16,13 +17,13 @@ import com.zuehlke.pgadmissions.domain.Score;
 @Component
 public class FeedbackCommentValidator extends AbstractValidator {
 
-	@Autowired
-	private ScoresValidator scoresValidator;
+    @Autowired
+    private ScoresValidator scoresValidator;
 
-	@Override
-	public boolean supports(Class<?> clazz) {
-		return clazz.isAssignableFrom(ReviewComment.class) || clazz.isAssignableFrom(InterviewComment.class) || clazz.isAssignableFrom(ReferenceComment.class);
-	}
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(ReviewComment.class) || clazz.isAssignableFrom(InterviewComment.class) || clazz.isAssignableFrom(ReferenceComment.class);
+    }
 
     @Override
     public void addExtraValidation(Object target, Errors errors) {
@@ -41,7 +42,7 @@ public class FeedbackCommentValidator extends AbstractValidator {
                 if (BooleanUtils.isNotTrue(comment.getConfirmNextStage())) {
                     errors.rejectValue("confirmNextStage", MANDATORY_CHECKBOX);
                 }
-                
+
                 ValidationUtils.rejectIfEmptyOrWhitespace(errors, "comment", EMPTY_FIELD_ERROR_MESSAGE);
             }
         } else if (target instanceof InterviewComment) {
@@ -64,19 +65,19 @@ public class FeedbackCommentValidator extends AbstractValidator {
             ValidationUtils.rejectIfEmptyOrWhitespace(errors, "suitableForUCL", EMPTY_DROPDOWN_ERROR_MESSAGE);
         }
 
-		Comment comment = (Comment) target;
-		List<Score> scores = comment.getScores();
-		if (scores != null) {
-			for (int i = 0; i < scores.size(); i++) {
-				try {
-					errors.pushNestedPath("scores[" + i + "]");
-					ValidationUtils.invokeValidator(scoresValidator, scores.get(i), errors);
-				} finally {
-					errors.popNestedPath();
-				}
-			}
-		}
+        Comment comment = (Comment) target;
+        List<Score> scores = comment.getScores();
+        if (scores != null) {
+            for (int i = 0; i < scores.size(); i++) {
+                try {
+                    errors.pushNestedPath("scores[" + i + "]");
+                    ValidationUtils.invokeValidator(scoresValidator, scores.get(i), errors);
+                } finally {
+                    errors.popNestedPath();
+                }
+            }
+        }
 
-	}
+    }
 
 }

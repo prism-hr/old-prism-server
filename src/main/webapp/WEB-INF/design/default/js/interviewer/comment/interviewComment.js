e3c5914115f4109a924a4c89b5d545afa533baa1
info <info@bomboca.co.uk>
2013-04-30 12:11:54 +0100

    PRISM-172 Merge changes from PRISM-233 branch to default.

--- src/main/webapp/WEB-INF/design/default/js/interviewer/comment/interviewComment.js
+++ src/main/webapp/WEB-INF/design/default/js/interviewer/comment/interviewComment.js
@@ -23,12 +23,21 @@ $(document).ready(function()
 			}
 			$('#interviewForm').submit();
 		};
-		var onCancel = function()
-		{
-			$('div.content-box-inner div.ajax').remove();
-		};
 		
-		modalPrompt(message, onOk, onCancel);
+		var section = $(this).closest('section.form-rows');
+		if (section.length == 1 && section.find('#confirmNextStage').length > 0) {
+			onOk();
+		}
+		else {
+			var message = 'Please confirm that you are satisfied with your comments. <b>You will not be able to change them.</b>';
+			
+			var onCancel = function()
+			{
+				$('div.content-box-inner div.ajax').remove();
+			};
+			
+			modalPrompt(message, onOk, onCancel);
+		}
 		return false;
 	});	
 	

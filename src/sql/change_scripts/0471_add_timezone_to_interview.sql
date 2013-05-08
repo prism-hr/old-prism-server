25fe724b25c43257664706f5c623d7c8329a76a6
jfi <jfi@zuhlke.com>
2013-05-08 12:25:55 +0100

    PRISM-301 Extending timezone size and changin default value

--- src/sql/change_scripts/0471_add_timezone_to_interview.sql
+++ src/sql/change_scripts/0471_add_timezone_to_interview.sql
@@ -1,6 +1,6 @@
-ALTER TABLE INTERVIEW ADD COLUMN time_zone VARCHAR(15) NOT NULL
+ALTER TABLE INTERVIEW ADD COLUMN time_zone VARCHAR(32) NOT NULL
 ;
 
--- all existing interviews took place in 'GMT' timezone
-UPDATE INTERVIEW SET time_zone = 'GMT'
+-- all existing interviews took place in 'Europe/London' timezone
+UPDATE INTERVIEW SET time_zone = 'Europe/London'
 ;

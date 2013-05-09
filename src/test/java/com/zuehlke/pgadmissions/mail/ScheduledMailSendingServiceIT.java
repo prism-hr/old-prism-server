6bb11ca02c33b2e1031e0a93ef8a3eda16ca93f9
jfi <jfi@zuhlke.com>
2013-05-09 14:48:16 +0100

    Grafting changeset 5574

--- src/test/java/com/zuehlke/pgadmissions/mail/ScheduledMailSendingServiceIT.java
+++ src/test/java/com/zuehlke/pgadmissions/mail/ScheduledMailSendingServiceIT.java
@@ -39,17 +39,16 @@ import com.zuehlke.pgadmissions.services.EmailTemplateService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/testMailSendingContext.xml")
 public class ScheduledMailSendingServiceIT {
-    
+
     @Autowired
     private ScheduledMailSendingService service;
-    
-    
+
     @Autowired
     SessionFactory sessionFactory;
-    
+
     @Autowired
     private PlatformTransactionManager transactionManager;
-    
+
     private Referee referee1;
     private Referee referee2;
     private Referee referee3;
@@ -58,136 +57,89 @@ public class ScheduledMailSendingServiceIT {
     private Integer referee3Id;
     private DateTime lastNotified;
     private ApplicationForm application2;
-    
+
     @Before
     public void setup() {
         TransactionTemplate template = new TransactionTemplate(transactionManager);
         template.execute(new TransactionCallbackWithoutResult() {
-            
+
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus status) {
                 ReminderInterval interval = new ReminderInterval();
                 interval.setDuration(1);
                 interval.setUnit(DurationUnitEnum.MINUTES);
                 sessionFactory.getCurrentSession().save(interval);
-                
-                EmailTemplate mailTemplate = new EmailTemplateBuilder()
-                    .subject("whatever")
-                    .name(EmailTemplateName.REFEREE_REMINDER)
-                    .content("whatever content")
-                    .active(true)
-                    .build();
-                
-                RegisteredUser refereeUser1 = new RegisteredUserBuilder()
-                     .email("amanda@mail.com")
-                     .firstName("Amanda")
-                     .lastName("Frijelloce")
-                     .build();
-                RegisteredUser refereeUser2 = new RegisteredUserBuilder()
-                    .email("nina@mail.com")
-                    .firstName("Nina")
-                    .lastName("Sinalefe")
-                    .build();
-                RegisteredUser refereeUser3 = new RegisteredUserBuilder()
-                    .email("gina@mail.com")
-                    .firstName("Gina")
-                    .lastName("Round")
-                    .build();
-                RegisteredUser programAdmin = new RegisteredUserBuilder()
-                    .email("jeppetto@mail.com")
-                    .build();
-                RegisteredUser applicant = new RegisteredUserBuilder()
-                    .firstName("Luigi")
-                    .lastName("Babbu")
-                    .build();
-                
+
+                EmailTemplate mailTemplate = new EmailTemplateBuilder().subject("whatever").name(EmailTemplateName.REFEREE_REMINDER)
+                        .content("whatever content").active(true).build();
+
+                RegisteredUser refereeUser1 = new RegisteredUserBuilder().email("amanda@mail.com").firstName("Amanda").lastName("Frijelloce").build();
+                RegisteredUser refereeUser2 = new RegisteredUserBuilder().email("nina@mail.com").firstName("Nina").lastName("Sinalefe").build();
+                RegisteredUser refereeUser3 = new RegisteredUserBuilder().email("gina@mail.com").firstName("Gina").lastName("Round").build();
+                RegisteredUser programAdmin = new RegisteredUserBuilder().email("jeppetto@mail.com").build();
+                RegisteredUser applicant = new RegisteredUserBuilder().firstName("Luigi").lastName("Babbu").build();
+
                 lastNotified = new DateTime(2013, 5, 7, 0, 0);
-                
-                Program program = new ProgramBuilder()
-                    .administrators(programAdmin)
-                    .title("ProgramTitle")
-                    .build();
-                
-                ApplicationForm application1 = new ApplicationFormBuilder()
-                    .applicant(applicant)
-                    .program(program)
-                    .applicationNumber("sampelNumber")
-                    .status(ApplicationFormStatus.INTERVIEW)
-                    .build();
-                
-                application2 = new ApplicationFormBuilder()
-                .applicant(applicant)
-                .program(program)
-                .applicationNumber("sampelNumber")
-                .status(ApplicationFormStatus.INTERVIEW)
-                .build();
-                
-                referee1 = new RefereeBuilder()
-                    .declined(false)
-                    .application(application1)
-                    .user(refereeUser1)
-                    .reference(new ReferenceCommentBuilder().build())
-                    .lastNotified(lastNotified.toDate())
-                    .build();
-                referee2 = new RefereeBuilder()
-                    .declined(false)
-                    .application(application2)
-                    .reference(new ReferenceCommentBuilder().build())
-                    .lastNotified(lastNotified.toDate())
-                    .user(refereeUser2)
-                    .build();
-                referee3 = new RefereeBuilder()
-                    .declined(false)
-                    .application(application1)
-                    .reference(new ReferenceCommentBuilder().build())
-                    .lastNotified(lastNotified.toDate())
-                    .user(refereeUser3)
-                    .build();
-                
-                saveObjects(interval, mailTemplate, refereeUser1, refereeUser2, refereeUser3,
-                        programAdmin, applicant, program, application1, application2, referee1, referee2, referee3);
+
+                Program program = new ProgramBuilder().administrators(programAdmin).title("ProgramTitle").build();
+
+                ApplicationForm application1 = new ApplicationFormBuilder().applicant(applicant).program(program).applicationNumber("sampelNumber")
+                        .status(ApplicationFormStatus.INTERVIEW).build();
+
+                application2 = new ApplicationFormBuilder().applicant(applicant).program(program).applicationNumber("sampelNumber")
+                        .status(ApplicationFormStatus.INTERVIEW).build();
+
+                referee1 = new RefereeBuilder().declined(false).application(application1).user(refereeUser1).reference(new ReferenceCommentBuilder().build())
+                        .lastNotified(lastNotified.toDate()).build();
+                referee2 = new RefereeBuilder().declined(false).application(application2).reference(new ReferenceCommentBuilder().build())
+                        .lastNotified(lastNotified.toDate()).user(refereeUser2).build();
+                referee3 = new RefereeBuilder().declined(false).application(application1).reference(new ReferenceCommentBuilder().build())
+                        .lastNotified(lastNotified.toDate()).user(refereeUser3).build();
+
+                saveObjects(interval, mailTemplate, refereeUser1, refereeUser2, refereeUser3, programAdmin, applicant, program, application1, application2,
+                        referee1, referee2, referee3);
                 referee1Id = referee1.getId();
                 referee2Id = referee2.getId();
                 referee3Id = referee3.getId();
             }
         });
     }
-    
-    private void saveObjects(Object ...objs) {
+
+    private void saveObjects(Object... objs) {
         Session session = sessionFactory.getCurrentSession();
         for (Object o : objs) {
             session.saveOrUpdate(o);
         }
         session.flush();
     }
-    
-    private void deleteObjects(Object ...objs) {
+
+    private void deleteObjects(Object... objs) {
         Session session = sessionFactory.getCurrentSession();
         for (Object o : objs) {
             session.delete(o);
         }
         session.flush();
     }
-    
+
     @After
     public void cleanup() {
         TransactionTemplate template = new TransactionTemplate(transactionManager);
         template.execute(new TransactionCallbackWithoutResult() {
-            
+
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus status) {
                 deleteObjects(referee1, referee2, referee3);
             }
         });
     }
- 
+
     @Test
     public void shouldSendEmailToOneRefereesAndUpdateHisStatus() {
-        
+
         service.setMailSender(new CustomMailSender());
-        
+
         service.sendReferenceReminder();
-   
+
         TransactionTemplate template = new TransactionTemplate(transactionManager);
         template.execute(new TransactionCallbackWithoutResult() {
             @Override
@@ -202,30 +154,28 @@ public class ScheduledMailSendingServiceIT {
         assertTrue(lastNotified.toDate().equals(referee2.getLastNotified()));
         assertTrue(lastNotified.toDate().before(referee3.getLastNotified()));
     }
-    
+
     class CustomMailSender extends MailSender {
-        
+
         CustomMailSender() {
-            this(service.getMailSender().javaMailSender, "false", "pgadmissions.jenkins@zuhlke.com"," uclprism@gmail.com",
-                    service.getMailSender().getEmailTemplateService(), service.getMailSender().getFreemarkerConfig());
+            this(service.getMailSender().javaMailSender, "false", "pgadmissions.jenkins@zuhlke.com", " uclprism@gmail.com", service.getMailSender()
+                    .getEmailTemplateService(), service.getMailSender().getFreemarkerConfig());
         }
 
-        public CustomMailSender(JavaMailSender javaMailSender, String production, String emailAddressFrom,
-                String emailAddressTo, EmailTemplateService emailTemplateService, FreeMarkerConfig freemarkerConfig) {
+        public CustomMailSender(JavaMailSender javaMailSender, String production, String emailAddressFrom, String emailAddressTo,
+                EmailTemplateService emailTemplateService, FreeMarkerConfig freemarkerConfig) {
             super(javaMailSender, production, emailAddressFrom, emailAddressTo, emailTemplateService, freemarkerConfig);
         }
-        
+
         @Override
         protected void sendEmailAsDevelopmentMessage(PrismEmailMessage message) {
             if (message.getTo().get(0).getEmail().equals(referee2.getUser().getEmail())) {
                 throw new PrismMailMessageException(message);
-            }
-            else {
+            } else {
                 super.sendEmailAsDevelopmentMessage(message);
             }
         }
-        
-        
+
     }
 
 }

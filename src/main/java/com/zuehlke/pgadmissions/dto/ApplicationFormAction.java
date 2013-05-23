cffc1ec53f941a52006e91dbda7a6e4e41eb9f7e
jfi <jfi@zuhlke.com>
2013-05-23 13:58:07 +0100

    PRISM-354 Moved action availability logic into enumeration

--- src/main/java/com/zuehlke/pgadmissions/dto/ApplicationFormAction.java
+++ src/main/java/com/zuehlke/pgadmissions/dto/ApplicationFormAction.java
@@ -1,37 +1,245 @@
 package com.zuehlke.pgadmissions.dto;
 
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.APPROVAL;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.INTERVIEW;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.REJECTED;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.REVIEW;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.UNSUBMITTED;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.VALIDATION;
+import static com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus.WITHDRAWN;
+
+import com.zuehlke.pgadmissions.domain.ApplicationForm;
+import com.zuehlke.pgadmissions.domain.Interview;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
+import com.zuehlke.pgadmissions.domain.Supervisor;
+import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
+
 public enum ApplicationFormAction {
 
-    VIEW_EDIT("view", "View / Edit"), //
-    VIEW("view", "View"), //
-    EMAIL_APPLICANT("emailApplicant", "Email applicant"), //
-    COMMENT("comment", "Comment"), //
-    WITHDRAW("withdraw", "Withdraw"), //
-    CONFIRM_ELIGIBILITY("validate", "Confirm Eligibility"), //
-    ADD_REFERENCE("reference", "Add reference"), //
-    COMPLETE_VALIDATION_STAGE("validate", "Complete Validation Stage"), //
-    ASSIGN_REVIEWERS("validate", "Assign Reviewers"), //
-    COMPLETE_REVIEW_STAGE("validate", "Complete Review Stage"), //
-    ADD_REVIEW("review", "Add review"), //
-    COMPLETE_INTERVIEW_STAGE("validate", "Complete Interview Stage"), //
-    ASSIGN_INTERVIEWERS("validate", "Assign Interviewers"), //
-    CONFIRM_INTERVIEW_TIME("interviewConfirm", "Confirm Interview Arrangements"), //
-    PROVIDE_INTERVIEW_AVAILABILITY("interviewVote", "Provide Interview Availability"), //
-    ADD_INTERVIEW_FEEDBACK("interviewFeedback", "Add interview feedback"), //
-    REVISE_APPROVAL("restartApproval", "Revise Approval"), //
-    APPROVE("validate", "Approve"), //
-    ASSIGN_SUPERVISORS("validate", "Assign Supervisors"), //
-    REVISE_APPROVAL_AS_ADMINISTRATOR("restartApprovalAsAdministrator", "Revise Approval"), //
-    CONFIRM_SUPERVISION("confirmSupervision", "Confirm supervision"), //
+    // GENERAL ACTIONS
+    VIEW_EDIT("view", "View / Edit", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (user.canEditAsAdministrator(application) || user.canEditAsApplicant(application)) {
+                actions.addAction(VIEW_EDIT);
+            }
+        }
+    }), //
+    VIEW("view", "View", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (!user.canEditAsAdministrator(application) && !user.canEditAsApplicant(application)) {
+                actions.addAction(VIEW);
+            }
+        }
+    }), //
+    EMAIL_APPLICANT("emailApplicant", "Email applicant", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() != UNSUBMITTED && user != application.getApplicant()) {
+                actions.addAction(EMAIL_APPLICANT);
+            }
+        }
+    }), //
+    COMMENT("comment", "Comment", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() != ApplicationFormStatus.UNSUBMITTED
+                    && (user.hasAdminRightsOnApplication(application) || user.isViewerOfProgramme(application) || user.isInRole(Authority.ADMITTER))) {
+                actions.addAction(COMMENT);
+            }
+        }
+    }), //
+    WITHDRAW("withdraw", "Withdraw", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (!application.isTerminated() && user == application.getApplicant()) {
+                actions.addAction(WITHDRAW);
+            }
+        }
+    }), //
+    CONFIRM_ELIGIBILITY("validate", "Confirm Eligibility", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (user.isInRole(Authority.ADMITTER) && !application.hasConfirmElegibilityComment()) {
+                actions.addAction(CONFIRM_ELIGIBILITY);
+            }
+            if (application.getAdminRequestedRegistry() != null && (application.getStatus() != WITHDRAWN && application.getStatus() != REJECTED)) {
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+    ADD_REFERENCE("reference", "Add reference", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.isSubmitted() && !application.isTerminated() && user.isRefereeOfApplicationForm(application)
+                    && !user.getRefereeForApplicationForm(application).hasResponded()) {
+                actions.addAction(ADD_REFERENCE);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+
+    // VALIDATION STAGE ACTIONS
+
+    COMPLETE_VALIDATION_STAGE("validate", "Complete Validation Stage", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == VALIDATION && nextStatus == null && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(COMPLETE_VALIDATION_STAGE);
+            }
+        }
+    }), //
+
+    // REVIEW STAGE ACTIONS
+
+    ASSIGN_REVIEWERS("validate", "Assign Reviewers", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (nextStatus == REVIEW && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(ASSIGN_REVIEWERS);
+            }
+        }
+    }), //
+    COMPLETE_REVIEW_STAGE("validate", "Complete Review Stage", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == REVIEW && nextStatus == null && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(COMPLETE_REVIEW_STAGE);
+            }
+        }
+    }), //
+    ADD_REVIEW("review", "Add review", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == REVIEW && user.isReviewerInLatestReviewRoundOfApplicationForm(application)
+                    && !user.hasRespondedToProvideReviewForApplicationLatestRound(application)) {
+                actions.addAction(ADD_REVIEW);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+
+    // INTERVIEW STAGE ACTIONS
+
+    ASSIGN_INTERVIEWERS("validate", "Assign Interviewers", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (nextStatus == INTERVIEW && (user.hasAdminRightsOnApplication(application) || user.isApplicationAdministrator(application))) {
+                actions.addAction(ASSIGN_INTERVIEWERS);
+            }
+        }
+    }), //
+    COMPLETE_INTERVIEW_STAGE("validate", "Complete Interview Stage", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == INTERVIEW && nextStatus == null && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(COMPLETE_INTERVIEW_STAGE);
+            }
+        }
+    }), //
+    CONFIRM_INTERVIEW_TIME("interviewConfirm", "Confirm Interview Arrangements", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            Interview interview = application.getLatestInterview();
+            if (application.getStatus() == INTERVIEW && nextStatus == null && user.hasAdminRightsOnApplication(application) && interview.isScheduling()
+                    && (user.isApplicationAdministrator(application) || user.hasAdminRightsOnApplication(application))) {
+                actions.addAction(CONFIRM_INTERVIEW_TIME);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+    PROVIDE_INTERVIEW_AVAILABILITY("interviewVote", "Provide Interview Availability", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            Interview interview = application.getLatestInterview();
+            if (application.getStatus() == INTERVIEW && nextStatus == null && interview.isScheduling() && interview.isParticipant(user)
+                    && !interview.getParticipant(user).getResponded()) {
+                actions.addAction(PROVIDE_INTERVIEW_AVAILABILITY);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+    ADD_INTERVIEW_FEEDBACK("interviewFeedback", "Add interview feedback", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            Interview interview = application.getLatestInterview();
+            if (application.getStatus() == INTERVIEW && nextStatus == null && user.isInterviewerOfApplicationForm(application) && interview.isScheduled()
+                    && !user.hasRespondedToProvideInterviewFeedbackForApplicationLatestRound(application)) {
+                actions.addAction(ADD_INTERVIEW_FEEDBACK);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+
+    // APPROVAL STAGE ACTIONS
+
+    REVISE_APPROVAL("restartApproval", "Revise Approval", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.isPendingApprovalRestart() && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(REVISE_APPROVAL);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+    APPROVE("validate", "Approve", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == APPROVAL) {
+                if (user.isInRoleInProgram(Authority.APPROVER, application.getProgram())) {
+                    actions.addAction(APPROVE);
+                    actions.setRequiresAttention(true);
+                } else if (user.isInRole(Authority.SUPERADMINISTRATOR)) {
+                    actions.addAction(APPROVE);
+                }
+            }
+        }
+    }), //
+    ASSIGN_SUPERVISORS("validate", "Assign Supervisors", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (nextStatus == APPROVAL && user.hasAdminRightsOnApplication(application)) {
+                actions.addAction(ASSIGN_SUPERVISORS);
+            }
+        }
+    }), //
+    REVISE_APPROVAL_AS_ADMINISTRATOR("restartApprovalAsAdministrator", "Revise Approval", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == APPROVAL && !application.isPendingApprovalRestart()
+                    && user.isInRoleInProgram(Authority.ADMINISTRATOR, application.getProgram())
+                    && user.isNotInRoleInProgram(Authority.APPROVER, application.getProgram()) && user.isNotInRole(Authority.SUPERADMINISTRATOR)) {
+                actions.addAction(REVISE_APPROVAL_AS_ADMINISTRATOR);
+                actions.setRequiresAttention(true);
+            }
+        }
+    }), //
+    CONFIRM_SUPERVISION("confirmSupervision", "Confirm supervision", new ActionPredicate() {
+        @Override
+        public void apply(ActionsDefinitions actions, RegisteredUser user, ApplicationForm application, ApplicationFormStatus nextStatus) {
+            if (application.getStatus() == APPROVAL) {
+                Supervisor primarySupervisor = application.getLatestApprovalRound().getPrimarySupervisor();
+                if (primarySupervisor != null && user == primarySupervisor.getUser() && !primarySupervisor.hasResponded()) {
+                    actions.addAction(CONFIRM_SUPERVISION);
+                    actions.setRequiresAttention(true);
+                }
+            }
+        }
+    }), //
     ;
 
     private final String id;
 
     private final String displayName;
 
-    private ApplicationFormAction(String id, String displayName) {
+    private final ActionPredicate predicate;
+
+    private ApplicationFormAction(String id, String displayName, ActionPredicate predicate) {
         this.id = id;
         this.displayName = displayName;
+        this.predicate = predicate;
     }
 
     public String getId() {
@@ -41,5 +249,14 @@ public enum ApplicationFormAction {
     public String getDisplayName() {
         return displayName;
     }
-    
+
+    public void applyAction(final ActionsDefinitions actions, final RegisteredUser user, final ApplicationForm application,
+            final ApplicationFormStatus nextStatus) {
+        predicate.apply(actions, user, application, nextStatus);
+    }
+
+    public static interface ActionPredicate {
+        public void apply(final ActionsDefinitions actions, final RegisteredUser user, final ApplicationForm application, final ApplicationFormStatus nextStatus);
+    }
+
 }

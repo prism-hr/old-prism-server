fe1c0208d9dc834323c222b4d3e44f7a7db73139
cls <cls@localhost>
2013-05-28 09:34:52 +0100

    Refactored method name

--- src/test/java/com/zuehlke/pgadmissions/controllers/SubmitApplicationFormControllerTest.java
+++ src/test/java/com/zuehlke/pgadmissions/controllers/SubmitApplicationFormControllerTest.java
@@ -313,7 +313,7 @@ public class SubmitApplicationFormControllerTest {
         EasyMock.expect(stageDurationMock.getUnit()).andReturn(DurationUnitEnum.DAYS);
         EasyMock.expect(stageDurationMock.getDurationInMinutes()).andReturn(1440);
         EasyMock.replay(stageDurationServiceMock, stageDurationMock);
-        applicationController.calculateAndSetValidationDueDate(applicationForm);
+        applicationController.assignValidationDueDate(applicationForm);
         Date oneDayMore = new SimpleDateFormat("yyyy/MM/dd").parse("2012/12/14");
         Assert.assertEquals(String.format("Dates are not the same [%s] [%s]", oneDayMore, applicationForm.getDueDate()), oneDayMore,
                 applicationForm.getDueDate());
@@ -327,7 +327,7 @@ public class SubmitApplicationFormControllerTest {
         EasyMock.expect(stageDurationMock.getUnit()).andReturn(DurationUnitEnum.DAYS);
         EasyMock.expect(stageDurationMock.getDurationInMinutes()).andReturn(1440);
         EasyMock.replay(stageDurationServiceMock, accessServiceMock, stageDurationMock);
-        applicationController.calculateAndSetValidationDueDate(applicationForm);
+        applicationController.assignValidationDueDate(applicationForm);
         Date dayAfterTomorrow = com.zuehlke.pgadmissions.utils.DateUtils.addWorkingDaysInMinutes(new Date(), 1440);
         Assert.assertTrue(String.format("Dates are not on the same day [%s] [%s]", dayAfterTomorrow, applicationForm.getDueDate()),
                 DateUtils.isSameDay(dayAfterTomorrow, applicationForm.getDueDate()));

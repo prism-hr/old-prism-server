d9720bfce3352856648e20734ccc7f05f2b3c8bb
ked <ked@LONN0166.ads.zuehlke.com>
2013-05-29 10:38:05 +0100

    * UCL IRIS profile controller backend changed to use existing dependency Jsoup rather than HtmlUnit

--- src/main/java/com/zuehlke/pgadmissions/services/UclIrisProfileService.java
+++ src/main/java/com/zuehlke/pgadmissions/services/UclIrisProfileService.java
@@ -1,33 +1,45 @@
 package com.zuehlke.pgadmissions.services;
 
-import org.apache.commons.lang.StringUtils;
-import org.springframework.stereotype.Service;
+import java.io.IOException;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UclIrisProfileService {
 
+    private Logger log = LoggerFactory.getLogger(UclIrisProfileService.class);
+    
     private static final String IRIS_PROFILE_URL = "http://iris.ucl.ac.uk/iris/browse/profile?upi=";
+    
+    public UclIrisProfileService() {
+    }
     
     public String geProfile(final String upi) {
         try {
-            WebClient client = new WebClient();
-            client.setJavaScriptEnabled(false);
-            HtmlPage page = (HtmlPage) client.getPage(IRIS_PROFILE_URL + StringUtils.upperCase(upi));
-            HtmlElement displayNameDiv = (HtmlElement) page.getFirstByXPath("//*[contains(@class, 'displayName')]");
-            if (displayNameDiv != null) {
-                return displayNameDiv.asText();
+            Document doc = downloadIrisProfile(upi);
+            Elements displayNameDiv = doc.select("div.displayName");
+            if (!displayNameDiv.isEmpty() && displayNameDiv.size() == 1) {
+                return displayNameDiv.text();                
             }
         } catch (Exception e) {
-            // do nothing
+            if (log.isDebugEnabled()) {
+                log.debug(e.getMessage(), e);
+            }
         }
         return StringUtils.EMPTY;
     }
     
     public boolean profileExists(final String upi) {
         return StringUtils.isNotEmpty(geProfile(upi));
+    }
+    
+    Document downloadIrisProfile(final String upi) throws IOException {
+        return Jsoup.connect(IRIS_PROFILE_URL + StringUtils.upperCase(upi)).get();
     }
 }

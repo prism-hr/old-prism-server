5b7eae768adcf4b7c04bceaeaf99bbd54f791916
ima <ima@localhost>
2013-05-31 08:17:31 +0100

    PRSIM-290. Fix Merge Issues and extract binding into methods

--- src/main/webapp/WEB-INF/design/default/js/prospectus/prospectus.js
+++ src/main/webapp/WEB-INF/design/default/js/prospectus/prospectus.js
@@ -1,53 +1,58 @@
 $(document).ready(function(){
-	
 		bindDatePickerEnabled($("#closingDate"));
-
 		getUpiForCurrentUser();
-		
 		bindAddClosingDateButtonAction();
-		
 		bindSaveButtonAction();
-		
 		bindProgramSelectChangeAction();
-		
-		$("#save-upi-go").bind('click', function() {
-			$("#irisSection").find("div.alert-error").remove();
-			$('#iris-profile-modal').modal();
-			$("#iris-profile-modal-iframe").attr("src", "http://iris.ucl.ac.uk/iris/browse/profile?upi=" + $('#upi').val());
-		});
-		
-		$("#iris-profile-modal-confirm-btn").bind('click', function() {
-			$('#iris-profile-modal').modal('hide');
-			$("#irisSection").find("div.alert-error").remove();
-			$.ajax({
-				type: 'POST',
-				dataType: "json",
-				statusCode: {
-					401: function() { window.location.reload(); },
-					500: function() { window.location.href = "/pgadmissions/error"; },
-					404: function() { window.location.href = "/pgadmissions/404"; },
-					400: function() { window.location.href = "/pgadmissions/400"; },                  
-					403: function() { window.location.href = "/pgadmissions/404"; }
-				},
-				url: "/pgadmissions/users/IRIS/",
-				data: {
-					upi : $('#upi').val()
-				}, 
-				success: function(data) {
-					if (!data.success) {
-						if (data.irisProfile != null) {
-		                    $('#upi').parent().append('<div class="alert alert-error"> <i class="icon-warning-sign"></i> ' + data.irisProfile  + '</div>');
-		                }
-					} else {
-						$("#iris-account-linked-message").show().find("span").html($('#upi').val());
-					}
-				},
-				complete: function() {
-				}
-			});
-		});
+		bindSaveUpiAction();
+		bindIrisProfileModalConfirmAction();
 });
-		
+
+function bindIrisProfileModalConfirmAction(){
+	$("#iris-profile-modal-confirm-btn").bind('click', function() {
+		$('#iris-profile-modal').modal('hide');
+		$("#irisSection").find("div.alert-error").remove();
+		$.ajax({
+			type: 'POST',
+			dataType: "json",
+			statusCode: {
+				401: function() { window.location.reload(); },
+				500: function() { window.location.href = "/pgadmissions/error"; },
+				404: function() { window.location.href = "/pgadmissions/404"; },
+				400: function() { window.location.href = "/pgadmissions/400"; },                  
+				403: function() { window.location.href = "/pgadmissions/404"; }
+			},
+			url: "/pgadmissions/users/IRIS/",
+			data: {
+				upi : $('#upi').val()
+			}, 
+			success: function(data) {
+				if (!data.success) {
+					if (data.irisProfile != null) {
+						$('#upi').parent().append('<div class="alert alert-error"> <i class="icon-warning-sign"></i> ' + data.irisProfile  + '</div>');
+					}
+				} else {
+					$("#iris-account-linked-message").show().find("span").html($('#upi').val());
+					$("#iris-account-not-linked-message").hide();
+				}
+			},
+			complete: function() {
+			}
+		});
+	});
+}
+
+function bindSaveUpiAction(){
+	$("#save-upi-go").bind('click', function() {
+		$("#irisSection").find("div.alert-error").remove();
+		if ($("#upi").val() == "") {
+			$('#upi').parent().append('<div class="alert alert-error"> <i class="icon-warning-sign"></i> You must make an entry.</div>');
+			return;
+		}
+		$('#iris-profile-modal').modal();
+		$("#iris-profile-modal-iframe").attr("src", "http://iris.ucl.ac.uk/iris/browse/profile?upi=" + $('#upi').val());
+	});
+}
 function bindAddClosingDateButtonAction(){
 	$("#addClosingDate").bind('click', function(){
 		$.ajax({
@@ -174,12 +179,6 @@ function bindSaveButtonAction(){
 		});
 
 	});
-
-			if ($("#upi").val() == "") {
-				$('#upi').parent().append('<div class="alert alert-error"> <i class="icon-warning-sign"></i> You must make an entry.</div>');
-				return;
-			}
-						$("#iris-account-not-linked-message").hide();
 }
 
 

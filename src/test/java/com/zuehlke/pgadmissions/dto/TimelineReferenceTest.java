9c2e35a94189fee18011b4f84ea5d88cedeadbec
ima <ima@localhost>
2013-06-10 08:43:38 +0100

    PRISM-422 Fix Timeline issue. Handle decline and other events without comments.

--- src/test/java/com/zuehlke/pgadmissions/dto/TimelineReferenceTest.java
+++ src/test/java/com/zuehlke/pgadmissions/dto/TimelineReferenceTest.java
@@ -9,8 +9,10 @@ import org.junit.Test;
 
 import com.zuehlke.pgadmissions.domain.Referee;
 import com.zuehlke.pgadmissions.domain.ReferenceComment;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
 import com.zuehlke.pgadmissions.domain.builders.RefereeBuilder;
 import com.zuehlke.pgadmissions.domain.builders.ReferenceCommentBuilder;
+import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
 
 public class TimelineReferenceTest {
 
@@ -46,5 +48,44 @@ public class TimelineReferenceTest {
 	@Test
 	public void shouldReturnRefereeAsCapacity(){
 		assertEquals("referee", new TimelineReference().getUserCapacity());
+	}
+
+	@Test
+	public void shouldReturnTooltipWithAuthorDetailsWhenNoReferenceComment(){
+		Referee referee = new RefereeBuilder().build();
+		RegisteredUser author = new RegisteredUserBuilder().firstName("Name").lastName("LastName").email("email@mail.com").build();
+		
+		TimelineReference timelineReference = new TimelineReference();
+		timelineReference.setReferee(referee);
+		timelineReference.setAuthor(author);
+		
+		assertEquals("Name LastName (email@mail.com) as: Referee", timelineReference.getTooltipMessage());
+	}
+
+	@Test
+	public void shouldReturnTooltipWithProviderDetailsWhenReferenceComment(){
+		RegisteredUser provider = new RegisteredUserBuilder().firstName("First").lastName("Last").email("firstlast@mail.com").build();
+		ReferenceComment comment = new ReferenceCommentBuilder().id(4).providedBy(provider).build();
+		Referee referee = new RefereeBuilder().reference(comment).build();
+		RegisteredUser author = new RegisteredUserBuilder().firstName("Name").lastName("LastName").email("email@mail.com").build();
+
+		TimelineReference timelineReference = new TimelineReference();
+		timelineReference.setReferee(referee);
+		timelineReference.setAuthor(author);
+		
+		assertEquals("First Last (firstlast@mail.com) as: Referee", timelineReference.getTooltipMessage());
+	}
+
+	@Test
+	public void shouldReturnTooltipWithAuthorDetailsWhenReferenceCommentWithNoProvider(){
+		ReferenceComment comment = new ReferenceCommentBuilder().id(4).build();
+		Referee referee = new RefereeBuilder().reference(comment).build();
+		RegisteredUser author = new RegisteredUserBuilder().firstName("Name").lastName("LastName").email("email@mail.com").build();
+		
+		TimelineReference timelineReference = new TimelineReference();
+		timelineReference.setReferee(referee);
+		timelineReference.setAuthor(author);
+		
+		assertEquals("Name LastName (email@mail.com) as: Referee", timelineReference.getTooltipMessage());
 	}
 }

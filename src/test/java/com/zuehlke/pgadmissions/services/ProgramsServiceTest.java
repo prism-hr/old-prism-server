d8e2b659171ad0404cb113b40bb6c12ee2e0f9c2
jfi <jfi@zuhlke.com>
2013-06-18 14:33:09 +0100

    PRISM-293 Added some missing tests

--- src/test/java/com/zuehlke/pgadmissions/services/ProgramsServiceTest.java
+++ src/test/java/com/zuehlke/pgadmissions/services/ProgramsServiceTest.java
@@ -1,5 +1,6 @@
 package com.zuehlke.pgadmissions.services;
 
+import static junit.framework.Assert.assertSame;
 import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
@@ -9,6 +10,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -25,20 +27,21 @@ import com.zuehlke.pgadmissions.dao.AdvertDAO;
 import com.zuehlke.pgadmissions.dao.ProgramDAO;
 import com.zuehlke.pgadmissions.dao.ProjectDAO;
 import com.zuehlke.pgadmissions.domain.Program;
+import com.zuehlke.pgadmissions.domain.RegisteredUser;
 import com.zuehlke.pgadmissions.domain.ScoringDefinition;
 import com.zuehlke.pgadmissions.domain.builders.ProgramBuilder;
+import com.zuehlke.pgadmissions.domain.enums.Authority;
 import com.zuehlke.pgadmissions.domain.enums.ScoringStage;
 
 public class ProgramsServiceTest {
 
     private ProgramDAO programDAOMock;
-    
+
     private AdvertDAO advertDAOMock;
 
     private ProjectDAO projectDAOMock;
-    
+
     private ProgramsService programsService;
-    
 
     @Test
     public void shouldGetAllPrograms() {
@@ -80,6 +83,40 @@ public class ProgramsServiceTest {
     }
 
     @Test
+    public void shouldGetProgramsForWhichCanManageProjectsIfAdmin() {
+        RegisteredUser userMock = EasyMock.createMock(RegisteredUser.class);
+
+        List<Program> programs = Collections.emptyList();
+        EasyMock.expect(programDAOMock.getAllPrograms()).andReturn(programs);
+        EasyMock.expect(userMock.isInRole(Authority.SUPERADMINISTRATOR)).andReturn(true);
+
+        EasyMock.replay(userMock, programDAOMock);
+        List<Program> returnedPrograms = programsService.getProgramsForWhichCanManageProjects(userMock);
+        EasyMock.verify(userMock, programDAOMock);
+
+        assertSame(programs, returnedPrograms);
+    }
+
+    @Test
+    public void shouldGetProgramsForWhichCanManageProjectsIfNotAdmin() {
+        RegisteredUser userMock = EasyMock.createMock(RegisteredUser.class);
+
+        EasyMock.expect(userMock.isInRole(Authority.SUPERADMINISTRATOR)).andReturn(false);
+        EasyMock.expect(userMock.getProgramsOfWhichAdministrator()).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(userMock.getProgramsOfWhichApprover()).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(userMock.getProgramsOfWhichReviewer()).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(userMock.getProgramsOfWhichInterviewer()).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(userMock.getProgramsOfWhichSupervisor()).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(programDAOMock.getProgramsOfWhichPreviousReviewer(userMock)).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(programDAOMock.getProgramsOfWhichPreviousInterviewer(userMock)).andReturn(Collections.<Program> emptyList());
+        EasyMock.expect(programDAOMock.getProgramsOfWhichPreviousSupervisor(userMock)).andReturn(Collections.<Program> emptyList());
+
+        EasyMock.replay(userMock, programDAOMock);
+        programsService.getProgramsForWhichCanManageProjects(userMock);
+        EasyMock.verify(userMock, programDAOMock);
+    }
+
+    @Test
     public void shouldApplyScoringDefinition() {
         Program program = new ProgramBuilder().build();
 
@@ -93,23 +130,21 @@ public class ProgramsServiceTest {
         assertNotNull(definition);
         assertEquals("Siala baba mak", definition.getContent());
     }
-    
+
     @Test
     public void shouldReturnClosingDatesMap() {
         Program program1 = new ProgramBuilder().code("p1").id(1).build();
         Program program2 = new ProgramBuilder().code("p2").id(2).build();
         expect(programDAOMock.getAllPrograms()).andReturn(Arrays.asList(program1, program2));
-        
+
         Capture<Date> dateCaptor = new Capture<Date>();
-        expect(programDAOMock.getNextClosingDateForProgram(eq(program1), EasyMock.capture(dateCaptor)))
-            .andReturn(new DateTime(2013, 2, 15, 00, 15).toDate());
-        expect(programDAOMock.getNextClosingDateForProgram(eq(program2), EasyMock.capture(dateCaptor)))
-            .andReturn(new DateTime(2013, 2, 13, 13, 15).toDate());
-        
+        expect(programDAOMock.getNextClosingDateForProgram(eq(program1), EasyMock.capture(dateCaptor))).andReturn(new DateTime(2013, 2, 15, 00, 15).toDate());
+        expect(programDAOMock.getNextClosingDateForProgram(eq(program2), EasyMock.capture(dateCaptor))).andReturn(new DateTime(2013, 2, 13, 13, 15).toDate());
+
         replay(programDAOMock);
         Map<String, String> result = programsService.getDefaultClosingDates();
         verify(programDAOMock);
-        
+
         Assert.assertNotNull(result);
         Assert.assertEquals(2, result.size());
         Assert.assertEquals("15 Feb 2013", result.get("p1"));

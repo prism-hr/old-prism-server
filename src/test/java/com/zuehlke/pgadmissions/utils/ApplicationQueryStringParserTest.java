622c3cd27f56f9d9abb0390a5e1427eef33ca95e
ima <ima@localhost>
2013-06-19 12:14:38 +0100

    PRISM-293 Fix Linking Project for users not registered Proper Highlighting on activation

--- src/test/java/com/zuehlke/pgadmissions/utils/ApplicationQueryStringParserTest.java
+++ src/test/java/com/zuehlke/pgadmissions/utils/ApplicationQueryStringParserTest.java
@@ -2,47 +2,29 @@ package com.zuehlke.pgadmissions.utils;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Test;
 
 public class ApplicationQueryStringParserTest {
 
 	@Test
-	public void shouldParseStringAndReturnCorrectValuesAsArray(){
-		String queryString = "program:TESTCODE||programhome:http://www.google.com||bacthdeadline:29-Oct-2012||projectTitle:Test project";
-		String[] parameters = new ApplicationQueryStringParser().parse(queryString);
-		assertEquals("TESTCODE", parameters[0]);
-		assertEquals("http://www.google.com", parameters[1]);
-		assertEquals("29-Oct-2012", parameters[2]);
-		assertEquals("Test project", parameters[3]);
+	public void shouldParseStringAndReturnCorrectValuesAsMap(){
+		String queryString = "program:TESTCODE||programHome:http://www.google.com||batchDeadline:29-Oct-2012||projectTitle:Test project";
+		Map<String, String> parameters = new ApplicationQueryStringParser().parse(queryString);
+		assertEquals(4, parameters.size());
+		assertEquals("TESTCODE", parameters.get("program"));
+		assertEquals("29-Oct-2012", parameters.get("batchDeadline"));
+		assertEquals("http://www.google.com",parameters.get("programHome"));
+		assertEquals("Test project", parameters.get("projectTitle"));
 	}
 	
 	@Test 
 	public void shouldReturnCorrectValuesIfOptionalParamsNotIncluded(){
 		String queryString = "program:TESTCODE";
-		String[] parameters = new ApplicationQueryStringParser().parse(queryString);
-		assertEquals("TESTCODE", parameters[0]);
-		assertNull( parameters[1]);
-		assertNull(parameters[2]);
-		assertNull( parameters[3]);
+		Map<String, String> parameters = new ApplicationQueryStringParser().parse(queryString);
+		assertEquals(1, parameters.size());
+		assertEquals("TESTCODE", parameters.get("program"));
 	}
 	
-	@Test 
-	public void shouldReturnCorrectValuesIfProjectTitleContainsDelimiter(){
-		String queryString = "program:TESTCODE||programhome:http://www.google.com||bacthdeadline:29-Oct-2012||projectTitle:Test||project";
-		String[] parameters = new ApplicationQueryStringParser().parse(queryString);
-		assertEquals("TESTCODE", parameters[0]);
-		assertEquals("http://www.google.com", parameters[1]);
-		assertEquals("29-Oct-2012", parameters[2]);
-		assertEquals("Test||project", parameters[3]);
-	}
-	
-	@Test 
-	public void shouldAddProtocolToUrlIfNotProvided(){
-		String queryString ="programhome:www.google.com";
-		String[] parameters = new ApplicationQueryStringParser().parse(queryString);
-		assertNull( parameters[0]);
-		assertEquals("http://www.google.com", parameters[1]);
-		assertNull(parameters[2]);
-		assertNull( parameters[3]);
-	}
 }

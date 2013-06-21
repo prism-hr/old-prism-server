040c03e9c83f8c63437cf7cc56615807bd5c1bb8
ked <ked@LONN0166.ads.zuehlke.com>
2013-06-21 12:03:42 +0100

    * Unit tests for PRISM-438

--- src/test/java/com/zuehlke/pgadmissions/controllers/ResearchOpportunitiesFeedControllerTest.java
+++ src/test/java/com/zuehlke/pgadmissions/controllers/ResearchOpportunitiesFeedControllerTest.java
@@ -1,7 +1,9 @@
 package com.zuehlke.pgadmissions.controllers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import junit.framework.Assert;
@@ -42,7 +44,7 @@ public class ResearchOpportunitiesFeedControllerTest {
         currentUser = new RegisteredUserBuilder().build();
         programsServiceMock = EasyMock.createMock(ProgramsService.class);
         feedServiceMock = EasyMock.createMock(ResearchOpportunitiesFeedService.class);
-        messageSourceMock = EasyMock.createMock(MessageSource.class);
+        messageSourceMock = EasyMock.createNiceMock(MessageSource.class);
         userServiceMock = EasyMock.createMock(UserService.class);
         controller = new ResearchOpportunitiesFeedController(userServiceMock, programsServiceMock, feedServiceMock, messageSourceMock) {
             @Override
@@ -100,5 +102,140 @@ public class ResearchOpportunitiesFeedControllerTest {
         EasyMock.verify(feedServiceMock);
         Assert.assertEquals(1, map.size());
         Assert.assertEquals(true, map.get("success"));
+    }
+    
+    @Test
+    public void shouldUpdateFeedById() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("selectedPrograms", Arrays.asList(1));
+        json.put("feedSize", "SMALL");
+        json.put("feedTitle", "foobar2");
+     
+        EasyMock.expect(feedServiceMock.updateFeed(feed.getId(), Arrays.asList(1), currentUser, FeedFormat.SMALL, "foobar2")).andReturn(feed);
+        EasyMock.expect(feedServiceMock.getIframeHtmlCode(feed)).andReturn(StringUtils.EMPTY);
+        EasyMock.replay(feedServiceMock);
+        
+        Map<String, Object> map = controller.updateFeedById(feed.getId(), json);
+
+        EasyMock.verify(feedServiceMock);
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(true, map.get("success"));
+        Assert.assertEquals(StringUtils.EMPTY, map.get("iframeCode"));
+    }
+    
+    @Test
+    public void shouldNotValidateIfTitleIsEmpty() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("selectedPrograms", Arrays.asList(1));
+        json.put("feedSize", "SMALL");
+        json.put("feedTitle", StringUtils.EMPTY);
+     
+        Map<String, Object> map = controller.updateFeedById(feed.getId(), json);
+
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(false, map.get("success"));
+        Assert.assertTrue(map.containsKey("feedTitle"));
+    }
+    
+    @Test
+    public void shouldNotValidateIfFeedFormatIsEmpty() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("selectedPrograms", Arrays.asList(1));
+        json.put("feedSize", StringUtils.EMPTY);
+        json.put("feedTitle", "foobar2");
+     
+        Map<String, Object> map = controller.updateFeedById(feed.getId(), json);
+
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(false, map.get("success"));
+        Assert.assertTrue(map.containsKey("feedSize"));
+    }
+    
+    @Test
+    public void shouldNotValidateIfProgramsIsEmpty() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("feedSize", "LARGE");
+        json.put("feedTitle", "foobar2");
+     
+        EasyMock.expect(messageSourceMock.getMessage(EasyMock.anyObject(String.class), EasyMock.anyObject(Object[].class), EasyMock.anyObject(Locale.class))).andReturn(StringUtils.EMPTY);
+        EasyMock.replay(messageSourceMock);
+        
+        Map<String, Object> map = controller.updateFeedById(feed.getId(), json);
+        EasyMock.verify(messageSourceMock);
+
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(false, map.get("success"));
+        Assert.assertTrue(map.containsKey("selectedPrograms"));
+    }
+    
+    @Test
+    public void shouldReturnFeedById() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        EasyMock.expect(feedServiceMock.getById(feed.getId())).andReturn(feed);
+        EasyMock.expect(feedServiceMock.getIframeHtmlCode(feed)).andReturn(StringUtils.EMPTY);
+        EasyMock.replay(feedServiceMock);
+        
+        List<Map<String,Object>> response = controller.getFeedById(feed.getId());
+        
+        EasyMock.verify(feedServiceMock);
+        
+        Assert.assertEquals(1, response.size());
+        
+        Map<String, Object> map = response.get(0);
+        Assert.assertEquals(5, map.size());
+        Assert.assertEquals(feed.getId(), map.get("id"));
+        Assert.assertEquals(feed.getTitle(), map.get("title"));
+        Assert.assertEquals(FeedFormat.LARGE, map.get("feedSize"));
+        Assert.assertEquals(Arrays.asList(1), map.get("selectedPrograms"));
+        Assert.assertEquals(StringUtils.EMPTY, map.get("iframeCode"));
+    }
+    
+    @Test
+    public void shouldSaveFeed() {
+        Program program = new ProgramBuilder().id(1).title("foo").build();
+        ResearchOpportunitiesFeed feed = new ResearchOpportunitiesFeedBuilder().id(1).title("foobar").feedFormat(FeedFormat.LARGE).programs(program).build();
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("selectedPrograms", Arrays.asList(1));
+        json.put("feedSize", "LARGE");
+        json.put("feedTitle", "foobar2");
+     
+        EasyMock.expect(feedServiceMock.saveNewFeed(Arrays.asList(1), currentUser, FeedFormat.LARGE, "foobar2")).andReturn(feed);
+        EasyMock.expect(feedServiceMock.isUniqueFeedTitleForUser("foobar2", currentUser)).andReturn(true);
+        EasyMock.expect(feedServiceMock.getIframeHtmlCode(feed)).andReturn(StringUtils.EMPTY);
+        EasyMock.replay(feedServiceMock);
+        
+        Map<String, Object> map = controller.saveFeed(json);
+
+        EasyMock.verify(feedServiceMock);
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(true, map.get("success"));
+        Assert.assertEquals(StringUtils.EMPTY, map.get("iframeCode"));
+    }
+    
+    @Test
+    public void shouldNotSaveFeedIfUserHasSimiliarFeedWithSameTitle() {
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("selectedPrograms", Arrays.asList(1));
+        json.put("feedSize", "LARGE");
+        json.put("feedTitle", "foobar2");
+     
+        EasyMock.expect(feedServiceMock.isUniqueFeedTitleForUser("foobar2", currentUser)).andReturn(false);
+        EasyMock.replay(feedServiceMock);
+        
+        Map<String, Object> map = controller.saveFeed(json);
+
+        EasyMock.verify(feedServiceMock);
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals(false, map.get("success"));
+        Assert.assertTrue(map.containsKey("feedTitle"));
     }
 }

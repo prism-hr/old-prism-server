133d596c6e69bdc8855799f5a7da6023c430d04d
jfi <jfi@zuhlke.com>
2013-07-30 10:52:07 +0100

    PRISM-518 More clean-up

--- src/main/java/com/zuehlke/pgadmissions/dao/SupervisorDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/SupervisorDAO.java
@@ -1,24 +1,14 @@
 package com.zuehlke.pgadmissions.dao;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.lang.time.DateUtils;
-import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
-import com.zuehlke.pgadmissions.domain.ReminderInterval;
 import com.zuehlke.pgadmissions.domain.Supervisor;
-import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
 
 @Repository
-@SuppressWarnings("unchecked")
 public class SupervisorDAO {
 
 	private final SessionFactory sessionFactory;
@@ -40,64 +30,8 @@ public class SupervisorDAO {
 		sessionFactory.getCurrentSession().saveOrUpdate(supervisor);
 	}
 
-	/**
-	 * @deprecated This method is now being replaced by the {@link #getPrimarySupervisorsDueNotification() getPrimarySupervisorsDueNotification} method.
-	 */
-	@Deprecated
-	public List<Supervisor> getSupervisorsDueNotification() {
-		return sessionFactory.getCurrentSession()
-		        .createCriteria(Supervisor.class, "supervisor")
-		        .add(Restrictions.isNull("lastNotified"))
-				.createAlias("approvalRound.application", "application")
-				.add(Restrictions.eq("application.status", ApplicationFormStatus.APPROVAL))
-				.add(Restrictions.eqProperty("approvalRound", "application.latestApprovalRound"))
-				.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
-				.list();
-	}
-
     public Supervisor getSupervisorByUser(RegisteredUser user) {
         return (Supervisor) sessionFactory.getCurrentSession().createCriteria(Supervisor.class).add(Restrictions.eq("user", user)).uniqueResult();
     }
 
-    public List<Supervisor> getPrimarySupervisorsDueNotification() {
-        return sessionFactory.getCurrentSession()
-                .createCriteria(Supervisor.class, "supervisor")
-                .add(Restrictions.isNull("lastNotified"))//A supervisor who hasn't confirmed supervision will not be reminded because of this restriction...seems to be wrong!
-                .add(Restrictions.isNull("confirmedSupervision"))
-                .add(Restrictions.eq("isPrimary", true))
-                .createAlias("approvalRound.application", "application")
-                .add(Restrictions.eq("application.status", ApplicationFormStatus.APPROVAL))
-                .add(Restrictions.eqProperty("approvalRound", "application.latestApprovalRound"))
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
-                .list();
-    }
-
-    public List<Supervisor> getPrimarySupervisorsDueReminder() {
-        Date today = Calendar.getInstance().getTime();
-        ReminderInterval reminderInterval = (ReminderInterval) sessionFactory.getCurrentSession().createCriteria(ReminderInterval.class).uniqueResult();
-        Date dateWithSubtractedInterval = DateUtils.addMinutes(today, -reminderInterval.getDurationInMinutes());
-        return sessionFactory.getCurrentSession()
-                .createCriteria(Supervisor.class, "supervisor")
-                .add(Restrictions.le("lastNotified", dateWithSubtractedInterval))
-                .add(Restrictions.eq("isPrimary", true))
-                .add(Restrictions.isNull("confirmedSupervision"))
-                .createAlias("approvalRound.application", "application")
-                .add(Restrictions.eq("application.status", ApplicationFormStatus.APPROVAL))
-                .add(Restrictions.eqProperty("approvalRound", "application.latestApprovalRound"))
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
-                .list();
-    }
-    
-    public List<Supervisor> getPrimarySupervisorsWhichHaveRecentlyBeenConfirmedInTheLast24Hours() {
-        return sessionFactory.getCurrentSession()
-                .createCriteria(Supervisor.class, "supervisor")
-                .add(Restrictions.ge("confirmedSupervisionDate", new DateTime().minusDays(1).toDate()))
-                .add(Restrictions.eq("confirmedSupervision", true))
-                .add(Restrictions.eq("isPrimary", true))
-                .createAlias("approvalRound.application", "application")
-                .add(Restrictions.eq("application.status", ApplicationFormStatus.APPROVAL))
-                .add(Restrictions.eqProperty("approvalRound", "application.latestApprovalRound"))
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
-                .list();
-    }
 }

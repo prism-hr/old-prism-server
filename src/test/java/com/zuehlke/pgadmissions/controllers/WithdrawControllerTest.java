935b109b14a88b65eb0b6599965459e9fb3233c6
jfi <jfi@zuhlke.com>
2013-11-05 16:01:15 +0000

    Refactored Withdraw controller and service

--- src/test/java/com/zuehlke/pgadmissions/controllers/WithdrawControllerTest.java
+++ src/test/java/com/zuehlke/pgadmissions/controllers/WithdrawControllerTest.java
@@ -1,7 +1,6 @@
 package com.zuehlke.pgadmissions.controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.easymock.EasyMock;
 import org.junit.Before;
@@ -11,18 +10,15 @@ import org.springframework.ui.ModelMap;
 import com.zuehlke.pgadmissions.components.ActionsProvider;
 import com.zuehlke.pgadmissions.domain.ApplicationForm;
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
-import com.zuehlke.pgadmissions.domain.StateChangeEvent;
 import com.zuehlke.pgadmissions.domain.builders.ApplicationFormBuilder;
 import com.zuehlke.pgadmissions.domain.builders.RegisteredUserBuilder;
 import com.zuehlke.pgadmissions.domain.builders.RoleBuilder;
-import com.zuehlke.pgadmissions.domain.builders.StateChangeEventBuilder;
 import com.zuehlke.pgadmissions.domain.enums.ApplicationFormStatus;
 import com.zuehlke.pgadmissions.domain.enums.Authority;
 import com.zuehlke.pgadmissions.dto.ApplicationFormAction;
 import com.zuehlke.pgadmissions.exceptions.application.MissingApplicationFormException;
 import com.zuehlke.pgadmissions.services.ApplicationFormAccessService;
 import com.zuehlke.pgadmissions.services.ApplicationsService;
-import com.zuehlke.pgadmissions.services.EventFactory;
 import com.zuehlke.pgadmissions.services.UserService;
 import com.zuehlke.pgadmissions.services.WithdrawService;
 
@@ -33,7 +29,6 @@ public class WithdrawControllerTest {
     private WithdrawService withdrawServiceMock;
     private RegisteredUser student;
 
-    private EventFactory eventFactoryMock;
     private UserService userServiceMock;
     private ApplicationFormAccessService accessServiceMock;
     private ActionsProvider actionsProviderMock;
@@ -47,49 +42,15 @@ public class WithdrawControllerTest {
         modelMap.put("user", student);
 
         actionsProviderMock.validateAction(applicationForm, student, ApplicationFormAction.WITHDRAW);
-        withdrawServiceMock.saveApplicationFormAndSendMailNotifications(applicationForm);
+        withdrawServiceMock.withdrawApplication(applicationForm);
 
-        StateChangeEvent event = new StateChangeEventBuilder().id(1).build();
-        EasyMock.expect(eventFactoryMock.createEvent(ApplicationFormStatus.WITHDRAWN)).andReturn(event);
         withdrawServiceMock.sendToPortico(applicationForm);
 
-        EasyMock.replay(withdrawServiceMock, eventFactoryMock, actionsProviderMock);
+        EasyMock.replay(withdrawServiceMock, actionsProviderMock);
         String view = withdrawController.withdrawApplicationAndGetApplicationList(modelMap);
-        EasyMock.verify(withdrawServiceMock, eventFactoryMock, actionsProviderMock);
+        EasyMock.verify(withdrawServiceMock, actionsProviderMock);
 
-        assertEquals(ApplicationFormStatus.WITHDRAWN, applicationForm.getStatus());
         assertEquals("redirect:/applications?messageCode=application.withdrawn&application=abc", view);
-
-        assertEquals(1, applicationForm.getEvents().size());
-        assertEquals(event, applicationForm.getEvents().get(0));
-    }
-
-    @Test
-    public void shouldChangeStatusToWithdrawnAndSetWithdrawnBeforeSubmitToTrue() {
-        ApplicationForm applicationForm = new ApplicationFormBuilder().status(ApplicationFormStatus.UNSUBMITTED).applicant(student).id(2)
-                .applicationNumber("abc").build();
-        ModelMap modelMap = new ModelMap();
-        modelMap.put("applicationForm", applicationForm);
-        modelMap.put("user", student);
-
-        actionsProviderMock.validateAction(applicationForm, student, ApplicationFormAction.WITHDRAW);
-        withdrawServiceMock.saveApplicationFormAndSendMailNotifications(applicationForm);
-
-        StateChangeEvent event = new StateChangeEventBuilder().id(1).build();
-        EasyMock.expect(eventFactoryMock.createEvent(ApplicationFormStatus.WITHDRAWN)).andReturn(event);
-        withdrawServiceMock.sendToPortico(applicationForm);
-
-        EasyMock.replay(withdrawServiceMock, eventFactoryMock);
-
-        String view = withdrawController.withdrawApplicationAndGetApplicationList(modelMap);
-
-        EasyMock.verify(withdrawServiceMock);
-        assertTrue(applicationForm.getWithdrawnBeforeSubmit());
-        assertEquals(ApplicationFormStatus.WITHDRAWN, applicationForm.getStatus());
-        assertEquals("redirect:/applications?messageCode=application.withdrawn&application=abc", view);
-
-        assertEquals(1, applicationForm.getEvents().size());
-        assertEquals(event, applicationForm.getEvents().get(0));
     }
 
     @Test
@@ -119,12 +80,11 @@ public class WithdrawControllerTest {
     public void setUp() {
         applicationsServiceMock = EasyMock.createMock(ApplicationsService.class);
         withdrawServiceMock = EasyMock.createMock(WithdrawService.class);
-        eventFactoryMock = EasyMock.createMock(EventFactory.class);
         userServiceMock = EasyMock.createMock(UserService.class);
         accessServiceMock = EasyMock.createMock(ApplicationFormAccessService.class);
         actionsProviderMock = EasyMock.createMock(ActionsProvider.class);
 
-        withdrawController = new WithdrawController(applicationsServiceMock, userServiceMock, withdrawServiceMock, eventFactoryMock, accessServiceMock,
+        withdrawController = new WithdrawController(applicationsServiceMock, userServiceMock, withdrawServiceMock, accessServiceMock,
                 actionsProviderMock);
 
         student = new RegisteredUserBuilder().id(1).username("mark").email("mark@gmail.com").firstName("mark").lastName("ham")

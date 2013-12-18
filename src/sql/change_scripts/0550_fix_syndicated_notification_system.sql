d51cba7b1ea5afe4b21ce8e52e625373f791f108
alastair <a.knowles@ucl.ac.uk>
2013-12-18 17:09:54 +0000

    Error in SQL change script.

--- src/sql/change_scripts/0550_fix_syndicated_notification_system.sql
+++ src/sql/change_scripts/0550_fix_syndicated_notification_system.sql
@@ -1,6 +1,4 @@
 ALTER TABLE REGISTERED_USER
 	ADD COLUMN latest_update_notification_date DATETIME AFTER latest_task_notification_date,
-	ADD COLUMN latest_digest_notification_type VARCHAR(50),
-	ADD INDEX (latest_update_notification_date),
-	ADD INDEX (latest_digest_notification_type)
+	ADD INDEX (latest_update_notification_date)
 ;

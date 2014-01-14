98d72e459963b8198fd539fc4e0ab7ce098180c7
alastair <a.knowles@ucl.ac.uk>
2014-01-14 13:42:30 +0000

    Finally fixed the hibernate proxy error with the approval around functionality.

--- src/main/java/com/zuehlke/pgadmissions/dao/ApprovalRoundDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/ApprovalRoundDAO.java
@@ -1,10 +1,12 @@
 package com.zuehlke.pgadmissions.dao;
 
+import org.hibernate.Hibernate;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.zuehlke.pgadmissions.domain.ApprovalRound;
+import com.zuehlke.pgadmissions.domain.Supervisor;
 
 @Repository
 public class ApprovalRoundDAO {
@@ -26,7 +28,15 @@ public class ApprovalRoundDAO {
 
 	public void save(ApprovalRound approvalRound) {
 		sessionFactory.getCurrentSession().saveOrUpdate(approvalRound);
-		
+	}
+	
+	public void saveAndInitialise(ApprovalRound approvalRound)
+	{
+	    save(approvalRound);
+	    for (Supervisor supervisor : approvalRound.getSupervisors()) {
+	        Hibernate.initialize(supervisor);
+	        Hibernate.initialize(supervisor.getUser());
+	    }
 	}
 
 }

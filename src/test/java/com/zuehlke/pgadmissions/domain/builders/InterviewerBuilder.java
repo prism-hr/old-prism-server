71cf91af020d6e159dd103d9391800a78b2178b6
jfi <jfi@zuhlke.com>
2014-01-27 12:45:58 +0000

    Cleaned up Interviewer and Reviewer entities CheckedStatus removed

--- src/test/java/com/zuehlke/pgadmissions/domain/builders/InterviewerBuilder.java
+++ src/test/java/com/zuehlke/pgadmissions/domain/builders/InterviewerBuilder.java
@@ -8,65 +8,44 @@ import com.zuehlke.pgadmissions.domain.Interviewer;
 import com.zuehlke.pgadmissions.domain.RegisteredUser;
 
 public class InterviewerBuilder {
-	private Integer id;
-	private RegisteredUser user;
-	private Date lastNotified;
-	private Interview interview;
-	private boolean requiresAdminNotification;
-	private Date dateAdminsNotified;
-	private boolean firstAdminNotification;
-	private InterviewComment interviewComment;
+    private Integer id;
+    private RegisteredUser user;
+    private Date lastNotified;
+    private Interview interview;
+    private InterviewComment interviewComment;
 
-	public InterviewerBuilder interviewComment(InterviewComment interviewComment) {
-		this.interviewComment = interviewComment;
-		return this;
-	}
+    public InterviewerBuilder interviewComment(InterviewComment interviewComment) {
+        this.interviewComment = interviewComment;
+        return this;
+    }
 
-	public InterviewerBuilder dateAdminsNotified(Date dateAdminsNotified) {
-		this.dateAdminsNotified = dateAdminsNotified;
-		return this;
-	}
+    public InterviewerBuilder interview(Interview interview) {
+        this.interview = interview;
+        return this;
+    }
 
-	public InterviewerBuilder requiresAdminNotification(boolean requiresAdminNotification) {
-		this.requiresAdminNotification = requiresAdminNotification;
-		return this;
-	}
+    public InterviewerBuilder lastNotified(Date lastNotified) {
+        this.lastNotified = lastNotified;
+        return this;
+    }
 
-	public InterviewerBuilder interview(Interview interview) {
-		this.interview = interview;
-		return this;
-	}
+    public InterviewerBuilder id(Integer id) {
+        this.id = id;
+        return this;
+    }
 
-	public InterviewerBuilder lastNotified(Date lastNotified) {
-		this.lastNotified = lastNotified;
-		return this;
-	}
+    public InterviewerBuilder user(RegisteredUser user) {
+        this.user = user;
+        return this;
+    }
 
-	public InterviewerBuilder id(Integer id) {
-		this.id = id;
-		return this;
-	}
-
-	public InterviewerBuilder user(RegisteredUser user) {
-		this.user = user;
-		return this;
-	}
-
-	public InterviewerBuilder firstAdminNotification(boolean firstNotification) {
-		this.firstAdminNotification = firstNotification;
-		return this;
-	}
-
-	public Interviewer build() {
-		Interviewer interviewer = new Interviewer();
-		interviewer.setId(id);
-		interviewer.setRequiresAdminNotification(requiresAdminNotification);
-		interviewer.setFirstAdminNotification(firstAdminNotification);
-		interviewer.setUser(user);
-		interviewer.setLastNotified(lastNotified);
-		interviewer.setInterview(interview);
-		interviewer.setDateAdminsNotified(dateAdminsNotified);
-		interviewer.setInterviewComment(interviewComment);
-		return interviewer;
-	}
+    public Interviewer build() {
+        Interviewer interviewer = new Interviewer();
+        interviewer.setId(id);
+        interviewer.setUser(user);
+        interviewer.setLastNotified(lastNotified);
+        interviewer.setInterview(interview);
+        interviewer.setInterviewComment(interviewComment);
+        return interviewer;
+    }
 }

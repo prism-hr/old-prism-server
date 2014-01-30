b02b607d0b6cb186698bfbca6bda0d8fead08a95
alastair <a.knowles@ucl.ac.uk>
2014-01-30 09:32:29 +0000

    Completed draft of the recommender functionality (backend).

--- src/main/java/com/zuehlke/pgadmissions/dao/AdvertDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/AdvertDAO.java
@@ -1,8 +1,9 @@
 package com.zuehlke.pgadmissions.dao;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -17,7 +18,6 @@ import com.zuehlke.pgadmissions.domain.Advert;
 import com.zuehlke.pgadmissions.domain.ApplicationFormUserRole;
 import com.zuehlke.pgadmissions.domain.Program;
 import com.zuehlke.pgadmissions.domain.Project;
-import com.zuehlke.pgadmissions.domain.RegisteredUser;
 import com.zuehlke.pgadmissions.domain.enums.Authority;
 
 @Repository
@@ -25,16 +25,13 @@ public class AdvertDAO {
 
     private final SessionFactory sessionFactory;
 
-    private final RoleDAO roleDAO;
-
     public AdvertDAO() {
-        this(null, null);
+        this(null);
     }
 
     @Autowired
-    public AdvertDAO(SessionFactory sessionFactory, RoleDAO roleDAO) {
+    public AdvertDAO(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-        this.roleDAO = roleDAO;
     }
 
     public void save(Advert advert) {
@@ -42,11 +39,13 @@ public class AdvertDAO {
     }
 
     public Program getProgram(Advert advert) {
-        return (Program) sessionFactory.getCurrentSession().createCriteria(Program.class).add(Restrictions.eq("advert", advert)).uniqueResult();
+        return (Program) sessionFactory.getCurrentSession().createCriteria(Program.class)
+                .add(Restrictions.eq("advert", advert)).uniqueResult();
     }
 
     public Project getProject(Advert advert) {
-        return (Project) sessionFactory.getCurrentSession().createCriteria(Project.class).add(Restrictions.eq("advert", advert)).uniqueResult();
+        return (Project) sessionFactory.getCurrentSession().createCriteria(Project.class)
+                .add(Restrictions.eq("advert", advert)).uniqueResult();
     }
 
     public Advert getAdvertById(int advertId) {
@@ -55,11 +54,24 @@ public class AdvertDAO {
 
     @SuppressWarnings("unchecked")
     public List<Advert> getActiveAdverts() {
-        return sessionFactory.getCurrentSession().createCriteria(Advert.class).add(Restrictions.eq("active", true)).list();
+        return sessionFactory.getCurrentSession().createCriteria(Advert.class)
+                .add(Restrictions.eq("active", true)).list();
     }
-
+    
+    /**
+     * Returns a list of other recommended opportunities for a given applicant as follows:
+     * - Query 1: programs related to the seed application through associations between users, applications and programs.
+     * - Query 2: projects related to the seed application through associations between users, applications and programs.
+     * - Query 3: projects related to the seed application through associations between users, applications and projects.
+     * @param user
+     * @return List<Advert>
+     * @author Alastair Knowles
+     */
     @SuppressWarnings("unchecked")
-    public List<Advert> getRecommendedAdverts(RegisteredUser user) {
+    public List<Advert> getRecommendedAdverts(String applicationNumber) {
+        Authority[] authoritiesToConsider = {Authority.APPLICANT, Authority.INTERVIEWER, Authority.REVIEWER,
+                Authority.SUGGESTEDSUPERVISOR, Authority.STATEADMINISTRATOR, Authority.SUGGESTEDSUPERVISOR, Authority.SUPERVISOR};
+        
         HashSet<Advert> adverts = new HashSet<Advert>((List<Advert>) sessionFactory.getCurrentSession().createCriteria(ApplicationFormUserRole.class)
                 .setProjection(Projections.groupProperty("program2.advert"))
                 .createAlias("applicationForm", "application", JoinType.INNER_JOIN)
@@ -69,45 +81,29 @@ public class AdvertDAO {
                 .createAlias("applicationFormUserRole2.applicationForm", "application3", JoinType.INNER_JOIN)
                 .createAlias("application3.program", "program2", JoinType.INNER_JOIN)
                 .createAlias("program2.advert", "advert", JoinType.INNER_JOIN)
-                .add(Restrictions.eq("user", user))
-                .add(Restrictions.eq("role", roleDAO.getRoleByAuthority(Authority.APPLICANT)))
-                .add(Restrictions.neProperty("user", "applicationFormUserRole2.user"))
-                .add(Restrictions.in("applicationFormUserRole2.role", Arrays.asList(Authority.APPLICANT, Authority.INTERVIEWER, Authority.REVIEWER,
-                        Authority.SUGGESTEDSUPERVISOR, Authority.STATEADMINISTRATOR, Authority.SUGGESTEDSUPERVISOR, Authority.SUPERVISOR)))
-                .add(Restrictions.eq("program.enabled", false)) 
+                .add(Restrictions.eq("application.applicationNumber", applicationNumber))
+                .add(Restrictions.neProperty("application.applicant", "application3.applicant"))
+                .add(Restrictions.in("applicationFormUserRole2.role.id", authoritiesToConsider))
+                .add(Restrictions.eq("program2.enabled", true)) 
                 .add(Restrictions.eq("advert.active", true)).list());
+        
         adverts.addAll((List<Advert>) sessionFactory.getCurrentSession().createCriteria(ApplicationFormUserRole.class)
-                .setProjection(Projections.groupProperty("program2.advert"))
+                .setProjection(Projections.groupProperty("project.advert"))
                 .createAlias("applicationForm", "application", JoinType.INNER_JOIN)
                 .createAlias("application.program", "program", JoinType.INNER_JOIN)
                 .createAlias("program.applications", "application2", JoinType.INNER_JOIN)
                 .createAlias("application2.applicationFormUserRoles", "applicationFormUserRole2", JoinType.INNER_JOIN)
                 .createAlias("applicationFormUserRole2.applicationForm", "application3", JoinType.INNER_JOIN)
                 .createAlias("application3.program", "program2", JoinType.INNER_JOIN)
-                .createAlias("program2.advert", "advert", JoinType.INNER_JOIN)
-                .add(Restrictions.eq("user", user))
-                .add(Restrictions.eq("role", roleDAO.getRoleByAuthority(Authority.APPLICANT)))
-                .add(Restrictions.neProperty("user", "applicationFormUserRole2.user"))
-                .add(Restrictions.in("applicationFormUserRole2.role", Arrays.asList(Authority.APPLICANT, Authority.INTERVIEWER, Authority.REVIEWER,
-                        Authority.SUGGESTEDSUPERVISOR, Authority.STATEADMINISTRATOR, Authority.SUGGESTEDSUPERVISOR, Authority.SUPERVISOR)))
-                .add(Restrictions.eq("program.enabled", false)) 
+                .createAlias("program2.projects", "project", JoinType.INNER_JOIN)
+                .createAlias("project.advert", "advert", JoinType.INNER_JOIN)
+                .createAlias("project.applications", "application4", JoinType.INNER_JOIN)
+                .add(Restrictions.eq("application.applicationNumber", applicationNumber))
+                .add(Restrictions.neProperty("application.applicant", "application4.applicant"))
+                .add(Restrictions.in("applicationFormUserRole2.role.id", authoritiesToConsider))
+                .add(Restrictions.eq("project.disabled", false)) 
                 .add(Restrictions.eq("advert.active", true)).list());
-        adverts.addAll((List<Advert>) sessionFactory.getCurrentSession().createCriteria(ApplicationFormUserRole.class)
-                .setProjection(Projections.groupProperty("program2.advert"))
-                .createAlias("applicationForm", "application", JoinType.INNER_JOIN)
-                .createAlias("application.project", "project", JoinType.INNER_JOIN)
-                .createAlias("project.applications", "application2", JoinType.INNER_JOIN)
-                .createAlias("application2.applicationFormUserRoles", "applicationFormUserRole2", JoinType.INNER_JOIN)
-                .createAlias("applicationFormUserRole2.applicationForm", "application3", JoinType.INNER_JOIN)
-                .createAlias("application3.program", "program2", JoinType.INNER_JOIN)
-                .createAlias("program2.advert", "advert", JoinType.INNER_JOIN)
-                .add(Restrictions.eq("user", user))
-                .add(Restrictions.eq("role", roleDAO.getRoleByAuthority(Authority.APPLICANT)))
-                .add(Restrictions.neProperty("user", "applicationFormUserRole2.user"))
-                .add(Restrictions.in("applicationFormUserRole2.role", Arrays.asList(Authority.APPLICANT, Authority.INTERVIEWER, Authority.REVIEWER,
-                        Authority.SUGGESTEDSUPERVISOR, Authority.STATEADMINISTRATOR, Authority.SUGGESTEDSUPERVISOR, Authority.SUPERVISOR)))
-                .add(Restrictions.eq("project.disabled", false))
-                .add(Restrictions.eq("advert.active", true)).list());
+        
         adverts.addAll((List<Advert>) sessionFactory.getCurrentSession().createCriteria(ApplicationFormUserRole.class)
                 .setProjection(Projections.groupProperty("project2.advert"))
                 .createAlias("applicationForm", "application", JoinType.INNER_JOIN)
@@ -115,18 +111,29 @@ public class AdvertDAO {
                 .createAlias("project.applications", "application2", JoinType.INNER_JOIN)
                 .createAlias("application2.applicationFormUserRoles", "applicationFormUserRole2", JoinType.INNER_JOIN)
                 .createAlias("applicationFormUserRole2.applicationForm", "application3", JoinType.INNER_JOIN)
-                .createAlias("application3.program", "program2", JoinType.INNER_JOIN)
-                .createAlias("program.projects", "project2")
+                .createAlias("application3.project", "project2", JoinType.INNER_JOIN)
                 .createAlias("project2.advert", "advert", JoinType.INNER_JOIN)
-                .add(Restrictions.eq("user", user))
-                .add(Restrictions.eq("role", roleDAO.getRoleByAuthority(Authority.APPLICANT)))
-                .add(Restrictions.neProperty("user", "applicationFormUserRole2.user"))
-                .add(Restrictions.in("applicationFormUserRole2.role", Arrays.asList(Authority.APPLICANT, Authority.INTERVIEWER, Authority.REVIEWER,
-                        Authority.SUGGESTEDSUPERVISOR, Authority.STATEADMINISTRATOR, Authority.SUGGESTEDSUPERVISOR, Authority.SUPERVISOR)))
+                .add(Restrictions.eq("application.applicationNumber", applicationNumber))
+                .add(Restrictions.neProperty("application.applicant", "application3.applicant"))
+                .add(Restrictions.in("applicationFormUserRole2.role.id", authoritiesToConsider))
                 .add(Restrictions.eq("project2.disabled", false)) 
                 .add(Restrictions.eq("advert.active", true)).list());
+        
         List<Advert> deduplicatedAdverts = new ArrayList<Advert>(adverts);
-        Collections.shuffle(deduplicatedAdverts);
+        
+        Collections.sort(deduplicatedAdverts, new Comparator<Advert>() {
+            @Override
+            public int compare(Advert advert1, Advert advert2) {
+                Date date1 = advert1.getLastEditedTimestamp();
+                Date date2 = advert2.getLastEditedTimestamp();
+                if (date1.after(date2))
+                    return 1;
+                if (date2.after(date1))
+                    return -1;
+                return 0;
+            }
+        });
+        
         return deduplicatedAdverts;
     }
 
@@ -136,5 +143,5 @@ public class AdvertDAO {
         }
         sessionFactory.getCurrentSession().delete(advert);
     }
-
+    
 }

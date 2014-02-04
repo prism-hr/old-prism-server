81abfb71f7ec5eccb16e0f24b8d9c6847cefe1e1
jfi <jfi@zuhlke.com>
2014-02-04 13:37:12 +0000

    Fixed adding last_edited_timestamp changescript (all zeros)

--- src/sql/change_scripts/0582_sort_adverts_on_edited_created_date.sql
+++ src/sql/change_scripts/0582_sort_adverts_on_edited_created_date.sql
@@ -1,4 +1,7 @@
 ALTER TABLE ADVERT
-	ADD COLUMN last_edited_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	ADD COLUMN last_edited_timestamp TIMESTAMP NOT NULL DEFAULT now() ON UPDATE now(),
 	ADD INDEX (last_edited_timestamp)
+;
+
+UPDATE ADVERT SET last_edited_timestamp = now()
 ;

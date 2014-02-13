5808b7f4de6d0e84f8f086dd776ed9952a0f3ae9
Juan Mingo <me@juanmingo.co.uk>
2014-02-13 16:13:28 +0000

    updated requests

--- src/main/webapp/WEB-INF/design/default/js/superAdmin/requests.js
+++ src/main/webapp/WEB-INF/design/default/js/superAdmin/requests.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-    $('#opportunityRequestsList').on('click', '.opportunityRequestActionType', function() {
+    $('.selectpicker').selectpicker();
+
+    $(document).on('change', 'select.opportunityRequestActionType', function() {
         requestId = $(this).attr('data-request-id');
         
         $('#ajaxloader').show();
-        switch ($(this).attr('data-value')) {
-        case 'approve':  window.location.href = "/pgadmissions/requests/edit/"+ requestId;
+        switch ($(this).val()) {
+        case 'approve':
+            window.location.href = "/pgadmissions/requests/edit/"+ requestId;
             break;
         case 'view':  window.location.href = "/pgadmissions/requests/edit/"+ requestId;
+            break;
+        case 'email':
+            $('#ajaxloader').fadeOut('fast');
+            $(this).val($("select.actionType option:first").val());
+            var subject = "Question Regarding UCL Prism Program request: " + $(this).attr('data-program');
+            var email = $(this).attr('data-email');
+            window.location.href ="mailto:" + email + "?subject=" + subject;
+            skip = true;
             break;
         };
     });

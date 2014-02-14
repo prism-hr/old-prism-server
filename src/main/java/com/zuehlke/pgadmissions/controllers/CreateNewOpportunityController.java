ffc734d6ea1c4891d85fb3839c6d3c9c73923ba6
jfi <jfi@zuhlke.com>
2014-02-14 16:38:34 +0000

    Missing change to prevous commit

--- src/main/java/com/zuehlke/pgadmissions/controllers/CreateNewOpportunityController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/CreateNewOpportunityController.java
@@ -90,7 +90,7 @@ public class CreateNewOpportunityController {
             return LOGIN_PAGE;
         }
 
-        opportunitiesService.createNewOpportunityRequestAndAuthor(opportunityRequest);
+        opportunitiesService.createOpportunityRequest(opportunityRequest, true);
 
         model.addAttribute("pendingUser", opportunityRequest.getAuthor());
         return REGISTER_COMPLETE_VIEW_NAME;

8e8bf6598f5f66c395ee1695e9c7a8c6d533db53
jfi <jfi@zuhlke.com>
2014-03-14 13:12:22 +0000

    Clean up in sql setup queries

--- src/sql/misc/insertTestDurationsForStages.mysql.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO STAGE_DURATION(stage, duration, unit) VALUES ('VALIDATION', 1, 'DAYS');
-INSERT INTO STAGE_DURATION(stage, duration, unit) VALUES ('REVIEW', 1, 'WEEKS');
-INSERT INTO STAGE_DURATION(stage, duration, unit) VALUES ('INTERVIEW', 2, 'DAYS');
-INSERT INTO STAGE_DURATION(stage, duration, unit) VALUES ('APPROVAL', 3, 'DAYS');

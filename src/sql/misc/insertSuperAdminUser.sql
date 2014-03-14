39d7e64c753800a6d1ba7c32416468fa65f11393
jfi <jfi@zuhlke.com>
2014-03-14 13:12:22 +0000

    Clean up in sql setup queries

--- src/sql/misc/insertSuperAdminUser.sql
+++
@@ -1,3 +0,0 @@
-Insert into REGISTERED_USER(username, password,   accountNonExpired,  accountNonLocked,  credentialsNonExpired,  enabled, firstName, lastName, email) values ('john@test.com', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1, 1, 1, 'john', 'doe', 'john@test.com')
-;
-Insert into USER_ROLE_LINK (registered_user_id,application_role_id) values((select id from REGISTERED_USER where username='john@test.com'), (select id from APPLICATION_ROLE where authority='SUPERADMINISTRATOR'));

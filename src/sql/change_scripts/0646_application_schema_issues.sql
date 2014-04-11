8d2be7916c67eda0a92cbfc6c89acb0c6a08ba19
alastair <a.knowles@ucl.ac.uk>
2014-04-11 21:13:01 +0100

    Most of the workflow engine.

--- src/sql/change_scripts/0646_application_schema_issues.sql
+++ src/sql/change_scripts/0646_application_schema_issues.sql
@@ -10,3 +10,35 @@ ALTER TABLE APPLICATION
 	DROP COLUMN submitted_ip_address,
 	CHANGE COLUMN new_submitted_ip_address submitted_ip_address VARCHAR(32)
 ;
+
+ALTER TABLE ROLE
+	DROP FOREIGN KEY role_ibfk_3,
+	DROP COLUMN update_scope_id
+;
+
+ALTER TABLE ACTION
+	DROP FOREIGN KEY action_ibfk_3,
+	DROP COLUMN update_scope_id
+;
+
+DROP TABLE APPLICATION_UPDATE_VIEW
+;
+
+DROP TABLE APPLICATION_UPDATE
+;
+
+DROP TABLE UPDATE_SCOPE
+;
+
+ALTER TABLE APPLICATION
+	DROP COLUMN update_timestamp,
+	DROP COLUMN display_range_timestamp
+;
+
+UPDATE USER_BATCH_NOTIFICATION
+SET last_notification_timestamp = DATE(last_notification_timestamp)
+;
+
+ALTER TABLE USER_BATCH_NOTIFICATION
+	MODIFY COLUMN last_notification_timestamp DATE NOT NULL
+;

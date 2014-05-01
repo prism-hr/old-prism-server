a2433903f837d52a9ec37ccb579ad93b767d9242
alastair <a.knowles@ucl.ac.uk>
2014-05-01 14:48:41 +0100

    Merging program and project into the same table.

--- src/sql/change_scripts/0655_application_schema_issues.sql
+++ src/sql/change_scripts/0655_application_schema_issues.sql
@@ -1,4 +1,42 @@
-/* Drop project and merge with advert */
+ALTER TABLE ADVERT
+	ADD COLUMN program_id INT(10) UNSIGNED AFTER id,
+	ADD INDEX (program_id),
+	ADD FOREIGN KEY (program_id) REFERENCES ADVERT (id)
+;
+
+UPDATE ADVERT INNER JOIN PROJECT
+	ON ADVERT.id = PROJECT.id
+SET ADVERT.program_id = PROJECT.program_id
+;
+
+UPDATE ADVERT
+SET program_id = id
+WHERE program_id IS NULL
+;
+
+ALTER TABLE APPLICATION
+	DROP FOREIGN KEY prog_app_fk,
+	DROP FOREIGN KEY project_fk,
+	DROP COLUMN program_id,
+	DROP COLUMN project_id
+;
+
+ALTER TABLE USER_ROLE
+	DROP FOREIGN KEY user_role_ibfk_3,
+	DROP FOREIGN KEY user_role_ibfk_4
+;
+
+UPDATE USER_ROLE
+SET project_id = program_id
+WHERE program_id IS NOT NULL
+;
+
+ALTER TABLE USER_ROLE
+	DROP COLUMN program_id,
+	DROP INDEX program_id,
+	CHANGE COLUMN project_id advert_id INT(10) UNSIGNED,
+	ADD FOREIGN KEY (advert_id) REFERENCES ADVERT (id)
+;
 
 /* Fix uniqueness constraints on imported data tables */
 

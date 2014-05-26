0992fe60295421fadf672a314c07ece7d52fa748
alastair <a.knowles@ucl.ac.uk>
2014-05-26 18:54:24 +0100

    Some more refactoring. Still not working but getting closer.

--- src/main/java/com/zuehlke/pgadmissions/dao/RoleDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/RoleDAO.java
@@ -78,8 +78,7 @@ public class RoleDAO {
     }
 
     public Role getCreatorRole(SystemAction action, PrismResource resource) {
-        return (Role) sessionFactory.getCurrentSession().createCriteria(RoleTransition.class)
-                //
+        return (Role) sessionFactory.getCurrentSession().createCriteria(RoleTransition.class) //
                 .setProjection(Projections.groupProperty("role")).createAlias("stateTransition", "stateTransition", JoinType.INNER_JOIN)
                 .createAlias("stateTransition.stateAction", "stateAction", JoinType.INNER_JOIN).add(Restrictions.eq("stateAction.state", resource.getState())) //
                 .add(Restrictions.eq("stateAction.action.id", action)) //
@@ -88,8 +87,18 @@ public class RoleDAO {
     }
 
     @SuppressWarnings("unchecked")
-    public List<Role> getExecutorRoles(User user, PrismResource resource, SystemAction action) {
+    public List<Role> getActionRoles(PrismResource resource, SystemAction action) {
         // Check that the action is valid
+        return (List<Role>) sessionFactory.getCurrentSession().createCriteria(StateAction.class) //
+                .setProjection(Projections.property("stateActionAssignment.role")) //
+                .createAlias("stateActionAssignments", "stateActionAssignment", JoinType.INNER_JOIN) //
+                .add(Restrictions.eq("state", resource.getState())) //
+                .add(Restrictions.eq("action.id", action)) //
+                .list();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Role> getActionInvokerRoles(User user, PrismResource resource, SystemAction action) {
         return (List<Role>) sessionFactory.getCurrentSession().createCriteria(StateAction.class) //
                 .setProjection(Projections.groupProperty("stateActionAssignment.role")) //
                 .createAlias("stateActionAssignments", "stateActionAssignment", JoinType.INNER_JOIN) //
@@ -108,6 +117,28 @@ public class RoleDAO {
                 .add(Restrictions.eq("user.parentUser", user)) //
                 .list();
     }
+    
+//    @SuppressWarnings("unchecked")
+//    public List<Role> getExecutorRoles(User user, PrismResource resource, SystemAction action) {
+//        // Check that the action is valid
+//        return (List<Role>) sessionFactory.getCurrentSession().createCriteria(StateAction.class) //
+//                .setProjection(Projections.groupProperty("stateActionAssignment.role")) //
+//                .createAlias("stateActionAssignments", "stateActionAssignment", JoinType.INNER_JOIN) //
+//                .createAlias("stateActionAssignment.role", "role", JoinType.INNER_JOIN) //
+//                .createAlias("role.userRoles", "userRole", JoinType.INNER_JOIN) //
+//                .createAlias("userRole.user", "user", JoinType.INNER_JOIN) //
+//                .createAlias("user.userAccount", "userAccount", JoinType.INNER_JOIN) //
+//                .add(Restrictions.eq("state", resource.getState())) //
+//                .add(Restrictions.eq("action.id", action)) //
+//                .add(Restrictions.disjunction() //
+//                        .add(Restrictions.eq("userRole.application", resource.getApplication())) //
+//                        .add(Restrictions.eq("userRole.project", resource.getProject())) //
+//                        .add(Restrictions.eq("userRole.program", resource.getProgram())) //
+//                        .add(Restrictions.eq("userRole.institution", resource.getInstitution())) //
+//                        .add(Restrictions.eq("userRole.system", resource.getSystem()))) //
+//                .add(Restrictions.eq("user.parentUser", user)) //
+//                .list();
+//    }
 
     @SuppressWarnings("unchecked")
     public List<User> getBy(Role role, PrismResource resource) {

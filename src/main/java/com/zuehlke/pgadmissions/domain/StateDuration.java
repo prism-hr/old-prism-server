34aad5576151c6cf832107c54de652bbb5a18480
jfi <jfi@zuhlke.com>
2014-05-30 15:11:24 +0100

    Added missing entities

---
+++ src/main/java/com/zuehlke/pgadmissions/domain/StateDuration.java
@@ -0,0 +1,84 @@
+package com.zuehlke.pgadmissions.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "state_duration")
+public class StateDuration {
+
+    @Id
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "system_id")
+    private System system;
+
+    @ManyToOne
+    @JoinColumn(name = "institution_id")
+    private Institution institution;
+
+    @ManyToOne
+    @JoinColumn(name = "program_id")
+    private Program program;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id", nullable = false)
+    private State state;
+
+    @Column(name = "expiry_duration", nullable = false)
+    private boolean expiryDuration;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public System getSystem() {
+        return system;
+    }
+
+    public void setSystem(System system) {
+        this.system = system;
+    }
+
+    public Institution getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.program = program;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public boolean isExpiryDuration() {
+        return expiryDuration;
+    }
+
+    public void setExpiryDuration(boolean expiryDuration) {
+        this.expiryDuration = expiryDuration;
+    }
+
+}

2a2cadaf413cc15d88ec85728e7ed35019cf5387
jfi <jfi@zuhlke.com>
2014-06-05 12:33:29 +0100

    Simplified MVC configuration to work with REST

--- src/main/java/com/zuehlke/pgadmissions/controllers/LoginController.java
+++ src/main/java/com/zuehlke/pgadmissions/controllers/LoginController.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.zuehlke.pgadmissions.services.ProgramService;
 import com.zuehlke.pgadmissions.services.UserService;
 
-@Controller
+@RestController
 @RequestMapping(value = { "/login", "/alreadyRegistered" })
 public class LoginController {
 

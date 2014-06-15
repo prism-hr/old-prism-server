91db5c63eff113ca58516df63557dc2c372d1825
alastair <a.knowles@ucl.ac.uk>
2014-06-15 22:17:51 +0100

    Added average application rating to the console list functionality.

--- src/main/java/com/zuehlke/pgadmissions/domain/Application.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/Application.java
@@ -5,7 +5,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.Valid;
 
 import org.hibernate.annotations.Cache;
@@ -52,7 +63,7 @@ public class Application extends PrismResourceDynamic {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id")
     private Project project;
-    
+
     @Column(name = "closing_date")
     @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
     private LocalDate closingDate;
--- src/sql/change_scripts/0671_application_schema_issues.sql
+++ src/sql/change_scripts/0671_application_schema_issues.sql
@@ -154,3 +154,18 @@ UPDATE STATE_ACTION
 SET precedence = NULL
 WHERE action_id = "APPLICATION_COMMENT"
 ;
+
+/* Application rating - denormalise */
+
+ALTER TABLE APPLICATION
+	ADD COLUMN average_rating DECIMAL(3,2) AFTER submitted_timestamp
+;
+
+UPDATE APPLICATION INNER JOIN (
+	SELECT application_id AS application_id,
+		ROUND(AVG(COMMENT.application_rating), 2) AS average_rating
+	FROM COMMENT
+	GROUP BY COMMENT.application_id) AS APPLICATION_RATING
+	ON APPLICATION.id = APPLICATION_RATING.application_id
+SET APPLICATION.average_rating = APPLICATION_RATING.average_rating
+;

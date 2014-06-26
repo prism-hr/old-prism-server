7f2e56f45e81884d2c1f809b21c3a5897a8fd471
jfi <jfi@zuhlke.com>
2014-06-26 16:48:13 +0100

    Made registration test modular

--- src/test/java/com/zuehlke/pgadmissions/integration/IT1SystemInitialisation.java
+++ src/test/java/com/zuehlke/pgadmissions/integration/IT1SystemInitialisation.java
@@ -1,9 +1,16 @@
 package com.zuehlke.pgadmissions.integration;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.io.Resources;
+import com.zuehlke.pgadmissions.domain.*;
+import com.zuehlke.pgadmissions.domain.System;
+import com.zuehlke.pgadmissions.domain.enums.PrismNotificationTemplate;
+import com.zuehlke.pgadmissions.domain.enums.PrismRole;
+import com.zuehlke.pgadmissions.domain.enums.PrismState;
+import com.zuehlke.pgadmissions.integration.helpers.RegistrationHelper;
+import com.zuehlke.pgadmissions.mail.MailSenderMock;
+import com.zuehlke.pgadmissions.services.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,109 +20,87 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
-import com.google.common.io.Resources;
-import com.zuehlke.pgadmissions.domain.Action;
-import com.zuehlke.pgadmissions.domain.Configuration;
-import com.zuehlke.pgadmissions.domain.NotificationConfiguration;
-import com.zuehlke.pgadmissions.domain.NotificationTemplate;
-import com.zuehlke.pgadmissions.domain.NotificationTemplateVersion;
-import com.zuehlke.pgadmissions.domain.Role;
-import com.zuehlke.pgadmissions.domain.Scope;
-import com.zuehlke.pgadmissions.domain.State;
-import com.zuehlke.pgadmissions.domain.StateDuration;
-import com.zuehlke.pgadmissions.domain.System;
-import com.zuehlke.pgadmissions.domain.User;
-import com.zuehlke.pgadmissions.domain.UserAccount;
-import com.zuehlke.pgadmissions.domain.UserRole;
-import com.zuehlke.pgadmissions.domain.enums.PrismNotificationTemplate;
-import com.zuehlke.pgadmissions.domain.enums.PrismRole;
-import com.zuehlke.pgadmissions.domain.enums.PrismState;
-import com.zuehlke.pgadmissions.mail.MailSenderMock;
-import com.zuehlke.pgadmissions.services.ActionService;
-import com.zuehlke.pgadmissions.services.EntityService;
-import com.zuehlke.pgadmissions.services.NotificationService;
-import com.zuehlke.pgadmissions.services.RoleService;
-import com.zuehlke.pgadmissions.services.StateService;
-import com.zuehlke.pgadmissions.services.SystemService;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/testWorkflowContext.xml")
 @Service
 public class IT1SystemInitialisation {
-    
+
     private final String EMAIL_DEFAULT_SUBJECT_DIRECTORY = "email/subject/";
 
     private final String EMAIL_DEFAULT_CONTENT_DIRECTORY = "email/content/";
-    
+
     @Value("${system.name}")
     private String systemName;
-    
+
     @Value("${system.user.firstName}")
     private String systemUserFirstName;
-    
+
     @Value("${system.user.lastName}")
     private String systemUserLastName;
-    
+
     @Value("${system.user.email}")
     private String systemUserEmail;
-    
+
     @Autowired
     private ActionService actionService;
-    
+
     @Autowired
     private EntityService entityService;
-    
+
     @Autowired
     private NotificationService notificationTemplateService;
-    
+
     @Autowired
     private RoleService roleService;
-    
+
     @Autowired
     private StateService stateService;
-    
+
     @Autowired
     private SystemService systemService;
-    
+
     @Autowired
     private MailSenderMock mailSenderMock;
-    
+
+    @Autowired
+    private RegistrationHelper registrationHelper;
+
     @Test
     @Transactional
     public void testSystemInitialisation() {
-        for (int i = 0; i < 2; i ++) {
-            activateSystemUserIfExists();
-            
+        for (int i = 0; i < 2; i++) {
             systemService.initialiseSystem();
-            
+
             verifyScopeCreation();
             verifyActionCreation();
-            verifyRoleCreation();       
+            verifyRoleCreation();
             verifyStateCreation();
-            
+
             System system = systemService.getSystem();
             assertEquals(system.getName(), systemName);
             assertEquals(system.getState().getId(), PrismState.SYSTEM_APPROVED);
-            
+
             User systemUser = system.getUser();
             assertEquals(systemUser.getFirstName(), systemUserFirstName);
             assertEquals(systemUser.getLastName(), systemUserLastName);
             assertEquals(systemUser.getEmail(), systemUserEmail);
-    
+
             for (UserRole userRole : systemUser.getUserRoles()) {
                 assertEquals(userRole.getRole().getId(), PrismRole.SYSTEM_ADMINISTRATOR);
             }
-            
-            verifyConfigurationCreation();      
+
+            verifyConfigurationCreation();
             verifyNotificationTemplateCreation(system);
             verifyStateDurationCreation();
-            
-            if (systemUser.getUserAccount() == null) {
-                mailSenderMock.assertEmailSent(systemUser, PrismNotificationTemplate.SYSTEM_COMPLETE_REGISTRATION_REQUEST);
+
+            if (i == 0) {
+                registrationHelper.assertActivationEmailRegisterAndActivateUser(systemUser, system, PrismNotificationTemplate.SYSTEM_COMPLETE_REGISTRATION_REQUEST);
             }
-            
+
             mailSenderMock.verify();
         }
     }
@@ -134,13 +119,13 @@ public class IT1SystemInitialisation {
             assertEquals(template.getId().getNotificationPurpose(), template.getNotificationPurpose());
             assertEquals(template.getId().getScope(), template.getScope().getId());
             assertEquals(PrismNotificationTemplate.getReminderTemplate(template.getId()), (template.getReminderTemplate()) == null ? null : template.getReminderTemplate().getId());
-            
+
             NotificationConfiguration configuration = notificationTemplateService.getConfiguration(system, template);
             assertEquals(configuration.getNotificationTemplate(), template);
             assertEquals(configuration.getNotificationTemplateVersion().getNotificationTemplate(), template);
             assertEquals(PrismNotificationTemplate.getReminderInterval(template.getId()), configuration.getReminderInterval());
             assertEquals(false, configuration.isEnabled());
-            
+
             NotificationTemplateVersion version = configuration.getNotificationTemplateVersion();
             assertEquals(getFileContent(EMAIL_DEFAULT_SUBJECT_DIRECTORY + template.getId().getInitialTemplateSubject()), version.getSubject());
             assertEquals(getFileContent(EMAIL_DEFAULT_CONTENT_DIRECTORY + template.getId().getInitialTemplateContent()), version.getContent());
@@ -157,7 +142,7 @@ public class IT1SystemInitialisation {
         for (State state : stateService.getStates()) {
             assertEquals(PrismState.getParentState(state.getId()), state.getParentState().getId());
             assertEquals(state.getId().getSequenceOrder(), state.getSequenceOrder());
-            assertEquals(state.getId().getScope(), state.getScope().getId());    
+            assertEquals(state.getId().getScope(), state.getScope().getId());
         }
     }
 
@@ -180,17 +165,6 @@ public class IT1SystemInitialisation {
         }
     }
 
-    private void activateSystemUserIfExists() {
-        System system = systemService.getSystem();
-        if (system != null) {
-            User systemUser = system.getUser();
-            if (systemUser != null) {
-                systemUser.setUserAccount(new UserAccount().withPassword("password").withEnabled(true));
-                entityService.save(systemUser);
-            }
-        }
-    }
-    
     private String getFileContent(String filePath) {
         try {
             return Joiner.on(java.lang.System.lineSeparator()).join(Resources.readLines(Resources.getResource(filePath), Charsets.UTF_8));
@@ -198,5 +172,5 @@ public class IT1SystemInitialisation {
             throw new Error("Could not access default notification template", e);
         }
     }
-    
+
 }

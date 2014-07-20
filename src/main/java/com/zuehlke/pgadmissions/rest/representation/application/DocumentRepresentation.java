3643a43f5c8bf175c49d820e3e727b763ac69108
jfi <jfi@zuhlke.com>
2014-07-20 14:51:30 +0200

    Displaying files and first version of download

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/application/DocumentRepresentation.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/application/DocumentRepresentation.java
@@ -4,11 +4,21 @@ public class DocumentRepresentation {
 
     private Integer id;
 
+    private String fileName;
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 }
--- src/main/java/com/zuehlke/pgadmissions/rest/resource/FileResource.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/resource/FileResource.java
@@ -1,22 +1,19 @@
 package com.zuehlke.pgadmissions.rest.resource;
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.Part;
-
-import org.bouncycastle.util.io.Streams;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.google.common.collect.ImmutableMap;
 import com.zuehlke.pgadmissions.domain.Document;
 import com.zuehlke.pgadmissions.domain.definitions.DocumentType;
 import com.zuehlke.pgadmissions.services.EntityService;
+import org.bouncycastle.util.io.Streams;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/files")
@@ -30,6 +27,18 @@ public class FileResource {
         Document document = new Document().withContent(Streams.readAll(part.getInputStream())).withContentType(part.getContentType()).withCreatedTimestamp(new DateTime()).withFileName(part.getName()).withType(DocumentType.COMMENT);
         Integer id = (Integer) entityService.save(document);
         return ImmutableMap.of("id", (Object) id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public void downloadFile(@PathVariable Integer fileId, HttpServletResponse response) throws IOException {
+        Document document = entityService.getById(Document.class, fileId);
+
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + document.getFileName() + "\"");
+        response.setContentType(document.getContentType());
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        outputStream.write(document.getContent());
+        response.flushBuffer();
     }
 
 }

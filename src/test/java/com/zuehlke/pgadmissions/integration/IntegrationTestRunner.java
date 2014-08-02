49cae10bca2f79c8b961eddefb2d4c0265e8bd4f
alastair <a.knowles@ucl.ac.uk>
2014-08-02 13:49:58 +0100

    Fixed merging problems and improved testing framework.

--- src/test/java/com/zuehlke/pgadmissions/integration/IntegrationTestRunner.java
+++ src/test/java/com/zuehlke/pgadmissions/integration/IntegrationTestRunner.java
@@ -1,6 +1,8 @@
 package com.zuehlke.pgadmissions.integration;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.zuehlke.pgadmissions.domain.PassedTest;
@@ -9,11 +11,17 @@ import com.zuehlke.pgadmissions.services.EntityService;
 @Service
 public class IntegrationTestRunner {
 
+    @Value("${integration.testing.clear.history}")
+    private Boolean clearTestHistory;
+    
     @Autowired
     private EntityService entityService;
     
     public <T extends IPrismIntegrationTest> void run(T integrationTest) throws Exception {
         String integrationTestId = integrationTest.getClass().getSimpleName();
+        if (BooleanUtils.isTrue(clearTestHistory)) {
+            clearTestHistory();
+        }
         if (entityService.getById(PassedTest.class, integrationTestId) == null) {
             integrationTest.run();
         }
@@ -23,6 +31,12 @@ public class IntegrationTestRunner {
         String integrationTestId = integrationTest.getClass().getSimpleName();
         if (entityService.getById(PassedTest.class, integrationTestId) == null) {
             entityService.save(new PassedTest().withId(integrationTest.getClass().getSimpleName()));
+        }
+    }
+    
+    private void clearTestHistory() {
+        for (PassedTest passedTest : entityService.list(PassedTest.class)) {
+            entityService.delete(passedTest);
         }
     }
     

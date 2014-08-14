cc2e7fec67acf9c51ba6999ad3f99c9004bfc41d
jfi <jfi@zuhlke.com>
2014-08-14 11:39:14 +0100

    First part of forgot password

--- src/main/java/com/zuehlke/pgadmissions/rest/resource/UserResource.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/resource/UserResource.java
@@ -1,14 +1,17 @@
 package com.zuehlke.pgadmissions.rest.resource;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.inject.Named;
-import javax.validation.Valid;
-import javax.ws.rs.WebApplicationException;
-
+import com.google.common.collect.ImmutableMap;
+import com.zuehlke.pgadmissions.domain.User;
 import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
+import com.zuehlke.pgadmissions.exceptions.WorkflowEngineException;
+import com.zuehlke.pgadmissions.rest.dto.UserRegistrationDTO;
+import com.zuehlke.pgadmissions.rest.representation.UserExtendedRepresentation;
+import com.zuehlke.pgadmissions.rest.representation.UserRepresentation;
+import com.zuehlke.pgadmissions.rest.validation.validator.UserRegistrationValidator;
+import com.zuehlke.pgadmissions.security.AuthenticationTokenUtils;
+import com.zuehlke.pgadmissions.services.ProgramService;
+import com.zuehlke.pgadmissions.services.RegistrationService;
+import com.zuehlke.pgadmissions.services.UserService;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,17 +23,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import com.google.common.collect.ImmutableMap;
-import com.zuehlke.pgadmissions.domain.User;
-import com.zuehlke.pgadmissions.exceptions.WorkflowEngineException;
-import com.zuehlke.pgadmissions.rest.dto.UserRegistrationDTO;
-import com.zuehlke.pgadmissions.rest.representation.UserExtendedRepresentation;
-import com.zuehlke.pgadmissions.rest.representation.UserRepresentation;
-import com.zuehlke.pgadmissions.rest.validation.validator.UserRegistrationValidator;
-import com.zuehlke.pgadmissions.security.AuthenticationTokenUtils;
-import com.zuehlke.pgadmissions.services.ProgramService;
-import com.zuehlke.pgadmissions.services.RegistrationService;
-import com.zuehlke.pgadmissions.services.UserService;
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.validation.Valid;
+import javax.ws.rs.WebApplicationException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -51,13 +49,13 @@ public class UserResource {
 
     @Autowired
     private RegistrationService registrationService;
-    
+
     @Autowired
     private UserService userService;
 
     @Autowired
     private Mapper dozerBeanMapper;
-    
+
     @RequestMapping(method = RequestMethod.GET)
     public UserExtendedRepresentation getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -71,7 +69,7 @@ public class UserResource {
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public Map<String, String> authenticate(@RequestParam(required = false, value = "username") String username,
-            @RequestParam(required = false, value = "password") String password) {
+                                            @RequestParam(required = false, value = "password") String password) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -80,26 +78,27 @@ public class UserResource {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String submitRegistration(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO)
+    public void submitRegistration(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO)
             throws WorkflowEngineException {
         registrationService.submitRegistration(userRegistrationDTO);
-        return "OK";
     }
 
     @RequestMapping(value = "/activate", method = RequestMethod.PUT)
-    public Map<String, String> activateAccount(@RequestParam String activationCode){
+    public Map<String, String> activateAccount(@RequestParam String activationCode) {
         User user = userService.getUserByActivationCode(activationCode);
-        if(user == null){
+        if (user == null) {
             throw new ResourceNotFoundException();
         }
-        boolean activated = userService.activateUser(user.getId());
-//        if(activated){
-            return ImmutableMap.of("status", "ACTIVATED", "user", user.getEmail());
-//        }
-//        return ImmutableMap.of("status", "ALREADY_ACTIVATED");
+        userService.activateUser(user.getId());
+        return ImmutableMap.of("status", "ACTIVATED", "user", user.getEmail());
     }
 
-    @RequestMapping(value="/suggestion", method = RequestMethod.GET, params = "searchTerm")
+    @RequestMapping(value = "/resetPassword", method = RequestMethod.POST)
+    public void resetPassword(@RequestParam String email) {
+        userService.resetPassword(email);
+    }
+
+    @RequestMapping(value = "/suggestion", method = RequestMethod.GET, params = "searchTerm")
     public List<UserRepresentation> getSimilarUsers(@RequestParam String searchTerm) {
         return userService.getSimilarUsers(searchTerm);
     }

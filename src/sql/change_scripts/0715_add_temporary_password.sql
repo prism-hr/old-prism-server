38deb27926dd3e0c4eea86223d2e8bebbf85c4f2
alastair <a.knowles@ucl.ac.uk>
2014-08-18 14:44:44 +0100

    Change script fixes.

--- src/sql/change_scripts/0715_add_temporary_password.sql
+++ src/sql/change_scripts/0715_add_temporary_password.sql
@@ -1,4 +1,6 @@
-alter table user_account
-add column temporary_password varchar(32),
-add column temporary_password_expiry_datetime datetime
+ALTER TABLE user_account
+	ADD COLUMN temporary_password varchar(32) AFTER password,
+	ADD COLUMN temporary_password_expiry_datetime datetime AFTER temporary_password,
+	ADD INDEX (temporary_password),
+	ADD INDEX (temporary_password_expiry_datetime datetime)
 ;

9325321389ae5b6e82690de514da44079eaa804c
alastair <a.knowles@ucl.ac.uk>
2014-08-18 22:01:53 +0100

    Further preparation on resolving resource list performance problems.

--- src/sql/change_scripts/0717_column_name_bug.sql
+++ src/sql/change_scripts/0717_column_name_bug.sql
@@ -1,3 +1,8 @@
 ALTER TABLE USER_ACCOUNT
 	CHANGE COLUMN temporary_password_expiry_datetime temporary_password_expiry_timestamp DATETIME
 ;
+
+ALTER TABLE APPLICATION
+	ADD INDEX (user_id, sequence_identifier)
+;
+

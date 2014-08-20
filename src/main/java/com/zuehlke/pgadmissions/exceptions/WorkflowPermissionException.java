639bbd6f06dbb1a110cfb974f6897d17872a75b8
alastair <a.knowles@ucl.ac.uk>
2014-08-20 15:06:04 +0100

    Fix to include validation for creating a duplicate resource when editing and existing one.

--- src/main/java/com/zuehlke/pgadmissions/exceptions/WorkflowPermissionException.java
+++ src/main/java/com/zuehlke/pgadmissions/exceptions/WorkflowPermissionException.java
@@ -1,38 +1,52 @@
 package com.zuehlke.pgadmissions.exceptions;
 
+import com.zuehlke.pgadmissions.domain.Action;
 import com.zuehlke.pgadmissions.domain.Resource;
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismAction;
 
 public class WorkflowPermissionException extends RuntimeException {
 
     private static final long serialVersionUID = -116272246657153672L;
 
+    private Action action;
+    
+    private Action fallbackAction;
+    
+    private Resource resource;
+    
     private Resource fallbackResource;
-
-    private PrismAction fallbackAction;
 
     private String message;
 
-    public WorkflowPermissionException(Resource fallbackResource, PrismAction fallbackAction) {
+    public WorkflowPermissionException(Action action,  Action fallbackAction, Resource resource, Resource fallbackResource) {
+        this.action = action;
         this.fallbackAction = fallbackAction;
         this.fallbackResource = fallbackResource;
     }
 
-    public WorkflowPermissionException(Resource fallbackResource, PrismAction fallbackAction, String message) {
-        this(fallbackResource, fallbackAction);
+    public WorkflowPermissionException(Action action, Action fallbackAction, Resource resource, Resource fallbackResource, String message) {
+        this(action, fallbackAction, resource, fallbackResource);
         this.message = message;
     }
 
-    public Resource getFallbackResource() {
-        return fallbackResource;
+    public final Action getAction() {
+        return action;
     }
 
-    public PrismAction getFallbackAction() {
+    public final Action getFallbackAction() {
         return fallbackAction;
+    }
+    
+    public final Resource getResource() {
+        return resource;
+    }
+
+    public final Resource getFallbackResource() {
+        return fallbackResource;
     }
 
     @Override
     public String getMessage() {
         return message;
     }
+    
 }

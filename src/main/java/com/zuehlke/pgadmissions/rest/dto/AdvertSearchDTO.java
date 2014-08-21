0264938a136d8bb55af49c6cbfaf674d186b238e
alastair <a.knowles@ucl.ac.uk>
2014-08-21 15:58:59 +0100

    Fix to mistake in get duplicate entity ... some more sql ...

--- src/main/java/com/zuehlke/pgadmissions/rest/dto/AdvertSearchDTO.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/dto/AdvertSearchDTO.java
@@ -1,9 +1,8 @@
 package com.zuehlke.pgadmissions.rest.dto;
 
-import com.zuehlke.pgadmissions.domain.definitions.PrismProgramType;
-import org.hibernate.validator.constraints.NotEmpty;
-
 import javax.validation.constraints.NotNull;
+
+import com.zuehlke.pgadmissions.domain.definitions.PrismProgramType;
 
 public class AdvertSearchDTO {
 
@@ -17,6 +16,46 @@ public class AdvertSearchDTO {
     private String institutionId;
 
     private String[] studyOptions;
+
+    public final PrismProgramType getProgramType() {
+        return programType;
+    }
+
+    public final void setProgramType(PrismProgramType programType) {
+        this.programType = programType;
+    }
+
+    public final String getKeywords() {
+        return keywords;
+    }
+
+    public final void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public final String getLocation() {
+        return location;
+    }
+
+    public final void setLocation(String location) {
+        this.location = location;
+    }
+
+    public final String getInstitutionId() {
+        return institutionId;
+    }
+
+    public final void setInstitutionId(String institutionId) {
+        this.institutionId = institutionId;
+    }
+
+    public final String[] getStudyOptions() {
+        return studyOptions;
+    }
+
+    public final void setStudyOptions(String[] studyOptions) {
+        this.studyOptions = studyOptions;
+    }
 
 }
 

5746c36340bbb03d51334e9eb592fc16e07a3d6f
alastair <a.knowles@ucl.ac.uk>
2014-08-31 11:50:03 +0100

    Merge

--- src/sql/change_scripts/0512_research_opportunity_feed.sql
+++ src/sql/change_scripts/0512_research_opportunity_feed.sql
@@ -6,7 +6,7 @@ CREATE TABLE RESEARCH_OPPORTUNITIES_FEED (
   INDEX registered_user_feed_fk_idx (registered_user_id ASC) ,
   CONSTRAINT registered_user_feed_fk
     FOREIGN KEY (registered_user_id )
-    REFERENCES pgadmissions.registered_user (id )
+    REFERENCES registered_user (id )
 )
 ;
 
@@ -18,11 +18,11 @@ CREATE TABLE RESEARCH_OPPORTUNITIES_FEED_PROGRAM_LINK (
   INDEX feed_program_id_fk_idx (program_id ASC) ,
   CONSTRAINT feed_id_fk
     FOREIGN KEY (feed_id )
-    REFERENCES pgadmissions.research_opportunities_feed (id )
+    REFERENCES research_opportunities_feed (id )
   ,
   CONSTRAINT feed_program_id_fk
     FOREIGN KEY (program_id )
-    REFERENCES pgadmissions.program (id )
+    REFERENCES program (id )
 )
 ;
 

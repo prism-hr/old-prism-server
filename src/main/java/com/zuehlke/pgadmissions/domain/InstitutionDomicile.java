d6bbda26c99f50f9902aca64d47150cda2e46962
jfi <jfi@zuhlke.com>
2014-09-09 11:04:23 +0100

    Added currency import

--- src/main/java/com/zuehlke/pgadmissions/domain/InstitutionDomicile.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/InstitutionDomicile.java
@@ -1,25 +1,12 @@
 package com.zuehlke.pgadmissions.domain;
 
+import org.apache.solr.analysis.*;
+import org.hibernate.search.annotations.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.apache.solr.analysis.ASCIIFoldingFilterFactory;
-import org.apache.solr.analysis.LowerCaseFilterFactory;
-import org.apache.solr.analysis.SnowballPorterFilterFactory;
-import org.apache.solr.analysis.StandardTokenizerFactory;
-import org.apache.solr.analysis.StopFilterFactory;
-import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.Analyzer;
-import org.hibernate.search.annotations.AnalyzerDef;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Index;
-import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.Parameter;
-import org.hibernate.search.annotations.Store;
-import org.hibernate.search.annotations.TokenFilterDef;
-import org.hibernate.search.annotations.TokenizerDef;
 
 @AnalyzerDef(name = "institutionDomicileNameAnalyzer", tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class), filters = {
         @TokenFilterDef(factory = LowerCaseFilterFactory.class), @TokenFilterDef(factory = StopFilterFactory.class),
@@ -36,6 +23,9 @@ public class InstitutionDomicile {
     @Column(name = "name", nullable = false, unique = true)
     @Field(analyzer = @Analyzer(definition = "institutionDomicileNameAnalyzer"), index = Index.YES, analyze = Analyze.YES, store = Store.NO)
     private String name;
+
+    @Column(name = "currency", nullable = false)
+    private String currency;
 
     @Column(name = "enabled", nullable = false)
     private Boolean enabled;
@@ -56,7 +46,15 @@ public class InstitutionDomicile {
         this.name = name;
     }
 
-    public boolean isEnabled() {
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Boolean getEnabled() {
         return enabled;
     }
 
@@ -74,9 +72,14 @@ public class InstitutionDomicile {
         return this;
     }
 
+    public InstitutionDomicile withCurrency(final String currency) {
+        this.currency = currency;
+        return this;
+    }
+
     public InstitutionDomicile withEnabled(Boolean enabled) {
         this.enabled = enabled;
         return this;
     }
-    
+
 }
--- src/main/java/com/zuehlke/pgadmissions/services/helpers/ImportedEntityServiceHelper.java
+++ src/main/java/com/zuehlke/pgadmissions/services/helpers/ImportedEntityServiceHelper.java
@@ -1,19 +1,20 @@
 package com.zuehlke.pgadmissions.services.helpers;
 
-import java.io.InputStreamReader;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.net.URL;
-import java.util.List;
-import java.util.Set;
-
-import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-
+import au.com.bytecode.opencsv.CSVReader;
+import com.google.common.base.Charsets;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Maps;
+import com.zuehlke.pgadmissions.domain.*;
+import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
+import com.zuehlke.pgadmissions.dto.AdvertCategoryImportRowDTO;
+import com.zuehlke.pgadmissions.exceptions.DataImportException;
+import com.zuehlke.pgadmissions.iso.jaxb.CountryCodesType;
+import com.zuehlke.pgadmissions.iso.jaxb.CountryType;
+import com.zuehlke.pgadmissions.referencedata.jaxb.LanguageQualificationTypes.LanguageQualificationType;
+import com.zuehlke.pgadmissions.referencedata.jaxb.ProgrammeOccurrences.ProgrammeOccurrence;
+import com.zuehlke.pgadmissions.services.ImportedEntityService;
+import com.zuehlke.pgadmissions.services.InstitutionService;
+import com.zuehlke.pgadmissions.services.NotificationService;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
@@ -23,30 +24,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Component;
 
-import au.com.bytecode.opencsv.CSVReader;
-
-import com.google.common.base.Charsets;
-import com.google.common.collect.HashMultimap;
-import com.zuehlke.pgadmissions.domain.ImportedEntity;
-import com.zuehlke.pgadmissions.domain.ImportedEntityFeed;
-import com.zuehlke.pgadmissions.domain.ImportedInstitution;
-import com.zuehlke.pgadmissions.domain.ImportedLanguageQualificationType;
-import com.zuehlke.pgadmissions.domain.Institution;
-import com.zuehlke.pgadmissions.domain.Program;
-import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
-import com.zuehlke.pgadmissions.dto.AdvertCategoryImportRowDTO;
-import com.zuehlke.pgadmissions.exceptions.DataImportException;
-import com.zuehlke.pgadmissions.iso.jaxb.CountryCodesType;
-import com.zuehlke.pgadmissions.iso.jaxb.CountryType;
-import com.zuehlke.pgadmissions.referencedata.jaxb.LanguageQualificationTypes.LanguageQualificationType;
-import com.zuehlke.pgadmissions.referencedata.jaxb.ProgrammeOccurrences.ProgrammeOccurrence;
-import com.zuehlke.pgadmissions.services.EntityService;
-import com.zuehlke.pgadmissions.services.ImportedEntityService;
-import com.zuehlke.pgadmissions.services.InstitutionService;
-import com.zuehlke.pgadmissions.services.NotificationService;
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.System;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Component
-@SuppressWarnings({ "unchecked", "rawtypes" })
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class ImportedEntityServiceHelper {
 
     private static final Logger logger = LoggerFactory.getLogger(ImportedEntityServiceHelper.class);
@@ -60,8 +55,8 @@ public class ImportedEntityServiceHelper {
     @Value("${import.institutionDomicile.location}")
     private String institutionDomicileImportLocation;
 
-    @Autowired
-    private EntityService entityService;
+    @Value("${import.countryCurrency.location}")
+    private String countryCurrencyImportLocation;
 
     @Autowired
     private ImportedEntityService importedEntityService;
@@ -110,7 +105,7 @@ public class ImportedEntityServiceHelper {
         logger.info("Starting the import from file: " + fileLocation);
 
         try {
-            List unmarshalled = unmarshallEntities(importedEntityFeed);
+            List unmarshalled = unmarshalEntities(importedEntityFeed);
 
             Class<ImportedEntity> importedEntityClass = (Class<ImportedEntity>) importedEntityFeed.getImportedEntityType().getEntityClass();
 
@@ -147,27 +142,46 @@ public class ImportedEntityServiceHelper {
     public void importInstitutionDomiciles() throws DataImportException {
         logger.info("Starting the import from file: " + institutionDomicileImportLocation);
         try {
-            List<CountryType> unmarshalled = unmarshallInstitutionDomiciles(institutionDomicileImportLocation);
-            mergeInstitutionDomiciles(unmarshalled);
+            List<CountryType> unmarshalled = unmarshalInstitutionDomiciles(institutionDomicileImportLocation);
+            Map<String, String> countryCurrencies = parseCountryCurrencies(countryCurrencyImportLocation);
+            mergeInstitutionDomiciles(unmarshalled, countryCurrencies);
         } catch (Exception e) {
             throw new DataImportException("Error during the import of file: " + institutionDomicileImportLocation, e);
         }
     }
 
-    public List<CountryType> unmarshallInstitutionDomiciles(final String fileLocation) throws Exception {
+    /**
+     * Parses country currency file.
+     *
+     * @return map of country codes to currency codes
+     */
+    private Map<String, String> parseCountryCurrencies(String importLocation) throws IOException {
+        Map<String, String> countryCurrencies = Maps.newHashMap();
+        URL fileUrl = new DefaultResourceLoader().getResource(importLocation).getURL();
+        CSVReader reader = new CSVReader(new InputStreamReader(fileUrl.openStream(), Charsets.UTF_8));
+        String[] row;
+        while ((row = reader.readNext()) != null) {
+            String countryCode = row[2];
+            String currencyCode = row[14];
+            countryCurrencies.put(countryCode, currencyCode);
+        }
+        return countryCurrencies;
+    }
+
+    public List<CountryType> unmarshalInstitutionDomiciles(final String fileLocation) throws Exception {
         try {
             URL fileUrl = new DefaultResourceLoader().getResource(fileLocation).getURL();
             JAXBContext jaxbContext = JAXBContext.newInstance(CountryCodesType.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            JAXBElement<CountryCodesType> unmarshaled = (JAXBElement<CountryCodesType>) unmarshaller.unmarshal(fileUrl);
-            CountryCodesType countryCodes = (CountryCodesType) unmarshaled.getValue();
+            JAXBElement<CountryCodesType> unmarshalled = (JAXBElement<CountryCodesType>) unmarshaller.unmarshal(fileUrl);
+            CountryCodesType countryCodes = (CountryCodesType) unmarshalled.getValue();
             return countryCodes.getCountry();
         } finally {
             Authenticator.setDefault(null);
         }
     }
 
-    private List<Object> unmarshallEntities(final ImportedEntityFeed importedEntityFeed) throws Exception {
+    private List<Object> unmarshalEntities(final ImportedEntityFeed importedEntityFeed) throws Exception {
         try {
             Authenticator.setDefault(new Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -186,8 +200,8 @@ public class ImportedEntityServiceHelper {
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             unmarshaller.setSchema(schema);
 
-            Object unmarshaled = unmarshaller.unmarshal(fileUrl);
-            return (List<Object>) PropertyUtils.getSimpleProperty(unmarshaled, importedEntityType.getJaxbPropertyName());
+            Object unmarshalled = unmarshaller.unmarshal(fileUrl);
+            return (List<Object>) PropertyUtils.getSimpleProperty(unmarshalled, importedEntityType.getJaxbPropertyName());
         } finally {
             Authenticator.setDefault(null);
         }
@@ -206,7 +220,7 @@ public class ImportedEntityServiceHelper {
     }
 
     private void mergeImportedInstitutions(Institution institution,
-            List<com.zuehlke.pgadmissions.referencedata.jaxb.Institutions.Institution> institutionDefinitions) throws Exception {
+                                           List<com.zuehlke.pgadmissions.referencedata.jaxb.Institutions.Institution> institutionDefinitions) throws Exception {
         importedEntityService.disableAllEntities(ImportedInstitution.class, institution);
         for (com.zuehlke.pgadmissions.referencedata.jaxb.Institutions.Institution transientImportedInstitution : institutionDefinitions) {
             importedEntityService.mergeImportedInstitution(institution, transientImportedInstitution);
@@ -228,10 +242,10 @@ public class ImportedEntityServiceHelper {
         }
     }
 
-    private void mergeInstitutionDomiciles(List<CountryType> countries) throws DataImportException {
+    private void mergeInstitutionDomiciles(List<CountryType> countries, Map<String, String> countryCurrencies) throws DataImportException {
         importedEntityService.disableAllInstitutionDomiciles();
         for (CountryType country : countries) {
-            importedEntityService.mergeInstitutionDomicile(country);
+            importedEntityService.mergeInstitutionDomicile(country, countryCurrencies);
         }
     }
 

cb8b5a6658953b79530aee6ed6c175eb4be97586
jfi <jfi@zuhlke.com>
2014-09-13 11:26:53 +0100

    Fixed several server problems, clean-up

--- src/main/java/com/zuehlke/pgadmissions/rest/converter/ProgramTypeConverter.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/converter/ProgramTypeConverter.java
@@ -1,16 +1,10 @@
 package com.zuehlke.pgadmissions.rest.converter;
 
-import org.dozer.DozerConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.zuehlke.pgadmissions.domain.ProgramType;
 import com.zuehlke.pgadmissions.domain.definitions.PrismProgramType;
-import com.zuehlke.pgadmissions.services.EntityService;
+import org.dozer.DozerConverter;
 
 public class ProgramTypeConverter extends DozerConverter<ProgramType, PrismProgramType> {
-
-    @Autowired
-    private EntityService entityService;
 
     public ProgramTypeConverter() {
         super(ProgramType.class, PrismProgramType.class);

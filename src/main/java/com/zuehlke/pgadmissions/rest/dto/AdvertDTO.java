2fcde1fbe2c8c15841c5f32ca1fbae028f9b2ef5
jfi <jfi@zuhlke.com>
2014-09-17 11:36:39 +0100

    Clean up, added duration unit to static data

--- src/main/java/com/zuehlke/pgadmissions/rest/dto/AdvertDTO.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/dto/AdvertDTO.java
@@ -3,6 +3,7 @@ package com.zuehlke.pgadmissions.rest.dto;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
 
 public class AdvertDTO {
 
@@ -12,6 +13,10 @@ public class AdvertDTO {
 
     @Size(max = 20000)
     private String description;
+
+    @URL
+    @Size(max = 2048)
+    private String applyLink;
 
     public String getSummary() {
         return summary;

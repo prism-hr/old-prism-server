1cc373de0d55545dbfe4adc621c1d9baf76dc810
alastair <a.knowles@ucl.ac.uk>
2014-10-26 13:29:29 +0000

    Merge

--- src/main/java/com/zuehlke/pgadmissions/rest/resource/InstitutionResource.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/resource/InstitutionResource.java
@@ -1,26 +1,21 @@
 package com.zuehlke.pgadmissions.rest.resource;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.zuehlke.pgadmissions.domain.advert.AdvertCompetency;
 import com.zuehlke.pgadmissions.domain.advert.AdvertTheme;
 import com.zuehlke.pgadmissions.domain.definitions.PrismLocale;
 import com.zuehlke.pgadmissions.domain.institution.Institution;
-import com.zuehlke.pgadmissions.exceptions.ResourceNotFoundException;
 import com.zuehlke.pgadmissions.rest.representation.resource.application.ImportedEntityRepresentation;
 import com.zuehlke.pgadmissions.services.AdvertService;
 import com.zuehlke.pgadmissions.services.InstitutionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/institutions")
@@ -30,7 +25,7 @@ public class InstitutionResource {
 
     @Autowired
     private AdvertService advertService;
-    
+
     @Autowired
     private InstitutionService institutionService;
 
@@ -48,16 +43,17 @@ public class InstitutionResource {
         return institutionRepresentations;
     }
 
-    @RequestMapping(value = "/{institutionId}/categoryTags", method = RequestMethod.GET, params = {"locale", "category"})
-    public List<String> getCategoryTags(@PathVariable Integer institutionId, @RequestParam PrismLocale locale, @RequestParam String category) throws Exception {
+    @RequestMapping(value = "/{institutionId}/categoryTags", method = RequestMethod.GET, params = "locale")
+    public Map<String, List<String>> getCategoryTags(@PathVariable Integer institutionId, @RequestParam PrismLocale locale) throws Exception {
+        Map<String, List<String>> categoryTags = Maps.newLinkedHashMap();
         Institution institution = institutionService.getById(institutionId);
-        if (category.equals("competencies")) {
-            return advertService.getLocalizedTags(institution, locale, AdvertCompetency.class);
-        } else if (category.equals("themes")) {
-            return advertService.getLocalizedTags(institution, locale, AdvertTheme.class);
-        }
-        log.error("Unknown category: " + category);
-        throw new ResourceNotFoundException();
+
+        String category = "competencies";
+        categoryTags.put(category, advertService.getLocalizedTags(institution, locale, AdvertCompetency.class));
+        category = "themes";
+        categoryTags.put(category, advertService.getLocalizedTags(institution, locale, AdvertTheme.class));
+
+        return categoryTags;
     }
 
 }

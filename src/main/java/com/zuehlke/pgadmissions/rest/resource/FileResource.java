18a88e324f6104b4b043fc30c7c7588257637a28
jfi <jfi@zuhlke.com>
2014-11-04 21:27:48 +0000

    Made PDF downloader handle multiple apps

--- src/main/java/com/zuehlke/pgadmissions/rest/resource/FileResource.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/resource/FileResource.java
@@ -1,8 +1,13 @@
 package com.zuehlke.pgadmissions.rest.resource;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Ints;
 import com.zuehlke.pgadmissions.domain.application.Application;
 import com.zuehlke.pgadmissions.domain.document.Document;
+import com.zuehlke.pgadmissions.exceptions.PrismBadRequestException;
 import com.zuehlke.pgadmissions.services.ApplicationDownloadService;
 import com.zuehlke.pgadmissions.services.DocumentService;
 import com.zuehlke.pgadmissions.services.EntityService;
@@ -13,6 +18,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -48,15 +54,28 @@ public class FileResource {
     }
 
     @RequestMapping(value = "/pdfDownload", method = RequestMethod.GET)
-    public void downloadPdf(@RequestParam(value = "applicationId") Integer applicationId, HttpServletResponse response) throws IOException {
-        Application application = entityService.getById(Application.class, applicationId);
-        String fileName = "application_" + application.getCode() + ".pdf";
+    public void downloadPdf(@RequestParam(value = "applicationIds") String applicationIds, HttpServletResponse response) throws IOException {
+        List<Integer> ids = null;
+        try {
+            ids = Lists.newArrayList(Iterables.transform(Splitter.on(",").split(applicationIds), Ints.stringConverter()));
+        } catch (Exception e) {
+            throw new PrismBadRequestException("Expected comma-separated list of application IDs");
+        }
+
+        String fileName;
+        if (ids.size() == 1) {
+            Application application = entityService.getById(Application.class, ids.get(0));
+            fileName = "application_" + application.getCode() + ".pdf";
+        } else {
+            fileName = "applications.pdf";
+        }
+
         response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
         response.setHeader("File-Name", fileName);
         response.setContentType("application/pdf");
 
         ServletOutputStream outputStream = response.getOutputStream();
-        applicationDownloadService.build(outputStream, applicationId);
+        applicationDownloadService.build(outputStream, ids.toArray(new Integer[ids.size()]));
     }
 
 }

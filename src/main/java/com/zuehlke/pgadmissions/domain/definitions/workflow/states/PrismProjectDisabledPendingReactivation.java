1b46da7370cf9c4fbe1d17d301eea5b1e7fe48a7
alastair <a.knowles@ucl.ac.uk>
2014-11-04 23:53:18 +0000

    Finished some more pieces on configuration ... fixed stupid problem with workflow configuration which kills maintenance thread.

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/states/PrismProjectDisabledPendingReactivation.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/states/PrismProjectDisabledPendingReactivation.java
@@ -27,13 +27,13 @@ public class PrismProjectDisabledPendingReactivation extends PrismWorkflowState 
                     new PrismStateTransition() // 
                         .withTransitionState(PrismState.PROJECT_DISABLED_COMPLETED) // 
                         .withTransitionAction(PrismAction.PROJECT_CONCLUDE) // 
-                        .withTransitionEvaluation(PrismStateTransitionEvaluation.APPLICATION_ASSIGNED_RECRUITER_OUTCOME)// 
+                        .withTransitionEvaluation(PrismStateTransitionEvaluation.APPLICATION_RECRUITED_OUTCOME)// 
                         .withPropagatedActions(Arrays.asList( //
                                 PrismAction.APPLICATION_TERMINATE)), // 
                     new PrismStateTransition() // 
                         .withTransitionState(PrismState.PROJECT_DISABLED_PENDING_REACTIVATION) // 
                         .withTransitionAction(PrismAction.PROJECT_CONCLUDE) // 
-                        .withTransitionEvaluation(PrismStateTransitionEvaluation.APPLICATION_ASSIGNED_RECRUITER_OUTCOME)))); //
+                        .withTransitionEvaluation(PrismStateTransitionEvaluation.APPLICATION_RECRUITED_OUTCOME)))); //
         
         stateActions.add(new PrismStateAction() //
             .withAction(PrismAction.PROJECT_EMAIL_CREATOR) //

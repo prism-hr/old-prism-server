dbabe3e8421232deea775265a0135d5cb6cf575c
alastair <a.knowles@ucl.ac.uk>
2014-11-16 16:13:53 +0000

    Fixed workflow engine bug.

--- src/main/java/com/zuehlke/pgadmissions/rest/resource/ResourceConfigurationResource.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/resource/ResourceConfigurationResource.java
@@ -1,27 +1,45 @@
 package com.zuehlke.pgadmissions.rest.resource;
 
-import com.google.common.base.CaseFormat;
-import com.google.common.collect.Lists;
-import com.zuehlke.pgadmissions.domain.definitions.PrismLocale;
-import com.zuehlke.pgadmissions.domain.definitions.PrismProgramType;
-import com.zuehlke.pgadmissions.domain.definitions.PrismWorkflowConfiguration;
-import com.zuehlke.pgadmissions.domain.resource.Resource;
-import com.zuehlke.pgadmissions.domain.workflow.WorkflowConfiguration;
-import com.zuehlke.pgadmissions.domain.workflow.WorkflowDefinition;
-import com.zuehlke.pgadmissions.rest.ResourceDescriptor;
-import com.zuehlke.pgadmissions.rest.RestApiUtils;
-import com.zuehlke.pgadmissions.rest.representation.configuration.AbstractConfigurationRepresentation;
-import com.zuehlke.pgadmissions.services.CustomizationService;
-import com.zuehlke.pgadmissions.services.EntityService;
-import com.zuehlke.pgadmissions.services.UserService;
-import com.zuehlke.pgadmissions.utils.WordUtils;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.apache.commons.beanutils.PropertyUtils;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.google.common.base.CaseFormat;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.zuehlke.pgadmissions.domain.definitions.PrismLocale;
+import com.zuehlke.pgadmissions.domain.definitions.PrismProgramType;
+import com.zuehlke.pgadmissions.domain.definitions.PrismWorkflowConfiguration;
+import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismNotificationDefinition;
+import com.zuehlke.pgadmissions.domain.resource.Resource;
+import com.zuehlke.pgadmissions.domain.workflow.NotificationDefinition;
+import com.zuehlke.pgadmissions.domain.workflow.WorkflowConfiguration;
+import com.zuehlke.pgadmissions.domain.workflow.WorkflowDefinition;
+import com.zuehlke.pgadmissions.exceptions.PrismValidationException;
+import com.zuehlke.pgadmissions.mail.MailSender;
+import com.zuehlke.pgadmissions.rest.ResourceDescriptor;
+import com.zuehlke.pgadmissions.rest.RestApiUtils;
+import com.zuehlke.pgadmissions.rest.dto.NotificationConfigurationDTO;
+import com.zuehlke.pgadmissions.rest.representation.configuration.AbstractConfigurationRepresentation;
+import com.zuehlke.pgadmissions.services.CustomizationService;
+import com.zuehlke.pgadmissions.services.EntityService;
+import com.zuehlke.pgadmissions.services.UserService;
+import com.zuehlke.pgadmissions.services.helpers.PropertyLoader;
+import com.zuehlke.pgadmissions.utils.WordUtils;
 
 @RestController
 @RequestMapping("api/{resourceScope:programs|institutions|systems}/{resourceId}/configuration/{configurationType:customQuestions|displayProperties|stateDurations|workflowProperties}")
@@ -63,72 +81,28 @@ public class ResourceConfigurationResource {
         }
         return representations;
     }
-//
-//    @RequestMapping(value = "/{resourceId}/notificationTemplates/{notificationTemplateId}", method = RequestMethod.PUT)
-//    public void updateNotificationConfiguration(@ModelAttribute ResourceDescriptor resourceDescriptor, @PathVariable Integer resourceId,
-//                                                @PathVariable String notificationTemplateId, @RequestParam(required = false) PrismLocale locale,
-//                                                @RequestParam(required = false) PrismProgramType programType, @Valid @RequestBody NotificationConfigurationDTO notificationConfigurationDTO,
-//                                                BindingResult validationErrors) throws Exception {
-//        NotificationDefinition template = notificationService.getById(PrismNotificationDefinition.valueOf(notificationTemplateId));
-//        Resource resource = entityService.getById(resourceDescriptor.getType(), resourceId);
-//
-//        validateTemplate(resource, template, notificationConfigurationDTO, validationErrors);
-//        if (validationErrors.hasErrors()) {
-//            throw new PrismValidationException("Invalid notification configuration", validationErrors);
-//        }
-//
-//        notificationService.updateNotificatonConfiguration(resource, locale, programType, template, notificationConfigurationDTO);
-//    }
-//
-//    @RequestMapping(value = "/{resourceId}/notificationTemplates/{notificationTemplateId}", method = RequestMethod.DELETE)
-//    public void restoreDefaultConfiguration(@ModelAttribute ResourceDescriptor resourceDescriptor, @PathVariable Integer resourceId,
-//                                            @PathVariable String notificationTemplateId, @RequestParam(required = false) PrismLocale locale,
-//                                            @RequestParam(required = false) PrismProgramType programType) throws Exception {
-//        Resource resource = entityService.getById(resourceDescriptor.getType(), resourceId);
-//        NotificationDefinition template = notificationService.getById(PrismNotificationDefinition.valueOf(notificationTemplateId));
-//        notificationService.restoreDefaultNotificationConfiguration(resource, locale, programType, template);
-//    }
-//
-//    @RequestMapping(value = "/{resourceId}/notificationTemplates/{notificationTemplateId}/descendants", method = RequestMethod.DELETE)
-//    public void restoreGlobalConfiguration(@ModelAttribute ResourceDescriptor resourceDescriptor, @PathVariable Integer resourceId,
-//                                           @PathVariable String notificationTemplateId, @RequestParam(required = false) PrismLocale locale,
-//                                           @RequestParam(required = false) PrismProgramType programType) throws Exception {
-//        Resource resource = entityService.getById(resourceDescriptor.getType(), resourceId);
-//        NotificationDefinition template = notificationService.getById(PrismNotificationDefinition.valueOf(notificationTemplateId));
-//        notificationService.restoreGlobalNotificationConfiguration(resource, locale, programType, template);
-//    }
-//
-//    private Map<String, String> validateTemplate(Resource resource, NotificationDefinition notificationTemplate,
-//                                                 NotificationConfigurationDTO notificationConfigurationDTO, BindingResult errors) {
-//        if (notificationTemplate.getId().getReminderDefinition() == null && notificationConfigurationDTO.getReminderInterval() != null) {
-//            errors.rejectValue("reminderInterval", "forbidden");
-//        } else if (notificationTemplate.getId().getReminderDefinition() != null && notificationConfigurationDTO.getReminderInterval() == null) {
-//            errors.rejectValue("reminderInterval", "notNull");
-//        }
-//
-//        PropertyLoader propertyLoader = applicationContext.getBean(PropertyLoader.class).localize(resource, userService.getCurrentUser());
-//        MailSender mailSender = applicationContext.getBean(MailSender.class).localize(propertyLoader);
-//
-//        Map<String, Object> model = mailSender.createNotificationModelForValidation(notificationTemplate);
-//
-//        String subject = null, content = null;
-//        try {
-//            subject = mailSender.processHeader(notificationTemplate.getId(), notificationConfigurationDTO.getSubject(), model);
-//        } catch (Exception e) {
-//            errors.rejectValue("subject", "invalid");
-//        }
-//
-//        try {
-//            content = mailSender.processContent(notificationTemplate.getId(), notificationConfigurationDTO.getContent(), model, subject);
-//        } catch (Exception e) {
-//            errors.rejectValue("content", "invalid");
-//        }
-//
-//        if (subject == null || content == null) {
-//            throw new PrismValidationException("Invalid template", errors);
-//        }
-//        return ImmutableMap.of("subject", subject, "content", content);
-//    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    public List<AbstractConfigurationRepresentation> updateConfigurations(
+            @ModelAttribute ResourceDescriptor resourceDescriptor,
+            @PathVariable Integer resourceId,
+            @ModelAttribute PrismWorkflowConfiguration configurationType,
+            @RequestParam(required = false) PrismLocale locale,
+            @RequestParam(required = false) PrismProgramType programType) throws Exception {
+        Resource resource = entityService.getById(resourceDescriptor.getType(), resourceId);
+
+        List<WorkflowConfiguration> configurations = customizationService.listConfigurations(configurationType, resource, locale, programType);
+
+        List<AbstractConfigurationRepresentation> representations = Lists.newArrayListWithCapacity(configurations.size());
+        String definitionPropertyName = configurationType.getDefinitionPropertyName();
+        for (WorkflowConfiguration configuration : configurations) {
+            WorkflowDefinition workflowDefinition = (WorkflowDefinition) PropertyUtils.getSimpleProperty(configuration, definitionPropertyName);
+            AbstractConfigurationRepresentation representation = dozerBeanMapper.map(configuration, configurationType.getRepresentationClass());
+            representation.setDefinitionId(workflowDefinition.getId());
+            representations.add(representation);
+        }
+        return representations;
+    }
 
     @ModelAttribute
     private ResourceDescriptor getResourceDescriptor(@PathVariable String resourceScope) {

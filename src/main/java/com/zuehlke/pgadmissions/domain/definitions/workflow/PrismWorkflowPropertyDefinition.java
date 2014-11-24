8505b2c4af14a233e2c770fdead6ed75481ac5a0
jfi <jfi@zuhlke.com>
2014-11-24 20:38:29 +0000

    Fixed few problems with workflow properties

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/PrismWorkflowPropertyDefinition.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/PrismWorkflowPropertyDefinition.java
@@ -1,75 +1,25 @@
 package com.zuehlke.pgadmissions.domain.definitions.workflow;
 
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_INTERVIEWER_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_INTERVIEWER_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_PRIMARY_SUPERVISOR_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_PRIMARY_SUPERVISOR_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_REFEREE_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_REFEREE_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_REVIEWER_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_REVIEWER_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_SECONDARY_SUPERVISOR_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_SECONDARY_SUPERVISOR_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_SUGGESTED_SUPERVISOR_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_ASSIGN_SUGGESTED_SUPERVISOR_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_CREATOR_DEMOGRAPHIC_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_CREATOR_DEMOGRAPHIC_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_CRIMINAL_CONVICTION_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_CRIMINAL_CONVICTION_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_COVERING_LETTER_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_COVERING_LETTER_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_CV_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_CV_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_PERSONAL_STATEMENT_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_PERSONAL_STATEMENT_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_RESEARCH_STATEMENT_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_DOCUMENT_RESEARCH_STATEMENT_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_EMPLOYMENT_POSITION_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_EMPLOYMENT_POSITION_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_FUNDING_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_FUNDING_PROOF_OF_AWARD_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_FUNDING_PROOF_OF_AWARD_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_FUNDING_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_LANGUAGE_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_LANGUAGE_PROOF_OF_AWARD_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_LANGUAGE_PROOF_OF_AWARD_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_LANGUAGE_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_OFFER_DETAIL_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_OFFER_DETAIL_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_POSITION_DETAIL_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_POSITION_DETAIL_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_PRIZE_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_PRIZE_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_QUALIFICATION_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_QUALIFICATION_PROOF_OF_AWARD_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_QUALIFICATION_PROOF_OF_AWARD_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_QUALIFICATION_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_RESIDENCE_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_RESIDENCE_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_STUDY_DETAIL_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_THEME_PRIMARY_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_THEME_PRIMARY_TOOLTIP;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_THEME_SECONDARY_LABEL;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.APPLICATION_THEME_SECONDARY_TOOLTIP;
+import com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition;
+
+import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.*;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.APPLICATION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismWorkflowPropertyCategory.APPLICATION_DATA;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismWorkflowPropertyCategory.APPLICATION_WORKFLOW;
 
-import com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition;
-
 public enum PrismWorkflowPropertyDefinition {
 
-    APPLICATION_STUDY_DETAIL(APPLICATION_DATA, null, null, false, true, null, null, APPLICATION, APPLICATION_STUDY_DETAIL_LABEL, APPLICATION_STUDY_DETAIL_LABEL), //
+    APPLICATION_STUDY_DETAIL(APPLICATION_DATA, null, null, false, null, null, null, APPLICATION, APPLICATION_STUDY_DETAIL_LABEL, APPLICATION_STUDY_DETAIL_LABEL), //
     APPLICATION_THEME_PRIMARY(APPLICATION_DATA, 0, 10, true, null, 1, 1, APPLICATION, APPLICATION_THEME_PRIMARY_LABEL, APPLICATION_THEME_PRIMARY_TOOLTIP), //
     APPLICATION_THEME_SECONDARY(APPLICATION_DATA, 0, 10, true, null, 0, 2, APPLICATION, APPLICATION_THEME_SECONDARY_LABEL, APPLICATION_THEME_SECONDARY_TOOLTIP), //
     APPLICATION_ASSIGN_SUGGESTED_SUPERVISOR(APPLICATION_DATA, 0, 999, true, null, 0, 5, APPLICATION, APPLICATION_ASSIGN_SUGGESTED_SUPERVISOR_LABEL,
             APPLICATION_ASSIGN_SUGGESTED_SUPERVISOR_TOOLTIP), //
-    APPLICATION_CREATOR_DEMOGRAPHIC(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_CREATOR_DEMOGRAPHIC_LABEL,
+    APPLICATION_CREATOR_DEMOGRAPHIC(APPLICATION_DATA, null, null, true, null, null, null, APPLICATION, APPLICATION_CREATOR_DEMOGRAPHIC_LABEL,
             APPLICATION_CREATOR_DEMOGRAPHIC_TOOLTIP), //
-    APPLICATION_LANGUAGE(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_LANGUAGE_LABEL, APPLICATION_LANGUAGE_TOOLTIP), //
+    APPLICATION_LANGUAGE(APPLICATION_DATA, null, null, true, null, null, null, APPLICATION, APPLICATION_LANGUAGE_LABEL, APPLICATION_LANGUAGE_TOOLTIP), //
     APPLICATION_LANGUAGE_PROOF_OF_AWARD(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_LANGUAGE_PROOF_OF_AWARD_LABEL,
             APPLICATION_LANGUAGE_PROOF_OF_AWARD_TOOLTIP), //
-    APPLICATION_RESIDENCE(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_RESIDENCE_LABEL, APPLICATION_RESIDENCE_TOOLTIP), //
+    APPLICATION_RESIDENCE(APPLICATION_DATA, null, null, true, null, null, null, APPLICATION, APPLICATION_RESIDENCE_LABEL, APPLICATION_RESIDENCE_TOOLTIP), //
     APPLICATION_QUALIFICATION(APPLICATION_DATA, 0, 10, true, null, 0, 10, APPLICATION, APPLICATION_QUALIFICATION_LABEL, APPLICATION_QUALIFICATION_TOOLTIP), //
     APPLICATION_QUALIFICATION_PROOF_OF_AWARD(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_QUALIFICATION_PROOF_OF_AWARD_LABEL,
             APPLICATION_QUALIFICATION_PROOF_OF_AWARD_TOOLTIP), //
@@ -87,7 +37,7 @@ public enum PrismWorkflowPropertyDefinition {
     APPLICATION_DOCUMENT_CV(APPLICATION_DATA, null, null, true, false, null, null, APPLICATION, APPLICATION_DOCUMENT_CV_LABEL, APPLICATION_DOCUMENT_CV_TOOLTIP), //
     APPLICATION_DOCUMENT_COVERING_LETTER(APPLICATION_DATA, null, null, false, false, null, null, APPLICATION, APPLICATION_DOCUMENT_COVERING_LETTER_LABEL,
             APPLICATION_DOCUMENT_COVERING_LETTER_TOOLTIP), //
-    APPLICATION_CRIMINAL_CONVICTION(APPLICATION_DATA, null, null, true, true, null, null, APPLICATION, APPLICATION_CRIMINAL_CONVICTION_LABEL,
+    APPLICATION_CRIMINAL_CONVICTION(APPLICATION_DATA, null, null, true, null, null, null, APPLICATION, APPLICATION_CRIMINAL_CONVICTION_LABEL,
             APPLICATION_CRIMINAL_CONVICTION_TOOLTIP), //
     APPLICATION_ASSIGN_REVIEWER(APPLICATION_WORKFLOW, 1, 999, null, null, 1, 999, APPLICATION, APPLICATION_ASSIGN_REVIEWER_LABEL,
             APPLICATION_ASSIGN_REVIEWER_TOOLTIP), //
@@ -97,9 +47,9 @@ public enum PrismWorkflowPropertyDefinition {
             APPLICATION_ASSIGN_PRIMARY_SUPERVISOR_TOOLTIP), //
     APPLICATION_ASSIGN_SECONDARY_SUPERVISOR(APPLICATION_WORKFLOW, 0, 999, true, null, 1, 1, APPLICATION, APPLICATION_ASSIGN_SECONDARY_SUPERVISOR_LABEL,
             APPLICATION_ASSIGN_SECONDARY_SUPERVISOR_TOOLTIP), //
-    APPLICATION_POSITION_DETAIL(APPLICATION_WORKFLOW, null, null, true, true, null, null, APPLICATION, APPLICATION_POSITION_DETAIL_LABEL,
+    APPLICATION_POSITION_DETAIL(APPLICATION_WORKFLOW, null, null, true, null, null, null, APPLICATION, APPLICATION_POSITION_DETAIL_LABEL,
             APPLICATION_POSITION_DETAIL_TOOLTIP), //
-    APPLICATION_OFFER_DETAIL(APPLICATION_WORKFLOW, null, null, true, true, null, null, APPLICATION, APPLICATION_OFFER_DETAIL_LABEL,
+    APPLICATION_OFFER_DETAIL(APPLICATION_WORKFLOW, null, null, true, null, null, null, APPLICATION, APPLICATION_OFFER_DETAIL_LABEL,
             APPLICATION_OFFER_DETAIL_TOOLTIP);
 
     private PrismWorkflowPropertyCategory category;
@@ -123,8 +73,8 @@ public enum PrismWorkflowPropertyDefinition {
     private PrismDisplayPropertyDefinition tooltip;
 
     private PrismWorkflowPropertyDefinition(PrismWorkflowPropertyCategory category, Integer minimumPermitted, Integer maximumPermitted, Boolean defaultEnabled,
-            Boolean defaultRequired, Integer defaultMinimum, Integer defaultMaximum, PrismScope scope, PrismDisplayPropertyDefinition label,
-            PrismDisplayPropertyDefinition tooltip) {
+                                            Boolean defaultRequired, Integer defaultMinimum, Integer defaultMaximum, PrismScope scope, PrismDisplayPropertyDefinition label,
+                                            PrismDisplayPropertyDefinition tooltip) {
         this.category = category;
         this.minimumPermitted = minimumPermitted;
         this.maximumPermitted = maximumPermitted;
@@ -137,56 +87,56 @@ public enum PrismWorkflowPropertyDefinition {
         this.tooltip = tooltip;
     }
 
-    public final PrismWorkflowPropertyCategory getCategory() {
+    public PrismWorkflowPropertyCategory getCategory() {
         return category;
     }
 
-    public final Integer getMinimumPermitted() {
+    public Integer getMinimumPermitted() {
         return minimumPermitted;
     }
 
-    public final Integer getMaximumPermitted() {
+    public Integer getMaximumPermitted() {
         return maximumPermitted;
     }
 
-    public final Boolean getDefaultEnabled() {
+    public Boolean getDefaultEnabled() {
         return defaultEnabled;
     }
 
-    public final Boolean getDefaultRequired() {
+    public Boolean getDefaultRequired() {
         return defaultRequired;
     }
 
-    public final Integer getDefaultMinimum() {
+    public Integer getDefaultMinimum() {
         return defaultMinimum;
     }
 
-    public final Integer getDefaultMaximum() {
+    public Integer getDefaultMaximum() {
         return defaultMaximum;
     }
 
-    public final PrismScope getScope() {
+    public PrismScope getScope() {
         return scope;
     }
 
-    public final PrismDisplayPropertyDefinition getLabel() {
+    public PrismDisplayPropertyDefinition getLabel() {
         return label;
     }
 
-    public final PrismDisplayPropertyDefinition getTooltip() {
+    public PrismDisplayPropertyDefinition getTooltip() {
         return tooltip;
     }
 
-    public final boolean isDefineRange() {
+    public boolean isDefineRange() {
         return defaultRequired == null;
     }
 
-    public final boolean isDefineOnOrOff() {
+    public boolean isDefineOnOrOff() {
         return !isDefineRange();
     }
 
-    public final boolean isCanBeDisabled() {
-        return !(defaultEnabled == null);
+    public boolean isCanBeDisabled() {
+        return defaultEnabled != null;
     }
 
 }

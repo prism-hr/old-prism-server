6d1b9177a27d0b594dfe6e0260e23f5364e602c1
alastair <a.knowles@ucl.ac.uk>
2014-11-24 19:10:52 +0000

    Fixed various problems to do with configuration.

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/configuration/NotificationConfigurationRepresentation.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/configuration/NotificationConfigurationRepresentation.java
@@ -2,11 +2,31 @@ package com.zuehlke.pgadmissions.rest.representation.configuration;
 
 public class NotificationConfigurationRepresentation extends WorkflowConfigurationRepresentation {
 
+    private String label;
+    
+    private String tooltip;
+    
     private String subject;
 
     private String content;
 
     private Integer reminderInterval;
+
+    public final String getLabel() {
+        return label;
+    }
+
+    public final void setLabel(String label) {
+        this.label = label;
+    }
+
+    public final String getTooltip() {
+        return tooltip;
+    }
+
+    public final void setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+    }
 
     public String getSubject() {
         return subject;

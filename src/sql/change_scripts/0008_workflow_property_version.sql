5263743ba2246237d5d99a7bdd80a825c97769af
jfi <jfi@zuhlke.com>
2014-12-08 20:58:18 +0000

    Changescript syntax fix

--- src/sql/change_scripts/0008_workflow_property_version.sql
+++ src/sql/change_scripts/0008_workflow_property_version.sql
@@ -3,6 +3,6 @@ set workflow_property_configuration_version = (
 	select version
 	from workflow_property_configuration
 	where institution_id = 5243
-	and active = 1))
+	and active = 1)
 where institution_id = 5243
 ;

1a011216389110cd46f1b687843f071f1c83fbca
alastair <a.knowles@ucl.ac.uk>
2014-12-13 16:08:16 +0000

    Stop showing locales that are not supported yet.

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismLocale.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismLocale.java
@@ -2,11 +2,7 @@ package com.zuehlke.pgadmissions.domain.definitions;
 
 public enum PrismLocale {
 
-    EN_GB, //
-    DE_DE, //
-    DE_AT, //
-    DE_CH, //
-    SR_RS;
+    EN_GB;
     
     public static PrismLocale getSystemLocale() {
         return EN_GB;

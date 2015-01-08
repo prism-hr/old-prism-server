3738446092f40eceb31844d3a274a27919b81615
jfi <jfi@zuhlke.com>
2015-01-08 15:48:52 +0000

    First (not finished) version of robots controller

--- src/main/java/com/zuehlke/pgadmissions/redirect/RobotController.java
+++ src/main/java/com/zuehlke/pgadmissions/redirect/RobotController.java
@@ -1,17 +1,44 @@
 package com.zuehlke.pgadmissions.redirect;
 
-import org.springframework.stereotype.Controller;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Maps;
+import com.google.common.io.Resources;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 
-@Controller
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
+
+@RestController
 @RequestMapping("api/robots")
 public class RobotController {
 
+    @Autowired
+    private FreeMarkerConfig freemarkerConfig;
+
+    @Value("${application.url}")
+    private String applicationUrl;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String serve() {
-        // TODO finish it
-        return "<http></http>";
+    public String serve(@RequestParam) throws IOException, TemplateException {
+        String templateContent = Resources.toString(Resources.getResource("template/robot_representation.ftl"), Charsets.UTF_8);
+        Template template = new Template("robot_representation", new StringReader(templateContent), freemarkerConfig.getConfiguration());
+
+        Map<String, Object> model = Maps.newHashMap();
+        model.put("title", "ttt");
+        model.put("description", "ddd");
+        model.put("hostname", "applicationUrl");
+
+        return FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
     }
 
 }

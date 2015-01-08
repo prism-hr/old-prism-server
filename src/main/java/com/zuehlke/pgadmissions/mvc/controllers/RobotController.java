4f84d0004affe94f1c6c3a65f35f1d3c739ef4ff
jfi <jfi@zuhlke.com>
2015-01-08 21:00:16 +0000

    Missing controller annotation

--- src/main/java/com/zuehlke/pgadmissions/mvc/controllers/RobotController.java
+++ src/main/java/com/zuehlke/pgadmissions/mvc/controllers/RobotController.java
@@ -11,6 +11,7 @@ import com.zuehlke.pgadmissions.services.ResourceService;
 import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +24,7 @@ import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 @RequestMapping("api/robots")
 public class RobotController {
 

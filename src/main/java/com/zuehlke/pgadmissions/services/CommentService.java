5d49feb599ac3fa2711e23d843f80b68f3809fd6
alastair <a.knowles@ucl.ac.uk>
2015-01-14 19:10:11 +0000

    Fixed problems with reference workflow.

--- src/main/java/com/zuehlke/pgadmissions/services/CommentService.java
+++ src/main/java/com/zuehlke/pgadmissions/services/CommentService.java
@@ -17,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.ValidationUtils;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
@@ -36,9 +35,7 @@ import com.zuehlke.pgadmissions.domain.comment.CommentCustomResponse;
 import com.zuehlke.pgadmissions.domain.comment.CommentTransitionState;
 import com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismAction;
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismActionCategory;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismActionRedactionType;
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismActionType;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismRole;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismRoleTransitionType;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState;
@@ -319,11 +316,6 @@ public class CommentService {
     }
 
     public void create(Comment comment) {
-        Resource resource = comment.getResource();
-        Action action = comment.getAction();
-
-        setCommentAuthorRoles(comment, resource, action);
-
         Set<CommentAssignedUser> transientAssignees = comment.getAssignedUsers();
         Set<CommentAssignedUser> persistentAssignees = Sets.newHashSet(transientAssignees);
         transientAssignees.clear();
@@ -512,21 +504,6 @@ public class CommentService {
                 .withAppointmentConditions(offerDetailNull ? null : offerDetail.getAppointmentConditions());
     }
 
-    private void setCommentAuthorRoles(Comment comment, Resource resource, Action action) {
-        if (action.getActionType() == PrismActionType.SYSTEM_INVOCATION) {
-            comment.setRole(PrismRole.SYSTEM_ADMINISTRATOR.toString());
-        } else {
-            if (action.getActionCategory() == PrismActionCategory.CREATE_RESOURCE) {
-                comment.setRole(roleService.getCreatorRole(resource).getId().toString());
-            } else {
-                comment.setRole(Joiner.on(", ").join(roleService.getActionOwnerRoles(comment.getUser(), resource, action)));
-                if (comment.getDelegateUser() != null) {
-                    comment.setDelegateRole(Joiner.on(", ").join(roleService.getActionOwnerRoles(comment.getDelegateUser(), resource, action)));
-                }
-            }
-        }
-    }
-
     private Set<ApplicationAssignedSupervisorRepresentation> buildApplicationSupervisorRepresentation(Comment assignmentComment) {
         Set<ApplicationAssignedSupervisorRepresentation> supervisors = Sets.newLinkedHashSet();
 
@@ -645,8 +622,8 @@ public class CommentService {
         for (CommentAssignedUser assignee : comment.getAssignedUsers()) {
             PrismRole roleId = assignee.getRole().getId();
             if (Arrays.asList(PrismRole.APPLICATION_INTERVIEWEE, PrismRole.APPLICATION_INTERVIEWER).contains(roleId)) {
-                Comment preference = new Comment().withResource(resource).withAction(action).withUser(assignee.getUser()).withRole(roleId.name())
-                        .withDeclinedResponse(false).withState(resource.getState()).withTransitionState(resource.getState()).withCreatedTimestamp(baseline);
+                Comment preference = new Comment().withResource(resource).withAction(action).withUser(assignee.getUser()).withDeclinedResponse(false)
+                        .withState(resource.getState()).withTransitionState(resource.getState()).withCreatedTimestamp(baseline);
                 preference.getAppointmentPreferences().add(new CommentAppointmentPreference().withDateTime(interviewDateTime));
                 create(preference);
                 resource.addComment(preference);

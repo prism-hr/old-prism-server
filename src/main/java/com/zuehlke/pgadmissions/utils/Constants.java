48397aa9b3792c2074b95d76da8522efbc066498
alastair <a.knowles@ucl.ac.uk>
2015-01-28 14:13:15 +0000

    Small further fix on rating.

--- src/main/java/com/zuehlke/pgadmissions/utils/Constants.java
+++ src/main/java/com/zuehlke/pgadmissions/utils/Constants.java
@@ -7,5 +7,7 @@ public class Constants {
     public static final String DOT = ".";
 
     public static final String FILE_EXTENSION_PDF = "pdf";
-    
+
+    public static final Integer DEFAULT_RATING = 3;
+
 }

26057da5220f2d23e6b59a4b0d9bc5e2c331a670
jfi <jfi@zuhlke.com>
2015-02-10 16:12:27 +0000

    Dropped production related change script

--- src/sql/change_scripts/0033_fix_16741.sql
+++
@@ -1,3 +0,0 @@
-insert into comment(application_id, user_id, action_id, declined_response, content, state_id, transition_state_id, created_timestamp)
-values(16741, 14112, "APPLICATION_COMPLETE_VERIFICATION_STAGE", 0, "Collect references now.", "APPLICATION_VERIFICATION_PENDING_COMPLETION", "APPLICATION_REFERENCE", "2015-01-09 10:18:13" + interval 1 hour)
-;

76c2a64f59685f0e8de5a00b288e3afa225bd342
jfi <jfi@zuhlke.com>
2015-02-19 19:16:43 +0000

    Implemented token renewing functionality

--- src/main/java/com/zuehlke/pgadmissions/security/AuthenticationTokenProcessingFilter.java
+++ src/main/java/com/zuehlke/pgadmissions/security/AuthenticationTokenProcessingFilter.java
@@ -1,14 +1,6 @@
 package com.zuehlke.pgadmissions.security;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.zuehlke.pgadmissions.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,7 +8,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.GenericFilterBean;
 
-import com.zuehlke.pgadmissions.services.UserService;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
 
@@ -29,6 +27,7 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = this.getAsHttpRequest(request);
+        HttpServletResponse httpResponse = this.getAsHttpResponse(response);
 
         String authToken = this.extractAuthTokenFromRequest(httpRequest);
         Integer userId = authenticationTokenHelper.getIdFromToken(authToken);
@@ -36,10 +35,14 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
         if (userId != null) {
 
             UserDetails userDetails = this.userService.getById(userId);
-            if (authenticationTokenHelper.validateToken(authToken, userDetails)) {
+            TokenValidityStatus tokenValidityStatus = authenticationTokenHelper.validateToken(authToken, userDetails);
+            if (tokenValidityStatus.isValid()) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                if(tokenValidityStatus.getRenewedToken() != null){
+                    httpResponse.setHeader("x-auth-token-renew", tokenValidityStatus.getRenewedToken());
+                }
             } else {
                 ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Session has expired");
                 return;
@@ -55,6 +58,14 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
         }
 
         return (HttpServletRequest) request;
+    }
+
+    private HttpServletResponse getAsHttpResponse(ServletResponse response) {
+        if (!(response instanceof HttpServletResponse)) {
+            throw new RuntimeException("Expecting an HTTP response");
+        }
+
+        return (HttpServletResponse) response;
     }
 
     private String extractAuthTokenFromRequest(HttpServletRequest httpRequest) {

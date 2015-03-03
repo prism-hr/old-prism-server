0ecac1c89c5e645e2a39403fe2901790b515d8c2
alastair <alastair@localhost>
2015-03-03 01:50:22 +0000

    Fixed another silly mistake.

--- src/main/java/com/zuehlke/pgadmissions/workflow/resolvers/state/duration/ApplicationClosingDateResolver.java
+++ src/main/java/com/zuehlke/pgadmissions/workflow/resolvers/state/duration/ApplicationClosingDateResolver.java
@@ -1,10 +1,12 @@
 package com.zuehlke.pgadmissions.workflow.resolvers.state.duration;
 
 import org.joda.time.LocalDate;
+import org.springframework.stereotype.Component;
 
 import com.zuehlke.pgadmissions.domain.comment.Comment;
 import com.zuehlke.pgadmissions.domain.resource.Resource;
 
+@Component
 public class ApplicationClosingDateResolver implements StateDurationResolver {
 
 	@Override

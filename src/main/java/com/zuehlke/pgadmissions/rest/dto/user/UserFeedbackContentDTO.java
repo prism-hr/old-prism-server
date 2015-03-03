ca2fbc4de669e8d0774b2191311f58929e22f230
jfi <jfi@zuhlke.com>
2015-03-03 18:23:45 +0000

    FeedbackService refactored to use composition

---
+++ src/main/java/com/zuehlke/pgadmissions/rest/dto/user/UserFeedbackContentDTO.java
@@ -0,0 +1,40 @@
+package com.zuehlke.pgadmissions.rest.dto.user;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+public class UserFeedbackContentDTO {
+
+	@NotNull
+	private Integer rating;
+
+	@NotEmpty
+	private String content;
+
+    private String featureRequests;
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFeatureRequests() {
+        return featureRequests;
+    }
+
+    public void setFeatureRequests(String featureRequests) {
+        this.featureRequests = featureRequests;
+    }
+}

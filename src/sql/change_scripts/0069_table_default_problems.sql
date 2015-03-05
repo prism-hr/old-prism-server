1f10631a12d4ac7962172c55476ebbd3a4405405
alastair <alastair@localhost>
2015-03-05 13:03:19 +0000

    Problem with id column in user feedback table.

--- src/sql/change_scripts/0069_table_default_problems.sql
+++ src/sql/change_scripts/0069_table_default_problems.sql
@@ -14,5 +14,9 @@ alter table user_account_external
 	convert to character set utf8 collate utf8_unicode_ci
 ;
 
+alter table user_feedback
+	modify column id int(10) unsigned not null auto_increment
+;
+
 set foreign_key_checks = 1
 ;

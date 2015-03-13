a3bc6a59177ac3ce83b732d7cf31a4434a18bad2
jfi <jfi@zuhlke.com>
2015-03-13 17:41:48 +0000

    Fixes

--- src/sql/change_scripts/0069_table_default_problems.sql
+++ src/sql/change_scripts/0069_table_default_problems.sql
@@ -1,21 +1,13 @@
 set foreign_key_checks = 0
 ;
 
-alter table user_feedback 
-	convert to character set utf8 collate utf8_unicode_ci
-;
-
-alter table state_termination 
+alter table state_termination
 	convert to character set utf8 collate utf8_unicode_ci,
 	engine = innodb
 ;
 
-alter table user_account_external 
+alter table user_account_external
 	convert to character set utf8 collate utf8_unicode_ci
-;
-
-alter table user_feedback
-	modify column id int(10) unsigned not null auto_increment
 ;
 
 set foreign_key_checks = 1

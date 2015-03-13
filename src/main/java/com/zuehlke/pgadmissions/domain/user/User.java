018b0760c11cec1970f23520b09ea6d40db579ee
jfi <jfi@zuhlke.com>
2015-03-13 16:59:12 +0000

    Fixed collaction problem and removed unnecessary column from user

--- src/main/java/com/zuehlke/pgadmissions/domain/user/User.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/user/User.java
@@ -1,37 +1,22 @@
 package com.zuehlke.pgadmissions.domain.user;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.OrderBy;
-import org.hibernate.annotations.Type;
-import org.joda.time.LocalDate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import com.google.common.collect.Sets;
 import com.zuehlke.pgadmissions.domain.UniqueEntity;
 import com.zuehlke.pgadmissions.domain.application.Application;
 import com.zuehlke.pgadmissions.domain.definitions.PrismLocale;
 import com.zuehlke.pgadmissions.domain.document.Document;
 import com.zuehlke.pgadmissions.domain.resource.Resource;
-import com.zuehlke.pgadmissions.domain.workflow.Scope;
 import com.zuehlke.pgadmissions.utils.ReflectionUtils;
+import org.hibernate.annotations.OrderBy;
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 
 @Entity
 @Table(name = "USER")
@@ -101,26 +86,22 @@ public class User implements UserDetails, UniqueEntity {
 	private LocalDate lastNotifiedDateApplication;
 
 	@ManyToOne
-	@JoinColumn(name = "latest_creation_scope_id")
-	private Scope latestCreationScope;
-
-	@ManyToOne
 	@JoinColumn(name = "parent_user_id")
 	private User parentUser;
-	
+
 	@OrderBy(clause = "last_name asc, first_name asc")
 	@OneToMany(mappedBy = "parentUser")
 	private Set<User> childUsers = Sets.newHashSet();
 
 	@OneToMany(mappedBy = "user")
 	private Set<UserRole> userRoles = Sets.newHashSet();
-	
+
 	@OneToMany(mappedBy = "user")
 	private Set<Application> applications = Sets.newHashSet();
-	
+
 	@OneToMany(mappedBy = "user")
 	private Set<UserInstitutionIdentity> institutionIdentities = Sets.newHashSet();
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -257,14 +238,6 @@ public class User implements UserDetails, UniqueEntity {
 		this.lastNotifiedDateApplication = lastNotifiedDateApplication;
 	}
 
-	public Scope getLatestCreationScope() {
-		return latestCreationScope;
-	}
-
-	public void setLatestCreationScope(Scope latestCreationScope) {
-		this.latestCreationScope = latestCreationScope;
-	}
-
 	public User getParentUser() {
 		return parentUser;
 	}
@@ -280,7 +253,7 @@ public class User implements UserDetails, UniqueEntity {
 	public Set<UserRole> getUserRoles() {
 		return userRoles;
 	}
-	
+
 	public Set<Application> getApplications() {
 		return applications;
 	}

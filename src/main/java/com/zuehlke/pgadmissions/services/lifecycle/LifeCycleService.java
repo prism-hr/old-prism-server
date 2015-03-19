48a31cc0c90a740353ec335781e3d431891483ef
jfi <jfi@zuhlke.com>
2015-03-19 16:31:12 +0000

    Some more i18n stuff

--- src/main/java/com/zuehlke/pgadmissions/services/lifecycle/LifeCycleService.java
+++ src/main/java/com/zuehlke/pgadmissions/services/lifecycle/LifeCycleService.java
@@ -1,12 +1,7 @@
 package com.zuehlke.pgadmissions.services.lifecycle;
 
-import java.io.IOException;
-
-import javax.annotation.Resource;
-import javax.inject.Inject;
-
-import org.apache.commons.lang.BooleanUtils;
-import org.springframework.beans.BeansException;
+import com.zuehlke.pgadmissions.services.SystemService;
+import com.zuehlke.pgadmissions.services.lifecycle.helpers.ImportedEntityServiceHelperSystem;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
@@ -14,14 +9,8 @@ import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import com.zuehlke.pgadmissions.exceptions.CustomizationException;
-import com.zuehlke.pgadmissions.exceptions.DataImportException;
-import com.zuehlke.pgadmissions.exceptions.DeduplicationException;
-import com.zuehlke.pgadmissions.exceptions.IntegrationException;
-import com.zuehlke.pgadmissions.exceptions.WorkflowConfigurationException;
-import com.zuehlke.pgadmissions.exceptions.WorkflowEngineException;
-import com.zuehlke.pgadmissions.services.SystemService;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.ImportedEntityServiceHelperSystem;
+import javax.annotation.Resource;
+import javax.inject.Inject;
 
 @Service
 public class LifeCycleService implements InitializingBean, ApplicationListener<ContextClosedEvent> {
@@ -32,30 +21,42 @@ public class LifeCycleService implements InitializingBean, ApplicationListener<C
     @Value("${startup.import.system.data}")
     private Boolean initializeData;
 
+    @Value("${startup.display.initialize}")
+    private Boolean initializeDisplayProperties;
+
+    @Value("${startup.display.initialize.drop}")
+    private Boolean dropDisplayProperties;
+
     @Inject
     private ImportedEntityServiceHelperSystem importedEntityServiceHelperSystem;
 
     @Inject
     private SystemService systemService;
-    
-	@Resource(name = "taskExecutor")
-	private ThreadPoolTaskExecutor executor;
+
+    @Resource(name = "taskExecutor")
+    private ThreadPoolTaskExecutor executor;
 
     @Override
-    public void afterPropertiesSet() throws WorkflowConfigurationException, DeduplicationException, DataImportException, IOException, InterruptedException,
-            CustomizationException, InstantiationException, IllegalAccessException, BeansException, WorkflowEngineException, IntegrationException {
-        if (BooleanUtils.isTrue(initializeWorkflow)) {
+    public void afterPropertiesSet() throws Exception {
+        if (initializeWorkflow) {
             systemService.initializeSystem();
         }
 
-        if (BooleanUtils.isTrue(initializeData)) {
+        if (initializeData) {
             importedEntityServiceHelperSystem.execute();
+        }
+
+        if (initializeDisplayProperties) {
+            if (dropDisplayProperties) {
+                systemService.dropDisplayProperties();
+            }
+            systemService.initializeDisplayProperties();
         }
     }
 
-	@Override
+    @Override
     public void onApplicationEvent(ContextClosedEvent event) {
-	    executor.shutdown();
+        executor.shutdown();
     }
 
 }

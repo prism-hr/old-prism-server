67898453098be9c1938a3b770684686dcfd0d57b
jfi <jfi@zuhlke.com>
2015-03-29 16:51:20 +0100

    More i18n

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismProgramCategory.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismProgramCategory.java
@@ -1,18 +1,12 @@
 package com.zuehlke.pgadmissions.domain.definitions;
 
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.PROGRAM_CATEGORY_EXPERIENCE;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.PROGRAM_CATEGORY_FUNDING;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.PROGRAM_CATEGORY_LEARNING;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.PROGRAM_CATEGORY_STUDY;
-import static com.zuehlke.pgadmissions.domain.definitions.PrismDisplayPropertyDefinition.PROGRAM_CATEGORY_WORK;
-
 public enum PrismProgramCategory {
 
-    STUDY(true, false, true, PROGRAM_CATEGORY_STUDY), //
-    FUNDING(false, true, true, PROGRAM_CATEGORY_FUNDING), //
-    EXPERIENCE(false, true, false, PROGRAM_CATEGORY_EXPERIENCE), //
-    WORK(false, true, false, PROGRAM_CATEGORY_WORK), //
-    LEARNING(true, false, false, PROGRAM_CATEGORY_LEARNING);
+    STUDY(true, false, true),
+    FUNDING(false, true, true),
+    EXPERIENCE(false, true, false),
+    WORK(false, true, false),
+    LEARNING(true, false, false);
 
     private boolean hasFee;
 
@@ -20,29 +14,22 @@ public enum PrismProgramCategory {
 
     private boolean requireDuration;
 
-    private PrismDisplayPropertyDefinition displayProperty;
-
-    private PrismProgramCategory(boolean hasFee, boolean hasPay, boolean requireDuration, PrismDisplayPropertyDefinition displayProperty) {
+    PrismProgramCategory(boolean hasFee, boolean hasPay, boolean requireDuration) {
         this.hasFee = hasFee;
         this.hasPay = hasPay;
         this.requireDuration = requireDuration;
-        this.displayProperty = displayProperty;
     }
 
-    public final boolean isHasFee() {
+    public boolean isHasFee() {
         return hasFee;
     }
 
-    public final boolean isHasPay() {
+    public boolean isHasPay() {
         return hasPay;
     }
 
-    public final boolean isRequireDuration() {
+    public boolean isRequireDuration() {
         return requireDuration;
-    }
-
-    public final PrismDisplayPropertyDefinition getDisplayProperty() {
-        return displayProperty;
     }
 
 }

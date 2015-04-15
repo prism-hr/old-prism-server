e430c25b63d90cf9227592984925882c74957f47
alastair <alastair@localhost>
2015-04-15 11:45:02 +0100

    Missing display property.

--- src/sql/change_scripts/0075_refactor_workflow.sql
+++ src/sql/change_scripts/0075_refactor_workflow.sql
@@ -1,3 +1,27 @@
+delete
+from user_role
+where role_id = "APPLICATION_SUGGESTED_SUPERVISOR"
+;
+
+delete
+from role_transition
+where "APPLICATION_SUGGESTED_SUPERVISOR" in (role_id, transition_role_id)
+;
+
+delete
+from comment_assigned_user
+where role_id = "APPLICATION_SUGGESTED_SUPERVISOR"
+;
+
+delete
+from role
+where id = "APPLICATION_SUGGESTED_SUPERVISOR"
+;
+
+update state_group
+set sequence_order = sequence_order - 1
+;
+
 alter table state_group
 	drop index scope_id,
 	change column sequence_order ordinal int(2) unsigned not null,

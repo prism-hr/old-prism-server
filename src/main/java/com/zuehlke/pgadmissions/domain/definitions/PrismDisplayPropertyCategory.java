f57d7f423e44bf5a61da075710112f8cb2afb32b
jfi <jfi@zuhlke.com>
2015-04-16 12:08:17 +0100

    Added display values for categories

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismDisplayPropertyCategory.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismDisplayPropertyCategory.java
@@ -1,12 +1,8 @@
 package com.zuehlke.pgadmissions.domain.definitions;
 
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.APPLICATION;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.INSTITUTION;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.PROGRAM;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.PROJECT;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.SYSTEM;
-
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope;
+
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.*;
 
 public enum PrismDisplayPropertyCategory {
 
@@ -31,6 +27,7 @@ public enum PrismDisplayPropertyCategory {
     SYSTEM_LOCALE(SYSTEM),
     SYSTEM_REFEREE_TYPE(SYSTEM),
     SYSTEM_RESERVE_STATUS(SYSTEM),
+    SYSTEM_DISPLAY_PROPERTY_CATEGORY(SYSTEM),
     SYSTEM_VALIDATION_ERROR(SYSTEM),
     SYSTEM_INTEGRATION(SYSTEM),
     SYSTEM_STATE_DURATION(SYSTEM),

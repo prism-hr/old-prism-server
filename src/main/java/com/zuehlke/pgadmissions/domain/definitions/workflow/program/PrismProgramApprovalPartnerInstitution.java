a95ebd0675d9d05f94f7c00d900b3bf849d68562
alastair <alastair@localhost>
2015-05-21 01:38:23 +0100

    Everything looking pretty good now. Some small client bugs to sort out.

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/program/PrismProgramApprovalPartnerInstitution.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/program/PrismProgramApprovalPartnerInstitution.java
@@ -3,13 +3,12 @@ package com.zuehlke.pgadmissions.domain.definitions.workflow.program;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismAction.PROGRAM_STARTUP;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismNotificationDefinition.PROGRAM_STARTUP_NOTIFICATION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismRole.PROGRAM_ADMINISTRATOR;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.PROGRAM_APPROVAL;
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateTransitionGroup.PROGRAM_STARTUP_TRANSITION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programEscalateUnapproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programViewEditUnapproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programWithdraw;
 
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateAction;
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateTransition;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismWorkflowState;
 
 public class PrismProgramApprovalPartnerInstitution extends PrismWorkflowState {
@@ -21,9 +20,7 @@ public class PrismProgramApprovalPartnerInstitution extends PrismWorkflowState {
         stateActions.add(new PrismStateAction() //
                 .withAction(PROGRAM_STARTUP) //
                 .withNotifications(PROGRAM_ADMINISTRATOR, PROGRAM_STARTUP_NOTIFICATION) //
-                .withTransitions(new PrismStateTransition() //
-                        .withTransitionState(PROGRAM_APPROVAL) //
-                        .withTransitionAction(PROGRAM_STARTUP)));
+                .withTransitions(PROGRAM_STARTUP_TRANSITION));
 
         stateActions.add(programViewEditUnapproved()); //
         stateActions.add(programWithdraw());
--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/project/PrismProjectApprovalPartnerInstitution.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/project/PrismProjectApprovalPartnerInstitution.java
@@ -3,13 +3,12 @@ package com.zuehlke.pgadmissions.domain.definitions.workflow.project;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismAction.PROJECT_STARTUP;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismNotificationDefinition.PROJECT_STARTUP_NOTIFICATION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismRole.PROJECT_ADMINISTRATOR;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.PROJECT_APPROVAL;
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateTransitionGroup.PROJECT_STARTUP_TRANSITION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.project.PrismProjectWorkflow.projectEscalateUnapproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.project.PrismProjectWorkflow.projectViewEditUnapproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.project.PrismProjectWorkflow.projectWithdraw;
 
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateAction;
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateTransition;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismWorkflowState;
 
 public class PrismProjectApprovalPartnerInstitution extends PrismWorkflowState {
@@ -21,10 +20,8 @@ public class PrismProjectApprovalPartnerInstitution extends PrismWorkflowState {
         stateActions.add(new PrismStateAction() //
                 .withAction(PROJECT_STARTUP) //
                 .withNotifications(PROJECT_ADMINISTRATOR, PROJECT_STARTUP_NOTIFICATION) //
-                .withTransitions(new PrismStateTransition() //
-                        .withTransitionState(PROJECT_APPROVAL) //
-                        .withTransitionAction(PROJECT_STARTUP)));
-        
+                .withTransitions(PROJECT_STARTUP_TRANSITION));
+
         stateActions.add(projectViewEditUnapproved()); //
         stateActions.add(projectWithdraw());
     }

05a22142f804438695e99cb617fe0d1cdf9c40ec
alastair <alastair@localhost>
2015-05-21 05:55:00 +0100

    Finally fixed sql problems with application week sequence.

--- src/sql/change_scripts/0120_fix_report_problem.sql
+++ src/sql/change_scripts/0120_fix_report_problem.sql
@@ -17,45 +17,9 @@ update application
 set application_month_sequence = date(concat(year(created_timestamp), "-", month(created_timestamp), "-01"))
 ;
 
-create procedure application_week_monday()
-begin
-	set @application_id = (
-		select id
-		from application
-		order by id
-		limit 1);
-		
-	loop1: while @application_id is not null do 
-		set @created_timestamp = (
-			select created_timestamp
-			from application
-			where id = @application_id);
-			
-		set @created_timestamp_day_of_week = (
-			select dayofweek(@created_timestamp));
-			
-		set @created_timestamp_day_of_week_shift = (
-			select if(@created_timestamp_day_of_week = 1, 6, (@created_timestamp_day_of_week - 2)));
-				
-		update application
-		set application_week_sequence = @created_timestamp - interval @created_timestamp_day_of_week_shift day;
-		
-		set @application_id = (
-			select id
-			from application
-			where id > @application_id
-			order by id
-			limit 1);
-			
-		if @application_id is null then
-			leave loop1;
-		end if;		
-	end while;
-end
-;
-
-call application_week_monday()
-;
-
-drop procedure application_week_monday
+update application
+set application_week_sequence = if(
+	dayofweek(created_timestamp) = 1,
+		date(created_timestamp - interval 6 day),
+		date(created_timestamp - interval (dayofweek(created_timestamp) - 2) day))
 ;

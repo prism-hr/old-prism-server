59ae4ed935238698d4445166b1b6f1e0cef38b79
alastair <alastair@localhost>
2015-06-01 16:54:07 +0100

    Fixed security problem.

--- src/main/java/com/zuehlke/pgadmissions/dao/ActionDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/ActionDAO.java
@@ -161,12 +161,15 @@ public class ActionDAO {
                         .add(Projections.max("stateAction.raisesUrgentFlag"), "raisesUrgentFlag") //
                         .add(Projections.max("primaryState"), "primaryState")) //
                 .createAlias(resourceReference, resourceReference, JoinType.INNER_JOIN) //
-                .createAlias(resourceReference + ".resourceConditions", "resourceCondition", JoinType.LEFT_OUTER_JOIN) //
+                .createAlias(resourceReference + ".resourceConditions", "resourceCondition", JoinType.INNER_JOIN) //
                 .createAlias("state", "state", JoinType.INNER_JOIN) //
                 .createAlias("state.stateActions", "stateAction", JoinType.INNER_JOIN) //
                 .createAlias("stateAction.action", "action", JoinType.INNER_JOIN) //
                 .createAlias("stateAction.stateActionAssignments", "stateActionAssignment", JoinType.LEFT_OUTER_JOIN) //
                 .add(Restrictions.in(resourceReference + ".id", resourceIds)) //
+                .add(Restrictions.disjunction() //
+                        .add(Restrictions.eq("resourceCondition.actionCondition", ACCEPT_APPLICATION)) //
+                        .add(Restrictions.eq("resourceCondition.partnerMode", true))) //
                 .add(Restrictions.eq("action.actionType", USER_INVOCATION)) //
                 .add(Restrictions.isNull("stateActionAssignment.id"));
 

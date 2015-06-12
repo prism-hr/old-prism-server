55cc3e3d76f1706a7bb2d28b8d02755837a8edab
felipe <felipe@localhost>
2015-06-12 12:58:12 +0100

    enabled all services that I disabled yesterday. Ups ;)

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismMaintenanceTask.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismMaintenanceTask.java
@@ -17,19 +17,19 @@ import com.zuehlke.pgadmissions.services.lifecycle.helpers.StateServiceHelperPro
 
 public enum PrismMaintenanceTask {
 
-//    SYSTEM_UPDATE_PROGRAM_STUDY_OPTION(ResourceServiceHelper.class), //
-//    SYSTEM_EXECUTE_ESCALATED_STATE_TRANSITION(StateServiceHelperEscalation.class), //
-//    SYSTEM_EXECUTE_PROPAGATED_STATE_TRANSITION(StateServiceHelperPropagation.class), //
-    SYSTEM_IMPORT_INSTITUTION_REFERENCE_DATA(ImportedEntityServiceHelperInstitution.class);//
-//    SYSTEM_EXPORT_APPLICATION(ApplicationExportServiceHelper.class), //
-//    SYSTEM_SEND_DEFERRED_WORKFLOW_NOTIFICATION(NotificationServiceHelperWorkflow.class), //
-//    SYSTEM_IMPORT_SYSTEM_REFERENCE_DATA(ImportedEntityServiceHelperSystem.class), //
-//    SYSTEM_UPDATE_ADVERT_CLOSING_DATE(AdvertServiceHelperClosingDate.class), //
-//    SYSTEM_UPDATE_ADVERT_EXCHANGE_RATE(AdvertServiceHelperExchangeRate.class), //
-//    SYSTEM_SEND_RECOMMENDATION_NOTIFICATION(NotificationServiceHelperRecommendation.class), //
-//    SYSTEM_EXPORT_DOCUMENT(DocumentServiceHelperExport.class), //
-//    SYSTEM_DELETE_DOCUMENT(DocumentServiceHelperDelete.class), //
-//    SYSTEM_EMAIL_BOUNCE_HANDLE(EmailBounceServiceHelper.class);
+    SYSTEM_UPDATE_PROGRAM_STUDY_OPTION(ResourceServiceHelper.class), //
+    SYSTEM_EXECUTE_ESCALATED_STATE_TRANSITION(StateServiceHelperEscalation.class), //
+    SYSTEM_EXECUTE_PROPAGATED_STATE_TRANSITION(StateServiceHelperPropagation.class), //
+    SYSTEM_IMPORT_INSTITUTION_REFERENCE_DATA(ImportedEntityServiceHelperInstitution.class),//
+    SYSTEM_EXPORT_APPLICATION(ApplicationExportServiceHelper.class), //
+    SYSTEM_SEND_DEFERRED_WORKFLOW_NOTIFICATION(NotificationServiceHelperWorkflow.class), //
+    SYSTEM_IMPORT_SYSTEM_REFERENCE_DATA(ImportedEntityServiceHelperSystem.class), //
+    SYSTEM_UPDATE_ADVERT_CLOSING_DATE(AdvertServiceHelperClosingDate.class), //
+    SYSTEM_UPDATE_ADVERT_EXCHANGE_RATE(AdvertServiceHelperExchangeRate.class), //
+    SYSTEM_SEND_RECOMMENDATION_NOTIFICATION(NotificationServiceHelperRecommendation.class), //
+    SYSTEM_EXPORT_DOCUMENT(DocumentServiceHelperExport.class), //
+    SYSTEM_DELETE_DOCUMENT(DocumentServiceHelperDelete.class), //
+    SYSTEM_EMAIL_BOUNCE_HANDLE(EmailBounceServiceHelper.class);
 
     private Class<? extends AbstractServiceHelper> executor;
 

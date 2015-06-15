6e0d37297115c346d9f12faee74a57026b2e69da
jfi <jfi@zuhlke.com>
2015-06-15 11:32:29 +0100

    Fixed issues with institution import

--- src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedEntityExtractor.java
+++ src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedEntityExtractor.java
@@ -1,12 +1,12 @@
 package com.zuehlke.pgadmissions.services.helpers.extractors;
 
-import java.util.List;
-
 import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
 import com.zuehlke.pgadmissions.domain.institution.Institution;
 
+import java.util.List;
+
 public interface ImportedEntityExtractor {
 
-    public List<String> extract(Institution institution, PrismImportedEntity prismImportedEntity, List<Object> definitions) throws Exception;
+    List<String> extract(Institution institution, PrismImportedEntity prismImportedEntity, List<Object> definitions) throws Exception;
 
 }
--- src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedInstitutionExtractor.java
+++ src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedInstitutionExtractor.java
@@ -1,21 +1,19 @@
 package com.zuehlke.pgadmissions.services.helpers.extractors;
 
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareCellsForSqlInsert;
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareStringForSqlInsert;
-
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-
-import org.springframework.stereotype.Component;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
 import com.zuehlke.pgadmissions.domain.imported.Domicile;
 import com.zuehlke.pgadmissions.domain.institution.Institution;
 import com.zuehlke.pgadmissions.services.ImportedEntityService;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+
+import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareCellsForSqlInsert;
+import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareStringForSqlInsert;
 
 @Component
 public class ImportedInstitutionExtractor implements ImportedEntityExtractor {
@@ -35,13 +33,16 @@ public class ImportedInstitutionExtractor implements ImportedEntityExtractor {
         for (Object definition : definitions) {
             com.zuehlke.pgadmissions.referencedata.jaxb.Institutions.Institution importedInstitution = (com.zuehlke.pgadmissions.referencedata.jaxb.Institutions.Institution) definition;
 
+            String code = importedInstitution.getCode();
+            Integer custom = (code.startsWith("CUST") || code.startsWith("PRISM_")) ? 1 : 0;
+
             List<String> cells = Lists.newLinkedList();
             cells.add(prepareStringForSqlInsert(institution.getId().toString()));
             cells.add(prepareStringForSqlInsert(domicilesByCode.get(importedInstitution.getDomicile())));
             cells.add(prepareStringForSqlInsert(importedInstitution.getCode()));
             cells.add(prepareStringForSqlInsert(importedInstitution.getName()));
             cells.add(prepareStringForSqlInsert(new Integer(1).toString()));
-            cells.add(prepareStringForSqlInsert(new Integer(0).toString()));
+            cells.add(prepareStringForSqlInsert(new Integer(custom).toString()));
             cells.add(prepareStringForSqlInsert(importedInstitution.getUcasId()));
             cells.add(prepareStringForSqlInsert(importedInstitution.getFacebookId()));
             String row = prepareCellsForSqlInsert(cells);

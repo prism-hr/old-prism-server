b89f60b01eac5c4697266bbf6c647c047c2b832e
alastair <alastair@localhost>
2015-07-01 04:07:43 +0100

    Renames to make the package structure understandable.

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/ResourceClientRepresentation.java
+++
@@ -1,9 +0,0 @@
-package com.zuehlke.pgadmissions.rest.representation.resource;
-
-public interface ResourceClientRepresentation {
-
-    public ResourceSummaryRepresentation getResourceSummary();
-
-    public void setResourceSummary(ResourceSummaryRepresentation resourceSummary);
-
-}

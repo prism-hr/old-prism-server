c41edf5f8b11633a96fe3d5942919c8c5366b2bb
jfi <jfi@zuhlke.com>
2015-07-19 17:57:10 +0100

    Added support for targetting institutions

--- src/main/java/com/zuehlke/pgadmissions/domain/TargetEntity.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/TargetEntity.java
@@ -2,8 +2,8 @@ package com.zuehlke.pgadmissions.domain;
 
 public interface TargetEntity {
 
-    public abstract Integer getId();
+    Integer getId();
 
-    public abstract void setId(Integer id);
+    void setId(Integer id);
 
 }

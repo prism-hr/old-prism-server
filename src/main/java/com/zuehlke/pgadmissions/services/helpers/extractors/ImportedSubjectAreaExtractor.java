90273cb1cc7196f23d6f76112b5f1fccc4bb16a1
jfi <jfi@zuhlke.com>
2015-07-23 15:32:31 +0100

    Fixed compilation errors

--- src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedSubjectAreaExtractor.java
+++ src/main/java/com/zuehlke/pgadmissions/services/helpers/extractors/ImportedSubjectAreaExtractor.java
@@ -1,18 +1,13 @@
 package com.zuehlke.pgadmissions.services.helpers.extractors;
 
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareBooleanForSqlInsert;
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareCellsForSqlInsert;
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareIntegerForSqlInsert;
-import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.prepareStringForSqlInsert;
+import com.google.common.collect.Lists;
+import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
+import org.springframework.stereotype.Component;
+import uk.co.alumeni.prism.api.model.imported.request.ImportedSubjectAreaRequest;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
-import uk.co.alumeni.prism.api.model.imported.request.ImportedSubjectAreaRequest;
-
-import com.google.common.collect.Lists;
-import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
+import static com.zuehlke.pgadmissions.utils.PrismQueryUtils.*;
 
 @Component
 public class ImportedSubjectAreaExtractor implements ImportedEntityExtractor<ImportedSubjectAreaRequest> {
@@ -24,7 +19,6 @@ public class ImportedSubjectAreaExtractor implements ImportedEntityExtractor<Imp
             List<String> cells = Lists.newLinkedList();
             cells.add(prepareStringForSqlInsert(definition.getName()));
             cells.add(prepareStringForSqlInsert(definition.getJacsCode()));
-            cells.add(prepareIntegerForSqlInsert(definition.getParentSubjectArea()));
             cells.add(prepareBooleanForSqlInsert(enable));
             rows.add(prepareCellsForSqlInsert(cells));
         }

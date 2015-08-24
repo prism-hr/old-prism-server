8aee989baefc7c5a5849b575221adebfda5a6a69
jfi <jfi@zuhlke.com>
2015-08-24 20:37:26 +0100

    Minor changescript fix

--- src/sql/change_scripts/0203_rename_required_sections.sql
+++ src/sql/change_scripts/0203_rename_required_sections.sql
@@ -13,7 +13,8 @@ SET
   advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_ADVERT_HEADER", "ADVERT_DETAILS"),
   advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_ADVERT_CATEGORIES_HEADER", "ADVERT_CATEGORIES"),
   advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_TARGETS_HEADER", "ADVERT_TARGETS"),
-  advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_COMPETENCES_HEADER", "ADVERT_COMPETENCES")
+  advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_COMPETENCES_HEADER", "ADVERT_COMPETENCES"),
+  advert_incomplete_section = replace(advert_incomplete_section, "SYSTEM_RESOURCE_DEPARTMENT_PROGRAMS_HEADER", "DEPARTMENT_PROGRAMS")
 ;
 
 UPDATE program

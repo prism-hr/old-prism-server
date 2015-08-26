26da596f605125baae91dc9d63c8a087e09cd6bf
jfi <jfi@zuhlke.com>
2015-08-26 15:58:31 +0100

    Fixed issues with saving competences

--- src/main/java/com/zuehlke/pgadmissions/workflow/evaluators/ResourceAdvertCategoriesEvaluator.java
+++ src/main/java/com/zuehlke/pgadmissions/workflow/evaluators/ResourceAdvertCategoriesEvaluator.java
@@ -1,9 +1,8 @@
 package com.zuehlke.pgadmissions.workflow.evaluators;
 
-import org.springframework.stereotype.Component;
-
 import com.zuehlke.pgadmissions.domain.advert.AdvertCategories;
 import com.zuehlke.pgadmissions.domain.resource.ResourceParent;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ResourceAdvertCategoriesEvaluator implements ResourceCompletenessEvaluator<ResourceParent> {
@@ -11,6 +10,9 @@ public class ResourceAdvertCategoriesEvaluator implements ResourceCompletenessEv
     @Override
     public boolean evaluate(ResourceParent resource) {
         AdvertCategories categories = resource.getAdvert().getCategories();
+        if (categories == null) {
+            return false;
+        }
         return !categories.getFunctions().isEmpty() || !categories.getIndustries().isEmpty() || !categories.getThemes().isEmpty();
     }
 

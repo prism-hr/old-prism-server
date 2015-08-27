93f61aa86df8606f5149e59a68bf37e22c904326
alastair <alastair@localhost>
2015-08-27 15:24:01 +0100

    Fixing merge problems again.

--- src/main/java/com/zuehlke/pgadmissions/rest/dto/resource/ResourceOpportunityDTO.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/dto/resource/ResourceOpportunityDTO.java
@@ -1,14 +1,16 @@
 package com.zuehlke.pgadmissions.rest.dto.resource;
 
-import com.zuehlke.pgadmissions.domain.definitions.PrismOpportunityType;
-import com.zuehlke.pgadmissions.rest.dto.advert.AdvertDTO;
-import com.zuehlke.pgadmissions.rest.dto.imported.ImportedEntityDTO;
-import uk.co.alumeni.prism.api.model.resource.ResourceOpportunityDefinition;
+import java.util.List;
+import java.util.Set;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.Set;
+
+import com.zuehlke.pgadmissions.domain.definitions.PrismOpportunityType;
+import com.zuehlke.pgadmissions.rest.dto.advert.AdvertDTO;
+import com.zuehlke.pgadmissions.rest.dto.imported.ImportedEntityDTO;
+
+import uk.co.alumeni.prism.api.model.resource.ResourceOpportunityDefinition;
 
 public class ResourceOpportunityDTO extends ResourceParentDivisionDTO implements
         ResourceOpportunityDefinition<AdvertDTO, PrismOpportunityType, ResourceStudyOptionDTO> {

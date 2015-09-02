30b3f1ba760e544f779bfe5cc152bdc912d42e9a
alastair <alastair@localhost>
2015-09-02 17:03:13 +0100

    Further small fixes for decline endorsement.

--- src/main/java/com/zuehlke/pgadmissions/dao/ActionDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/ActionDAO.java
@@ -184,7 +184,8 @@ public class ActionDAO {
                         .add(Projections.groupProperty(resourceReference + ".id"), "resourceId") //
                         .add(Projections.groupProperty("action.id"), "actionId") //
                         .add(Projections.max("stateAction.raisesUrgentFlag"), "raisesUrgentFlag") //
-                        .add(Projections.max("primaryState"), "primaryState")) //
+                        .add(Projections.max("primaryState"), "primaryState") //
+                        .add(Projections.property("action.declinableAction"), "declinable")) //
                 .createAlias(resourceReference, resourceReference, JoinType.INNER_JOIN) //
                 .createAlias(resourceReference + ".resourceConditions", "resourceCondition", JoinType.LEFT_OUTER_JOIN) //
                 .createAlias("state", "state", JoinType.INNER_JOIN) //

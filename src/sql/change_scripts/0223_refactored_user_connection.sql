11f4845989a0a5f3d6e4a291330d322b1e3da192
alastair <alastair@localhost>
2015-09-06 14:11:20 +0100

    change script fix.

--- src/sql/change_scripts/0223_refactored_user_connection.sql
+++ src/sql/change_scripts/0223_refactored_user_connection.sql
@@ -7,15 +7,6 @@ alter table advert_target_advert
 	add foreign key (target_advert_user_id) references user (id)
 ;
 
-update advert inner join advert_target_advert
-	on advert.id = advert_target_advert.advert_id
-set advert_target_advert.advert_user_id = advert.user_id
-;
-
-alter table advert_target_advert
-	modify column advert_user_id int(10) unsigned not null
-;
-
 alter table project
 	add column staff_email_list text after name,
 	add column staff_email_list_group int(1) unsigned after staff_email_list,

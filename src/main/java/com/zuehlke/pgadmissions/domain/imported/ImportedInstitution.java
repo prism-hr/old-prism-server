34d2d2e4053eb3745af02079c335dabc75dac9e1
jfi <jfi@zuhlke.com>
2015-09-06 16:56:00 +0100

    Fixed bugs when saving qualifications

--- src/main/java/com/zuehlke/pgadmissions/domain/imported/ImportedInstitution.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/imported/ImportedInstitution.java
@@ -1,31 +1,20 @@
 package com.zuehlke.pgadmissions.domain.imported;
 
-import static com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity.IMPORTED_INSTITUTION;
-
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import com.google.common.base.Objects;
 import com.google.common.collect.Sets;
 import com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity;
 import com.zuehlke.pgadmissions.domain.imported.mapping.ImportedInstitutionMapping;
 import com.zuehlke.pgadmissions.domain.resource.Institution;
-
 import uk.co.alumeni.prism.api.model.imported.ImportedEntityResponseDefinition;
 import uk.co.alumeni.prism.api.model.imported.ImportedInstitutionDefinition;
 
+import javax.persistence.*;
+import java.util.Set;
+
+import static com.zuehlke.pgadmissions.domain.definitions.PrismImportedEntity.IMPORTED_INSTITUTION;
+
 @Entity
-@Table(name = "imported_institution", uniqueConstraints = { @UniqueConstraint(columnNames = { "imported_domicile_id", "name" }) })
+@Table(name = "imported_institution", uniqueConstraints = {@UniqueConstraint(columnNames = {"imported_domicile_id", "name"})})
 public class ImportedInstitution extends ImportedEntity<Integer, ImportedInstitutionMapping>
         implements ImportedInstitutionDefinition<ImportedEntitySimple>, ImportedEntityResponseDefinition<Integer>, ImportedEntityIndexable {
 
@@ -178,8 +167,13 @@ public class ImportedInstitution extends ImportedEntity<Integer, ImportedInstitu
         return this;
     }
 
-    public ImportedInstitution withUcasIds(final String ucasIds) {
+    public ImportedInstitution withUcasIds(String ucasIds) {
         this.ucasIds = ucasIds;
+        return this;
+    }
+
+    public ImportedInstitution withIndexed(Boolean indexed) {
+        this.indexed = indexed;
         return this;
     }
 

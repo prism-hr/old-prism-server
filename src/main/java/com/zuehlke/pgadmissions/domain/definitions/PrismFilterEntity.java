3e67d5d34a5b0381611d96c30f2242bd9ac056a6
alastair <alastair@localhost>
2015-09-21 11:34:20 +0100

    Merge

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismFilterEntity.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismFilterEntity.java
@@ -1,33 +1,30 @@
 package com.zuehlke.pgadmissions.domain.definitions;
 
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.DEPARTMENT;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.INSTITUTION;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.PROGRAM;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.PROJECT;
-
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope;
 import com.zuehlke.pgadmissions.workflow.selectors.summary.ApplicationByEmployingResourceScope;
 import com.zuehlke.pgadmissions.workflow.selectors.summary.ApplicationByImportedRejectionReasonSelector;
 import com.zuehlke.pgadmissions.workflow.selectors.summary.ApplicationByQualifyingResourceScopeSelector;
 import com.zuehlke.pgadmissions.workflow.selectors.summary.PrismResourceSummarySelector;
 
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismScope.*;
+
 public enum PrismFilterEntity {
 
-    FILTER_DOMICILE("application_personal_detail.imported_domicile_id"), //
-    FILTER_NATIONALITY("application_personal_detail.imported_nationality_id"), //
-    FILTER_AGE_RANGE("application_personal_detail.imported_age_range_id"), //
-    FILTER_DISABILITY("application_personal_detail.imported_disability_id"), //
-    FILTER_ETHNICITY("application_personal_detail.imported_ethnicity_id"), //
-    FILTER_GENDER("application_personal_detail.imported_gender_id"), //
-    FILTER_OPPORTUNITY_TYPE("application_program_detail.imported_opportunity_type_id"), //
-    FILTER_REJECTION_REASON("application.id", ApplicationByImportedRejectionReasonSelector.class), //
-    FILTER_STUDY_OPTION("application_program_detail.imported_study_option_id"), //
-    FILTER_UNIVERSITY_INSTITUTION("application.id", INSTITUTION, ApplicationByQualifyingResourceScopeSelector.class), //
-    FILTER_UNIVERSITY_DEPARTMENT("application.id", DEPARTMENT, ApplicationByQualifyingResourceScopeSelector.class), //
-    FILTER_UNIVERSITY_PROGRAM("application.id", PROGRAM, ApplicationByQualifyingResourceScopeSelector.class), //
-    FILTER_UNIVERSITY_PROJECT("application.id", PROJECT, ApplicationByQualifyingResourceScopeSelector.class), //
-    FILTER_EMPLOYER_INSTITUTION("application.id", INSTITUTION, ApplicationByEmployingResourceScope.class), //
-    FILTER_EMPLOYER_DEPARTMENT("application.id", DEPARTMENT, ApplicationByEmployingResourceScope.class);
+    DOMICILE("application_personal_detail.imported_domicile_id"), //
+    NATIONALITY("application_personal_detail.imported_nationality_id"), //
+    AGE_RANGE("application_personal_detail.imported_age_range_id"), //
+    DISABILITY("application_personal_detail.imported_disability_id"), //
+    ETHNICITY("application_personal_detail.imported_ethnicity_id"), //
+    GENDER("application_personal_detail.imported_gender_id"), //
+    OPPORTUNITY_TYPE("application_program_detail.imported_opportunity_type_id"), //
+    REJECTION_REASON("application.id", ApplicationByImportedRejectionReasonSelector.class), //
+    STUDY_OPTION("application_program_detail.imported_study_option_id"), //
+    UNIVERSITY_INSTITUTION("application.id", INSTITUTION, ApplicationByQualifyingResourceScopeSelector.class), //
+    UNIVERSITY_DEPARTMENT("application.id", DEPARTMENT, ApplicationByQualifyingResourceScopeSelector.class), //
+    UNIVERSITY_PROGRAM("application.id", PROGRAM, ApplicationByQualifyingResourceScopeSelector.class), //
+    UNIVERSITY_PROJECT("application.id", PROJECT, ApplicationByQualifyingResourceScopeSelector.class), //
+    EMPLOYER_INSTITUTION("application.id", INSTITUTION, ApplicationByEmployingResourceScope.class), //
+    EMPLOYER_DEPARTMENT("application.id", DEPARTMENT, ApplicationByEmployingResourceScope.class);
 
     private String filterColumn;
 

183ebaf86a915ed1f5d95ab999a833b50cfa4a75
jfi <jfi@zuhlke.com>
2015-09-25 16:52:37 +0100

    Minor fix to image representation

--- src/main/java/com/zuehlke/pgadmissions/mapping/DocumentMapper.java
+++ src/main/java/com/zuehlke/pgadmissions/mapping/DocumentMapper.java
@@ -1,18 +1,17 @@
 package com.zuehlke.pgadmissions.mapping;
 
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Service;
-
 import com.zuehlke.pgadmissions.domain.document.Document;
 import com.zuehlke.pgadmissions.rest.representation.DocumentRepresentation;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @Transactional
 public class DocumentMapper {
 
     public DocumentRepresentation getDocumentRepresentation(Integer document) {
-        return new DocumentRepresentation().withId(document);
+        return document != null ? new DocumentRepresentation().withId(document) : null;
     }
 
     public DocumentRepresentation getDocumentRepresentation(Document document) {

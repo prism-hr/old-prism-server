014837684eeb0b47dcd6ca72c701a44571d9c8cd
alastair <alastair@localhost>
2015-09-28 15:04:44 +0100

    Fix null pointer problem in institution import.

--- src/sql/change_scripts/0001_further_connections.sql
+++ src/sql/change_scripts/0001_further_connections.sql
@@ -2,9 +2,3 @@ alter table user_personal_detail
 	change column imported_nationality_id nationality_id varchar(10),
 	change column imported_domicile_id domicile_id varchar(10)
 ;
-
-alter table user_personal_detail
-	change column imported_nationality_id nationality_id varchar(10),
-	change column imported_domicile_id domicile_id varchar(10)
-;
-

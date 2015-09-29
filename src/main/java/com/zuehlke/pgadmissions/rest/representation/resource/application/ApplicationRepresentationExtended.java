aa9f38a61ab9fb913122a40ea288d391472ef96e
alastair <alastair@localhost>
2015-09-29 17:30:25 +0100

    Fixed referees not responded problem.

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/application/ApplicationRepresentationExtended.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/application/ApplicationRepresentationExtended.java
@@ -2,12 +2,24 @@ package com.zuehlke.pgadmissions.rest.representation.resource.application;
 
 import java.util.List;
 
+import com.zuehlke.pgadmissions.rest.representation.user.UserRepresentationSimple;
+
 public class ApplicationRepresentationExtended extends ApplicationRepresentationSimple {
 
+    private List<UserRepresentationSimple> refereesNotResponded;
+    
     private List<ApplicationAssignedHiringManagerRepresentation> assignedSupervisors;
     
     private ApplicationOfferRepresentation offerRecommendation;
     
+    public List<UserRepresentationSimple> getRefereesNotResponded() {
+        return refereesNotResponded;
+    }
+
+    public void setRefereesNotResponded(List<UserRepresentationSimple> refereesNotResponded) {
+        this.refereesNotResponded = refereesNotResponded;
+    }
+
     public List<ApplicationAssignedHiringManagerRepresentation> getAssignedSupervisors() {
         return assignedSupervisors;
     }

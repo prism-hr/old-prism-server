1262e2be2932af7a80e2a231e32c47638d1fafd5
alastair <alastair@localhost>
2015-10-01 15:52:34 +0100

    Fix creating resource with connection.

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/ResourceParentRepresentation.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/ResourceParentRepresentation.java
@@ -1,8 +1,5 @@
 package com.zuehlke.pgadmissions.rest.representation.resource;
 
-import java.util.List;
-
-import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismActionCondition;
 import com.zuehlke.pgadmissions.rest.representation.advert.AdvertRepresentationSimple;
 
 public class ResourceParentRepresentation extends ResourceRepresentationExtended {
@@ -10,8 +7,6 @@ public class ResourceParentRepresentation extends ResourceRepresentationExtended
     private String importedCode;
 
     private AdvertRepresentationSimple advert;
-
-    private List<PrismActionCondition> externalConditions;
 
     @Override
     public String getImportedCode() {
@@ -29,14 +24,6 @@ public class ResourceParentRepresentation extends ResourceRepresentationExtended
 
     public void setAdvert(AdvertRepresentationSimple advert) {
         this.advert = advert;
-    }
-
-    public List<PrismActionCondition> getExternalConditions() {
-        return externalConditions;
-    }
-
-    public void setExternalConditions(List<PrismActionCondition> externalConditions) {
-        this.externalConditions = externalConditions;
     }
 
 }

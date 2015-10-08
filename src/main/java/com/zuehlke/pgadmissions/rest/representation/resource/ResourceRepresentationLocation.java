d1a4af6d1a372ff44f348645f55dd3b42d9f5e62
jfi <jfi@zuhlke.com>
2015-10-08 15:59:32 +0100

    Added user to resource representation location

--- src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/ResourceRepresentationLocation.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/resource/ResourceRepresentationLocation.java
@@ -1,10 +1,13 @@
 package com.zuehlke.pgadmissions.rest.representation.resource;
 
 import com.zuehlke.pgadmissions.rest.representation.address.AddressRepresentation;
+import com.zuehlke.pgadmissions.rest.representation.user.UserRepresentation;
 
 public class ResourceRepresentationLocation extends ResourceRepresentationCreation {
 
     private AddressRepresentation address;
+
+    private UserRepresentation user;
 
     public AddressRepresentation getAddress() {
         return address;
@@ -14,4 +17,11 @@ public class ResourceRepresentationLocation extends ResourceRepresentationCreati
         this.address = address;
     }
 
+    public UserRepresentation getUser() {
+        return user;
+    }
+
+    public void setUser(UserRepresentation user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/zuehlke/pgadmissions/rest/representation/user/UserRepresentation.java
+++ src/main/java/com/zuehlke/pgadmissions/rest/representation/user/UserRepresentation.java
@@ -1,16 +1,26 @@
 package com.zuehlke.pgadmissions.rest.representation.user;
 
-import static org.apache.commons.lang3.ObjectUtils.compare;
-
 import com.google.common.base.Objects;
 
+import static org.apache.commons.lang3.ObjectUtils.compare;
+
 public class UserRepresentation implements Comparable<UserRepresentation> {
+
+    private Integer id;
 
     private String firstName;
 
     private String lastName;
 
     private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -34,6 +44,11 @@ public class UserRepresentation implements Comparable<UserRepresentation> {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public UserRepresentation withId(final Integer id) {
+        this.id = id;
+        return this;
     }
 
     public UserRepresentation withFirstName(String firstName) {

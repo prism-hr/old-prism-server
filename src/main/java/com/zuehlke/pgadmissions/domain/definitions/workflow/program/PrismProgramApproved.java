b94b3e3f41bd629be7961bf8dcd63f2b98606cce
alastair <alastair@localhost>
2015-10-10 23:39:19 +0100

    Put back the endorsement actions.

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/program/PrismProgramApproved.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/program/PrismProgramApproved.java
@@ -1,10 +1,14 @@
 package com.zuehlke.pgadmissions.domain.definitions.workflow.program;
 
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismAction.PROGRAM_ENDORSE;
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismRoleGroup.PROGRAM_ENDORSER_GROUP;
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateTransitionGroup.PROGRAM_ENDORSE_TRANSITION;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programCreateProject;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programEmailCreatorApproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programTerminateApproved;
 import static com.zuehlke.pgadmissions.domain.definitions.workflow.program.PrismProgramWorkflow.programViewEditApproved;
 
+import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismStateAction;
 import com.zuehlke.pgadmissions.domain.definitions.workflow.PrismWorkflowState;
 
 public class PrismProgramApproved extends PrismWorkflowState {
@@ -13,6 +17,23 @@ public class PrismProgramApproved extends PrismWorkflowState {
     protected void setStateActions() {
         stateActions.add(programCreateProject());
         stateActions.add(programEmailCreatorApproved());
+
+        stateActions.add(new PrismStateAction() //
+                .withAction(PROGRAM_ENDORSE) //
+                .withRaisesUrgentFlag() //
+                .withPartnerAssignments(PROGRAM_ENDORSER_GROUP) //
+                .withTransitions(PROGRAM_ENDORSE_TRANSITION));
+
+        stateActions.add(new PrismStateAction() //
+                .withAction(PROGRAM_ENDORSE) //
+                .withPartnerAssignments(PROGRAM_ENDORSER_GROUP) //
+                .withTransitions(PROGRAM_ENDORSE_TRANSITION));
+
+        stateActions.add(new PrismStateAction() //
+                .withAction(PROGRAM_ENDORSE) //
+                .withPartnerAssignments(PROGRAM_ENDORSER_GROUP) //
+                .withTransitions(PROGRAM_ENDORSE_TRANSITION));
+
         stateActions.add(programTerminateApproved()); //
         stateActions.add(programViewEditApproved()); //
     }

fb41054cbf8fc7da7d18d0d17a8ff3e3788f071a
alastair <alastair@localhost>
2015-10-12 12:49:50 +0100

    further

--- src/main/java/com/zuehlke/pgadmissions/dao/SystemDAO.java
+++ src/main/java/com/zuehlke/pgadmissions/dao/SystemDAO.java
@@ -1,8 +1,11 @@
 package com.zuehlke.pgadmissions.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.type.StringType;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,15 +14,28 @@ public class SystemDAO {
     @Inject
     private SessionFactory sessionFactory;
 
+    @SuppressWarnings("unchecked")
     public void clearSchema() {
-        sessionFactory.getCurrentSession().createSQLQuery( //
-                "call procedure_clear_schema()") //
-                .executeUpdate();
-    }
+        List<String> tables = sessionFactory.getCurrentSession().createSQLQuery( //
+                "select information_schema.tables.table_name as table_name "
+                        + "from information_schema.tables "
+                        + "where information_schema.tables.table_schema = 'prism' "
+                        + "and information_schema.tables.table_name != 'changelog'") //
+                .addScalar("table_name", StringType.INSTANCE) //
+                .list();
 
-    public void resetAmazon() {
         sessionFactory.getCurrentSession().createSQLQuery( //
-                "call procedure_reset_amazon()") //
+                "set session foreign_key_checks = 0") //
+                .executeUpdate();
+
+        tables.forEach(table -> {
+            sessionFactory.getCurrentSession().createSQLQuery( //
+                    "truncate table " + table)
+                    .executeUpdate();
+        });
+
+        sessionFactory.getCurrentSession().createSQLQuery( //
+                "set session foreign_key_checks = 1") //
                 .executeUpdate();
     }
 

ebaeb7f146b1a4aa99fdab49a7579cbc5bc9fac3
jfi <jfi@zuhlke.com>
2015-10-12 23:01:08 +0100

    Finalized request user functionality

--- src/sql/change_scripts/0010_fix_endorsement.sql
+++ src/sql/change_scripts/0010_fix_endorsement.sql
@@ -1,34 +1,52 @@
-delete
-from state_transition
-where state_action_id in (
-	select id
-	from state_action
-	where action_id in (
-		select id
-		from action
-		where id like "%_ENDORSE"))
+DELETE FROM comment_state
+WHERE comment_id IN (
+  SELECT id FROM comment
+  WHERE action_id LIKE "%_ENDORSE"
+)
 ;
 
-delete
-from state_action_assignment
-where state_action_id in (
-	select id
-	from state_action
-	where action_id in (
-		select id
-		from action
-		where id like "%_ENDORSE"))
+DELETE FROM comment_transition_state
+WHERE comment_id IN (
+  SELECT id FROM comment
+  WHERE action_id LIKE "%_ENDORSE"
+)
 ;
 
-delete
-from state_action
-where action_id in (
-	select id
-	from action
-	where id like "%_ENDORSE")
+DELETE FROM comment
+WHERE action_id LIKE "%_ENDORSE"
 ;
 
-delete
-from action
-where id like "%_ENDORSE"
+DELETE
+FROM state_transition
+WHERE state_action_id IN (
+  SELECT id
+  FROM state_action
+  WHERE action_id IN (
+    SELECT id
+    FROM action
+    WHERE id LIKE "%_ENDORSE"))
+;
+
+DELETE
+FROM state_action_assignment
+WHERE state_action_id IN (
+  SELECT id
+  FROM state_action
+  WHERE action_id IN (
+    SELECT id
+    FROM action
+    WHERE id LIKE "%_ENDORSE"))
+;
+
+DELETE
+FROM state_action
+WHERE action_id IN (
+  SELECT id
+  FROM action
+  WHERE id LIKE "%_ENDORSE")
+;
+
+DELETE
+FROM action
+WHERE id LIKE "%_ENDORSE"
 ;

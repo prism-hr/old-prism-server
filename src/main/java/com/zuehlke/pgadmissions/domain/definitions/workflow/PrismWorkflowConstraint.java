12fdfc25134513b151d05653ba5671ba6c8d9e9e
jfi <jfi@zuhlke.com>
2015-11-02 19:51:29 +0100

    Handling extra section requirements for user profile

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/PrismWorkflowConstraint.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/workflow/PrismWorkflowConstraint.java
@@ -2,35 +2,37 @@ package com.zuehlke.pgadmissions.domain.definitions.workflow;
 
 public enum PrismWorkflowConstraint {
 
-    APPLICATION_QUALIFICATION(1, 10), //
-    APPLICATION_EMPLOYMENT_POSITION(0, 10), //
-    APPLICATION_REFEREE_ASSIGNMENT(2, 3), //
-    APPLICATION_DOCUMENT_CV(1, 1), //
-    APPLICATION_DOCUMENT_COVERING_LETTER(0, 1), //
-    APPLICATION_REVIEWER_ASSIGNMENT(1, 999), //
-    APPLICATION_INTERVIEWER_ASSIGNMENT(1, 999), //
-    APPLICATION_HIRING_MANAGER_ASSIGNMENT(1, 999);
+    APPLICATION_QUALIFICATION(1, 10),
+    APPLICATION_EMPLOYMENT_POSITION(0, 10),
+    APPLICATION_REFEREE_ASSIGNMENT(2, 3),
+    APPLICATION_DOCUMENT_CV(1, 1),
+    APPLICATION_DOCUMENT_COVERING_LETTER(0, 1),
+    APPLICATION_REVIEWER_ASSIGNMENT(1, 999),
+    APPLICATION_INTERVIEWER_ASSIGNMENT(1, 999),
+    APPLICATION_HIRING_MANAGER_ASSIGNMENT(1, 999),
+    USER_ACCOUNT_QUALIFICATION(0, 30),
+    USER_ACCOUNT_EMPLOYMENT_POSITION(0, 30),
+    USER_ACCOUNT_REFEREE_ASSIGNMENT(0, 30),
+    USER_ACCOUNT_DOCUMENT_CV(0, 1),
+    USER_ACCOUNT_DOCUMENT_COVERING_LETTER(1, 1);
 
     private Integer minimumPermitted;
 
     private Integer maximumPermitted;
 
-    private PrismWorkflowConstraint(Integer minimumPermitted, Integer maximumPermitted) {
+    PrismWorkflowConstraint(Integer minimumPermitted, Integer maximumPermitted) {
         this.minimumPermitted = minimumPermitted;
         this.maximumPermitted = maximumPermitted;
     }
 
-    public final Integer getMinimumPermitted() {
+    public Integer getMinimumPermitted() {
         return minimumPermitted;
     }
 
-    public final Integer getMaximumPermitted() {
+    public Integer getMaximumPermitted() {
         return maximumPermitted;
     }
 
-    public boolean isInRange(Integer value) {
-        return value >= this.getMinimumPermitted() && value >= this.getMaximumPermitted();
-    }
 
     public boolean isRequired() {
         return this.getMinimumPermitted() > 0;

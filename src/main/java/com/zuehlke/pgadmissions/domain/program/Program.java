2667cc7ca99db617ac81027c17b570740a20d936
jfi <jfi@zuhlke.com>
2015-11-13 13:54:56 +0000

    Fixed issue with incorrect Program signature

--- src/main/java/com/zuehlke/pgadmissions/domain/program/Program.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/program/Program.java
@@ -1,31 +1,5 @@
 package com.zuehlke.pgadmissions.domain.program;
 
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.PROGRAM_DISABLED_COMPLETED;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.PROGRAM_REJECTED;
-import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.PROGRAM_WITHDRAWN;
-
-import java.math.BigDecimal;
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-import org.apache.commons.lang.BooleanUtils;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.OrderBy;
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-
 import com.google.common.collect.Sets;
 import com.zuehlke.pgadmissions.domain.advert.Advert;
 import com.zuehlke.pgadmissions.domain.application.Application;
@@ -35,17 +9,24 @@ import com.zuehlke.pgadmissions.domain.document.Document;
 import com.zuehlke.pgadmissions.domain.imported.OpportunityType;
 import com.zuehlke.pgadmissions.domain.institution.Institution;
 import com.zuehlke.pgadmissions.domain.project.Project;
-import com.zuehlke.pgadmissions.domain.resource.Resource;
-import com.zuehlke.pgadmissions.domain.resource.ResourceCondition;
-import com.zuehlke.pgadmissions.domain.resource.ResourceOpportunity;
-import com.zuehlke.pgadmissions.domain.resource.ResourcePreviousState;
-import com.zuehlke.pgadmissions.domain.resource.ResourceState;
-import com.zuehlke.pgadmissions.domain.resource.ResourceStudyLocation;
-import com.zuehlke.pgadmissions.domain.resource.ResourceStudyOption;
+import com.zuehlke.pgadmissions.domain.resource.*;
 import com.zuehlke.pgadmissions.domain.system.System;
 import com.zuehlke.pgadmissions.domain.user.User;
 import com.zuehlke.pgadmissions.domain.user.UserRole;
 import com.zuehlke.pgadmissions.domain.workflow.State;
+import org.apache.commons.lang.BooleanUtils;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.OrderBy;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+import static com.zuehlke.pgadmissions.domain.definitions.workflow.PrismState.*;
 
 @Entity
 @Table(name = "PROGRAM")
@@ -75,7 +56,7 @@ public class Program extends ResourceOpportunity {
     @Fetch(FetchMode.SELECT)
     @JoinColumn(name = "institution_id", nullable = false)
     private Institution institution;
-    
+
     @ManyToOne
     @Fetch(FetchMode.SELECT)
     @JoinColumn(name = "institution_partner_id")
@@ -100,7 +81,7 @@ public class Program extends ResourceOpportunity {
 
     @Column(name = "title", nullable = false)
     private String title;
-    
+
     @OneToOne
     @JoinColumn(name = "background_image_id")
     private Document backgroundImage;
@@ -232,7 +213,7 @@ public class Program extends ResourceOpportunity {
     public void setInstitution(Institution institution) {
         this.institution = institution;
     }
-    
+
     @Override
     public Institution getPartner() {
         return partner;
@@ -676,9 +657,11 @@ public class Program extends ResourceOpportunity {
 
     @Override
     public ResourceSignature getResourceSignature() {
-        ResourceSignature signature = super.getResourceSignature();
+        ResourceSignature signature;
         if (BooleanUtils.isTrue(imported)) {
-            signature.addProperty("importedCode", importedCode);
+            signature = new ResourceSignature().addProperty("importedCode", importedCode);
+        } else {
+            signature = super.getResourceSignature();
         }
         signature.addExclusion("state.id", PROGRAM_REJECTED);
         signature.addExclusion("state.id", PROGRAM_WITHDRAWN);

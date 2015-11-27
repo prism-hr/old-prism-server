afba05b85872f52d23fb6e3a8b2ea7bac8445fde
alastair <alastair@localhost>
2015-11-27 13:48:39 +0000

    Drop strange constructor injection in test package.

--- src/main/java/uk/co/alumeni/prism/rest/controller/DocumentController.java
+++ src/main/java/uk/co/alumeni/prism/rest/controller/DocumentController.java
@@ -1,13 +1,30 @@
 package uk.co.alumeni.prism.rest.controller;
 
+import static uk.co.alumeni.prism.domain.document.PrismFileCategory.DOCUMENT;
+import static uk.co.alumeni.prism.domain.document.PrismFileCategory.IMAGE;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+
 import uk.co.alumeni.prism.domain.application.Application;
 import uk.co.alumeni.prism.domain.document.Document;
 import uk.co.alumeni.prism.domain.document.PrismFileCategory.PrismImageCategory;
@@ -16,16 +33,6 @@ import uk.co.alumeni.prism.exceptions.ResourceNotFoundException;
 import uk.co.alumeni.prism.services.ApplicationDownloadService;
 import uk.co.alumeni.prism.services.DocumentService;
 import uk.co.alumeni.prism.services.EntityService;
-
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-import static uk.co.alumeni.prism.domain.document.PrismFileCategory.DOCUMENT;
-import static uk.co.alumeni.prism.domain.document.PrismFileCategory.IMAGE;
 
 @RestController
 @RequestMapping("/api")

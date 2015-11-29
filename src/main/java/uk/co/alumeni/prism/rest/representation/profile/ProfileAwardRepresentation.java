87f3991749660c050912f22be59c8213741c1c7e
alastair <alastair@localhost>
2015-11-29 16:21:00 +0000

    Profile award representation.

---
+++ src/main/java/uk/co/alumeni/prism/rest/representation/profile/ProfileAwardRepresentation.java
@@ -0,0 +1,49 @@
+package uk.co.alumeni.prism.rest.representation.profile;
+
+import org.joda.time.LocalDate;
+
+import uk.co.alumeni.prism.rest.representation.resource.application.ApplicationSectionRepresentation;
+
+public class ProfileAwardRepresentation extends ApplicationSectionRepresentation {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate awardDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getAwardDate() {
+        return awardDate;
+    }
+
+    public void setAwardDate(LocalDate awardDate) {
+        this.awardDate = awardDate;
+    }
+
+}

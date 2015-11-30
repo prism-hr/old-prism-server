e7aa622e7979abf2077ae30a14f9ef08005f0fc7
alastair <alastair@localhost>
2015-11-30 19:09:04 +0000

    Added emails and fixed issues with mappers.

--- src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/application/PrismApplicationValidationPendingCompletion.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/application/PrismApplicationValidationPendingCompletion.java
@@ -1,5 +1,7 @@
 package uk.co.alumeni.prism.domain.definitions.workflow.application;
 
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismNotificationDefinition.APPLICATION_COMPLETE_VALIDATION_STAGE_REQUEST;
+
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismWorkflowState;
 
 public class PrismApplicationValidationPendingCompletion extends PrismWorkflowState {
@@ -9,7 +11,8 @@ public class PrismApplicationValidationPendingCompletion extends PrismWorkflowSt
         stateActions.add(PrismApplicationWorkflow.applicationComment()); //
 
         stateActions.add(PrismApplicationValidation.applicationCompleteValidation(state) //
-                .withRaisesUrgentFlag()); //
+                .withRaisesUrgentFlag() //
+                .withNotification(APPLICATION_COMPLETE_VALIDATION_STAGE_REQUEST)); //
 
         stateActions.add(PrismApplicationWorkflow.applicationEmailCreator()); //
         stateActions.add(PrismApplicationWorkflow.applicationEscalate()); //

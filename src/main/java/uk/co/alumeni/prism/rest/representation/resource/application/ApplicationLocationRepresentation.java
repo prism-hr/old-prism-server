a33852f4da131cd50498f23e7b140b6caffdb420
alastair <alastair@localhost>
2015-11-30 14:54:48 +0000

    Fixed few small issues with further fields for lahp, and added new data to pdf download.

--- src/main/java/uk/co/alumeni/prism/rest/representation/resource/application/ApplicationLocationRepresentation.java
+++ src/main/java/uk/co/alumeni/prism/rest/representation/resource/application/ApplicationLocationRepresentation.java
@@ -1,7 +1,13 @@
 package uk.co.alumeni.prism.rest.representation.resource.application;
 
-import org.joda.time.DateTime;
+import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import com.google.common.base.Joiner;
+
+import jersey.repackaged.com.google.common.collect.Lists;
 import uk.co.alumeni.prism.rest.representation.resource.ResourceRepresentationRelation;
 
 public class ApplicationLocationRepresentation extends ApplicationSectionRepresentation {
@@ -11,6 +17,8 @@ public class ApplicationLocationRepresentation extends ApplicationSectionReprese
     private ResourceRepresentationRelation resource;
 
     private String description;
+
+    private LocalDate descriptionDate;
 
     private Boolean preference;
 
@@ -61,6 +69,11 @@ public class ApplicationLocationRepresentation extends ApplicationSectionReprese
         return this;
     }
 
+    public ApplicationLocationRepresentation withDescriptionDate(LocalDate descriptionDate) {
+        this.descriptionDate = descriptionDate;
+        return this;
+    }
+
     public ApplicationLocationRepresentation withPreference(Boolean preference) {
         this.preference = preference;
         return this;
@@ -69,6 +82,24 @@ public class ApplicationLocationRepresentation extends ApplicationSectionReprese
     public ApplicationLocationRepresentation withLastUpdateTimestamp(DateTime lastUpdatedTimestamp) {
         setLastUpdatedTimestamp(lastUpdatedTimestamp);
         return this;
+    }
+
+    public String getDisplayName() {
+        String displayName = resource.getDisplayName();
+        if (!(description == null || descriptionDate == null)) {
+            List<String> suffixParts = Lists.newLinkedList();
+            if (description != null) {
+                suffixParts.add(description);
+            }
+
+            if (descriptionDate != null) {
+                suffixParts.add("" + descriptionDate.getMonthOfYear() + '/' + descriptionDate.getYear());
+            }
+
+            displayName = displayName + " - " + Joiner.on(" - ").join(suffixParts);
+        }
+
+        return displayName;
     }
 
 }

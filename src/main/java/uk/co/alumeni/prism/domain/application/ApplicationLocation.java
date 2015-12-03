655284e42cdf48ef9e5d10811bd118ce2a2ffa80
jfi <jfi@zuhlke.com>
2015-12-03 18:28:57 +0000

    Merge with default

--- src/main/java/uk/co/alumeni/prism/domain/application/ApplicationLocation.java
+++ src/main/java/uk/co/alumeni/prism/domain/application/ApplicationLocation.java
@@ -1,19 +1,11 @@
 package uk.co.alumeni.prism.domain.application;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
-
 import uk.co.alumeni.prism.domain.UniqueEntity;
 import uk.co.alumeni.prism.domain.advert.Advert;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "application_location", uniqueConstraints = { @UniqueConstraint(columnNames = { "application_id", "location_advert_id" }) })
@@ -34,10 +26,10 @@ public class ApplicationLocation extends ApplicationTagSection<Advert>implements
     @Column(name = "description")
     private String description;
 
-    @Column(name = "description_year", nullable = false)
+    @Column(name = "description_year")
     private Integer descriptionYear;
 
-    @Column(name = "description_month", nullable = false)
+    @Column(name = "description_month")
     private Integer descriptionMonth;
 
     @Column(name = "preference", nullable = false)

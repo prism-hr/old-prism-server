a501ec054e45ae38cc3ad5bc705e77440ebbd688
alastair <alastair@localhost>
2015-12-10 19:10:47 +0000

    Sorted out problem with address / location setting for departments and positions.

--- src/main/java/uk/co/alumeni/prism/workflow/transition/creators/ProgramCreator.java
+++ src/main/java/uk/co/alumeni/prism/workflow/transition/creators/ProgramCreator.java
@@ -30,6 +30,7 @@ public class ProgramCreator implements ResourceCreator<ResourceOpportunityDTO> {
         ResourceParent parentResource = resourceCreatorUtils.getParentResource(newResource);
 
         Advert advert = advertService.createAdvert(newResource, user);
+        advertService.createAdvertLocation(advert, parentResource.getAdvert());
 
         Program program = new Program().withImportedCode(newResource.getImportedCode()).withUser(user).withParentResource(parentResource).withAdvert(advert)
                 .withName(advert.getName()).withDurationMinimum(newResource.getDurationMinimum()).withDurationMaximum(newResource.getDurationMaximum());

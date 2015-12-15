698c6a9bd55ca24a1e15e7f6d9fa16cedddb2d08
alastair <alastair@localhost>
2015-12-15 20:22:38 +0000

    Finished for day.

--- src/main/java/uk/co/alumeni/prism/workflow/evaluators/ResourceImagesEvaluator.java
+++ src/main/java/uk/co/alumeni/prism/workflow/evaluators/ResourceImagesEvaluator.java
@@ -1,12 +1,13 @@
 package uk.co.alumeni.prism.workflow.evaluators;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
+
 import uk.co.alumeni.prism.domain.advert.Advert;
 import uk.co.alumeni.prism.domain.resource.Institution;
 import uk.co.alumeni.prism.domain.resource.ResourceParent;
 import uk.co.alumeni.prism.services.AdvertService;
-
-import javax.inject.Inject;
 
 @Component
 public class ResourceImagesEvaluator implements ResourceCompletenessEvaluator<ResourceParent> {

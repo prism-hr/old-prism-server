bfe872284b7118d305c05efc938a58f479e1fde0
alastair <alastair@localhost>
2016-01-11 14:46:34 +0000

    Clean up after merge.

--- src/main/java/uk/co/alumeni/prism/domain/workflow/NotificationConfigurationDocument.java
+++ src/main/java/uk/co/alumeni/prism/domain/workflow/NotificationConfigurationDocument.java
@@ -1,9 +1,16 @@
 package uk.co.alumeni.prism.domain.workflow;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import uk.co.alumeni.prism.domain.UniqueEntity;
 import uk.co.alumeni.prism.domain.document.Document;
-
-import javax.persistence.*;
 
 @Entity
 @Table(name = "notification_configuration_document", uniqueConstraints = { @UniqueConstraint(columnNames = { "notification_configuration_id", "document_id" }) })

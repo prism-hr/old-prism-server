714b487dbd33dea5eb289beab3cef07b87797af4
alastair <alastair@localhost>
2016-01-28 18:59:09 +0000

    Finished fixing user management infrastructure to enable editing of invited use in case invitee makes a mistake.

--- src/main/java/uk/co/alumeni/prism/mapping/DepartmentMapper.java
+++ src/main/java/uk/co/alumeni/prism/mapping/DepartmentMapper.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole;
 import uk.co.alumeni.prism.domain.resource.Department;
+import uk.co.alumeni.prism.domain.user.User;
 import uk.co.alumeni.prism.rest.representation.resource.DepartmentRepresentationClient;
 import uk.co.alumeni.prism.rest.representation.resource.ResourceParentRepresentation;
 
@@ -19,18 +20,20 @@ public class DepartmentMapper {
     @Inject
     private ResourceMapper resourceMapper;
 
-    public ResourceParentRepresentation getDepartmentRepresentation(Department department, List<PrismRole> overridingRoles) {
-        return getDepartmentRepresentation(department, ResourceParentRepresentation.class, overridingRoles);
+    public ResourceParentRepresentation getDepartmentRepresentation(Department department, List<PrismRole> overridingRoles, User currentUser) {
+        return getDepartmentRepresentation(department, ResourceParentRepresentation.class, overridingRoles, currentUser);
     }
 
-    public DepartmentRepresentationClient getDepartmentRepresentationClient(Department department, List<PrismRole> overridingRoles) {
-        DepartmentRepresentationClient representation = getDepartmentRepresentation(department, DepartmentRepresentationClient.class, overridingRoles);
+    public DepartmentRepresentationClient getDepartmentRepresentationClient(Department department, List<PrismRole> overridingRoles, User currentUser) {
+        DepartmentRepresentationClient representation = getDepartmentRepresentation(department, DepartmentRepresentationClient.class, overridingRoles,
+                currentUser);
         resourceMapper.appendResourceSummaryRepresentation(department, representation);
         return representation;
     }
 
-    private <T extends ResourceParentRepresentation> T getDepartmentRepresentation(Department department, Class<T> returnType, List<PrismRole> overridingRoles) {
-        return resourceMapper.getResourceParentRepresentation(department, returnType, overridingRoles);
+    private <T extends ResourceParentRepresentation> T getDepartmentRepresentation(Department department, Class<T> returnType, List<PrismRole> overridingRoles,
+            User currentUser) {
+        return resourceMapper.getResourceParentRepresentation(department, returnType, overridingRoles, currentUser);
     }
 
 }

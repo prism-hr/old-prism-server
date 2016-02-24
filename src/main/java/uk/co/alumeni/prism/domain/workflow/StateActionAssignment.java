4ab62a63dab5ed37759107c785f48974fa168cf9
alastair <alastair@localhost>
2016-02-24 10:55:53 +0000

    Further ...

--- src/main/java/uk/co/alumeni/prism/domain/workflow/StateActionAssignment.java
+++ src/main/java/uk/co/alumeni/prism/domain/workflow/StateActionAssignment.java
@@ -1,11 +1,23 @@
 package uk.co.alumeni.prism.domain.workflow;
 
-import com.google.common.collect.Sets;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import uk.co.alumeni.prism.domain.UniqueEntity;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismActionEnhancement;
 
-import javax.persistence.*;
-import java.util.Set;
+import com.google.common.collect.Sets;
 
 @Entity
 @Table(name = "state_action_assignment", uniqueConstraints = { @UniqueConstraint(columnNames = { "state_action_id", "role_id", "external_mode" }) })

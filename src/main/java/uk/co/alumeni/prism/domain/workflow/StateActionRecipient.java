044d1212059fdefc97cd46b78bcdd6a1888ab1ed
alastair <alastair@localhost>
2016-02-24 10:55:53 +0000

    Further ...

--- src/main/java/uk/co/alumeni/prism/domain/workflow/StateActionRecipient.java
+++ src/main/java/uk/co/alumeni/prism/domain/workflow/StateActionRecipient.java
@@ -1,8 +1,15 @@
 package uk.co.alumeni.prism.domain.workflow;
 
-import uk.co.alumeni.prism.domain.UniqueEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
-import javax.persistence.*;
+import uk.co.alumeni.prism.domain.UniqueEntity;
 
 @Entity
 @Table(name = "state_action_recipient", uniqueConstraints = { @UniqueConstraint(columnNames = { "state_action_assignment_id", "role_id", "external_mode" }) })

2833df4357754df9e8e1a66c0655c1469a773041
alastair <alastair@localhost>
2016-02-29 12:24:40 +0000

    Sending the view timestamp information.

--- src/main/java/uk/co/alumeni/prism/rest/dto/MessageDTO.java
+++ src/main/java/uk/co/alumeni/prism/rest/dto/MessageDTO.java
@@ -1,11 +1,13 @@
 package uk.co.alumeni.prism.rest.dto;
 
-import org.hibernate.validator.constraints.NotEmpty;
-import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole;
-import uk.co.alumeni.prism.rest.dto.user.UserEmailDTO;
+import java.util.List;
 
 import javax.validation.constraints.Size;
-import java.util.List;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole;
+import uk.co.alumeni.prism.rest.dto.user.UserEmailDTO;
 
 public class MessageDTO {
 

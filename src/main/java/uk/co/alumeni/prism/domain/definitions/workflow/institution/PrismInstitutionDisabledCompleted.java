a62d37f61915d4c7b20ced0bc6bda80bb5054c59
alastair <alastair@localhost>
2016-03-04 12:24:44 +0000

    Fix graft problems.

--- src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/institution/PrismInstitutionDisabledCompleted.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/institution/PrismInstitutionDisabledCompleted.java
@@ -1,12 +1,13 @@
 package uk.co.alumeni.prism.domain.definitions.workflow.institution;
 
-import static uk.co.alumeni.prism.domain.definitions.workflow.institution.PrismInstitutionWorkflow.institutionEmailCreatorApproved;
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismAction.INSTITUTION_RESTORE;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismAction.INSTITUTION_VIEW_EDIT;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismActionEnhancement.INSTITUTION_VIEW_AS_USER;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismActionEnhancement.INSTITUTION_VIEW_EDIT_AS_USER;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRoleGroup.INSTITUTION_ADMINISTRATOR_GROUP;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRoleGroup.INSTITUTION_VIEWER_GROUP;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismState.INSTITUTION_APPROVED;
+import static uk.co.alumeni.prism.domain.definitions.workflow.institution.PrismInstitutionWorkflow.institutionEmailCreatorApproved;
 import static uk.co.alumeni.prism.domain.definitions.workflow.institution.PrismInstitutionWorkflow.institutionViewEditAbstract;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismStateAction;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismStateTransition;

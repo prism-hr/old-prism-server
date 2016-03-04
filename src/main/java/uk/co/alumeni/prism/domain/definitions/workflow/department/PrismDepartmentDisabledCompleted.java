761f098db333ba3cb39b1fa937790173558f4c45
alastair <alastair@localhost>
2016-03-04 12:13:42 +0000

    Made it possible to edit configuration (e.g. email messages for a disable resource).

--- src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/department/PrismDepartmentDisabledCompleted.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/department/PrismDepartmentDisabledCompleted.java
@@ -1,12 +1,13 @@
 package uk.co.alumeni.prism.domain.definitions.workflow.department;
 
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismAction.DEPARTMENT_RESTORE;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismAction.DEPARTMENT_VIEW_EDIT;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismActionEnhancement.DEPARTMENT_VIEW_AS_USER;
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismActionEnhancement.DEPARTMENT_VIEW_EDIT_AS_USER;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRoleGroup.DEPARTMENT_ADMINISTRATOR_GROUP;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRoleGroup.DEPARTMENT_VIEWER_GROUP;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismState.DEPARTMENT_APPROVED;
 import static uk.co.alumeni.prism.domain.definitions.workflow.department.PrismDepartmentWorkflow.departmentViewEditAbstract;
-import uk.co.alumeni.prism.domain.definitions.workflow.PrismAction;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismStateAction;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismStateTransition;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismWorkflowState;
@@ -18,14 +19,14 @@ public class PrismDepartmentDisabledCompleted extends PrismWorkflowState {
         stateActions.add(PrismDepartmentWorkflow.departmentSendMessageApproved()); //
 
         stateActions.add(new PrismStateAction() //
-                .withAction(PrismAction.DEPARTMENT_RESTORE) //
+                .withAction(DEPARTMENT_RESTORE) //
                 .withAssignments(DEPARTMENT_ADMINISTRATOR_GROUP) //
                 .withStateTransitions(new PrismStateTransition() //
                         .withTransitionState(DEPARTMENT_APPROVED) //
                         .withTransitionAction(DEPARTMENT_VIEW_EDIT)));
 
         stateActions.add(departmentViewEditAbstract() //
-                .withAssignments(DEPARTMENT_ADMINISTRATOR_GROUP, DEPARTMENT_VIEW_AS_USER) //
+                .withAssignments(DEPARTMENT_ADMINISTRATOR_GROUP, DEPARTMENT_VIEW_EDIT_AS_USER) //
                 .withAssignments(DEPARTMENT_VIEWER_GROUP, DEPARTMENT_VIEW_AS_USER) //
                 .withPartnerAssignments(DEPARTMENT_ADMINISTRATOR_GROUP, DEPARTMENT_VIEW_AS_USER)); //
     }

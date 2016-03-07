72b21990ce67e41d67ad89bab040a1b5e6edae41
alastair <alastair@localhost>
2016-03-07 17:06:01 +0000

    Improved notification model.

--- src/main/java/uk/co/alumeni/prism/dao/NotificationDAO.java
+++ src/main/java/uk/co/alumeni/prism/dao/NotificationDAO.java
@@ -1,6 +1,7 @@
 package uk.co.alumeni.prism.dao;
 
 import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismNotificationPurpose.REQUEST;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismNotificationType.INDIVIDUAL;
 
 import java.util.Collection;
@@ -21,7 +22,6 @@ import org.springframework.stereotype.Repository;
 
 import uk.co.alumeni.prism.domain.comment.Comment;
 import uk.co.alumeni.prism.domain.comment.CommentState;
-import uk.co.alumeni.prism.domain.definitions.workflow.PrismNotificationPurpose;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismScope;
 import uk.co.alumeni.prism.domain.resource.Resource;
 import uk.co.alumeni.prism.domain.user.User;
@@ -71,7 +71,8 @@ public class NotificationDAO {
     }
 
     public List<UserNotificationDefinitionDTO> getIndividualRequestDefinitions(PrismScope scope, PrismScope parentScope, Resource resource) {
-        return getIndividualRequestDefinitionCriteria(workflowDAO.getWorkflowCriteriaList(scope, parentScope, getInvidualRequestDefinitionsProjection()), resource)
+        return getIndividualRequestDefinitionCriteria(workflowDAO.getWorkflowCriteriaList(scope, parentScope, getInvidualRequestDefinitionsProjection()),
+                resource)
                 .setResultTransformer(Transformers.aliasToBean(UserNotificationDefinitionDTO.class)) //
                 .list();
     }
@@ -80,9 +81,9 @@ public class NotificationDAO {
             Collection<Integer> targeterEntities, Resource resource) {
         return getIndividualRequestDefinitionCriteria(
                 workflowDAO.getWorkflowCriteriaList(scope, targeterScope, targetScope, targeterEntities, getInvidualRequestDefinitionsProjection()), resource)
-                        .add(WorkflowDAO.getTargetActionConstraint())
-                        .setResultTransformer(Transformers.aliasToBean(UserNotificationDefinitionDTO.class)) //
-                        .list();
+                .add(WorkflowDAO.getTargetActionConstraint())
+                .setResultTransformer(Transformers.aliasToBean(UserNotificationDefinitionDTO.class)) //
+                .list();
     }
 
     public List<UserNotificationDefinitionDTO> getIndividualUpdateDefinitions(PrismScope scope, Comment comment, Collection<User> exclusions) {
@@ -101,7 +102,8 @@ public class NotificationDAO {
                 .setResultTransformer(Transformers.aliasToBean(UserNotificationDefinitionDTO.class)).list();
     }
 
-    public List<UserNotificationDefinitionDTO> getIndividualUpdateDefinitions(PrismScope scope, PrismScope parentScope, Comment comment, Collection<User> exclusions) {
+    public List<UserNotificationDefinitionDTO> getIndividualUpdateDefinitions(PrismScope scope, PrismScope parentScope, Comment comment,
+            Collection<User> exclusions) {
         Criteria criteria = getWorkflowCriteriaListComment(scope) //
                 .createAlias("resource." + parentScope.getLowerCamelName(), "parentResource", JoinType.INNER_JOIN) //
                 .createAlias("parentResource.userRoles", "userRole", JoinType.INNER_JOIN) //
@@ -180,7 +182,7 @@ public class NotificationDAO {
         });
 
         return criteria.add(Restrictions.ge("notifiedTimestamp", baseline)) //
-                .add(Restrictions.eq("notificationDefinition.notificationPurpose", PrismNotificationPurpose.REQUEST)) //
+                .add(Restrictions.eq("notificationDefinition.notificationPurpose", REQUEST)) //
                 .setResultTransformer(Transformers.aliasToBean(UserNotificationDTO.class)) //
                 .list();
     }

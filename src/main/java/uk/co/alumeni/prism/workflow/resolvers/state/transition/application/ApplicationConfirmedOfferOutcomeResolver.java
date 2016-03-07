f08a3f24279018a098746317d6eda40e95727628
alastair <alastair@localhost>
2016-03-07 15:34:59 +0000

    Fixes / improvemets to offer acceptance phase.

--- src/main/java/uk/co/alumeni/prism/workflow/resolvers/state/transition/application/ApplicationConfirmedOfferOutcomeResolver.java
+++ src/main/java/uk/co/alumeni/prism/workflow/resolvers/state/transition/application/ApplicationConfirmedOfferOutcomeResolver.java
@@ -24,7 +24,7 @@ import uk.co.alumeni.prism.services.StateService;
 import uk.co.alumeni.prism.workflow.resolvers.state.transition.StateTransitionResolver;
 
 @Component
-public class ApplicationConfirmedOfferOutcome implements StateTransitionResolver<Application> {
+public class ApplicationConfirmedOfferOutcomeResolver implements StateTransitionResolver<Application> {
 
     @Inject
     private ApplicationService applicationService;

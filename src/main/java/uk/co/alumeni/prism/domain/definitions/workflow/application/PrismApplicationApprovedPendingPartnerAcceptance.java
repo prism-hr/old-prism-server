1b56130ae07570712b9fe59efb69bc0718322aa3
alastair <alastair@localhost>
2016-03-09 12:50:38 +0000

    Finished fixing the approved stage workflow for lahp.

--- src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/application/PrismApplicationApprovedPendingPartnerAcceptance.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/workflow/application/PrismApplicationApprovedPendingPartnerAcceptance.java
@@ -14,7 +14,7 @@ import static uk.co.alumeni.prism.domain.definitions.workflow.PrismState.APPLICA
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismState.APPLICATION_APPROVED_COMPLETED;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismState.APPLICATION_APPROVED_PENDING_OFFER_ACCEPTANCE;
 import static uk.co.alumeni.prism.domain.definitions.workflow.PrismStateTransitionEvaluation.APPLICATION_PROVIDED_PARTNER_APPROVAL_OUTCOME;
-import static uk.co.alumeni.prism.domain.definitions.workflow.application.PrismApplicationApproved.applicationCompleteApprovedWithAppointee;
+import static uk.co.alumeni.prism.domain.definitions.workflow.application.PrismApplicationApproved.applicationCompleteApproved;
 import static uk.co.alumeni.prism.domain.definitions.workflow.application.PrismApplicationWorkflow.applicationCommentWithViewerRecruiter;
 import static uk.co.alumeni.prism.domain.definitions.workflow.application.PrismApplicationWorkflow.applicationEmailCreatorWithViewerRecruiter;
 import static uk.co.alumeni.prism.domain.definitions.workflow.application.PrismApplicationWorkflow.applicationEscalate;
@@ -28,7 +28,7 @@ public class PrismApplicationApprovedPendingPartnerAcceptance extends PrismWorkf
     @Override
     protected void setStateActions() {
         stateActions.add(applicationCommentWithViewerRecruiter()); //
-        stateActions.add(applicationCompleteApprovedWithAppointee(state)); //
+        stateActions.add(applicationCompleteApproved(state)); //
         stateActions.add(applicationEmailCreatorWithViewerRecruiter()); //
         stateActions.add(applicationEscalate(APPLICATION_APPROVED_COMPLETED)); //
 

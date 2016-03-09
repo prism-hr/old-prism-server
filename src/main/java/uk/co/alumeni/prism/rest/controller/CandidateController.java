509161c539d02d229a4e29daac0f285964f9aca3
jfi <jfi@zuhlke.com>
2016-03-09 19:08:06 +0100

    Missing candidates endpoint, NPE fix with summaries

--- src/main/java/uk/co/alumeni/prism/rest/controller/CandidateController.java
+++ src/main/java/uk/co/alumeni/prism/rest/controller/CandidateController.java
@@ -1,27 +1,30 @@
 package uk.co.alumeni.prism.rest.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.inject.Inject;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import uk.co.alumeni.prism.domain.user.User;
 import uk.co.alumeni.prism.mapping.ProfileMapper;
+import uk.co.alumeni.prism.mapping.UserMapper;
 import uk.co.alumeni.prism.rest.dto.profile.ProfileListFilterDTO;
 import uk.co.alumeni.prism.rest.representation.profile.ProfileListRowRepresentation;
 import uk.co.alumeni.prism.rest.representation.profile.ProfileRepresentationSummary;
+import uk.co.alumeni.prism.rest.representation.user.UserProfileRepresentation;
+import uk.co.alumeni.prism.services.UserService;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/candidates")
 public class CandidateController {
+
+    @Inject
+    private UserService userService;
+
+    @Inject
+    private UserMapper userMapper;
 
     @Inject
     private ProfileMapper profileMapper;
@@ -40,5 +43,13 @@ public class CandidateController {
     @RequestMapping(value = "{userId}", method = RequestMethod.GET, params = "type=summary")
     public ProfileRepresentationSummary getUserProfileSummary(@PathVariable Integer userId) {
         return profileMapper.getProfileRepresentationSummary(userId);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "{userId}", method = RequestMethod.GET)
+    public UserProfileRepresentation getUserProfile(@PathVariable Integer userId) {
+        User user = userService.getById(userId);
+        // TODO implement security check
+        return userMapper.getUserProfileRepresentation(user);
     }
 }

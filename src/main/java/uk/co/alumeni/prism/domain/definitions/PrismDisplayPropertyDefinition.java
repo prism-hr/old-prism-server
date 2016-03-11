1302420feb785e0e68f9da99a43256f4d3028ad1
alastair <alastair@localhost>
2016-03-11 19:17:54 +0000

    Fixed issues with connections during organization creation.

--- src/main/java/uk/co/alumeni/prism/domain/definitions/PrismDisplayPropertyDefinition.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/PrismDisplayPropertyDefinition.java
@@ -1541,7 +1541,7 @@ public enum PrismDisplayPropertyDefinition implements PrismConfigurationCategori
      */
 
     SYSTEM_ADVERTISE_LOOKUP_ORGANIZATION_LABEL(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Lookup Organization"),
-    SYSTEM_ADVERTISE_LOOKUP_ORGANIZATION_HINT(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Find your organization within the list"),
+    SYSTEM_ADVERTISE_LOOKUP_ORGANIZATION_HINT(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Start typing the name of your organization"),
     SYSTEM_ADVERTISE_LOOKUP_GOOGLE_ORGANIZATION_LABEL(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Lookup Organization"),
     SYSTEM_ADVERTISE_LOOKUP_GOOGLE_ORGANIZATION_HINT(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Start typing the name of your organization"),
     SYSTEM_ADVERTISE_LOOKUP_GOOGLE_ORGANIZATION_PLACEHOLDER(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "e.g. Google Inc"),
@@ -1573,6 +1573,15 @@ public enum PrismDisplayPropertyDefinition implements PrismConfigurationCategori
     SYSTEM_ADVERTISE_UNIVERSITY_ORGANIZATION_ALREADY_REGISTERED(
             PrismDisplayPropertyCategory.SYSTEM_ADVERTISE,
             "If you have already registered your department, you don't need to do this again. Just login with the account that you provided, and nagivate to your departments in the menu."),
+    SYSTEM_HTML_RESOURCE_FAMILY_LOOKUP_INSTITUTION_LABEL(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Lookup"),
+    SYSTEM_HTML_RESOURCE_FAMILY_CANNOT_FIND_THE_INSTITUTION(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "I cannot find the"),
+    SYSTEM    SYSTEM_HTML_RESOURCE_FAMILY_LOOKUP_INSTITUTION_LABEL(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Lookup"),
+    SYSTEM_HTML_RESOURCE_FAMILY_CANNOT_FIND_THE_INSTITUTION(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "I cannot find the"),
+    SYSTEM_ADVERTISE_MEMBER_OF(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Are you a member of"), 
+    SYSTEM_ADVERTISE_ME(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "This is me"), 
+    SYSTEM_ADVERTISE_NOT_ME(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "This is not me"),
+    SYSTEM_ADVERTISE_CLAIM(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Claim"),
+    SYSTEM_ADVERTISE_YOUR_COMPANY(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "as your company"), 
 
     /*
      * *************** SYSTEM INVITE SECTION *********************
@@ -1975,8 +1984,7 @@ public enum PrismDisplayPropertyDefinition implements PrismConfigurationCategori
     SYSTEM_HTML_RESOURCE_FAMILY_APPLICATION_LOCATION_OPPORTUNITY_TYPE_LABEL(PrismDisplayPropertyCategory.SYSTEM_CREATE_RESOURCE_FAMILY, "Qualification Type"),
     SYSTEM_HTML_RESOURCE_FAMILY_APPLICATION_LOCATION_OPPORTUNITY_TYPE_HINT(PrismDisplayPropertyCategory.SYSTEM_CREATE_RESOURCE_FAMILY,
             "Select the type of your qualification"),
-    SYSTEM_HTML_RESOURCE_FAMILY_LOOKUP_INSTITUTION_LABEL(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "Lookup"),
-    SYSTEM_HTML_RESOURCE_FAMILY_CANNOT_FIND_THE_INSTITUTION(PrismDisplayPropertyCategory.SYSTEM_ADVERTISE, "I cannot find the"),
+
 
     /*
      * *************** SYSTEM HTML RESOURCE CONFIGURATION *********************

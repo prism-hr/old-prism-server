5a3bf6560ba3828ffc66abe346069066683a73b1
alastair <alastair@localhost>
2016-03-12 00:53:41 +0000

    Problem with imports.

--- src/main/java/uk/co/alumeni/prism/dto/EntityOpportunityCategoryDTO.java
+++ src/main/java/uk/co/alumeni/prism/dto/EntityOpportunityCategoryDTO.java
@@ -8,7 +8,6 @@ import static uk.co.alumeni.prism.PrismConstants.ORDERING_PRECISION;
 import static uk.co.alumeni.prism.PrismConstants.ZERO;
 
 import java.math.BigDecimal;
-import org.apache.commons.lang3.BooleanUtils;
 
 import uk.co.alumeni.prism.domain.definitions.PrismOpportunityType;
 

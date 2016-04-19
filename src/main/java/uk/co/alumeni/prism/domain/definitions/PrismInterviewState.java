6d31496e91b36e8bc67924be13da9dfea034be01
alastair <alastair@localhost>
2016-04-19 13:34:29 +0100

    Some testing and minor fixes.

--- src/main/java/uk/co/alumeni/prism/domain/definitions/PrismInterviewState.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/PrismInterviewState.java
@@ -1,9 +1,24 @@
 package uk.co.alumeni.prism.domain.definitions;
 
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRole.APPLICATION_INTERVIEWER;
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRole.APPLICATION_POTENTIAL_INTERVIEWER;
+import static uk.co.alumeni.prism.domain.definitions.workflow.PrismRole.APPLICATION_SCHEDULED_INTERVIEWER;
+import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole;
+
 public enum PrismInterviewState {
 
-    TAKEN_PLACE,
-    SCHEDULED,
-    TO_BE_SCHEDULED
+    TAKEN_PLACE(APPLICATION_INTERVIEWER),
+    SCHEDULED(APPLICATION_SCHEDULED_INTERVIEWER),
+    TO_BE_SCHEDULED(APPLICATION_POTENTIAL_INTERVIEWER);
+
+    private PrismRole interviewerRole;
+
+    private PrismInterviewState(PrismRole interviewerRole) {
+        this.interviewerRole = interviewerRole;
+    }
+
+    public PrismRole getInterviewerRole() {
+        return interviewerRole;
+    }
 
 }

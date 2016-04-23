01c4750ffd8c264edb1bc0e1d079c6965a244863
alastair <alastair@localhost>
2016-04-23 15:06:08 +0100

    Fixed problem with advert locations.

--- src/main/java/uk/co/alumeni/prism/domain/address/AddressDefinition.java
+++ src/main/java/uk/co/alumeni/prism/domain/address/AddressDefinition.java
@@ -1,6 +1,8 @@
 package uk.co.alumeni.prism.domain.address;
 
-public abstract class AddressDefinition<T> {
+import static org.apache.commons.lang3.ObjectUtils.compare;
+
+public abstract class AddressDefinition<T extends Comparable<T>> implements Comparable<AddressDefinition<T>> {
 
     public abstract String getAddressLine1();
 
@@ -25,5 +27,16 @@ public abstract class AddressDefinition<T> {
     public abstract T getDomicile();
 
     public abstract void setDomicile(T domicile);
+
+    @Override
+    public int compareTo(AddressDefinition<T> other) {
+        int compare = compare(getDomicile(), other.getDomicile());
+        compare = compare == 0 ? compare(getAddressCode(), other.getAddressCode()) : compare;
+        compare = compare == 0 ? compare(getAddressRegion(), other.getAddressRegion()) : compare;
+        compare = compare == 0 ? compare(getAddressTown(), other.getAddressTown()) : compare;
+        compare = compare == 0 ? compare(getAddressLine2(), other.getAddressLine2()) : compare;
+        compare = compare == 0 ? compare(getAddressLine1(), other.getAddressLine1()) : compare;
+        return compare;
+    }
 
 }

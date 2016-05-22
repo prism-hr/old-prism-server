619e57524a60f6a805354044bbf87a394d76bc9f
alastair <alastair@localhost>
2016-05-22 22:23:00 +0100

    Changed the apis to get the data for the filter dropdowns to make them work dynamically depending upon the current filter query.

---
+++ src/main/java/uk/co/alumeni/prism/dto/AdvertThemeSummaryDTO.java
@@ -0,0 +1,51 @@
+package uk.co.alumeni.prism.dto;
+
+import static com.google.common.base.Objects.equal;
+import static org.apache.commons.lang3.ObjectUtils.compare;
+
+import com.google.common.base.Objects;
+
+public class AdvertThemeSummaryDTO extends AdvertCategorySummaryDTO implements Comparable<AdvertThemeSummaryDTO> {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (!getClass().equals(object.getClass())) {
+            return false;
+        }
+        return equal(id, ((AdvertThemeSummaryDTO) object).getId());
+    }
+
+    @Override
+    public int compareTo(AdvertThemeSummaryDTO other) {
+        return compare(name, other.getName());
+    }
+
+}
--- src/main/java/uk/co/alumeni/prism/rest/representation/advert/AdvertCategorySummaryRepresentation.java
+++ src/main/java/uk/co/alumeni/prism/rest/representation/advert/AdvertCategorySummaryRepresentation.java
@@ -1,22 +1,8 @@
 package uk.co.alumeni.prism.rest.representation.advert;
 
-import static com.google.common.base.Objects.equal;
-
-import com.google.common.base.Objects;
-
-public class AdvertCategorySummaryRepresentation<T> {
-
-    private T id;
+public class AdvertCategorySummaryRepresentation {
 
     private Long advertCount;
-
-    public T getId() {
-        return id;
-    }
-
-    public void setId(T id) {
-        this.id = id;
-    }
 
     public Long getAdvertCount() {
         return advertCount;
@@ -24,23 +10,6 @@ public class AdvertCategorySummaryRepresentation<T> {
 
     public void setAdvertCount(Long advertCount) {
         this.advertCount = advertCount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public boolean equals(Object object) {
-        if (object == null) {
-            return false;
-        }
-        if (!getClass().equals(object.getClass())) {
-            return false;
-        }
-        return equal(id, ((AdvertCategorySummaryRepresentation<T>) object).getId());
     }
 
 }

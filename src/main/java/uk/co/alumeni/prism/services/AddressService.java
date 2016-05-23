487110019c2f7e7ddb164c2b26f8776873794bea
alastair <alastair@localhost>
2016-05-23 15:29:18 +0100

    Also remove wrong google types from address service.

--- src/main/java/uk/co/alumeni/prism/services/AddressService.java
+++ src/main/java/uk/co/alumeni/prism/services/AddressService.java
@@ -53,8 +53,7 @@ public class AddressService {
 
     private static final List<String> googleLocationTypes = newArrayList("country", "administrative_area_level_1", "administrative_area_level_2",
             "administrative_area_level_3", "administrative_area_level_4", "administrative_area_level_5", "political", "postal_town", "locality", "sublocality",
-            "sublocality_level_1", "sublocality_level_2", "sublocality_level_3", "sublocality_level_4", "sublocality_level_5", "neighborhood", "premise",
-            "subpremise", "airport");
+            "sublocality_level_1", "sublocality_level_2", "sublocality_level_3", "sublocality_level_4", "sublocality_level_5", "neighborhood", "airport");
 
     @Value("${integration.google.api.key}")
     private String googleApiKey;

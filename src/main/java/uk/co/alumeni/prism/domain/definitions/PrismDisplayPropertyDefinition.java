094907814b61454840d8d98b4eb7774a583b4c18
juan <juan@localhost>
2016-06-07 19:43:24 +0200

    about and contact labels

--- src/main/java/uk/co/alumeni/prism/domain/definitions/PrismDisplayPropertyDefinition.java
+++ src/main/java/uk/co/alumeni/prism/domain/definitions/PrismDisplayPropertyDefinition.java
@@ -1369,6 +1369,8 @@ public enum PrismDisplayPropertyDefinition implements PrismConfigurationCategori
     SYSTEM_HTML_GENERAL_HEADER_PROFILE(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Profile"),
     SYSTEM_HTML_GENERAL_HEADER_LOGOUT(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Logout"),
     SYSTEM_HTML_GENERAL_ENQUIRY_SUBJECT(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Question Regarding UCL Prism Application {{code}}"),
+    SYSTEM_HTML_GENERAL_ABOUT(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "About Prism"),
+    SYSTEM_HTML_GENERAL_CONTACT(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Contact us"),
     SYSTEM_HTML_GENERAL_PRIVACY_POLICY(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Privacy Policy"),
     SYSTEM_HTML_GENERAL_TERMS_AND_CONDITIONS(PrismDisplayPropertyCategory.SYSTEM_GENERAL, "Terms & Conditions"),
     SYSTEM_HTML_GENERAL_MEMBER_UNIVERSITY(PrismDisplayPropertyCategory.SYSTEM_GENERAL, " Are you part of this university?"),

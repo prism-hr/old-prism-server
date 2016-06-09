a03d0d5478af63143020fded7ef7c06bda2d9c1c
jfi <jfi@localhost>
2016-06-09 15:04:01 +0200

    First version of contact page

--- src/main/java/uk/co/alumeni/prism/rest/controller/UserFeedbackController.java
+++ src/main/java/uk/co/alumeni/prism/rest/controller/UserFeedbackController.java
@@ -1,29 +1,23 @@
 package uk.co.alumeni.prism.rest.controller;
 
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.validation.Valid;
-
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import uk.co.alumeni.prism.domain.user.User;
 import uk.co.alumeni.prism.domain.user.UserFeedback;
 import uk.co.alumeni.prism.mapping.UserMapper;
+import uk.co.alumeni.prism.rest.dto.user.UserContactDTO;
 import uk.co.alumeni.prism.rest.dto.user.UserFeedbackDTO;
 import uk.co.alumeni.prism.rest.representation.user.UserFeedbackRepresentation;
 import uk.co.alumeni.prism.services.UserFeedbackService;
 import uk.co.alumeni.prism.services.UserService;
 
-import com.google.common.collect.Lists;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/feedback")
+@RequestMapping("/api")
 public class UserFeedbackController {
 
     @Inject
@@ -36,23 +30,25 @@ public class UserFeedbackController {
     private UserFeedbackService userFeedbackService;
 
     @PreAuthorize("isAuthenticated()")
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/feedback", method = RequestMethod.POST)
     public void createFeedback(@RequestBody @Valid UserFeedbackDTO userFeedbackDTO) {
         userFeedbackService.createFeedback(userFeedbackDTO);
     }
 
     @PreAuthorize("permitAll")
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/feedback", method = RequestMethod.GET)
     public List<UserFeedbackRepresentation> getUserFeedback(@RequestParam("ratingThreshold") Integer ratingThreshold,
             @RequestParam("lastSequenceIdentifier") String lastSequenceIdentifier) {
         List<UserFeedback> userFeedbacks = userFeedbackService.getUserFeedback(ratingThreshold, lastSequenceIdentifier);
-        List<UserFeedbackRepresentation> representations = Lists.newArrayListWithCapacity(userFeedbacks.size());
 
         User currentUser = userService.getCurrentUser();
-        for (UserFeedback userFeedback : userFeedbacks) {
-            representations.add(userMapper.getUserFeedbackRepresentation(userFeedback, currentUser));
-        }
-        return representations;
+        return userFeedbacks.stream().map(userFeedback -> userMapper.getUserFeedbackRepresentation(userFeedback, currentUser)).collect(Collectors.toList());
+    }
+
+    @PreAuthorize("permitAll")
+    @RequestMapping(value = "/contact", method = RequestMethod.POST)
+    public void postMessage(@RequestBody @Valid UserContactDTO userContactDTO) {
+        userFeedbackService.postContactMessage(userContactDTO);
     }
 
 }

3b7ee314ab247efcda89138fb32247ca5904abe5
jfi <jfi@localhost>
2016-11-30 15:22:17 +0100

    Reinstating import

--- src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismMaintenanceTask.java
+++ src/main/java/com/zuehlke/pgadmissions/domain/definitions/PrismMaintenanceTask.java
@@ -1,26 +1,13 @@
 package com.zuehlke.pgadmissions.domain.definitions;
 
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.AbstractServiceHelper;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.AdvertServiceHelperClosingDate;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.AdvertServiceHelperExchangeRate;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.ApplicationExportServiceHelper;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.DocumentServiceHelperDelete;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.DocumentServiceHelperExport;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.EmailBounceServiceHelper;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.ImportedEntityServiceHelperInstitution;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.ImportedEntityServiceHelperSystem;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.NotificationServiceHelperRecommendation;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.NotificationServiceHelperWorkflow;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.ResourceServiceHelper;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.StateServiceHelperEscalation;
-import com.zuehlke.pgadmissions.services.lifecycle.helpers.StateServiceHelperPropagation;
+import com.zuehlke.pgadmissions.services.lifecycle.helpers.*;
 
 public enum PrismMaintenanceTask {
 
     SYSTEM_UPDATE_PROGRAM_STUDY_OPTION(ResourceServiceHelper.class), //
     SYSTEM_EXECUTE_ESCALATED_STATE_TRANSITION(StateServiceHelperEscalation.class), //
     SYSTEM_EXECUTE_PROPAGATED_STATE_TRANSITION(StateServiceHelperPropagation.class), //
-//    SYSTEM_IMPORT_INSTITUTION_REFERENCE_DATA(ImportedEntityServiceHelperInstitution.class),//
+    SYSTEM_IMPORT_INSTITUTION_REFERENCE_DATA(ImportedEntityServiceHelperInstitution.class),//
     SYSTEM_EXPORT_APPLICATION(ApplicationExportServiceHelper.class), //
     SYSTEM_SEND_DEFERRED_WORKFLOW_NOTIFICATION(NotificationServiceHelperWorkflow.class), //
     SYSTEM_IMPORT_SYSTEM_REFERENCE_DATA(ImportedEntityServiceHelperSystem.class), //

f5d7a783f9600fa97a0fd0b7cef16c93c7774d95
alastair <alastair@localhost>
2017-01-10 15:21:03 +0000

    Fixed issues reported (visibility of application after reference provision and duplicate qualification issue).

---
+++ src/sql/change_scripts/0122_weaken_application_user_constaints.sql
@@ -0,0 +1,25 @@
+set foreign_key_checks = 0
+;
+
+alter table application_qualification
+  drop index application_id,
+  add unique index (application_id, advert_id, start_year, start_month)
+;
+
+alter table application_employment_position
+  drop index application_id,
+  add unique index (application_id, advert_id, start_year, start_month)
+;
+
+alter table user_qualification
+  drop index user_id,
+  add unique index (user_id, advert_id, start_year, start_month)
+;
+
+alter table user_employment_position
+  drop index user_id,
+  add unique index (user_id, advert_id, start_year, start_month)
+;
+
+set foreign_key_checks = 1
+;

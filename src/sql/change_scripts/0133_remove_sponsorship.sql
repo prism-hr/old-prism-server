4db883ffb5f1a0859e0eb354a38e5ec43885106c
alastair <alastair@localhost>
2017-03-28 09:06:31 +0100

    Finished removing sponsor and enabling institution viewer.

--- src/sql/change_scripts/0133_remove_sponsorship.sql
+++ src/sql/change_scripts/0133_remove_sponsorship.sql
@@ -8,10 +8,26 @@ FROM user_role
 WHERE role_id LIKE '%SPONSOR%'
 ;
 
+DELETE
+FROM user_notification
+WHERE notification_definition_id LIKE '%SPONSOR%';
+
 ALTER TABLE advert
   DROP COLUMN sponsorship_purpose,
   DROP COLUMN sponsorship_target,
   DROP COLUMN sponsorship_secured
 ;
+
+ALTER TABLE COMMENT
+  DROP FOREIGN KEY comment_ibfk_18,
+  DROP INDEX institution_sponsor_id,
+  DROP FOREIGN KEY comment_ibfk_19,
+  DROP INDEX sponsorship_rejection_id,
+  DROP COLUMN sponsorship_currency_specified,
+  DROP COLUMN sponsorship_currency_converted,
+  DROP COLUMN sponsorship_amount_specified,
+  DROP COLUMN sponsorship_amount_converted,
+  DROP COLUMN sponsorship_target_fulfilled,
+  DROP COLUMN sponsorship_rejection_id;
 
 # Drop the comment columns

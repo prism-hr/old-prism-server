035399b80105f67450fb8ebf21ad467b1f643eeb
alastair <alastair@localhost>
2017-10-12 20:27:32 +0100

    Notification enhancement.

--- src/main/java/uk/co/alumeni/prism/services/lifecycle/helpers/NotificationServiceHelperReminder.java
+++ src/main/java/uk/co/alumeni/prism/services/lifecycle/helpers/NotificationServiceHelperReminder.java
@@ -1,12 +1,17 @@
 package uk.co.alumeni.prism.services.lifecycle.helpers;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Component;
+import uk.co.alumeni.prism.domain.definitions.workflow.PrismAction;
 import uk.co.alumeni.prism.mapping.UserMapper;
 import uk.co.alumeni.prism.rest.representation.user.UserActivityRepresentation;
 import uk.co.alumeni.prism.services.NotificationService;
 import uk.co.alumeni.prism.services.UserService;
 
 import javax.inject.Inject;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
@@ -37,7 +42,38 @@ public class NotificationServiceHelperReminder extends PrismServiceHelperAbstrac
         if (!isShuttingDown()) {
             UserActivityRepresentation userActivityRepresentation = userMapper.getUserActivityRepresentation(user);
             if (userActivityRepresentation != null) {
-                notificationService.sendUserReminderNotification(user, userActivityRepresentation);
+                List<UserActivityRepresentation.ResourceActivityRepresentation> resourceActivityRepresentations = userActivityRepresentation.getResourceActivities();
+                if (CollectionUtils.isNotEmpty(resourceActivityRepresentations)) {
+                    Iterator<UserActivityRepresentation.ResourceActivityRepresentation> resourceActivityRepresentationIterator = resourceActivityRepresentations.iterator();
+                    while (resourceActivityRepresentationIterator.hasNext()) {
+                        UserActivityRepresentation.ResourceActivityRepresentation resourceActivityRepresentation = resourceActivityRepresentationIterator.next();
+                        List<UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation> actionActivityRepresentations =
+                                resourceActivityRepresentation.getActions();
+                        if (CollectionUtils.isNotEmpty(actionActivityRepresentations)) {
+                            Iterator<UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation> actionActivityRepresentationIterator =
+                                    actionActivityRepresentations.iterator();
+                            while (actionActivityRepresentationIterator.hasNext()) {
+                                UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation actionActivityRepresentation =
+                                        actionActivityRepresentationIterator.next();
+                                PrismAction action = actionActivityRepresentation.getAction().getId();
+                                if (action.isExcludeFromReminder()) {
+                                    // Remove if action excluded
+                                    actionActivityRepresentationIterator.remove();
+                                }
+                            }
+                            
+                            if (CollectionUtils.isEmpty(actionActivityRepresentations)) {
+                                // Remove if actions empty
+                                resourceActivityRepresentationIterator.remove();
+                            }
+                        }
+                    }
+                    
+                    if (CollectionUtils.isNotEmpty(resourceActivityRepresentations)) {
+                        // Send if everything has not been removed
+                        notificationService.sendUserReminderNotification(user, userActivityRepresentation);
+                    }
+                }
             }
         }
     }

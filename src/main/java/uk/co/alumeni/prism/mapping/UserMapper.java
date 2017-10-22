f9c582d698b5d7da14afe464e0da7fe2d1c12e6a
alastair <alastair@localhost>
2017-10-22 12:27:58 +0100

    Fix issue with notifications.

--- src/main/java/uk/co/alumeni/prism/mapping/UserMapper.java
+++ src/main/java/uk/co/alumeni/prism/mapping/UserMapper.java
@@ -5,10 +5,12 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.TreeMultimap;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.BooleanUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import uk.co.alumeni.prism.domain.definitions.PrismRoleContext;
+import uk.co.alumeni.prism.domain.definitions.workflow.PrismAction;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismRole.PrismRoleCategory;
 import uk.co.alumeni.prism.domain.definitions.workflow.PrismScope;
@@ -33,6 +35,7 @@ import uk.co.alumeni.prism.utils.PrismJsonMappingUtils;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -50,84 +53,84 @@ import static uk.co.alumeni.prism.utils.PrismStringUtils.getObfuscatedEmail;
 @Service
 @Transactional
 public class UserMapper {
-
+    
     @Inject
     private ResourceMapper resourceMapper;
-
+    
     @Inject
     private AdvertService advertService;
-
+    
     @Inject
     private AdvertMapper advertMapper;
-
+    
     @Inject
     private RoleService roleService;
-
+    
     @Inject
     private ResourceService resourceService;
-
+    
     @Inject
     private UserFeedbackService userFeedbackService;
-
+    
     @Inject
     private UserService userService;
-
+    
     @Inject
     private SystemService systemService;
-
+    
     @Inject
     private ApplicationMapper applicationMapper;
-
+    
     @Inject
     private ProfileMapper profileMapper;
-
+    
     @Inject
     private ScopeMapper scopeMapper;
-
+    
     @Inject
     private DocumentMapper documentMapper;
-
+    
     @Inject
     private PrismJsonMappingUtils prismJsonMappingUtils;
-
+    
     @Inject
     private ApplicationContext applicationContext;
-
+    
     public <T extends UserDescriptor> UserRepresentationSimple getUserRepresentationSimple(T user, User currentUser) {
         return getUserRepresentation(user, currentUser, UserRepresentationSimple.class);
     }
-
+    
     public <T extends UserDescriptor> UserRepresentationSimple getUserRepresentationSimpleWithEmail(T user, User currentUser) {
         return getUserRepresentation(user, currentUser, true, UserRepresentationSimple.class);
     }
-
+    
     public UserRepresentationExtended getUserRepresentationExtended(User user, User currentUser) {
         UserRepresentationExtended representation = getUserRepresentation(user, currentUser, UserRepresentationExtended.class);
         representation.setSendActivityNotification(user.getUserAccount().getSendActivityNotification());
         representation.setUserRoles(getUserRoleRepresentations(user));
-
+        
         representation.setParentUser(user.getEmail());
         representation.setLinkedUsers(userService.getLinkedUsers(user).stream().map(u -> u.getEmail()).collect(Collectors.toList()));
-
+        
         representation.setConnectedWithLinkedin(user.getUserAccount().getLinkedinId() != null);
         representation.setRequiredFeedbackRoleCategory(userFeedbackService.getRequiredFeedbackRoleCategory(user));
         return representation;
     }
-
+    
     public List<UserRepresentationInvitationBounced> getUserUnverifiedRepresentations(Resource resource, UserListFilterDTO filterDTO) {
         String noDiagnosis = applicationContext.getBean(PropertyLoader.class).localizeLazy(systemService.getSystem())
                 .loadLazy(SYSTEM_NO_DIAGNOSTIC_INFORMATION);
-
+        
         User currentUser = userService.getCurrentUser();
         return userService.getBouncedOrUnverifiedUsers(resource, filterDTO).stream()
                 .map(user -> getUserRepresentationInvitationBounced(user, currentUser, noDiagnosis)).collect(Collectors.toList());
     }
-
+    
     public UserFeedbackRepresentation getUserFeedbackRepresentation(UserFeedback userFeedback, User currentUser) {
         UserFeedbackRepresentation representation = new UserFeedbackRepresentation()
                 .withResource(resourceMapper.getResourceRepresentationSimple(userFeedback.getResource()))
                 .withUser(getUserRepresentationSimple(userFeedback.getUser(), currentUser)).withRoleCategory(userFeedback.getRoleCategory());
-
+        
         if (BooleanUtils.isFalse(userFeedback.getDeclinedResponse())) {
             representation.setRating(userFeedback.getRating());
             representation.setContent(userFeedback.getContent());
@@ -135,30 +138,30 @@ public class UserMapper {
             representation.setRecommended(userFeedback.getRecommended());
             representation.setCreatedTimestamp(userFeedback.getCreatedTimestamp());
         }
-
+        
         return representation;
     }
-
+    
     private UserRepresentationInvitationBounced getUserRepresentationInvitationBounced(User user, User currentUser, String noDiagnosisMessage) {
         UserRepresentationInvitationBounced representation = getUserRepresentation(user, currentUser, true, UserRepresentationInvitationBounced.class);
-
+        
         String bounceMessage = user.getEmailBouncedMessage();
         if (bounceMessage != null) {
             JsonPrimitive diagnosis = new JsonParser().parse(bounceMessage).getAsJsonObject().getAsJsonPrimitive("diagnosticCode");
             representation.setDiagnosticInformation(diagnosis != null ? diagnosis.getAsString() : noDiagnosisMessage);
         }
-
+        
         return representation;
     }
-
+    
     public <T extends UserDescriptor, U extends UserRepresentationSimple> U getUserRepresentation(T user, User currentUser, Class<U> returnType) {
         return getUserRepresentation(user, currentUser, false, returnType);
     }
-
+    
     public <T extends UserRepresentationSimple> T getUserRepresentationWithEmail(User user, User currentUser, Class<T> returnType) {
         return getUserRepresentation(user, currentUser, true, returnType);
     }
-
+    
     public List<UserRepresentationSimple> getUserRepresentations(List<UserSelectionDTO> users) {
         User currentUser = userService.getCurrentUser();
         List<UserRepresentationSimple> representations = newLinkedList();
@@ -169,11 +172,11 @@ public class UserMapper {
         });
         return representations;
     }
-
+    
     public UserActivityRepresentation getUserActivityRepresentation(Integer user) {
         return getUserActivityRepresentation(userService.getById(user));
     }
-
+    
     public UserActivityRepresentation getUserActivityRepresentation(User user) {
         UserAccount userAccount = user.getUserAccount();
         if (userAccount != null) {
@@ -184,22 +187,22 @@ public class UserMapper {
             } else {
                 representation = prismJsonMappingUtils.readValue(userActivityCache, UserActivityRepresentation.class);
             }
-
+            
             representation.setCacheIncrement(userAccount.getActivityCachedIncrement());
             return representation;
         }
-
+        
         return null;
     }
-
+    
     public UserActivityRepresentation getUserActivityRepresentationFresh(Integer user) {
         return getUserActivityRepresentationFresh(userService.getById(user));
     }
-
+    
     public UserActivityRepresentation getUserActivityRepresentationFresh(User user) {
         Map<PrismScope, PrismRoleCategory> defaultRoleCategories = roleService.getDefaultRoleCategories(user);
         PrismRoleCategory defaultRoleCategory = defaultRoleCategories.size() == 0 ? null : defaultRoleCategories.values().iterator().next();
-
+        
         Integer readMessageCount = userService.getUserReadMessageCount(user, user);
         Integer unreadMessageCount = userService.getUserUnreadMessageCount(user, user);
         UserActivityRepresentation representation = new UserActivityRepresentation().withDefaultRoleCategory(defaultRoleCategory)
@@ -211,11 +214,11 @@ public class UserMapper {
                 .withAppointmentActivities(applicationMapper.getApplicationAppointmentRepresentations(user))
                 .withUnverifiedUserActivities(getUserUnverifiedRepresentations(user))
                 .withAdvertTargetActivities(advertMapper.getAdvertTargetRepresentations(advertService.getAdvertTargetsReceived(user), user));
-
+        
         userService.setUserActivityCache(user, representation, now());
         return representation;
     }
-
+    
     public UserRepresentationSimple getUserRepresentationSimple(ProfileEntityDTO profileEntity, User user) {
         UserRepresentationSimple representation = new UserRepresentationSimple();
         representation.setId(profileEntity.getUserId());
@@ -227,35 +230,67 @@ public class UserMapper {
         representation.setAccountImageUrl(profileEntity.getUserAccountImageUrl());
         return representation;
     }
-
+    
+    public List<UserActivityRepresentation.ResourceActivityRepresentation> filterResourceActivitiesForNotification(
+            List<UserActivityRepresentation.ResourceActivityRepresentation> resourceActivityRepresentations) {
+        if (CollectionUtils.isNotEmpty(resourceActivityRepresentations)) {
+            Iterator<UserActivityRepresentation.ResourceActivityRepresentation> resourceActivityRepresentationIterator = resourceActivityRepresentations.iterator();
+            while (resourceActivityRepresentationIterator.hasNext()) {
+                UserActivityRepresentation.ResourceActivityRepresentation resourceActivityRepresentation = resourceActivityRepresentationIterator.next();
+                List<UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation> actionActivityRepresentations =
+                        resourceActivityRepresentation.getActions();
+                if (CollectionUtils.isNotEmpty(actionActivityRepresentations)) {
+                    Iterator<UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation> actionActivityRepresentationIterator =
+                            actionActivityRepresentations.iterator();
+                    while (actionActivityRepresentationIterator.hasNext()) {
+                        UserActivityRepresentation.ResourceActivityRepresentation.ActionActivityRepresentation actionActivityRepresentation =
+                                actionActivityRepresentationIterator.next();
+                        PrismAction action = actionActivityRepresentation.getAction().getId();
+                        if (action.isExcludeFromReminder()) {
+                            // Remove if action excluded
+                            actionActivityRepresentationIterator.remove();
+                        }
+                    }
+                    
+                    if (CollectionUtils.isEmpty(actionActivityRepresentations)) {
+                        // Remove if actions empty
+                        resourceActivityRepresentationIterator.remove();
+                    }
+                }
+            }
+        }
+        
+        return resourceActivityRepresentations;
+    }
+    
     public List<UserRolesRepresentation> getUserRoleRepresentations(User user) {
         HashMultimap<ResourceRepresentationIdentity, PrismRole> index = HashMultimap.create();
         resourceService.getResourceRoles(user).forEach( //
                 userRole -> index.put(new ResourceRepresentationIdentity().withScope(userRole.getScope()).withId(userRole.getId()), userRole.getRole()));
-
+        
         return index.keySet().stream()
                 .map(resource -> new UserRolesRepresentation(resource, newArrayList(index.get(resource))))
                 .collect(Collectors.toList());
     }
-
+    
     private <T extends UserDescriptor, U extends UserRepresentationSimple> U getUserRepresentation(T user, User currentUser, boolean forceReturnEmail,
             Class<U> returnType) {
         U representation = instantiate(returnType);
-
+        
         representation.setId(user.getId());
         representation.setFirstName(user.getFirstName());
         representation.setLastName(user.getLastName());
         representation.setEmail(userService.getSecuredUserEmailAddress(user.getEmail(), currentUser, forceReturnEmail));
-
+        
         if (UserDescriptorExtended.class.isAssignableFrom(user.getClass())) {
             UserDescriptorExtended<?, ?> userExtended = (UserDescriptorExtended<?, ?>) user;
             representation.setFirstName2(userExtended.getFirstName2());
             representation.setFirstName3(userExtended.getFirstName3());
             representation.setFullName(userExtended.getFullName());
-
+            
             representation.setAccountProfileUrl(userExtended.getLinkedinProfileUrl());
             representation.setAccountImageUrl(userExtended.getLinkedinImageUrl());
-
+            
             Object portraitImage = userExtended.getPortraitImage();
             if (portraitImage != null) {
                 if (portraitImage.getClass().equals(Integer.class)) {
@@ -264,13 +299,13 @@ public class UserMapper {
                     representation.setPortraitImage(documentMapper.getDocumentRepresentation((Document) portraitImage));
                 }
             }
-
+            
             representation.setEditable(userExtended.checkUserEditable(currentUser));
         }
-
+        
         return representation;
     }
-
+    
     private List<ResourceUserUnverifiedRepresentation> getUserUnverifiedRepresentations(User user) {
         Map<String, ResourceUserUnverifiedRepresentation> representations = newLinkedHashMap();
         TreeMultimap<UserRepresentationUnverified, PrismRoleContext> userRepresentationIndex = TreeMultimap.create();
@@ -278,7 +313,7 @@ public class UserMapper {
             Integer departmentId = unverifiedUser.getDepartmentId();
             Integer institutionId = unverifiedUser.getInstitutionId();
             String resourceKey = Joiner.on("|").skipNulls().join(institutionId, departmentId);
-
+            
             List<UserRepresentationUnverified> userRepresentations;
             ResourceUserUnverifiedRepresentation representation = representations.get(resourceKey);
             if (representation == null) {
@@ -291,24 +326,24 @@ public class UserMapper {
             } else {
                 userRepresentations = representation.getUsers();
             }
-
+            
             UserRepresentationUnverified userRepresentation = new UserRepresentationUnverified().withId(unverifiedUser.getUserId())
                     .withFirstName(unverifiedUser.getUserFirstName())
                     .withLastName(unverifiedUser.getUserLastName()).withEmail(unverifiedUser.getUserEmail())
                     .withAccountProfileUrl(unverifiedUser.getUserLinkedinProfileUrl())
                     .withAccountImageUrl(unverifiedUser.getUserLinkedinImageUrl())
                     .withPortraitImage(documentMapper.getDocumentRepresentation(unverifiedUser.getUserPortraitImageId()));
-
+            
             userRepresentations.add(userRepresentation);
             PrismRole unverifiedRole = unverifiedUser.getRoleId();
             userRepresentationIndex.put(userRepresentation, unverifiedRole.name().contains("STUDENT") ? STUDENT : VIEWER);
         }
-
+        
         userRepresentationIndex.keySet().forEach(unverifiedUser -> {
             unverifiedUser.setContexts(newLinkedList(userRepresentationIndex.get(unverifiedUser)));
         });
-
+        
         return newLinkedList(representations.values());
     }
-
+    
 }
